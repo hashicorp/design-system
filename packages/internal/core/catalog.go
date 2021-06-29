a6e474696c4abec53da28fd6d38f7daaa4a7b4fb
Alasdair Monk <alasdair.monk@gmail.com>
2021-06-29 12:36:37 +0000

    Comments

--- packages/internal/core/catalog.go
+++ packages/internal/core/catalog.go
@@ -9,11 +9,13 @@ import (
 func CreateCatalogFile(icons []Icon, fileName string) {
 	var newIconset []Icon
 
+	// We don't want all the data output to the catalog so lets recreate our Icon array
 	for _, i := range icons {
 		icon := Icon{Name: i.Name, Size: i.Size}
 		newIconset = append(newIconset, icon)
 	}
 
+	// Marshall to json and write to disk
 	file, _ := json.Marshal(newIconset)
 	ioutil.WriteFile(fileName, file, 0644)
 }
