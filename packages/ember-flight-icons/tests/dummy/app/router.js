3c96c72c6d2ba905de6c549a9d149fc09c1c6e5c
Melanie Sumner <melanie@hashicorp.com>
2021-08-30 15:06:12 -0500

    feature: added routes for design and engineeringn documentation

--- packages/ember-flight-icons/tests/dummy/app/router.js
+++ packages/ember-flight-icons/tests/dummy/app/router.js
@@ -6,4 +6,7 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('engineering');
+  this.route('design');
+});
--- packages/ember-flight-icons/tests/dummy/app/routes/application.js
+++ packages/ember-flight-icons/tests/dummy/app/routes/application.js
@@ -1,25 +1,3 @@
 import Route from '@ember/routing/route';
-import fetch from 'fetch';
-import { getOwner } from '@ember/application';
 
-export default class ApplicationRoute extends Route {
-  get contextRootURL() {
-    const config = getOwner(this).resolveRegistration('config:environment');
-    return config.rootURL || '/';
-  }
-
-  async model() {
-    const response = await fetch(
-      `${this.contextRootURL}@hashicorp/ember-flight-icons/icons/_catalog.json`
-    );
-    const json = await response.json();
-
-    return json.map(({ Name, Size }) => {
-      return {
-        name: `${Name}`,
-        size: `${Size}`,
-        searchable: `${Name}`,
-      };
-    });
-  }
-}
+export default class ApplicationRoute extends Route {}
