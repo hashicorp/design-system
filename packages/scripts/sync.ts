aaf488305d6ce669956abe357559ee86a428b5e7
Cristiano Rastelli <public@didoo.net>
2021-09-07 14:13:05 +0100

    made sure the .env variables are available

--- packages/scripts/sync.ts
+++ packages/scripts/sync.ts
@@ -1,3 +1,8 @@
-const sayhi: string = 'Hello World!';
+import dotenv from 'dotenv'
+
+// read the environment variables from the ".env" file
+dotenv.config()
+
+const sayhi: string = `Hello World! This is your Figma file ID: ${process.env.FLIGHT_FILE_ID}`;
 
 console.log(sayhi)
