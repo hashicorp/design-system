abe5a8e259b5feee141769a9fac83e34d03eb01c
Cristiano Rastelli <public@didoo.net>
2021-09-08 21:24:51 +0100

    moved export logic to standalone exportAssetsToFolder

---
+++ packages/scripts/sync-parts/exportAssetsToFolder.ts
@@ -0,0 +1,49 @@
+import * as FigmaExport from '@figma-export/types';
+import * as figmaExport from '@figma-export/core';
+import { requirePackages } from '@figma-export/cli/dist/utils';
+import outputComponentsAsSvg from '@figma-export/output-components-as-svg';
+
+import { ConfigData } from '../@types/ConfigData';
+import { AssetCoreData, AssetsMetadata } from '../@types/AssetsMetadata';
+import { getAssetFileName } from './getAssetFileName';
+
+export async function exportAssetsToFolder({ config, assetsMetadata } : { config: ConfigData, assetsMetadata: AssetsMetadata }): Promise<FigmaExport.PageNode[]> {
+
+    // override the "figma-export" functions (see: https://github.com/marcomontalbano/figma-export/tree/master/packages/output-components-as-svg)
+
+    // we removed the SVGO transformer, we will do it later in the "build" part with other parameters
+    const transformer: FigmaExport.StringTransformer[] = [];
+    const outputter: FigmaExport.ComponentOutputter[] = [
+        outputComponentsAsSvg({
+            output: config.outputFolder,
+            // IMPORTANT: when exported by Figma by default the variants' name looks like this: "Size=16, Style=Color"
+            // and we want to sanitize it (otherwise variants with the same props/values will override one another)
+            // so this is used to change icon's name to a "standard" format that will be used across the entire script
+            getBasename: (options: FigmaExport.ComponentOutputterParamOption): string => {
+                const assetCoreData: AssetCoreData = assetsMetadata[options.id];
+                return `${getAssetFileName(assetCoreData)}.svg`
+            },
+            // by default figma-export adds the "page" name to the path (so creating an extra folder)
+            // but we prefer to have all the icons saved under the "SVG" format (so we're open in the future to have other formats too)
+            getDirname: (): string => 'svg',
+        })
+    ];
+
+    // TODO maybe there is a cleaner way to do this with async/await? (ask help to the team)
+    let result: FigmaExport.PageNode[] = [];
+
+    await figmaExport.components({
+        fileId: config.figmaFile.id,
+        concurrency: 30,
+        token: process.env.FIGMA_TOKEN || 'MISSING-TOKEN-ADD-IT-TO-ENV-FILE',
+        onlyFromPages: [config.figmaFile.page],
+        transformers: requirePackages<FigmaExport.StringTransformer>(transformer),
+        outputters: requirePackages<FigmaExport.ComponentOutputter>(outputter, { output: config.outputFolder }),
+    }).then(async (figmaExportPageNode) => {
+        result = figmaExportPageNode;
+    }).catch((err: Error) => {
+        console.error(err);
+    });
+
+    return result;
+}
--- packages/scripts/sync.ts
+++ packages/scripts/sync.ts
@@ -5,17 +5,9 @@ import dotenv from 'dotenv';
 import fs from 'fs-extra';
 import del from 'del';
 import chalk from 'chalk';
-
-import * as FigmaExport from '@figma-export/types';
-import * as figmaExport from '@figma-export/core';
-import { requirePackages } from '@figma-export/cli/dist/utils';
-import { StringTransformer, ComponentOutputter } from '@figma-export/types';
-import outputComponentsAsSvg from '@figma-export/output-components-as-svg';
 import isEqual from 'lodash/isEqual';
 
-import { AssetCoreData } from './@types/AssetsMetadata';
 import { getAssetsMetadata } from './sync-parts/getAssetsMetadata';
-import { getAssetFileName } from './sync-parts/getAssetFileName';
 import { getAssetsCatalog } from './sync-parts/getAssetsCatalog';
 
 // read the environment variables from the ".env" file
@@ -23,6 +15,7 @@ dotenv.config();
 
 // read our custom config
 import { config } from './config';
+import { exportAssetsToFolder } from './sync-parts/exportAssetsToFolder';
 
 (async () => {
     try {
@@ -52,53 +45,29 @@ async function sync() {
     }
     del.sync(config.outputFolder, { force: true });
 
-    // let's retrieve the assets metadata via REST api
+    // retrieve the assets metadata via REST api
     const assetsMetadata = await getAssetsMetadata();
     if (VERBOSE) {
         console.log('\nRetrieved assetsMetadata:\n', JSON.stringify(assetsMetadata));
     }
 
-    // we removed the SVGO transformer, we will do it later in the "build" part with other parameters
-    const transformer: StringTransformer[] = [];
-    const outputter: ComponentOutputter[] = [
-        outputComponentsAsSvg({
-            output: config.outputFolder,
-            // IMPORTANT: when exported by Figma by default the variants' name looks like this: "Size=16, Style=Color"
-            // and we want to sanitize it (otherwise variants with the same props/values will override one another)
-            // so this is used to change icon's name to a "standard" format that will be used across the entire script
-            getBasename: (options: FigmaExport.ComponentOutputterParamOption): string => {
-                const assetCoreData: AssetCoreData = assetsMetadata[options.id];
-                return `${getAssetFileName(assetCoreData)}.svg`
-            },
-            // by default figma-export adds the "page" name to the path (so creating an extra folder)
-            // but we prefer to have all the icons saved under the "SVG" format (so we're open in the future to have other formats too)
-            getDirname: (): string => 'svg',
-        })
-    ];
+    // export the assets from Figma to the filesystem
+    const figmaExportPageNode = await exportAssetsToFolder({ config, assetsMetadata });
 
-    // TODO move to a standalone function and file
-    await figmaExport.components({
-        fileId: config.figmaFile.id,
-        concurrency: 30,
-        token: process.env.FIGMA_TOKEN || 'MISSING-TOKEN-ADD-IT-TO-ENV-FILE',
-        onlyFromPages: [config.figmaFile.page],
-        transformers: requirePackages<FigmaExport.StringTransformer>(transformer),
-        outputters: requirePackages<FigmaExport.ComponentOutputter>(outputter, { output: config.outputFolder }),
-    }).then(async (figmaExportPageNode) => {
+    // check that all the assets have been exported correctly
+    const assetsExportedIDs = figmaExportPageNode[0].components.map((component) => component.id);
+    const assetsExpectedIDs = Object.keys(assetsMetadata);
+    if (isEqual(assetsExportedIDs.sort(), assetsExpectedIDs.sort())) {
 
-        const assetsExportedIDs = figmaExportPageNode[0].components.map((component) => component.id);
-        const assetsExpectedIDs = Object.keys(assetsMetadata);
-        if (isEqual(assetsExportedIDs.sort(), assetsExpectedIDs.sort())) {
-            // get the assets "catalog" (will be used by multiple consumers)
-            const assetsCatalog = getAssetsCatalog({ config, assetsMetadata, figmaExportPageNode });
-            // and save it as JSON file
+        // get the assets "catalog" (will be used by multiple consumers) and save it as JSON file
+        const assetsCatalog = getAssetsCatalog({ config, assetsMetadata, figmaExportPageNode });
+        try {
             fs.writeJsonSync(`${config.outputFolder}/catalog.json`, assetsCatalog, { spaces: 2 });
-
-        } else {
-            console.log(chalk.red(`WARNING:\nThe number of assets retrieved (${assetsExportedIDs.length}) and the number of assets expected (${assetsExpectedIDs.length}) are different. Please check why, this is unexpected.`));
+        } catch (err) {
+            console.error(err);
         }
 
-    }).catch((err: Error) => {
-        console.error(err);
-    });
+    } else {
+        console.log(chalk.red(`WARNING:\nThe number of assets retrieved (${assetsExportedIDs.length}) and the number of assets expected (${assetsExpectedIDs.length}) are different. Please check why, this is unexpected.`));
+    }
 }
