1384211aa0627bfe5085ecbd70b4cb3cb403d91b
Cristiano Rastelli <public@didoo.net>
2021-09-15 11:03:34 +0100

    updated the generation of the SVG bundle to remove the “fill” attribute for “dynamic” color

--- packages/flight-icons/scripts/build-parts/generateBundleSVGSprite.ts
+++ packages/flight-icons/scripts/build-parts/generateBundleSVGSprite.ts
@@ -26,8 +26,11 @@ export async function generateBundleSVGSprite({ config, catalog } : { config: Co
     // add the SVGs to the sprite
     for(const { fileName } of catalog.assets) {
         let svgSource = await fs.readFile(`${tempSVGFolderPath}/${fileName}.svg`, 'utf8');
-        // replace #000001 ("dynamic" color in Figma) with "currentColor"
-        svgSource = svgSource.replace(/"#000001"/gi, '"currentColor"');
+        // completely remove any "fill" attribute that has #000001 ("dynamic" color in Figma, equivalent of "currentColor") as value
+        // the reason for this is that the Ember addon uses the "fill" attribute with "currentColor" value (or a value passed as prop by the user)
+        // to set the color of the children <path> elements (see https://github.com/hashicorp/flight/issues/200) so the only way to have it work properly
+        // is not to use `fill="currentColor"` on the <path> elements, but to completely remove the fill attribute so it's inherited from the parent <SVG> element
+        svgSource = svgSource.replace(/ fill="#000001"/gi, '');
         // add the processed SVG content to the sprite
         sprites.add(fileName, svgSource);
     }
