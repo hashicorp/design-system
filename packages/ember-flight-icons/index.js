c52cdacb32e063e679d9594c56aaa0affcf2e347
Cristiano Rastelli <public@didoo.net>
2021-10-12 10:36:27 +0100

    added comments + small tuning

--- packages/ember-flight-icons/index.js
+++ packages/ember-flight-icons/index.js
@@ -6,7 +6,12 @@ module.exports = {
   name: require('./package').name,
 
   contentFor(type) {
-    if (type === 'body-footer' || type === 'test-embed-sprite') {
+    // notice: the "body-footer" is used in the normal app, while the "ember-testing-sprite-embed" is used
+    // in the testing environment to inject the sprite in the #ember-testing "app root element" (ENV.APP.rootElement)
+    // otherwise the @percy/ember package ignores everything else and the SVG sprite is not added to the DOM
+    // see thread: https://hashicorp.slack.com/archives/C11JCBJTW/p1633978558343000
+    // see: https://github.com/percy/percy-ember/blob/master/addon-test-support/%40percy/ember/index.js#L33
+    if (type === 'body-footer' || type === 'ember-testing-sprite-embed') {
       return flightIconSprite;
     }
   },
