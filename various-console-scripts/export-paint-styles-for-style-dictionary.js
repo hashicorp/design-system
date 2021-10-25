a4256c69bf39571c707a0854a8a58eee1edbec60
Cristiano Rastelli <public@didoo.net>
2021-10-25 10:56:20 +0100

    improved exporting of paint style to use aliases of colors

--- various-console-scripts/export-paint-styles-for-style-dictionary.js
+++ various-console-scripts/export-paint-styles-for-style-dictionary.js
@@ -1,11 +1,12 @@
 var styles = figma.getLocalPaintStyles();
 
-var paletteSD = { palette: {} };
-var colorSD = { color: {} };
+var paletteSD = {};
+var paletteMapping = {};
+var colorSD = {};
 
 var rgbToHex2 = (r, g, b) => '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 
-var getColorHEX = (colorRGB, category, name) => {
+var getObjectColorHEX = (colorRGB, category) => {
     // Figma stores the "paints" styles in fractional format [0-1]
     // so we have to take the r/g/b values and multiply them by 255
     // https://forum.figma.com/t/dynamically-create-palette-with-createpaintstyle-locked-on-hextorgba-javascipt-function/5367/2
@@ -19,12 +20,55 @@ var getColorHEX = (colorRGB, category, name) => {
     };
 };
 
+var getObjectColorAlias = (colorRGB, category) => {
+    const colorObject = getObjectColorHEX(colorRGB, category);
+    // override hex color with a Style Dictionary alias
+    colorObject.value = `{color.palette.${paletteMapping[colorObject.value]}.value}`;
+    return colorObject;
+};
+
+
+// BUILD THE "PALETTE" OF COLORS
+
 styles.forEach(style => {
     const parts = style.name.toLowerCase().replace(' ','-').split('/');
     const category = parts[0];
-    if (category === 'old' || category === 'gradients') {
+
+    if (category === 'palette') {
+        // Unfortunately Figma uses "proxies" so you can't pass around references
+        // (to functions, for example, like in our case), but you have to build your own objects out of them
+        // see: https://www.figma.com/plugin-docs/editing-properties/
+        const colorRGB = {
+            r: style.paints[0].color.r,
+            g: style.paints[0].color.g,
+            b: style.paints[0].color.b,
+        }
+
+        const colorGroup = parts[1];
+        const colorName = parts[2];
+        if (colorGroup === 'alpha') {
+            return;
+        }
+        const hexColor = getObjectColorHEX(colorRGB, category, style.name);
+        paletteSD[colorName] = hexColor;
+        paletteMapping[hexColor.value] = colorName;
+    }
+});
+
+console.log(JSON.stringify({ color: { palette: paletteSD } }, null, 4));
+// console.log(JSON.stringify(paletteMapping, null, 4));
+
+
+
+// BUILD THE LIST OF SEMANTIC COLORS
+
+styles.forEach(style => {
+    const parts = style.name.toLowerCase().replace(' ','-').split('/');
+    const category = parts[0];
+    if (category === 'palette' || category === 'gradients') {
         return;
     }
+    console.log(category, parts);
 
     // Unfortunately Figma uses "proxies" so you can't pass around references
     // (to functions, for example, like in our case), but you have to build your own objects out of them
@@ -35,30 +79,23 @@ styles.forEach(style => {
         b: style.paints[0].color.b,
     }
 
-    if (category === 'palette') {
-        const colorGroup = parts[1];
-        const colorName = parts[2];
-        if (colorGroup === 'alpha') {
-            return;
-        }
-        paletteSD.palette[colorName] = getColorHEX(colorRGB, category, style.name);
-    } else if (category === 'neutral') {
-        if (!colorSD.color.hasOwnProperty(category)) {
-            colorSD.color[category] = {};
+    if (category === 'neutral') {
+        if (!colorSD.hasOwnProperty(category)) {
+            colorSD[category] = {};
         }
         const colorName = parts[1];
         if (colorName.match(/^alpha/)) { return; }
-        colorSD.color[category][colorName] = getColorHEX(colorRGB, category, style.name);
+        colorSD[category][colorName] = getObjectColorAlias(colorRGB, category);
     } else {
-        if (!colorSD.color.hasOwnProperty(category)) {
-            colorSD.color[category] = {};
+        if (!colorSD.hasOwnProperty(category)) {
+            colorSD[category] = {};
         }
         const colorContext = parts[1];
-        if (!colorSD.color[category].hasOwnProperty(colorContext)) {
-            colorSD.color[category][colorContext] = {};
+        if (!colorSD[category].hasOwnProperty(colorContext)) {
+            colorSD[category][colorContext] = {};
         }
         const colorName = parts[2];
-        colorSD.color[category][colorContext][colorName] = getColorHEX(colorRGB, category, style.name);
+        colorSD[category][colorContext][colorName] = getObjectColorAlias(colorRGB, category);
     }
 
     // extra checks
@@ -71,5 +108,4 @@ styles.forEach(style => {
 
 });
 
-console.log(JSON.stringify(paletteSD, null, 4));
-console.log(JSON.stringify(colorSD, null, 4));
+console.log(JSON.stringify({ color: colorSD }, null, 4));
