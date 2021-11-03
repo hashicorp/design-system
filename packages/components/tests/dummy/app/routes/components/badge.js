c2c53740f78c3455b80fb26e488de68f8e46042a
Cristiano Rastelli <public@didoo.net>
2021-11-03 16:48:04 +0000

    added proper looping on “badge” page over component variants

--- packages/components/tests/dummy/app/routes/components/badge.js
+++ packages/components/tests/dummy/app/routes/components/badge.js
@@ -1,5 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default class ComponentsBadgeRoute extends Route {
-    TYPES = ['flat','inverted','outlined'];
+    model() {
+        // TODO! likely these come from the component `_config.js` file
+        const TYPES = ['flat','inverted','outlined'];
+        const COLORS = ['neutral', 'highlight', 'success', 'warning', 'error'];
+        const SIZES = ['small', 'medium', 'large'];
+        return { TYPES, COLORS, SIZES };
+    }
 }
