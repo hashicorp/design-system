595078d012565f77235251b512b627ebac571b52
Cristiano Rastelli <public@didoo.net>
2021-11-03 19:38:04 +0000

    added check on parameters passed to the “badge” component

---
+++ packages/components/addon/components/hds/badge/consts.js
@@ -0,0 +1,6 @@
+export const DEFAULT_SIZE = 'medium';
+export const DEFAULT_COLOR = 'neutral';
+export const DEFAULT_TYPE = 'filled';
+export const SIZES = ['small', 'medium', 'large'];
+export const TYPES = ['filled','inverted','outlined'];
+export const COLORS = ['neutral', 'neutral-dark-mode', 'highlight', 'success', 'warning', 'critical'];
