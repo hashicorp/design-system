2ccbe909d437e7c0bd2569c7a037ef57eed8d27e
Cristiano Rastelli <public@didoo.net>
2021-11-03 19:25:42 +0000

    finished first styling iteration in scss using maps/loops + dummy page with all the variants permutations

--- packages/components/tests/dummy/app/routes/components/badge.js
+++ packages/components/tests/dummy/app/routes/components/badge.js
@@ -3,9 +3,9 @@ import Route from '@ember/routing/route';
 export default class ComponentsBadgeRoute extends Route {
     model() {
         // TODO! likely these come from the component `_config.js` file
-        const TYPES = ['flat','inverted','outlined'];
-        const COLORS = ['neutral', 'neutral-dark-mode', 'highlight', 'success', 'warning', 'critical'];
         const SIZES = ['small', 'medium', 'large'];
-        return { TYPES, COLORS, SIZES };
+        const TYPES = ['filled','inverted','outlined'];
+        const COLORS = ['neutral', 'neutral-dark-mode', 'highlight', 'success', 'warning', 'critical'];
+        return { SIZES, TYPES, COLORS };
     }
 }
