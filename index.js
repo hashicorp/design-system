a39d1608e8d11786b00325c5b74966266171524d
Melanie Sumner <melanie@hashicorp.com>
2021-11-10 09:51:53 -0600

    update to badge component for sass consumption by apps

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// let Funnel = require('broccoli-funnel');
+// let mergeTrees = require('broccoli-merge-trees');
+// let path = require('path');
+
 module.exports = {
   name: require('./package').name,
+
+  isDevelopingAddon() {
+    return true;
+  },
+
+  included: function (/* app */) {
+    this._super.included.apply(this, arguments);
+  },
 };
