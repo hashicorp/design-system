5ebfabe080999493c12e5624434517b57fd1467c
Cristiano Rastelli <public@didoo.net>
2021-11-12 10:05:31 +0000

    fixed silly mistake

--- tests/dummy/app/routes/components/badge.js
+++ tests/dummy/app/routes/components/badge.js
@@ -2,10 +2,10 @@ import Route from '@ember/routing/route';
 
 export default class ComponentsBadgeRoute extends Route {
   model() {
-    // TODO! these should come from the component file
-    const SIZES = ['small', 'medium', 'large'];
-    const TYPES = ['filled', 'inverted', 'outlined'];
-    const COLORS = [
+    // TODO! these should come from the components' files
+    const BADGE_SIZES = ['small', 'medium', 'large'];
+    const BADGE_TYPES = ['filled', 'inverted', 'outlined'];
+    const BADGE_COLORS = [
       'neutral',
       'neutral-dark-mode',
       'highlight',
@@ -13,6 +13,16 @@ export default class ComponentsBadgeRoute extends Route {
       'warning',
       'critical',
     ];
-    return { SIZES, TYPES, COLORS };
+    const BADGE_COUNT_SIZES = ['small', 'medium', 'large'];
+    const BADGE_COUNT_TYPES = ['filled', 'inverted', 'outlined'];
+    const BADGE_COUNT_COLORS = ['neutral', 'neutral-dark-mode'];
+    return {
+      BADGE_SIZES,
+      BADGE_TYPES,
+      BADGE_COLORS,
+      BADGE_COUNT_SIZES,
+      BADGE_COUNT_TYPES,
+      BADGE_COUNT_COLORS,
+    };
   }
 }
