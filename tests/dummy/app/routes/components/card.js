528e1dff92be613ef8509ba7bedd81bbe9ebd486
Cristiano Rastelli <public@didoo.net>
2021-11-17 12:22:19 +0100

    renamed “elevation” to level + renamed “edge” to “border” + other fine tunings of the showcase

--- tests/dummy/app/routes/components/card.js
+++ tests/dummy/app/routes/components/card.js
@@ -3,8 +3,8 @@ import Route from '@ember/routing/route';
 export default class ComponentsCardRoute extends Route {
   model() {
     // TODO! these should come from the component file
-    const ELEVATIONS = ['base', 'low', 'mid', 'high', 'higher'];
+    const LEVELS = ['base', 'mid', 'high'];
     const BACKGROUNDS = ['neutral-0', 'neutral-50'];
-    return { ELEVATIONS, BACKGROUNDS };
+    return { LEVELS, BACKGROUNDS };
   }
 }
