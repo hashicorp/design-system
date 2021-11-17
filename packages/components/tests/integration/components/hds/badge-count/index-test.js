9b5fbfac976c60f95fe5d552a90f55bd259c82dd
Cristiano Rastelli <public@didoo.net>
2021-11-17 23:43:59 +0100

    small cleanup
    (to be more in sync with other components)

--- packages/components/tests/integration/components/hds/badge-count/index-test.js
+++ packages/components/tests/integration/components/hds/badge-count/index-test.js
@@ -20,7 +20,7 @@ module('Integration | Component | hds/badge-count/index', function (hooks) {
     );
     assert.dom('div#test-badge-count').hasClass('hds-badge-count');
   });
-  test('it should render the neutral color as the default if no color is declared', async function (assert) {
+  test('it should have the default color if no @color prop is declared', async function (assert) {
     await render(
       hbs`<Hds::BadgeCount @text="text renders" id="test-badge-count" />`
     );
@@ -28,7 +28,7 @@ module('Integration | Component | hds/badge-count/index', function (hooks) {
       .dom('div#test-badge-count')
       .hasClass('hds-badge-count--color-neutral');
   });
-  test('it should render the right CSS color class if the @color prop is declared', async function (assert) {
+  test('it should have the right color class based on the @color prop', async function (assert) {
     await render(
       hbs`<Hds::BadgeCount @text="text renders" id="test-badge-count" @color="neutral-dark-mode" />`
     );
@@ -36,25 +36,25 @@ module('Integration | Component | hds/badge-count/index', function (hooks) {
       .dom('div#test-badge-count')
       .hasClass('hds-badge-count--color-neutral-dark-mode');
   });
-  test('it should render the medium size if no size is declared', async function (assert) {
+  test('it should have the medium size if no @size prop is declared', async function (assert) {
     await render(
       hbs`<Hds::BadgeCount @text="text renders" id="test-badge-count" />`
     );
     assert.dom('div#test-badge-count').hasClass('hds-badge-count--size-medium');
   });
-  test('it should render the right CSS size class if the @size prop is declared', async function (assert) {
+  test('it should have the right size class based on the @size prop', async function (assert) {
     await render(
       hbs`<Hds::BadgeCount @text="text renders" id="test-badge-count" @size="small" />`
     );
     assert.dom('div#test-badge-count').hasClass('hds-badge-count--size-small');
   });
-  test('it should render the filled type if no type is declared', async function (assert) {
+  test('it should have the filled type if no @type prop is declared', async function (assert) {
     await render(
       hbs`<Hds::BadgeCount @text="text renders" id="test-badge-count" />`
     );
     assert.dom('div#test-badge-count').hasClass('hds-badge-count--type-filled');
   });
-  test('it should render the right CSS type class if @type prop is declared', async function (assert) {
+  test('it should have the right type class based on the @type prop', async function (assert) {
     await render(
       hbs`<Hds::BadgeCount @text="text renders" id="test-badge-count" @type="inverted" />`
     );
