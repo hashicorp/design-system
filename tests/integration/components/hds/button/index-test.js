dfd6c1a9e71ba5b3e6d5f1d5a0bd576f180afd2b
Melanie Sumner <melanie@hashicorp.com>
2021-11-18 09:51:55 -0600

    made test names more specific to button component

--- tests/integration/components/hds/button/index-test.js
+++ tests/integration/components/hds/button/index-test.js
@@ -6,25 +6,29 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | hds/button/index', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders with the defined text', async function (assert) {
+  test('it renders a button with the defined text', async function (assert) {
     await render(hbs`<Hds::Button @text="Copy to Clipboard" />`);
     assert.dom(this.element).hasText('Copy to Clipboard');
   });
-  test('it should render a CSS class that matches the component name', async function (assert) {
+  test('it should render a CSS class that matches the button component name', async function (assert) {
     await render(hbs`<Hds::Button @text="Copy to Clipboard" />`);
     assert.dom('button').hasClass('hds-button');
   });
-  test('it should render the medium size if no size is defined', async function (assert) {
-    await render(hbs`<Hds::Button @text="click me" />`);
+  test('it should render the medium size button by default', async function (assert) {
+    await render(hbs`<Hds::Button @text="Copy to Clipboard" />`);
     assert.dom('button').hasClass('hds-button--size-medium');
   });
-  test('it should render an icon if an icon name is passed', async function (assert) {
+  test('it should render the primary button by default', async function (assert) {
+    await render(hbs`<Hds::Button @text="Copy to Clipboard" />`);
+    assert.dom('button').hasClass('hds-button--color-primary');
+  });
+  test('it should render an icon in the button if an icon name is passed', async function (assert) {
     await render(
       hbs`<Hds::Button @text="Copy to Clipboard" @icon="clipboard-copy" />`
     );
     assert.dom('svg.flight-icon').exists();
   });
-  test('it should have aria-label if isIconOnly is set to true', async function (assert) {
+  test('it should have aria-label on the button element if isIconOnly is set to true', async function (assert) {
     await render(
       hbs`<Hds::Button @text="copy to clipboard" @icon="clipboard-copy" @isIconOnly=true />`
     );
