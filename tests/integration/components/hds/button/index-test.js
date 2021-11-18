85ee6950cbafa7a6bfe937a533aeeb8bb00d3070
Melanie Sumner <melanie@hashicorp.com>
2021-11-18 09:41:33 -0600

    added some tests

--- tests/integration/components/hds/button/index-test.js
+++ tests/integration/components/hds/button/index-test.js
@@ -6,12 +6,28 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | hds/button/index', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it renders with the defined text', async function (assert) {
+    await render(hbs`<Hds::Button @text="Copy to Clipboard" />`);
+    assert.dom(this.element).hasText('Copy to Clipboard');
+  });
+  test('it should render a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::Button @text="Copy to Clipboard" />`);
+    assert.dom('button').hasClass('hds-button');
+  });
+  test('it should render the medium size if no size is defined', async function (assert) {
     await render(hbs`<Hds::Button @text="click me" />`);
-
-    assert.dom(this.element).hasText('click me');
+    assert.dom('button').hasClass('hds-button--size-medium');
+  });
+  test('it should render an icon if an icon name is passed', async function (assert) {
+    await render(
+      hbs`<Hds::Button @text="Copy to Clipboard" @icon="clipboard-copy" />`
+    );
+    assert.dom('svg.flight-icon').exists();
+  });
+  test('it should have aria-label if isIconOnly is set to true', async function (assert) {
+    await render(
+      hbs`<Hds::Button @text="copy to clipboard" @icon="clipboard-copy" @isIconOnly=true />`
+    );
+    assert.dom('button').hasAria('label', 'copy to clipboard');
   });
 });
