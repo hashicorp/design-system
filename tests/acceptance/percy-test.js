f67600e41c87341c23f5c155165b32cdfcdd444d
Amy Lam <amy.r.lam@gmail.com>
2021-11-24 14:49:40 -0800

    Add snapshot test

---
+++ tests/acceptance/percy-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import percySnapshot from '@percy/ember';
+
+module('Acceptance | Percy test', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('Take percy snapshots', async function (assert) {
+    await visit('/components/badge');
+    await percySnapshot('Badge page');
+
+    await visit('/components/card');
+    await percySnapshot('Card page');
+
+    assert.ok(true);
+  });
+});
