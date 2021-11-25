149e312fb6889abad68dc148dc02350f38eed738
Cristiano Rastelli <public@didoo.net>
2021-11-25 10:23:47 +0000

    changed the way we capture screenshots in Percy

--- tests/acceptance/percy-test.js
+++ tests/acceptance/percy-test.js
@@ -6,19 +6,17 @@ import percySnapshot from '@percy/ember';
 module('Acceptance | Percy test', function (hooks) {
   setupApplicationTest(hooks);
 
+  // Percy (percySnapshot) doesn't allow to target specific DOM elements, so we have to "blacklist" the elements
+  // that we want to exclude from the snapshots using their own "Percy-specific CSS".
+  // see: https://docs.percy.io/docs/percy-specific-css#section-hiding-regions-with-percy-specific-css
+  const percyCSSExclude = `header, footer, main section:not([data-test-percy]) { display: none; }`;
+
   test('Take percy snapshots', async function (assert) {
     await visit('/components/badge');
-    await percySnapshot('Badge - Showcase', '[data-test-vrt="badge-showcase"]');
-    await percySnapshot(
-      'BadgeCount - Showcase',
-      '[data-test-vrt="badge-count-showcase"]'
-    );
+    await percySnapshot('Badge', { percyCSSExclude });
 
     await visit('/components/card');
-    await percySnapshot(
-      'Card Container - Showcase',
-      '[data-test-vrt="card-container-showcase"]'
-    );
+    await percySnapshot('Card', { percyCSSExclude });
 
     assert.ok(true);
   });
