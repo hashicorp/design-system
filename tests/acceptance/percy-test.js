907a75c03bf604771db3d0746cf059b99666f3f1
Cristiano Rastelli <public@didoo.net>
2021-11-25 10:36:54 +0000

    hopefully final fix

--- tests/acceptance/percy-test.js
+++ tests/acceptance/percy-test.js
@@ -6,17 +6,12 @@ import percySnapshot from '@percy/ember';
 module('Acceptance | Percy test', function (hooks) {
   setupApplicationTest(hooks);
 
-  // Percy (percySnapshot) doesn't allow to target specific DOM elements, so we have to "blacklist" the elements
-  // that we want to exclude from the snapshots using their own "Percy-specific CSS".
-  // see: https://docs.percy.io/docs/percy-specific-css#section-hiding-regions-with-percy-specific-css
-  const percyCSSExclude = `header, footer, main section:not([data-test-percy]) { display: none; }`;
-
   test('Take percy snapshots', async function (assert) {
     await visit('/components/badge');
-    await percySnapshot('Badge', { percyCSSExclude });
+    await percySnapshot('Badge');
 
     await visit('/components/card');
-    await percySnapshot('Card', { percyCSSExclude });
+    await percySnapshot('Card');
 
     assert.ok(true);
   });
