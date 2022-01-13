66f67337086116fdd719adbb20722d67b16d55ba
Cristiano Rastelli <public@didoo.net>
2022-01-13 18:05:54 +0000

    commit with all the typography-related changes from “spike-generate-helper-classes” branch

--- scripts/build-parts/generateCssHelpers.ts
+++ scripts/build-parts/generateCssHelpers.ts
@@ -2,9 +2,7 @@ import fs from 'fs-extra';
 
 import { Dictionary, Platform }  from 'style-dictionary';
 
-// import { generateColorHelpers } from './generateColorHelpers';
 import { generateElevationHelpers } from './generateElevationHelpers';
-import { generateSpacingHelpers } from './generateSpacingHelpers';
 import { generateTypographyHelpers } from './generateTypographyHelpers';
 
 export const PREFIX = 'hds';
@@ -15,6 +13,12 @@ export async function generateCssHelpers(dictionary: Dictionary, config: Platfor
 
     // tried to use style-dictionary/lib/common/formatHelpers/fileHeader.js but didn't work
     const header = `/**\n * Do not edit directly\n * Generated on ${new Date().toUTCString()}\n */\n`;
+
+    if (dictionary.tokens.typography) {
+        const helpers = generateTypographyHelpers(dictionary.tokens.typography);
+        const content = `${header}\n${helpers.join('\n')}\n`;
+        await fs.writeFile(`${config.buildPath}/helpers/typography.css`, content);
+    }
 
     if (dictionary.tokens.elevation) {
         const helpers = generateElevationHelpers(dictionary.tokens.elevation);
