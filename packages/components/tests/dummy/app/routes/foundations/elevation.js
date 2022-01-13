bc0a97d772a8be69f2de55032d917fda18c9c363
Cristiano Rastelli <public@didoo.net>
2022-01-13 14:25:08 +0000

    added documentation on how to use the “elevation” CSS helpers

--- packages/components/tests/dummy/app/routes/foundations/elevation.js
+++ packages/components/tests/dummy/app/routes/foundations/elevation.js
@@ -1,17 +1,17 @@
 import Route from '@ember/routing/route';
 
+export const ELEVATIONS = ['inset', 'low', 'mid', 'high', 'higher', 'overlay'];
+export const SURFACES = [
+  'inset',
+  'base',
+  'low',
+  'mid',
+  'high',
+  'higher',
+  'overlay',
+];
 export default class FoundationsElevationRoute extends Route {
   model() {
-    const ELEVATIONS = ['inset', 'low', 'mid', 'high', 'higher', 'overlay'];
-    const SURFACES = [
-      'inset',
-      'base',
-      'low',
-      'mid',
-      'high',
-      'higher',
-      'overlay',
-    ];
     return { ELEVATIONS, SURFACES };
   }
 }
