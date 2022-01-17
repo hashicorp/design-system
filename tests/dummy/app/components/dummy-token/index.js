21258061ba395835607c13ad11ca8514809e391b
Cristiano Rastelli <public@didoo.net>
2022-01-17 17:31:31 +0000

    refactored for better logic responsibility

--- tests/dummy/app/components/dummy-token/index.js
+++ tests/dummy/app/components/dummy-token/index.js
@@ -3,8 +3,35 @@ import Component from '@glimmer/component';
 export default class DummyPlaceholderIndexComponent extends Component {
   get token() {
     let { token } = this.args;
-    return token;
+    return {
+      name: token.name,
+      value: token.value,
+      category: token.attributes.category,
+      original_value: token.original.value,
+      comment: token?.documentation?.comment
+        ? token.documentation.comment
+        : false,
+    };
   }
+
+  get isAlias() {
+    return (
+      this.token.original &&
+      this.token.original.value !== this.token.value &&
+      this.token.original.value.includes('{')
+    );
+  }
+
+  get isColor() {
+    return (
+      this.token.value.startsWith('#') || this.token.value.startsWith('rgb')
+    );
+  }
+
+  get isDeprecated() {
+    return this.token.deprecated;
+  }
+
   get colorPreviewStyle() {
     return this.token.isColor ? `color: ${this.token.value}` : undefined;
   }
--- tests/dummy/app/routes/foundations/tokens.js
+++ tests/dummy/app/routes/foundations/tokens.js
@@ -1,28 +1,9 @@
 import Route from '@ember/routing/route';
 
-import TOKENSJSON from '@hashicorp/design-system-tokens/docs/products/tokens.json';
+import TOKENS_RAW from '@hashicorp/design-system-tokens/docs/products/tokens.json';
 
 export default class FoundationsElevationRoute extends Route {
   model() {
-    const TOKENSLIST = TOKENSJSON.map((token) => {
-      return {
-        name: token.name,
-        value: token.value,
-        category: token.attributes.category,
-        original_value: token.original.value,
-        searchable: `${token.name} ${token.value}`,
-        comment: token?.documentation?.comment
-          ? token.documentation.comment
-          : false,
-        isAlias:
-          token.original &&
-          token.original.value !== token.value &&
-          token.original.value.includes('{'),
-        isColor: token.value.startsWith('#') || token.value.startsWith('rgb'),
-        isDeprecated: token.deprecated,
-      };
-    });
-
-    return { TOKENSLIST };
+    return { TOKENS_RAW };
   }
 }
