5b17df7a15d56afa5db0ad4785628030bfc88497
Cristiano Rastelli <public@didoo.net>
2022-01-17 19:08:17 +0000

    added showcase for typography

--- tests/dummy/app/routes/foundations/typography.js
+++ tests/dummy/app/routes/foundations/typography.js
@@ -1,20 +1,25 @@
 import Route from '@ember/routing/route';
 
-export const STYLES = {
-  'heading-0': ['bold'],
-  'heading-1': ['bold'],
-  'heading-2': ['bold'],
-  'heading-3': ['semibold'],
-  'heading-4': ['medium'],
-  'display-large': ['medium', 'semibold'],
-  'display-small': ['medium', 'semibold'],
-  'body-large': ['regular', 'medium', 'semibold'],
-  'body-base': ['regular', 'medium', 'semibold'],
-  'body-small': ['regular', 'medium', 'semibold'],
-};
+export const FONT_FAMILIES = ['sans-display', 'sans-text', 'mono-code'];
+export const FONT_WEIGHTS = ['regular', 'medium', 'semibold', 'bold'];
+export const DISPLAY_STYLES = [
+  'display-500',
+  'display-400',
+  'display-300',
+  'display-200',
+  'display-100',
+];
+export const BODY_STYLES = ['body-300', 'body-200', 'body-100'];
+export const CODE_STYLES = ['code-100'];
 
 export default class FoundationsElevationRoute extends Route {
   model() {
-    return { STYLES };
+    return {
+      FONT_FAMILIES,
+      FONT_WEIGHTS,
+      DISPLAY_STYLES,
+      BODY_STYLES,
+      CODE_STYLES,
+    };
   }
 }
