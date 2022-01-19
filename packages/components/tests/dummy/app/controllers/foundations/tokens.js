bbdbabbe0d5045fc05139eedf3cf862b3cc1e6a4
Cristiano Rastelli <public@didoo.net>
2022-01-19 12:18:59 +0000

    removed “filter” functionality
    (will be added in a separate PR)

--- packages/components/tests/dummy/app/controllers/foundations/tokens.js
+++ packages/components/tests/dummy/app/controllers/foundations/tokens.js
@@ -1,48 +1,18 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
-import { restartableTask, timeout } from 'ember-concurrency';
 
-const DEBOUNCE_MS = 250;
 export default class TokensController extends Controller {
-  queryParams = ['query'];
-  @tracked query = null;
-  @tracked model;
+  get groupedTokens() {
+    const groupedTokens = {};
 
-  get filteredTokens() {
-    let query = this.query;
-    let tokens = this.model.TOKENS_RAW;
-
-    if (query) {
-      return tokens.filter((token) => {
-        return (
-          token.name.indexOf(query) !== -1 || token.value.indexOf(query) !== -1
-        );
-      });
-    } else {
-      return tokens;
-    }
-  }
-
-  get filteredAndGroupedTokens() {
-    const filteredAndGroupedTokens = {};
-
-    this.filteredTokens.forEach((token) => {
+    this.model.TOKENS_RAW.forEach((token) => {
       const category = token.attributes.category;
-      if (!filteredAndGroupedTokens[category]) {
-        filteredAndGroupedTokens[category] = [];
+      if (!groupedTokens[category]) {
+        groupedTokens[category] = [];
       }
 
-      filteredAndGroupedTokens[category].push(token);
+      groupedTokens[category].push(token);
     });
 
-    return filteredAndGroupedTokens;
-  }
-
-  @restartableTask *searchTokens(query) {
-    yield timeout(DEBOUNCE_MS);
-
-    this.query = query;
-
-    return this.filteredAndGroupedTokens;
+    return groupedTokens;
   }
 }
