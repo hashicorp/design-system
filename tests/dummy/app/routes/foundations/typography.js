284e4467db4c6588c34b993ce8b2374010e1f636
Cristiano Rastelli <public@didoo.net>
2022-02-08 19:15:06 +0000

    simplified code

--- tests/dummy/app/routes/foundations/typography.js
+++ tests/dummy/app/routes/foundations/typography.js
@@ -11,6 +11,19 @@ export const DISPLAY_STYLES = [
 ];
 export const BODY_STYLES = ['body-300', 'body-200', 'body-100'];
 export const CODE_STYLES = ['code-100'];
+// we add all the allowed combinations here, per design specs
+export const STYLES_COMBINATIONS = {
+  'display-500': ['bold'],
+  'display-400': ['medium', 'semibold', 'bold'],
+  'display-300': ['medium', 'semibold', 'bold'],
+  'display-200': ['semibold'],
+  'display-100': ['medium'],
+  'body-300': ['regular', 'medium', 'semibold'],
+  'body-200': ['regular', 'medium', 'semibold'],
+  'body-100': ['regular', 'medium', 'semibold'],
+  // TODO! ask @heather what are the conditions here
+  'code-100': ['regular', 'medium', 'semibold', 'bold'],
+};
 
 export default class FoundationsElevationRoute extends Route {
   model() {
@@ -20,6 +33,7 @@ export default class FoundationsElevationRoute extends Route {
       DISPLAY_STYLES,
       BODY_STYLES,
       CODE_STYLES,
+      STYLES_COMBINATIONS,
     };
   }
 }
