b3776a68e85d85a9a3eb14f96bce8edb50395d3b
Cristiano Rastelli <public@didoo.net>
2022-02-08 19:15:06 +0000

    simplified code

--- packages/components/tests/dummy/app/controllers/foundations/typography.js
+++ packages/components/tests/dummy/app/controllers/foundations/typography.js
@@ -6,6 +6,7 @@ import {
   DISPLAY_STYLES,
   BODY_STYLES,
   CODE_STYLES,
+  STYLES_COMBINATIONS,
 } from '../../routes/foundations/typography';
 
 export default class TypographyController extends Controller {
@@ -19,57 +20,7 @@ export default class TypographyController extends Controller {
     return [...DISPLAY_STYLES, ...BODY_STYLES, ...CODE_STYLES];
   }
   get stylesCombinations() {
-    const combinations = [];
-    // we add all these conditionals here, per design specs
-    DISPLAY_STYLES.forEach((style) => {
-      const weights = [];
-      this.weights.forEach((weight) => {
-        const isAllowed =
-          (style === 'display-500' && weight === 'bold') ||
-          (style === 'display-400' &&
-            (weight === 'bold' ||
-              weight === 'semibold' ||
-              weight === 'medium')) ||
-          (style === 'display-300' &&
-            (weight === 'bold' ||
-              weight === 'semibold' ||
-              weight === 'medium')) ||
-          (style === 'display-200' && weight === 'semibold') ||
-          (style === 'display-100' && weight === 'medium');
-        if (isAllowed) {
-          weights.push(weight);
-        }
-      });
-      combinations.push({
-        name: style,
-        weights: weights,
-      });
-    });
-    BODY_STYLES.forEach((style) => {
-      const weights = [];
-      this.weights.forEach((weight) => {
-        const isAllowed = weight !== 'bold';
-        if (isAllowed) {
-          weights.push(weight);
-        }
-      });
-      combinations.push({
-        name: style,
-        weights: weights,
-      });
-    });
-    CODE_STYLES.forEach((style) => {
-      const weights = [];
-      this.weights.forEach((weight) => {
-        // TODO! ask @heather what are the conditions here
-        weights.push(weight);
-      });
-      combinations.push({
-        name: style,
-        weights: weights,
-      });
-    });
-    return combinations;
+    return STYLES_COMBINATIONS;
   }
   get csshelpers() {
     const helpers = [];
