3fd0f7f7471636b2635eaa078a715f9441526406
Cristiano Rastelli <public@didoo.net>
2022-02-08 19:01:34 +0000

    updated docs for typography to show only “allowed” styles

--- packages/components/tests/dummy/app/controllers/foundations/typography.js
+++ packages/components/tests/dummy/app/controllers/foundations/typography.js
@@ -18,6 +18,59 @@ export default class TypographyController extends Controller {
   get styles() {
     return [...DISPLAY_STYLES, ...BODY_STYLES, ...CODE_STYLES];
   }
+  get stylesCombinations() {
+    const combinations = [];
+    // we add all these conditionals here, per design specs
+    DISPLAY_STYLES.forEach((style) => {
+      const weights = [];
+      this.weights.forEach((weight) => {
+        const isAllowed =
+          (style === 'display-500' && weight === 'bold') ||
+          (style === 'display-400' &&
+            (weight === 'bold' ||
+              weight === 'semibold' ||
+              weight === 'medium')) ||
+          (style === 'display-300' &&
+            (weight === 'bold' ||
+              weight === 'semibold' ||
+              weight === 'medium')) ||
+          (style === 'display-200' && weight === 'semibold') ||
+          (style === 'display-100' && weight === 'medium');
+        if (isAllowed) {
+          weights.push(weight);
+        }
+      });
+      combinations.push({
+        name: style,
+        weights: weights,
+      });
+    });
+    BODY_STYLES.forEach((style) => {
+      const weights = [];
+      this.weights.forEach((weight) => {
+        const isAllowed = weight !== 'bold';
+        if (isAllowed) {
+          weights.push(weight);
+        }
+      });
+      combinations.push({
+        name: style,
+        weights: weights,
+      });
+    });
+    CODE_STYLES.forEach((style) => {
+      const weights = [];
+      this.weights.forEach((weight) => {
+        // TODO! ask @heather what are the conditions here
+        weights.push(weight);
+      });
+      combinations.push({
+        name: style,
+        weights: weights,
+      });
+    });
+    return combinations;
+  }
   get csshelpers() {
     const helpers = [];
     this.families.forEach((family) => {
