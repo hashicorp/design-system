e72212342d8867fb150c0fbf8ada290092cb2975
Cristiano Rastelli <public@didoo.net>
2022-02-24 22:16:25 +0000

    added focus-trap events for debugging

--- packages/components/addon/components/hds/disclosure/index.js
+++ packages/components/addon/components/hds/disclosure/index.js
@@ -1,31 +1,62 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { guidFor } from '@ember/object/internals';
 
 export default class HdsDisclosureComponent extends Component {
   // @tracked isExpanded = true;
   @tracked isExpanded = this.args.isExpanded;
+  @tracked isDeactivating = false;
+
+  toggleId = guidFor(this);
 
   @action
-  toggle() {
-    console.log('toggle', this.isExpanded);
+  onToggle() {
+    console.log('onToggle', this.isExpanded);
     this.isExpanded = !this.isExpanded;
+  }
+
+  // THESE ARE USED FOR DEBUGGING, WILL BE REMOVED
+  @action
+  onTestItWorks() {
+    console.log('it works!');
+  }
+
+  @action
+  onActivate() {
+    console.log(
+      'onActivate',
+      `isExpanded=${this.isExpanded}`,
+      `isDeactivating=${this.isDeactivating}`
+    );
+  }
+
+  @action
+  onPostActivate() {
+    console.log(
+      'onPostActivate',
+      `isExpanded=${this.isExpanded}`,
+      `isDeactivating=${this.isDeactivating}`
+    );
   }
 
   @action
   onDeactivate() {
-    console.log('onDeactivate', this.isExpanded);
+    console.log(
+      'onDeactivate',
+      `isExpanded=${this.isExpanded}`,
+      `isDeactivating=${this.isDeactivating}`
+    );
     this.isExpanded = false;
+    this.isDeactivating = true;
   }
 
   @action
-  onToggle() {
-    console.log('onToggle');
-    this.toggle();
-  }
-
-  @action
-  onTestItWorks() {
-    console.log('it works!');
+  onPostDeactivate() {
+    console.log(
+      'onPostDeactivate',
+      `isExpanded=${this.isExpanded}`,
+      `isDeactivating=${this.isDeactivating}`
+    );
   }
 }
