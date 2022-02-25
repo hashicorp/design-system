a4145317503205fdfda75c1bbcddbb3b0654f3bc
Cristiano Rastelli <public@didoo.net>
2022-02-25 15:22:49 +0000

    committing test for future reference and not losing all the progress
    (not working, problems with click outside)

--- packages/components/addon/components/hds/disclosure/index.js
+++ packages/components/addon/components/hds/disclosure/index.js
@@ -4,59 +4,90 @@ import { tracked } from '@glimmer/tracking';
 import { guidFor } from '@ember/object/internals';
 
 export default class HdsDisclosureComponent extends Component {
-  // @tracked isActive = true;
-  @tracked isActive = this.args.isActive;
-  @tracked isDeactivating = false;
+  @tracked isActive = this.args.isActive ?? undefined;
+  @tracked isInsideToggleContainer = false;
+  @tracked isDeactivatingWithClickOnTrigger = false;
+  @tracked myTriggerNode;
+  @tracked myEventPath = [];
 
   toggleId = guidFor(this);
 
   @action
-  onToggle() {
-    console.log('onToggle', this.isActive);
+  onClickToggle(event) {
+    console.log(
+      'aaa',
+      event.currentTarget,
+      event.target.isSameNode(this.myTriggerNode)
+    );
+    console.log('bbb', this.myEventPath.includes(event.currentTarget));
+    console.warn(
+      'onClickToggle1',
+      `isActive=${this.isActive}`,
+      `isInsideToggleContainer=${this.isInsideToggleContainer}`,
+      `isDeactivatingWithClickOnTrigger=${this.isDeactivatingWithClickOnTrigger}`
+    );
+    // if (
+    //   !this.isActive &&
+    //   !this.isInsideToggleContainer &&
+    //   !this.isDeactivatingWithClickOnTrigger
+    // ) {
+    //   this.isActive = true;
+    // } else {
+    //   this.isActive = false;
+    // }
+    // if (!this.isActive && this.isDeactivatingWithClickOnTrigger) {
+    //   console.log('>>>>> Exception!!');
+    // } else {
+    //   this.isActive = !this.isActive;
+    // }
     this.isActive = !this.isActive;
-  }
-
-  // THESE ARE USED FOR DEBUGGING, WILL BE REMOVED
-  @action
-  onTestItWorks() {
-    console.log('it works!');
-  }
-
-  @action
-  onActivate() {
-    console.log(
-      'onActivate',
+    this.isDeactivatingWithClickOnTrigger = false;
+    this.isInsideToggleContainer = false;
+    console.warn(
+      'onClickToggle2',
       `isActive=${this.isActive}`,
-      `isDeactivating=${this.isDeactivating}`
+      `isInsideToggleContainer=${this.isInsideToggleContainer}`,
+      `isDeactivatingWithClickOnTrigger=${this.isDeactivatingWithClickOnTrigger}`
     );
+    console.log(this.isActive ? '[OPENED]' : '[CLOSED]');
   }
 
   @action
-  onPostActivate() {
-    console.log(
-      'onPostActivate',
-      `isActive=${this.isActive}`,
-      `isDeactivating=${this.isDeactivating}`
+  clickOutsideDeactivates(event) {
+    this.isInsideToggleContainer = event.path.some(
+      (node) => node.id === this.toggleId
     );
+    console.log(
+      'clickOutsideDeactivates',
+      `isActive=${this.isActive}`,
+      `isInsideToggleContainer=${this.isInsideToggleContainer}`,
+      event,
+      event.target,
+      event.currentTarget
+    );
+    this.myTriggerNode = event.target;
+    this.myEventPath = event.path;
+    return true;
   }
 
   @action
   onDeactivate() {
     console.log(
-      'onDeactivate',
+      'onDeactivate1',
       `isActive=${this.isActive}`,
-      `isDeactivating=${this.isDeactivating}`
+      `isInsideToggleContainer=${this.isInsideToggleContainer}`
     );
+    // this.isActive = false;
+    // this.isInsideToggleContainer = false;
+    if (this.isInsideToggleContainer) {
+      this.isDeactivatingWithClickOnTrigger = true;
+      this.isInsideToggleContainer = false;
+    }
     this.isActive = false;
-    this.isDeactivating = true;
-  }
-
-  @action
-  onPostDeactivate() {
     console.log(
-      'onPostDeactivate',
+      'onDeactivate2',
       `isActive=${this.isActive}`,
-      `isDeactivating=${this.isDeactivating}`
+      `isInsideToggleContainer=${this.isInsideToggleContainer}`
     );
   }
 }
