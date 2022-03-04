3e18dc20142b0cadb0c1701d7ad8e580339aa08d
Amy Lam <amy.r.lam@gmail.com>
2022-03-04 16:45:20 -0700

    Remove individual's token from repo
    User should generate their own and use locally

--- packages/figma-scripting/plugins/_figmaToken.ts
+++ packages/figma-scripting/plugins/_figmaToken.ts
@@ -4,4 +4,4 @@
 // and generate a new one using the "Get personal access token" link
 // or you can go directly in the Figma website, under the user settings
 // and create one from there (so you can also assign a name to it)
-export const figmaToken = '228668-d76c9279-7eca-453a-b3f6-189ed4f98b7c';
+export const figmaToken = 'copy-your-token-here';
