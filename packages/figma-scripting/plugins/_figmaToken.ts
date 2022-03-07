1f844f6c77d15f1d19018030ffe90bb9124f393a
Amy Lam <amy.r.lam@gmail.com>
2022-03-07 12:58:14 -0700

    Sync .env and .gitignore's
    Have figma-scripting match flight-icons

--- packages/figma-scripting/plugins/_figmaToken.ts
+++
@@ -1,7 +0,0 @@
-// This is an api key created from the account of Cristiano Rastelli
-// If you need to create a new token this is the URL to use:
-// https://www.figma.com/developers/api#access-tokens
-// and generate a new one using the "Get personal access token" link
-// or you can go directly in the Figma website, under the user settings
-// and create one from there (so you can also assign a name to it)
-export const figmaToken = 'copy-your-token-here';
