b6673a6c3d312dbd0967f6a0aa5e828fc8bfa504
Melanie Sumner <melanie@hashicorp.com>
2022-03-08 14:16:54 -0600

    updates to unit tests, still WIP

--- packages/components/tests/unit/helpers/hds-link-to-query-test.js
+++ packages/components/tests/unit/helpers/hds-link-to-query-test.js
@@ -6,15 +6,7 @@ module('Unit | Helper | hds-link-to-query', function (hooks) {
   setupTest(hooks);
 
   test('returns the same object that is passed as argument', async function (assert) {
-    let result = hdsLinkToQuery([{ category: 'learn', page: '1' }]);
-
-    assert.deepEqual(result, {
-      isQueryParams: true,
-      values: {
-        category: 'learn',
-        page: '1',
-      },
-    });
+    assert.equal(hdsLinkToQuery(['test']), 'test');
   });
 
   test('returns an empty object if no argument is passed', async function (assert) {
