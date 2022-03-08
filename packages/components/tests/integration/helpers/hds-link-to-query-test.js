297dbb0c0201d071dfa2d75ac73ce127f9916d53
Melanie Sumner <melanie@hashicorp.com>
2022-03-08 13:19:43 -0600

    updated test; WIP but more correct

--- packages/components/tests/integration/helpers/hds-link-to-query-test.js
+++ packages/components/tests/integration/helpers/hds-link-to-query-test.js
@@ -1,4 +1,4 @@
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
@@ -7,10 +7,20 @@ module('Integration | Helper | hds-link-to-query', function (hooks) {
   setupRenderingTest(hooks);
 
   test('returns the same object that is passed as argument', async function (assert) {
-    this.set('query', 'test');
+    this.set('queryParams', "['category']");
+
+    await render(
+      hbs`<Hds::LinkTo::Standalone @route="index" @query={{this.category}} @text="home" @icon="collections" />`
+    );
+
+    assert.equal(this.query, 'category');
+  });
+
+  skip('returns an empty object if no argument is passed', async function (assert) {
+    this.set('query', '');
 
     await render(hbs`{{hds-link-to-query this.query}}`);
 
-    assert.equal(this.query, 'test');
+    assert.equal(this.query, '');
   });
 });
