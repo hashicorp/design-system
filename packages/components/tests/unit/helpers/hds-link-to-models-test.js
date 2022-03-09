2a26c0518b340182c646dc3a8595c5d6dd848286
Cristiano Rastelli <public@didoo.net>
2022-03-09 11:53:14 +0000

    added unit test for “hds-link-to-models”

---
+++ packages/components/tests/unit/helpers/hds-link-to-models-test.js
@@ -0,0 +1,28 @@
+import { module, test } from 'qunit';
+import { hdsLinkToModels } from '@hashicorp/design-system-components/helpers/hds-link-to-models';
+
+module('Unit | Helper | hds-link-to-models', function () {
+  test('returns the same array of models that is passed as argument', async function (assert) {
+    // NOTICE: helpers arguments are positional, so we have to use this trick
+    const args = new Array(2);
+    args[1] = ['model-1', 'model-2', 'model-3'];
+    assert.deepEqual(hdsLinkToModels(args), ['model-1', 'model-2', 'model-3']);
+  });
+  test('returns an array containing the model if a single model is passed as argument', async function (assert) {
+    const args = new Array(2);
+    args[0] = 'model';
+    assert.deepEqual(hdsLinkToModels(args), ['model']);
+  });
+  test('returns an empty array if no argument is passed', async function (assert) {
+    const args = new Array(2);
+    assert.deepEqual(hdsLinkToModels(args), []);
+  });
+  test('it should throw an assertion if both "model" and "models" are provided', async function (assert) {
+    const args = new Array(2);
+    args[0] = 'model';
+    args[1] = ['model-1', 'model-2', 'model-3'];
+    assert.throws(function () {
+      hdsLinkToModels(args);
+    });
+  });
+});
--- packages/components/tests/unit/helpers/hds-link-to-query-test.js
+++ packages/components/tests/unit/helpers/hds-link-to-query-test.js
@@ -5,7 +5,6 @@ module('Unit | Helper | hds-link-to-query', function () {
   test('returns the same object that is passed as argument', async function (assert) {
     assert.equal(hdsLinkToQuery(['test']), 'test');
   });
-
   test('returns an empty object if no argument is passed', async function (assert) {
     assert.deepEqual(hdsLinkToQuery([]), {});
   });
