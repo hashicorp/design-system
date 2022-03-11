dd3e273f8bf2744bd224a3d51ed398109ac990ce
Cristiano Rastelli <public@didoo.net>
2022-03-11 16:27:23 +0000

    porting of the git diff of the branch “breadcrumb-component” from @hashicorp/design-system-components

---
+++ packages/components/addon/components/hds/breadcrumb/item.js
@@ -0,0 +1,50 @@
+import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/template';
+import { assert } from '@ember/debug';
+
+export default class HdsBreadcrumbItemComponent extends Component {
+  /**
+   * @param maxWidth
+   * @type {string}
+   * @default undefined
+   * @description A parameter that can be applied to an "item" to limit its max-width
+   */
+  get maxWidth() {
+    let { maxWidth } = this.args;
+
+    if (maxWidth) {
+      assert(
+        `@maxWidth for "Hds::Breadcrumb::Item" must be a size as number in 'px' or in 'em' (eg. '200px' or '24em'), received: ${maxWidth}`,
+        maxWidth.match(/^\d+(px|em)$/)
+      );
+
+      return maxWidth;
+    } else {
+      return undefined;
+    }
+  }
+
+  /**
+   * Get the inline style to apply to the item.
+   * @method BreadcrumbItem#itemStyle
+   * @return {string} The "style" attribute to apply to the item.
+   */
+  get itemStyle() {
+    if (this.maxWidth) {
+      return htmlSafe(`max-width: ${this.maxWidth}`);
+    } else {
+      return undefined;
+    }
+  }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method BreadcrumbItem#classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-breadcrumb__item'];
+
+    return classes.join(' ');
+  }
+}
