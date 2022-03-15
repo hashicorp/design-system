231094f4f05078be2cbf42296c5bc4bc1eb7d3fc
Amy Lam <amy.r.lam@gmail.com>
2022-03-14 20:53:34 -0600

    Change patch
    Module was not reading from npm anyways

--- packages/components/tests/dummy/app/routes/foundations/tokens.js
+++ packages/components/tests/dummy/app/routes/foundations/tokens.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 
-import TOKENS_RAW from '@hashicorp/design-system-tokens/dist/docs/products/tokens.json';
+import TOKENS_RAW from '../../node_modules/@hashicorp/design-system-tokens/dist/docs/products/tokens.json';
 
 export default class FoundationsTokensRoute extends Route {
   model() {
