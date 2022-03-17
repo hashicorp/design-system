b908cd321f5b3fa9a137bc807c885fed75b3b4e2
Melanie Sumner <melanie@hashicorp.com>
2022-03-17 11:37:22 -0500

    added invocation examples

--- packages/components/tests/dummy/app/routes/components/cta.js
+++ packages/components/tests/dummy/app/routes/components/cta.js
@@ -1,10 +1,13 @@
 import Route from '@ember/routing/route';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ComponentsCtaRoute extends Route {
+  @tracked isAction = false;
+
   @action
   mySpecialAction() {
-    let mSA = 'my special action';
-    console.log(`${mSA}`);
+    this.isAction = !this.isAction;
+    console.log(`isAction is ${this.isAction}`);
   }
 }
