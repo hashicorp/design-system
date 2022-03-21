88527a2ffc6214c749a3de6e834ab9dd98fd2ccf
Cristiano Rastelli <public@didoo.net>
2022-03-21 13:40:13 +0000

    removed “disabled” state

--- packages/components/tests/dummy/app/routes/components/cta.js
+++ packages/components/tests/dummy/app/routes/components/cta.js
@@ -5,7 +5,7 @@ import { SIZES } from '@hashicorp/design-system-components/components/hds/cta';
 export default class ComponentsButtonRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
-    const STATES = ['default', 'hover', 'active', 'focus', 'disabled'];
+    const STATES = ['default', 'hover', 'active', 'focus'];
     return { SIZES, STATES };
   }
 }
