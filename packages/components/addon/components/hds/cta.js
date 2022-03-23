a902509ff316f3e5fb3d2368219e43c169720acc
Cristiano Rastelli <public@didoo.net>
2022-03-23 16:53:50 +0000

    pushed changes to show what the issue is

--- packages/components/addon/components/hds/cta.js
+++ packages/components/addon/components/hds/cta.js
@@ -4,7 +4,11 @@ import { action } from '@ember/object';
 export default class HdsCtaComponent extends Component {
   @action
   doRouteTransition(e) {
+    console.log('This is triggered by every instance of the CTA in the page');
     if (document.activeElement) {
+      console.log(
+        'This should be triggered by only the "active" instance of the CTA, but is not (look in the devtools console)'
+      );
       e.preventDefault();
       document.activeElement.click();
     }
