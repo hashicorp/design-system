0986f605e59f3e2d65ce48ab2a0b69c45ebf0ba7
Cristiano Rastelli <public@didoo.net>
2022-03-23 21:09:37 +0000

    updated the showcase for the “Dropdown/Toggle” to show all the possible variants and states

--- packages/components/tests/dummy/app/routes/components/dropdown.js
+++ packages/components/tests/dummy/app/routes/components/dropdown.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
 
-export default class ComponentsDropdownRoute extends Route {}
+// TODO! import from the component JS file
+const TOGGLE_BUTTON_COLORS = ['primary', 'secondary'];
+const TOGGLE_MORE_SIZES = ['medium', 'small'];
+export default class ComponentsDropdownRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    const STATES = ['default', 'hover', 'active', 'focus', 'disabled'];
+    return { TOGGLE_BUTTON_COLORS, TOGGLE_MORE_SIZES, STATES };
+  }
+}
