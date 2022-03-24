3b6193e58f9c7a962e81da080ac48ad6cae63a9d
Cristiano Rastelli <public@didoo.net>
2022-03-24 14:25:53 +0000

    split the “Dropdown::Toggle” in specialized sub-components

---
+++ packages/components/addon/components/hds/dropdown/toggle-user.js
@@ -0,0 +1,41 @@
+import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+
+export default class HdsDropdownToggleUserComponent extends Component {
+  /**
+   * @param text
+   * @type {string}
+   * @description The text of the `aria-label` applied to the toggle
+   */
+  get text() {
+    let { text } = this.args;
+
+    assert(
+      '@text for "Hds::Dropdown::ToggleUser" must have a valid value',
+      text !== undefined
+    );
+
+    return text;
+  }
+
+  /**
+   * @param isDisabled
+   * @type {boolean}
+   * @default null
+   * @description Sets the native HTML attribute `disabled` on the button element. Default is null (doesn't render the attribute).
+   */
+  get isDisabled() {
+    return this.args.isDisabled ?? null;
+  }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method Button#classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-dropdown-toggle', 'hds-dropdown-toggle--user'];
+
+    return classes.join(' ');
+  }
+}
--- packages/components/tests/dummy/app/routes/components/dropdown.js
+++ packages/components/tests/dummy/app/routes/components/dropdown.js
@@ -1,9 +1,7 @@
 import Route from '@ember/routing/route';
 
-import {
-  BUTTON_COLORS as TOGGLE_BUTTON_COLORS,
-  MORE_SIZES as TOGGLE_MORE_SIZES,
-} from '@hashicorp/design-system-components/components/hds/dropdown/toggle';
+import { COLORS as TOGGLE_BUTTON_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/toggle-button';
+import { SIZES as TOGGLE_MORE_SIZES } from '@hashicorp/design-system-components/components/hds/dropdown/toggle-more';
 
 export default class ComponentsDropdownRoute extends Route {
   model() {
