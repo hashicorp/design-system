99107ee70029015942b9ac22fc08e32afef32590
Cristiano Rastelli <public@didoo.net>
2022-03-25 10:52:12 +0000

    removed “disabled" state

--- packages/components/tests/dummy/app/routes/components/copy-button.js
+++ packages/components/tests/dummy/app/routes/components/copy-button.js
@@ -3,14 +3,7 @@ import Route from '@ember/routing/route';
 export default class ComponentsCopyButtonRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
-    const STATES = [
-      'default',
-      'hover',
-      'active',
-      'focus',
-      'disabled',
-      'success',
-    ];
+    const STATES = ['default', 'hover', 'active', 'focus', 'success'];
     return { STATES };
   }
 }
