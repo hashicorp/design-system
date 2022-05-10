f9bd60a5aa9b59612bcfb5b029a234b1cb27032e
Melanie Sumner <melanie@hashicorp.com>
2022-05-09 19:05:12 -0500

    adds a sidebar to component pages

---
+++ packages/components/tests/dummy/app/components/dummy-toc.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { service } from '@ember/service';
+
+
+export default class DummyTocComponent extends Component {
+  @service router;
+
+  get dummyRouteUrl() {
+    let dummyPathUrl = this.router.currentURL;
+    return dummyPathUrl;
+  }
+}
