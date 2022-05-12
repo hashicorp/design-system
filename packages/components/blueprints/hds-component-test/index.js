bf13448fa50f928a83f1661200cf54b326a0049f
Cristiano Rastelli <public@didoo.net>
2022-05-12 10:44:15 +0100

    added automatic adding of CSS import for dummy documentation page + other small tunings

--- packages/components/blueprints/hds-component-test/index.js
+++ packages/components/blueprints/hds-component-test/index.js
@@ -36,15 +36,48 @@ module.exports = {
   },
 
   afterInstall(options) {
-    updateRouter.call(this, options);
+    updateDummyAppRouter.call(this, options);
+    updateDummyAppCSS.call(this, options);
   },
 };
 
-function updateRouter(options) {
+function updateDummyAppRouter(options) {
   const newRouteToAdd = `components/${options.entity.name}`; // we prefix all the component routes with "components"
   const routerFilePath = `${options.project.root}/tests/dummy/app/router.js`;
-  let source = fs.readFileSync(routerFilePath, 'utf-8');
+  const source = fs.readFileSync(routerFilePath, 'utf-8');
   let oldRoutes = new EmberRouterGenerator(source);
   let newRoutes = oldRoutes['add'](newRouteToAdd, options);
   fs.writeFileSync(routerFilePath, newRoutes.code());
+}
+
+function updateDummyAppCSS(options) {
+  const parts = options.entity.name.split('/');
+  const fileName = `db-${parts.pop()}`;
+  const cssFilePath = `${options.project.root}/tests/dummy/app/styles/app.scss`;
+  const source = fs.readFileSync(cssFilePath, 'utf-8');
+  const oldLinesArray = source.split(/\r?\n/);
+  const firstComponentImportIndex =
+    oldLinesArray.findIndex((line) =>
+      line.match(/^\/\/ START COMPONENT PAGES IMPORTS/)
+    ) + 1;
+  const lastComponentImportIndex =
+    oldLinesArray.findIndex((line) =>
+      line.match(/^\/\/ END COMPONENT PAGES IMPORTS/)
+    ) - 1;
+  const importLinesArray = oldLinesArray.slice(
+    firstComponentImportIndex,
+    lastComponentImportIndex + 1
+  );
+  importLinesArray.push(
+    `@import "./pages/${parts.concat([fileName]).join('/')}";`
+  );
+  const newImportLinesArray = importLinesArray
+    .filter((line, index, self) => self.indexOf(line) === index)
+    .sort();
+  const newLinesArray = [].concat(
+    oldLinesArray.slice(0, firstComponentImportIndex),
+    newImportLinesArray,
+    oldLinesArray.slice(lastComponentImportIndex + 1)
+  );
+  fs.writeFileSync(cssFilePath, newLinesArray.join('\n'));
 }
