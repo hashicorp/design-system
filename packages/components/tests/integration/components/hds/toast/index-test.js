4ce1fddd6c1dd522fa351bca8774ad06c68829f1
Cristiano Rastelli <public@didoo.net>
2022-05-16 12:59:54 +0100

    updated tests for `Alert` and `Toast`

--- packages/components/tests/integration/components/hds/toast/index-test.js
+++ packages/components/tests/integration/components/hds/toast/index-test.js
@@ -9,12 +9,12 @@ module('Integration | Component | hds/toast/index', function (hooks) {
   // notice: "toast" is a wrapper around the "hds::alert" so we test only very specific things
 
   test('it renders the "toast"', async function (assert) {
-    await render(hbs`<Hds::Toast @text="alert text" />`);
+    await render(hbs`<Hds::Toast />`);
     assert.dom(this.element).exists();
   });
 
   test('it should render with a CSS class that matches the component name', async function (assert) {
-    await render(hbs`<Hds::Toast @title="alert text" id="test-toast" />`);
+    await render(hbs`<Hds::Toast id="test-toast" />`);
     assert.dom('#test-toast').hasClass('hds-toast');
   });
 });
