44686a8ec72bdf575133f05aa73bcfe2b6529dfc
Cristiano Rastelli <public@didoo.net>
2022-05-18 14:21:30 +0100

    added list of supported types to the `Form::TextInput` component

--- packages/components/tests/dummy/app/routes/components/form/text-input.js
+++ packages/components/tests/dummy/app/routes/components/form/text-input.js
@@ -1,3 +1,38 @@
 import Route from '@ember/routing/route';
 
-export default class ComponentsFormTextInputRoute extends Route {}
+// Notice: this will later be imported directly from the component
+// This list comes from Structure, and is the list of official HTML supported types for the <input> element (see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input)
+// We will likely review what we're supporting for HDS
+const TYPES = [
+  'button',
+  'checkbox',
+  'color',
+  'date',
+  'datetime-local', // added as replacement for "datetime"
+  // 'datetime', // this is obsolete: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+  'email',
+  'file',
+  'hidden',
+  'image',
+  'month',
+  'number',
+  'password',
+  'radio',
+  'range',
+  'reset',
+  'search',
+  'submit',
+  'tel',
+  'text',
+  'time',
+  'url',
+  'week',
+];
+
+export default class ComponentsFormTextInputRoute extends Route {
+  model() {
+    return {
+      TYPES,
+    };
+  }
+}
