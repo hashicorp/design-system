fcb425af3ffd91d1e6202002c0c0618e87088dbf
Cristiano Rastelli <public@didoo.net>
2022-05-19 17:08:36 +0100

    added basic state showcase for `Select`

--- packages/components/tests/dummy/app/routes/components/form/select.js
+++ packages/components/tests/dummy/app/routes/components/form/select.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
 
-export default class ComponentsFormSelectRoute extends Route {}
+export default class ComponentsFormSelectRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    const STATES = ['default', 'readonly', 'disabled'];
+    return {
+      STATES,
+    };
+  }
+}
