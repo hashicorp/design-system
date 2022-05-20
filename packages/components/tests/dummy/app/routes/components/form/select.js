3e223017500799ce564edee2dc1c71d2834ba38f
Cristiano Rastelli <public@didoo.net>
2022-05-20 15:47:41 +0100

    added basic state showcase for `Checkbox`

--- packages/components/tests/dummy/app/routes/components/form/select.js
+++ packages/components/tests/dummy/app/routes/components/form/select.js
@@ -3,7 +3,8 @@ import Route from '@ember/routing/route';
 export default class ComponentsFormSelectRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
-    const STATES = ['default', 'readonly', 'disabled'];
+    // notice: 'readonly' is not a valid attribute for `<select>`
+    const STATES = ['default', 'disabled'];
     return {
       STATES,
     };
