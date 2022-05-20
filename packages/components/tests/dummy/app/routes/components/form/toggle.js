1c943da987c52820b54e909e53cb6980b1ac6447
Cristiano Rastelli <public@didoo.net>
2022-05-20 16:46:02 +0100

    added basic state showcase for `Toggle`

--- packages/components/tests/dummy/app/routes/components/form/toggle.js
+++ packages/components/tests/dummy/app/routes/components/form/toggle.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
 
-export default class ComponentsFormToggleRoute extends Route {}
+export default class ComponentsFormToggleRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    // notice: 'readonly' is not a valid attribute for `<checkbox>`
+    const STATES = ['default', 'disabled'];
+    return {
+      STATES,
+    };
+  }
+}
