12db4532e19aaef08ef17237232409c81452678b
Cristiano Rastelli <public@didoo.net>
2022-05-23 18:52:50 +0100

    added missing function that includes CSS file for component in HDS main file

--- packages/components/blueprints/hds-component/index.js
+++ packages/components/blueprints/hds-component/index.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const stringUtil = require('ember-cli-string-utils');
+const fs = require('fs');
 
 module.exports = {
   description: 'Generates all files for an HDS component',
@@ -23,4 +24,37 @@ module.exports = {
         .join(' > '),
     };
   },
+
+  afterInstall(options) {
+    updateHDSComponentsCSS.call(this, options);
+  },
+};
+
+const updateHDSComponentsCSS = (options) => {
+  const name = options.entity.name;
+  const cssFilePath = `${options.project.root}/app/styles/@hashicorp/design-system-components.scss`;
+  const source = fs.readFileSync(cssFilePath, 'utf-8');
+  const oldLinesArray = source.split(/\r?\n/);
+  const firstComponentImportIndex =
+    oldLinesArray.findIndex((line) =>
+      line.match(/^\/\/ START COMPONENTS CSS FILES IMPORTS/)
+    ) + 1;
+  const lastComponentImportIndex =
+    oldLinesArray.findIndex((line) =>
+      line.match(/^\/\/ END COMPONENT CSS FILES IMPORTS/)
+    ) - 1;
+  const importLinesArray = oldLinesArray.slice(
+    firstComponentImportIndex,
+    lastComponentImportIndex + 1
+  );
+  importLinesArray.push(`@use "../components/${name}";`);
+  const newImportLinesArray = importLinesArray
+    .filter((line, index, self) => self.indexOf(line) === index)
+    .sort();
+  const newLinesArray = [].concat(
+    oldLinesArray.slice(0, firstComponentImportIndex),
+    newImportLinesArray,
+    oldLinesArray.slice(lastComponentImportIndex + 1)
+  );
+  fs.writeFileSync(cssFilePath, newLinesArray.join('\n'));
 };
