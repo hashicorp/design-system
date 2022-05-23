f70ae03eff019b01ebfc97cfbf182e6f169e765f
Cristiano Rastelli <public@didoo.net>
2022-05-23 11:36:50 +0100

    added basic state showcase for `Radio` + other small updates

--- packages/components/tests/dummy/app/routes/components/form/radio.js
+++ packages/components/tests/dummy/app/routes/components/form/radio.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
 
-export default class ComponentsFormRadioRoute extends Route {}
+export default class ComponentsFormRadioRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    // notice: 'readonly' is not a valid attribute for `<radio>`
+    const STATES = ['default', 'disabled'];
+    return {
+      STATES,
+    };
+  }
+}
