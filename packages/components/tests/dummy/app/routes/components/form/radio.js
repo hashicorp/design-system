c247f58caa221ef9a4e721b525db975b3c20fe97
Cristiano Rastelli <public@didoo.net>
2022-05-25 13:46:53 +0100

    added mocking of states to `radio` showcase

--- packages/components/tests/dummy/app/routes/components/form/radio.js
+++ packages/components/tests/dummy/app/routes/components/form/radio.js
@@ -3,8 +3,7 @@ import Route from '@ember/routing/route';
 export default class ComponentsFormRadioRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
-    // notice: 'readonly' is not a valid attribute for `<radio>`
-    const STATES = ['default', 'disabled'];
+    const STATES = ['default', 'hover', 'active', 'focus'];
     return {
       STATES,
     };
