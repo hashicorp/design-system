2074988de18625972ae07e0480933839a2158b7b
Cristiano Rastelli <public@didoo.net>
2022-05-25 12:22:34 +0100

    added function to mock states from other PR

---
+++ packages/components/tests/dummy/app/controllers/components.js
@@ -0,0 +1,27 @@
+import Controller from '@ember/controller';
+import { scheduleOnce } from '@ember/runloop';
+
+function replaceDummyStates() {
+  document.querySelectorAll('[mock-state-value]').forEach((element) => {
+    let targets;
+    if (element.attributes['mock-state-selector']) {
+      targets = element.querySelectorAll(
+        element.attributes['mock-state-selector'].value
+      );
+    } else {
+      targets = [element];
+    }
+    const states = element.attributes['mock-state-value'].value.split('+');
+    const classes = states.map((state) => `mock-${state.trim()}`);
+    targets.forEach((target) => {
+      target.classList.add(...classes);
+    });
+  });
+}
+
+export default class ComponentsController extends Controller {
+  constructor() {
+    super(...arguments);
+    scheduleOnce('afterRender', this, replaceDummyStates);
+  }
+}
