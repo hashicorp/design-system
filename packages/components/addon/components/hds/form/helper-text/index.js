cf678d64ccb0b1944d2330bc5b03f8a08aaff286
Alex <alex-ju@users.noreply.github.com>
2022-05-25 10:52:29 +0100

    Set a unique id on help-text and/or error only when a fieldId is provided
    To avoid accidentally creating help-text/error elements with the same id

--- packages/components/addon/components/hds/form/helper-text/index.js
+++ packages/components/addon/components/hds/form/helper-text/index.js
@@ -8,6 +8,18 @@ export default class HdsFormHelperTextIndexComponent extends Component {
   // }
 
   /**
+   * Determines the unique ID to assign to the element
+   * @method id
+   * @return {string} The "id" attribute to apply to the element.
+   */
+  get id () {
+    let { fieldId } = this.args;
+    if (fieldId) {
+      return `helper-text-${fieldId}`;
+    }
+  }
+
+  /**
    * Get the class names to apply to the component.
    * @method classNames
    * @return {string} The "class" attribute to apply to the component.
