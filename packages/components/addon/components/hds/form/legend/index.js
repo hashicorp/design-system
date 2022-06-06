a233901b3d5d550fe52b46c0e833246a0485b585
Cristiano Rastelli <public@didoo.net>
2022-06-06 18:55:41 +0100

    added `form::label` component

---
+++ packages/components/addon/components/hds/form/legend/index.js
@@ -0,0 +1,24 @@
+import Component from '@glimmer/component';
+
+export default class HdsFormLegendIndexComponent extends Component {
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-form-legend'];
+
+    // add typographic classes
+    classes.push('hds-typography-body-200', 'hds-font-weight-semibold');
+
+    // add a class based on the @contextualClass argument
+    // notice: this will *not* be documented for public use
+    // the reason for this is that the contextual component declarations don't pass attributes to the component
+    if (this.args._contextualClass) {
+      classes.push(this.args._contextualClass);
+    }
+
+    return classes.join(' ');
+  }
+}
