122c9f10d8fb94fe1ea297fe1cc5f69b61260b4c
Cristiano Rastelli <public@didoo.net>
2022-06-07 18:26:04 +0100

    refactored `select` to follow same pattern as other components (`base` + `field`)

---
+++ packages/components/addon/components/hds/form/select/base.js
@@ -0,0 +1,22 @@
+import Component from '@glimmer/component';
+
+export default class HdsFormSelectBaseComponent extends Component {
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-form-select'];
+
+    // add typographic classes
+    classes.push('hds-typography-body-200', 'hds-font-weight-regular');
+
+    // add a class based on the @isInvalid argument
+    if (this.args.isInvalid) {
+      classes.push(`hds-form-select--is-invalid`);
+    }
+
+    return classes.join(' ');
+  }
+}
