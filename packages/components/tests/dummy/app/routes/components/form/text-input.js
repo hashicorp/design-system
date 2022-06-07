4f0ce396d61ba4eb1d59ae6e65e2808ca7ec3ec0
Cristiano Rastelli <public@didoo.net>
2022-06-07 22:01:13 +0100

    some docs updates

--- packages/components/tests/dummy/app/routes/components/form/text-input.js
+++ packages/components/tests/dummy/app/routes/components/form/text-input.js
@@ -6,14 +6,9 @@ export default class ComponentsFormTextInputRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
     const STATES = ['default', 'hover', 'active', 'focus'];
-    const SAMPLE_ERROR_MESSAGES = [
-      'First error message',
-      'Second error message',
-    ];
     return {
       TYPES,
       STATES,
-      SAMPLE_ERROR_MESSAGES,
     };
   }
 }
