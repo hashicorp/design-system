12c9be55d5e21a51dff1f6e04ab9a570e6b52787
Alex <alex-ju@users.noreply.github.com>
2022-06-08 16:35:34 +0100

    Create relationship between fieldset and helper-text/error

--- packages/components/addon/components/hds/form/fieldset/index.js
+++ packages/components/addon/components/hds/form/fieldset/index.js
@@ -1,5 +1,8 @@
 import Component from '@glimmer/component';
 
+import { guid } from '../utils/guid';
+import { describedBy } from '../utils/describedby';
+
 export default class HdsFormFieldsetIndexComponent extends Component {
   /**
    * Sets the layout of the group
@@ -10,6 +13,22 @@ export default class HdsFormFieldsetIndexComponent extends Component {
    */
   get layout() {
     return this.args.layout ?? 'vertical';
+  }
+
+  /**
+   * Calculates the unique ID to assign to the fieldset
+   */
+  get id() {
+    return guid(this);
+  }
+
+  /**
+   * Get the array of IDs for elements that relate to this fieldset.
+   * @method ariaDescribedBy
+   * @return {string} The "aria-describedby" attribute to apply to the component.
+   */
+  get ariaDescribedBy() {
+    return describedBy(this);
   }
 
   /**
