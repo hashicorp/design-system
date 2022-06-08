b084dfabbd49d9af0965db0c18139c7a3cb8749d
Alex <alex-ju@users.noreply.github.com>
2022-06-08 16:05:17 +0100

    Create fieldset component
    `HdsFormFieldsetIndexComponent` borrows the logic from the current `[checkbox/radio/toggle]/group.js` which consists in dealing with classes and the layout

---
+++ packages/components/addon/components/hds/form/fieldset/index.js
@@ -0,0 +1,29 @@
+import Component from '@glimmer/component';
+
+export default class HdsFormFieldsetIndexComponent extends Component {
+  /**
+   * Sets the layout of the group
+   *
+   * @param layout
+   * @type {enum}
+   * @default 'vertical'
+   */
+  get layout() {
+    return this.args.layout ?? 'vertical';
+  }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    // we just need a class for the layout
+    let classes = ['hds-form-group'];
+
+    // add a class based on the @layout argument
+    classes.push(`hds-form-group--layout-${this.layout}`);
+
+    return classes.join(' ');
+  }
+}
