d2e27099224dc864087750086e209dcda07189bc
Cristiano Rastelli <public@didoo.net>
2022-06-10 00:42:46 +0100

    more documentation updates

--- packages/components/addon/components/hds/form/toggle/base.js
+++ packages/components/addon/components/hds/form/toggle/base.js
@@ -15,6 +15,7 @@ export default class HdsFormToggleBaseComponent extends Component {
     }
 
     // add a class based on the @_wrapperClass argument
+    // we need to use this special argument to pass a class to the wrapping element, because the ...attributes is applied to the control
     // notice: this will *not* be documented for public use
     // the reason for this is that the spread (...) of attributes is applied to the <input> element, but the component has a wrapping container and we need to pass a class to it
     if (this.args._wrapperClass) {
