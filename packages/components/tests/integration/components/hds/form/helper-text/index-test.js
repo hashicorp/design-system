d5b436bb2a865ace49f5b43feaeeff3bf55978a9
Cristiano Rastelli <public@didoo.net>
2022-06-13 15:15:34 +0100

    added integration tests for `label` + `helper-text` + `error` + `legend` + `field` + `fieldset`

--- packages/components/tests/integration/components/hds/form/helper-text/index-test.js
+++ packages/components/tests/integration/components/hds/form/helper-text/index-test.js
@@ -9,12 +9,55 @@ module(
     setupRenderingTest(hooks);
 
     test('it renders the component', async function (assert) {
-      await render(hbs`<Hds::Form::HelperText />`);
-      assert.dom(this.element).exists();
+      await render(hbs`<Hds::Form::HelperText id="test-form-helper-text" />`);
+      assert.dom('#test-form-helper-text').exists();
     });
     test('it should render with a CSS class that matches the component name', async function (assert) {
       await render(hbs`<Hds::Form::HelperText id="test-form-helper-text" />`);
       assert.dom('#test-form-helper-text').hasClass('hds-form-helper-text');
+    });
+    test('it should render with a CSS class provided via the @contextualClass argument', async function (assert) {
+      await render(
+        hbs`<Hds::Form::HelperText @contextualClass="my-class" id="test-form-helper-text" />`
+      );
+      assert.dom('#test-form-helper-text').hasClass('my-class');
+    });
+
+    // CONTENT
+
+    test('it renders a helper text with the defined text', async function (assert) {
+      await render(
+        hbs`<Hds::Form::HelperText id="test-form-helper-text">This is the helper text</Hds::Form::HelperText>`
+      );
+      assert.dom('#test-form-helper-text').hasText('This is the helper text');
+    });
+    test('it renders a helper text with the yielded content', async function (assert) {
+      assert.expect(2);
+      await render(
+        hbs`<Hds::Form::HelperText id="test-form-helper-text"><pre>This is an HTML element inside the helper text</pre></Hds::Form::HelperText>`
+      );
+      assert.dom('#test-form-helper-text > pre').exists();
+      assert
+        .dom('#test-form-helper-text pre')
+        .hasText('This is an HTML element inside the helper text');
+    });
+
+    // ATTRIBUTES
+
+    test('it renders a helper text with the correct "id" attribute if the @controlId argument is provided', async function (assert) {
+      await render(
+        hbs`<Hds::Form::HelperText @controlId="my-control-id">This is the helper text</Hds::Form::HelperText>`
+      );
+      assert.dom('#helper-text-my-control-id').exists();
+    });
+    test('it should spread all the attributes passed to the component', async function (assert) {
+      assert.expect(3);
+      await render(
+        hbs`<Hds::Form::HelperText id="test-form-helper-text" class="my-class" data-test1 data-test2="test" />`
+      );
+      assert.dom('#test-form-helper-text').hasClass('my-class');
+      assert.dom('#test-form-helper-text').hasAttribute('data-test1');
+      assert.dom('#test-form-helper-text').hasAttribute('data-test2', 'test');
     });
   }
 );
---
+++ packages/components/tests/integration/components/hds/form/legend/index-test.js
@@ -0,0 +1,58 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | hds/form/legend/index', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the component', async function (assert) {
+    await render(hbs`<Hds::Form::Legend id="test-form-legend" />`);
+    assert.dom('#test-form-legend').exists();
+  });
+  test('it renders the element as <legend>', async function (assert) {
+    await render(hbs`<Hds::Form::Legend id="test-form-legend" />`);
+    assert.dom('#test-form-legend').hasTagName('legend');
+  });
+  test('it should render with a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::Form::Legend id="test-form-legend" />`);
+    assert.dom('#test-form-legend').hasClass('hds-form-legend');
+  });
+  test('it should render with a CSS class provided via the @contextualClass argument', async function (assert) {
+    await render(
+      hbs`<Hds::Form::Legend @contextualClass="my-class" id="test-form-legend" />`
+    );
+    assert.dom('#test-form-legend').hasClass('my-class');
+  });
+
+  // CONTENT
+
+  test('it renders a legend with the defined text', async function (assert) {
+    await render(
+      hbs`<Hds::Form::Legend id="test-form-legend">This is the legend</Hds::Form::Legend>`
+    );
+    assert.dom('#test-form-legend').hasText('This is the legend');
+  });
+  test('it renders a legend with the yielded content', async function (assert) {
+    assert.expect(2);
+    await render(
+      hbs`<Hds::Form::Legend id="test-form-legend"><pre>This is an HTML element inside the legend</pre></Hds::Form::Legend>`
+    );
+    assert.dom('#test-form-legend > pre').exists();
+    assert
+      .dom('#test-form-legend pre')
+      .hasText('This is an HTML element inside the legend');
+  });
+
+  // ATTRIBUTES
+
+  test('it should spread all the attributes passed to the component', async function (assert) {
+    assert.expect(3);
+    await render(
+      hbs`<Hds::Form::Legend id="test-form-legend" class="my-class" data-test1 data-test2="test" />`
+    );
+    assert.dom('#test-form-legend').hasClass('my-class');
+    assert.dom('#test-form-legend').hasAttribute('data-test1');
+    assert.dom('#test-form-legend').hasAttribute('data-test2', 'test');
+  });
+});
