9cdef31f9fe44b06e64f7e2194ea70765df6d64d
Cristiano Rastelli <public@didoo.net>
2022-06-15 12:20:27 +0100

    added integration tests for `form controls`

---
+++ packages/components/tests/integration/components/hds/form/error/index-test.js
@@ -0,0 +1,72 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | hds/form/error/index', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the component', async function (assert) {
+    await render(hbs`<Hds::Form::Error id="test-form-error" />`);
+    assert.dom('#test-form-error').exists();
+  });
+  test('it should render with a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::Form::Error id="test-form-error" />`);
+    assert.dom('#test-form-error').hasClass('hds-form-error');
+  });
+  test('it should render with a CSS class provided via the @contextualClass argument', async function (assert) {
+    await render(
+      hbs`<Hds::Form::Error @contextualClass="my-class" id="test-form-error" />`
+    );
+    assert.dom('#test-form-error').hasClass('my-class');
+  });
+
+  // CONTENT
+
+  test('it renders an error with the defined text', async function (assert) {
+    await render(
+      hbs`<Hds::Form::Error id="test-form-error">This is the error</Hds::Form::Error>`
+    );
+    assert.dom('#test-form-error').hasText('This is the error');
+  });
+  test('it renders an error with the yielded content', async function (assert) {
+    assert.expect(2);
+    await render(
+      hbs`<Hds::Form::Error id="test-form-error"><pre>This is an HTML element inside the error</pre></Hds::Form::Error>`
+    );
+    assert.dom('#test-form-error pre').exists();
+    assert
+      .dom('#test-form-error pre')
+      .hasText('This is an HTML element inside the error');
+  });
+  test('it renders multiple error messages as contextual components', async function (assert) {
+    assert.expect(2);
+    await render(
+      hbs`<Hds::Form::Error id="test-form-error" as |E|><E.Message>First error message</E.Message><E.Message>Second error message</E.Message></Hds::Form::Error>`
+    );
+    assert
+      .dom('#test-form-error .hds-form-error__message')
+      .exists({ count: 2 });
+    assert
+      .dom('#test-form-error .hds-form-error__message')
+      .hasText('First error message');
+  });
+
+  // ATTRIBUTES
+
+  test('it renders an error with the correct "id" attribute if the @controlId argument is provided', async function (assert) {
+    await render(
+      hbs`<Hds::Form::Error @controlId="my-control-id">This is the error</Hds::Form::Error>`
+    );
+    assert.dom('#error-my-control-id').exists();
+  });
+  test('it should spread all the attributes passed to the component', async function (assert) {
+    assert.expect(3);
+    await render(
+      hbs`<Hds::Form::Error id="test-form-error" class="my-class" data-test1 data-test2="test" />`
+    );
+    assert.dom('#test-form-error').hasClass('my-class');
+    assert.dom('#test-form-error').hasAttribute('data-test1');
+    assert.dom('#test-form-error').hasAttribute('data-test2', 'test');
+  });
+});
