35946566878f0e2ea2c6726bf7af5597993abb91
Cristiano Rastelli <public@didoo.net>
2022-06-17 15:51:10 +0200

    Added `text-input` form control as component

---
+++ packages/components/addon/components/hds/form/text-input/base.js
@@ -0,0 +1,56 @@
+import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+
+// notice: we don't support all the possible HTML types, only a subset
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+export const DEFAULT_TYPE = 'text';
+export const TYPES = [
+  'text',
+  'email',
+  'password',
+  'url',
+  'search',
+  'date',
+  'time',
+];
+
+export default class HdsFormTextInputBaseComponent extends Component {
+  /**
+   * Sets the type of input
+   *
+   * @param type
+   * @type {string}
+   * @default 'text'
+   */
+  get type() {
+    let { type = DEFAULT_TYPE } = this.args;
+
+    assert(
+      `@type for "Hds::Form::TextInput" must be one of the following: ${TYPES.join(
+        ', '
+      )}, received: ${type}`,
+      TYPES.includes(type)
+    );
+
+    return type;
+  }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-form-text-input'];
+
+    // add typographic classes
+    classes.push('hds-typography-body-200', 'hds-font-weight-regular');
+
+    // add a class based on the @isInvalid argument
+    if (this.args.isInvalid) {
+      classes.push(`hds-form-text-input--is-invalid`);
+    }
+
+    return classes.join(' ');
+  }
+}
