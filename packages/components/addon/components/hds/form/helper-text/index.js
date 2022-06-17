5abb75848fd551b43a3991762f41144227acf1c1
Cristiano Rastelli <public@didoo.net>
2022-06-17 15:40:18 +0200

    Added `base elements` for `form controls` as components

---
+++ packages/components/addon/components/hds/form/helper-text/index.js
@@ -0,0 +1,56 @@
+import Component from '@glimmer/component';
+export const ID_PREFIX = 'helper-text-';
+
+const NOOP = () => {};
+
+export default class HdsFormHelperTextIndexComponent extends Component {
+  /**
+   * Determines the unique ID to assign to the element
+   * @method id
+   * @return {(string|boolean)} The "id" attribute to apply to the element or false, if no controlId is provided
+   */
+  get id() {
+    let { controlId } = this.args;
+    if (controlId) {
+      return `${ID_PREFIX}${controlId}`;
+    }
+    return false;
+  }
+
+  /**
+   * @param onInsert
+   * @type {function}
+   * @default () => {}
+   */
+  get onInsert() {
+    let { onInsert } = this.args;
+
+    // notice: this is a guard used to prevent triggering an error when the component is used as standalone element
+    if (typeof onInsert === 'function') {
+      return onInsert;
+    } else {
+      return NOOP;
+    }
+  }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-form-helper-text'];
+
+    // add typographic classes
+    classes.push('hds-typography-body-100', 'hds-font-weight-regular');
+
+    // add a class based on the @contextualClass argument
+    // notice: this will *not* be documented for public use
+    // the reason for this is that the contextual component declarations don't pass attributes to the component
+    if (this.args.contextualClass) {
+      classes.push(this.args.contextualClass);
+    }
+
+    return classes.join(' ');
+  }
+}
