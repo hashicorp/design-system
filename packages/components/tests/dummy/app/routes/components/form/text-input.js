084327c02c5805f0be6b1f90a53a00e9ae24914e
Cristiano Rastelli <public@didoo.net>
2022-06-17 15:51:37 +0200

    Added documentation for `text-input` component

---
+++ packages/components/tests/dummy/app/routes/components/form/text-input.js
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route';
+
+import { TYPES } from '@hashicorp/design-system-components/components/hds/form/text-input/base';
+
+export default class ComponentsFormTextInputRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    const STATES = ['default', 'hover', 'active', 'focus'];
+    return {
+      TYPES,
+      STATES,
+    };
+  }
+}
