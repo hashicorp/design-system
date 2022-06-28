c9f5ec25bf7d0774cbe15afd23d812561eae91a5
Cristiano Rastelli <public@didoo.net>
2022-06-28 13:06:03 +0100

    removed `active` state from showcase of form controls

--- packages/components/tests/dummy/app/routes/components/form/toggle.js
+++ packages/components/tests/dummy/app/routes/components/form/toggle.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default class ComponentsFormToggleRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
-    const STATES = ['default', 'hover', 'active', 'focus'];
+    const STATES = ['default', 'hover', 'focus'];
     return {
       STATES,
     };
