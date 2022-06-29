b83958b7adf06cb68eb60a1592825ff20b71b100
Cristiano Rastelli <public@didoo.net>
2022-06-29 12:30:21 +0100

    updated “Component API” documentation for `Card::Container`

--- packages/components/tests/dummy/app/routes/components/card.js
+++ packages/components/tests/dummy/app/routes/components/card.js
@@ -1,11 +1,20 @@
 import Route from '@ember/routing/route';
 
 import {
+  DEFAULT_LEVEL as CONTAINER_DEFAULT_LEVEL,
   LEVELS as CONTAINER_LEVELS,
+  HOVER_LEVELS as CONTAINER_HOVER_LEVELS,
+  ACTIVE_LEVELS as CONTAINER_ACTIVE_LEVELS,
   BACKGROUNDS as CONTAINER_BACKGROUNDS,
 } from '@hashicorp/design-system-components/components/hds/card/container';
 export default class ComponentsCardRoute extends Route {
   model() {
-    return { CONTAINER_LEVELS, CONTAINER_BACKGROUNDS };
+    return {
+      CONTAINER_DEFAULT_LEVEL,
+      CONTAINER_LEVELS,
+      CONTAINER_HOVER_LEVELS,
+      CONTAINER_ACTIVE_LEVELS,
+      CONTAINER_BACKGROUNDS,
+    };
   }
 }
