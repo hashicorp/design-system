811d1acba57852616e831ee17ba795225267b04d
Alex <alex-ju@users.noreply.github.com>
2022-07-14 17:52:52 +0100

    Add secondary color

--- packages/components/tests/dummy/app/routes/components/tag.js
+++ packages/components/tests/dummy/app/routes/components/tag.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
+import { COLORS } from '@hashicorp/design-system-components/components/hds/tag';
 
 export default class ComponentsTagRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
     const STATES = ['default', 'hover', 'active', 'focus'];
-    return { STATES };
+    return { COLORS, STATES };
   }
 }
