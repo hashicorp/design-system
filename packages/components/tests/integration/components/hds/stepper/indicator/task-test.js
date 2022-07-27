738e54ad2908dc95557d0aac1cdcf7026722e785
Jory Tindall <jory.tindall@hashicorp.com>
2022-07-26 17:54:30 -0700

    Wrote tests for indicator::task

--- packages/components/tests/integration/components/hds/stepper/indicator/task-test.js
+++ packages/components/tests/integration/components/hds/stepper/indicator/task-test.js
@@ -20,5 +20,86 @@ module(
         .dom('#test-stepper-indicator-task')
         .hasClass('hds-stepper-indicator-task');
     });
+
+    // IS INTERACTIVE
+
+    test('it should render the non-interactive variant by default', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="incomplete" />`
+      );
+      assert.dom('#test-stepper-indicator-task').hasClass('hds-stepper-indicator-task')
+    });
+
+    test('it should render the interactive variant if passed the isInteractive property', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="incomplete" @isInteractive={{true}} />`
+      );
+      assert.dom('#test-stepper-indicator-task').hasClass('hds-stepper-indicator-task--is-interactive');
+    });
+
+    // STATUS
+
+    // -- NON INTERACTIVE
+
+    test('it should render the correct status if the @status="incomplete" prop is declared', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="incomplete" />`
+      );
+      assert.dom("#test-stepper-indicator-task").hasClass('hds-stepper-indicator-task--status-incomplete');
+    })
+    test('it should render the correct status if the @status="progress" prop is declared', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="progress" />`
+      );
+      assert.dom("#test-stepper-indicator-task").hasClass('hds-stepper-indicator-task--status-progress');
+    })
+    test('it should render the correct status if the @status="processing" prop is declared', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="processing" />`
+      );
+      assert.dom("#test-stepper-indicator-task").hasClass('hds-stepper-indicator-task--status-processing');
+    })
+    test('it should render the correct status if the @status="complete" prop is declared', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="complete" />`
+      );
+      assert.dom("#test-stepper-indicator-task").hasClass('hds-stepper-indicator-task--status-complete');
+    })
+
+    // -- INTERACTIVE
+
+    test('it should render the correct status if the @status="incomplete" prop is declared', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="incomplete" @isInteractive={{true}} />`
+      );
+      assert.dom("#test-stepper-indicator-task").hasClass('hds-stepper-indicator-task--status-incomplete').hasClass('hds-stepper-indicator-task--is-interactive');
+    })
+    test('it should render the correct status if the @status="progress" prop is declared', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="progress" @isInteractive={{true}} />`
+      );
+      assert.dom("#test-stepper-indicator-task").hasClass('hds-stepper-indicator-task--status-progress').hasClass('hds-stepper-indicator-task--is-interactive');
+    })
+    test('it should render the correct status if the @status="processing" prop is declared', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="processing" @isInteractive={{true}} />`
+      );
+      assert.dom("#test-stepper-indicator-task").hasClass('hds-stepper-indicator-task--status-processing').hasClass('hds-stepper-indicator-task--is-interactive');
+    })
+    test('it should render the correct status if the @status="complete" prop is declared', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="complete" @isInteractive={{true}} />`
+      );
+      assert.dom("#test-stepper-indicator-task").hasClass('hds-stepper-indicator-task--status-complete').hasClass('hds-stepper-indicator-task--is-interactive');
+    })
+
+    // ICON
+
+    test('it should render a flight icon for any of the variants', async function (assert) {
+      await render(
+        hbs`<Hds::Stepper::Indicator::Task id="test-stepper-indicator-task" @status="processing" />`
+      );
+      assert.dom(this.element.querySelector('.flight-icon.flight-icon-loading')).exists();
+    })
   }
 );
