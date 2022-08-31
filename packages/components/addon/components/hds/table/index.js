10283fab8d63df155716703f784c7086b2530df4
Melanie Sumner <melanie@hashicorp.com>
2022-08-31 15:16:09 -0500

    added basic table

--- packages/components/addon/components/hds/table/index.js
+++ packages/components/addon/components/hds/table/index.js
@@ -1,23 +1,69 @@
 import Component from '@glimmer/component';
 
+/**
+ *
+ * `Table` renders an HTML table styled using the `pdsTable` class. It is primarily used to reduce
+ * boilerplate, drive consistency, and hook into column sorting logic.
+ *
+ *
+ * ```
+ * <Table @model={{this.data}} @sortProperty="name" @sortDescending={{false}}>
+ *   <:head as |h|>
+ *     <h.SortBy @field="name">Name</h.SortBy>
+ *     <th>Unsortable Data</th>
+ *   </:head>
+ *   <:body as |row|>
+ *     <tr>
+ *       <td>{{row.name}}</td>
+ *       <td>{{row.complex}} {{row.data}} {{row.goes.here}}</td>
+ *     </tr>
+ *   </:body>
+ * </Table>
+ * ```
+ *
+ * ## :head named block
+ *
+ * This represents the `thead` of the table. It yields within a single `tr` that is meant
+ * to represent column headers.
+ *
+ * It also yields a single contextual component, `SortBy` which applies the `@sortProperty` and
+ * `@sortDescending` args from the `Table` component. This way only `@field` needs to be set to
+ * determine the visual state and the generated anchor href.
+ *
+ *
+ * ## :body named block
+ *
+ * This represents the `tbody` of the table. It yields `n` number of times where `n` is equal to
+ * the length of the `@model` arg provided to `Table`. The yield happens directly in the `tbody`
+ * element to allow for multiple `tr`s for a single record. The yielded value is the exact record
+ * from the `@model` array.
+ *
+ * @class Table
+ */
 export default class HdsTableIndexComponent extends Component {
-  // UNCOMMENT THIS IF YOU NEED A CONSTRUCTOR
-  // constructor() {
-  //   super(...arguments);
-  //   // ADD YOUR ASSERTIONS HERE
-  // }
-
   /**
-   * Get the class names to apply to the component.
-   * @method classNames
-   * @return {string} The "class" attribute to apply to the component.
+   * Sets the data source that gets yielded by the `:body` named block.
+   * @argument model
+   * @type {?array}
    */
-  get classNames() {
-    let classes = ['hds-table'];
+  /**
+   * Specifies which field the `@model` is sorted by. This **does not perform a sort**.
+   * It is only used to set states on the contextual `SortBy` components.
+   * @argument sortProperty
+   * @type {?string}
+   */
+  /**
+   * Specifies which direction the `@model` is sorted. Descending when `true`, ascending when
+   * `false`. This **does not perform a sort**. It is only used to set states on the
+   * contextual `SortBy` components.
+   * @argument sortDescending
+   * @type {?boolean}
+   */
 
-    // add a class based on the @xxx argument
-    // classes.push(`hds-table--[variant]-${this.xxx}`);
-
-    return classes.join(' ');
+  // Convenience getter since all query params are strings.
+  get sortDescending() {
+    const arg = this.args.sortDescending;
+    if (typeof arg === 'boolean') return arg;
+    return arg === 'true';
   }
 }
--- packages/components/tests/dummy/app/routes/components/table.js
+++ packages/components/tests/dummy/app/routes/components/table.js
@@ -1,3 +1,32 @@
 import Route from '@ember/routing/route';
+import { tracked } from '@glimmer/tracking';
 
-export default class ComponentsTableRoute extends Route {}
+export default class ComponentsTableRoute extends Route {
+  queryParams = ['sortby', 'sortdescending'];
+  @tracked sortby = 'year';
+  @tracked sortdescending = false;
+
+  model() {
+    return [
+      { artist: 'Nick Drake', album: 'Pink Moon', year: '1972' },
+      { artist: 'The Beatles', album: 'Abbey Road', year: '1969' },
+      { artist: 'Melanie', album: 'Candles in the Rain', year: '1971' },
+      { artist: 'Bob Dylan', album: 'Bringing It All Back Home', year: '1965' },
+      { artist: 'James Taylor', album: 'Sweet Baby James', year: '1970' },
+      {
+        artist: 'Simon and Garfunkel',
+        album: 'Bridge Over Troubled Waters',
+        year: '1970',
+      },
+    ];
+  }
+
+  // get records() {
+  //   return this.model();
+  // }
+
+  // get sortedRecords() {
+  //   const sorted = this.records.slice().sortBy(this.sortby || 'year');
+  //   return this.sortdescending ? sorted.reverse() : sorted;
+  // }
+}
