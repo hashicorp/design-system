797bbddb5f14d11770c6d19dcd8581f2dbb78aa1
Cristiano Rastelli <public@didoo.net>
2022-09-01 14:20:14 +0100

    initial setup of `storybook/ember`

---
+++ packages/components/.storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  staticDirs: [
+    '../dist',
+  ],
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../addon/components/hds/**/*.stories.mdx",
+    "../addon/components/hds/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  features: {
+    postcss: false,
+  },
+  framework: "@storybook/ember",
+  core: {
+    builder: "@storybook/builder-webpack4",
+  },
+};
