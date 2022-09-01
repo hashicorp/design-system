a224ec79dbde4be952bc3b9dc0af866974ff813d
Cristiano Rastelli <public@didoo.net>
2022-09-01 13:55:41 +0100

    add stylelint

---
+++ packages/components/.stylelintrc.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-prettier/recommended',
+    'stylelint-config-prettier-scss',
+  ],
+  rules: {
+  },
+};
