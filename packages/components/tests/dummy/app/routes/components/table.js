abca3044831b2e9096b4b0156054793579dc526d
Melanie Sumner <melanie@hashicorp.com>
2022-09-07 12:07:41 -0500

    fixing linter

--- packages/components/tests/dummy/app/routes/components/table.js
+++ packages/components/tests/dummy/app/routes/components/table.js
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default class ComponentsTableRoute extends Route {
-
   model() {
     return [
       { artist: 'Nick Drake', album: 'Pink Moon', year: '1972' },
