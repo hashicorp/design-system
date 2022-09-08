33b4a07a20012fe3082b3449b9be15d03c08a328
Jory Tindall <jory.tindall@hashicorp.com>
2022-09-08 11:13:22 -0700

    lint

--- packages/components/tests/dummy/app/routes/components/badge-count.js
+++ packages/components/tests/dummy/app/routes/components/badge-count.js
@@ -1,17 +1,17 @@
 import Route from '@ember/routing/route';
 
 import {
-    SIZES as BADGE_COUNT_SIZES,
-    TYPES as BADGE_COUNT_TYPES,
-    COLORS as BADGE_COUNT_COLORS,
+  SIZES as BADGE_COUNT_SIZES,
+  TYPES as BADGE_COUNT_TYPES,
+  COLORS as BADGE_COUNT_COLORS,
 } from '@hashicorp/design-system-components/components/hds/badge-count';
 
 export default class ComponentsBadgeCountRoute extends Route {
-    model() {
-        return {
-            BADGE_COUNT_SIZES,
-            BADGE_COUNT_TYPES,
-            BADGE_COUNT_COLORS,
-        }
-    }
+  model() {
+    return {
+      BADGE_COUNT_SIZES,
+      BADGE_COUNT_TYPES,
+      BADGE_COUNT_COLORS,
+    };
+  }
 }
