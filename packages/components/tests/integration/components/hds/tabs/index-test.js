a5369fae0172cd44b83cdd5bcc98e9e64bae7f50
Kristin Bradley <kristin.bradley@hashicorp.com>
2022-09-26 16:17:23 -0700

    HDS-605 Update wording in docs, plan out initial tests

--- packages/components/tests/integration/components/hds/tabs/index-test.js
+++ packages/components/tests/integration/components/hds/tabs/index-test.js
@@ -7,11 +7,36 @@ module('Integration | Component | hds/tabs/index', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders the component', async function (assert) {
-    await render(hbs`<Hds::Tabs />`);
+    await render(hbs`
+      <Hds::Tabs as |T|>
+        <T.Tab>One</T.Tab>
+        <T.Tab>Two</T.Tab>
+        <T.Panel>Content 1</T.Panel>
+        <T.Panel>Content 2</T.Panel>
+      </Hds::Tabs>
+    `);
     assert.dom(this.element).exists();
   });
+
   test('it should render with a CSS class that matches the component name', async function (assert) {
-    await render(hbs`<Hds::Tabs id="test-tabs" />`);
+    await render(hbs`
+      <Hds::Tabs id="test-tabs" as |T|>
+        <T.Tab>One</T.Tab>
+        <T.Tab>Two</T.Tab>
+        <T.Panel>Content 1</T.Panel>
+        <T.Panel>Content 2</T.Panel>
+      </Hds::Tabs>
+    `);
     assert.dom('#test-tabs').hasClass('hds-tabs');
   });
+
+  // TODO: Must be 2 or more Tabs & Panels
+
+  // TODO: Must have same number of Tabs & Panels
+
+  // TODO: First tab should be selected & first panel contents should display by default
+
+  // TODO: If isSelected is used on a tab, it should be selected and its panel displayed by default
+
+  // TODO: Only one Tab max should have isSelected set
 });
