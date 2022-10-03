433467547c87cf560b514df3aaff9aae94b09b08
Cristiano Rastelli <public@didoo.net>
2022-10-03 15:48:37 +0100

    installed `empress/field-guide` in the `website` application
    command: `npx ember install field-guide field-guide-default-template`

--- website/config/environment.js
+++ website/config/environment.js
@@ -5,7 +5,8 @@ module.exports = function (environment) {
     modulePrefix: 'website',
     environment,
     rootURL: '/',
-    locationType: 'history',
+    locationType: 'trailing-history',
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -21,6 +22,19 @@ module.exports = function (environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
+
+    'field-guide': {
+      name: 'Product Name',
+      copyright: 'This is the default copyright string - update before publishing',
+
+      social: [{
+        name: 'github',
+        title: 'Design System Documentation - Repository',
+        link: 'https://github.com/empress/field-guide?update-with-your-repo-url'
+      }]
+    },
+
+    historySupportMiddleware: true
   };
 
   if (environment === 'development') {
---
+++ website/docs/index.md
@@ -0,0 +1,7 @@
+# Welcome
+
+Welcome to Field Guide 🎉
+
+You have now successfully setup your own Field Guide, if things are looking a bit bare bones right now that's because Field Guide is designed to provide as few styles as possible so it doesn't interfere with your own styleguide or design system.
+
+If you want to see an example of a full Field Guide in the wild you can [check out the Ember Styleguide](https://ember-styleguide.netlify.com/).
