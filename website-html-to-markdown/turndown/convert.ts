fdffdd981b54bde4f2f6cd148ab231c83fd9a4a8
Cristiano Rastelli <public@didoo.net>
2022-10-04 16:53:24 +0200

    added actual conversion code to `Turndown` convert script

--- website-html-to-markdown/turndown/convert.ts
+++ website-html-to-markdown/turndown/convert.ts
@@ -1,4 +1,4 @@
-import { glob} from 'glob';
+import { glob } from 'glob';
 import fs from 'fs-extra';
 import path from 'path';
 // import del from 'del';
@@ -6,61 +6,102 @@ import chalk from 'chalk';
 
 import TurndownService from 'turndown';
 
-const sourceFolder = path.resolve(__dirname, '../../packages/components/tests/dummy/app/templates/');
+const sourceFolder = path.resolve(
+  __dirname,
+  '../../packages/components/tests/dummy/app/templates/'
+);
 const tempFolder = path.resolve(__dirname, './temp');
 
 (async () => {
-    try {
-        console.log(`\n==========\n${chalk.cyan('Turndown conversion started...')}\n==========\n`);
+  try {
+    console.log(
+      `\n==========\n${chalk.cyan(
+        'Turndown conversion started...'
+      )}\n==========\n`
+    );
 
-        await convert();
+    await convert();
 
-        console.log(`\n==========\n${chalk.green('Turndown conversion completed.')}\n==========\n`);
-
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-
-    }
+    console.log(
+      `\n==========\n${chalk.green(
+        'Turndown conversion completed.'
+      )}\n==========\n`
+    );
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 })();
 
 async function convert() {
-
-    // prepare the temporary folder
-    try {
-        console.log('Preparing the "temp" folder');
-        if (fs.existsSync(tempFolder)) {
-            fs.emptyDirSync(tempFolder);
-        } else {
-            fs.mkdirsSync(tempFolder);
-        }
-    } catch (err) {
-        console.error(err);
+  // prepare the temporary folder
+  try {
+    console.log('Preparing the "temp" folder');
+    if (fs.existsSync(tempFolder)) {
+      fs.emptyDirSync(tempFolder);
+    } else {
+      fs.mkdirsSync(tempFolder);
     }
+  } catch (err) {
+    console.error(err);
+  }
 
-    // process the HBS files
-    glob(sourceFolder + "/**/*.hbs", {}, async function (_error, files) {
-        // console.log(files);
-        for(const filePath of files) {
+  // process the HBS files
+  glob(sourceFolder + '/**/*.hbs', {}, async function (_error, files) {
+    // console.log(files);
+    for (const filePath of files) {
+      // get the relative path of the file, in relation to the "source" folder
+      const fileRelativePath = path.relative(sourceFolder, filePath);
 
-            // get the relative path of the file, in relation to the "source" folder
-            const fileRelativePath = path.relative(sourceFolder, filePath);
-            console.log(`Processing HBS file ${fileRelativePath}`);
+      // we need to skip some files...
+      // if (
+      //   [
+      //     'index.hbs',
+      //     'application.hbs',
+      //     'components.hbs',
+      //     'content.hbs',
+      //     'foundations.hbs',
+      //     'overrides.hbs',
+      //     'utilities.hbs',
+      //   ].includes(fileRelativePath)
+      // ) {
+      //   continue;
+      // }
 
-            // we need to skip some files...
-            if (['index.hbs', 'application.hbs', 'components.hbs', 'content.hbs', 'foundations.hbs', 'overrides.hbs', 'utilities.hbs'].includes(fileRelativePath)) {
-                continue;
-            }
+      // let's use a single file for testing
+      if (fileRelativePath !== 'components/alert.hbs') {
+        continue;
+      }
 
-            // we read the handlebars source (made of HTML + Handlebars code) to process it
-            const hbsSource = await fs.readFile(filePath, 'utf8');
+      console.log(`Processing HBS file ${fileRelativePath}`);
 
-            // convert the file using Turndown
-            const turndownService = new TurndownService()
-            const markdownContent = turndownService.turndown(hbsSource)
+      // we read the handlebars source (made of HTML + Handlebars code) to process it
+      const hbsSource = await fs.readFile(filePath, 'utf8');
 
-            // we use 'outputFile' instead of 'writeFile' to automatically create the sub-folders
-            await fs.outputFile(`${tempFolder}/${fileRelativePath.replace('.hbs','.md')}`, markdownContent);
-        }
-    });
+      // convert the file using Turndown
+      const turndownService = new TurndownService({
+        headingStyle: 'atx',
+        codeBlockStyle: 'fenced',
+      });
+      turndownService.addRule('keep', {
+        filter: function (node: HTMLElement) {
+          return (
+            node.nodeName === 'SECTION' &&
+            node.hasAttribute('data-test-percy')
+          );
+        },
+        replacement: function (content, node) {
+          // notice this returns the HTML/HBS quite mangled up :(
+          return node.outerHTML;
+        },
+      });
+      const markdownContent = turndownService.turndown(hbsSource);
+
+      // we use 'outputFile' instead of 'writeFile' to automatically create the sub-folders
+      await fs.outputFile(
+        `${tempFolder}/${fileRelativePath.replace('.hbs', '.md')}`,
+        markdownContent
+      );
+    }
+  });
 }
