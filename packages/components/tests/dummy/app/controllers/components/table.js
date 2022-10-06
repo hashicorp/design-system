ac3e60f485e140485f4637c921750024ed27bc97
Melanie Sumner <melanie@hashicorp.com>
2022-10-06 14:09:04 -0500

    added table-sortable controller to dummy app

---
+++ packages/components/tests/dummy/app/controllers/components/table.js
@@ -0,0 +1,21 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class ComponentsTableController extends Controller {
+  @tracked sortBy = '';
+  @tracked sortOrder = 'asc';
+  queryParams = ['sortBy', 'sortOrder'];
+
+  @action
+  setSortBy(value) {
+    this.sortBy = value;
+    return this.sortBy;
+  }
+
+  @action
+  setSortOrder(value) {
+    this.sortOrder = value;
+    return this.sortOrder;
+  }
+}
