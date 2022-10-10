3e343e213d59c808a3bc6dea4f612ecede1233ab
Melanie Sumner <melanie@hashicorp.com>
2022-10-10 16:28:52 -0500

    pushing latest updates to application controller

--- packages/components/tests/dummy/app/controllers/application.js
+++ packages/components/tests/dummy/app/controllers/application.js
@@ -4,6 +4,7 @@ import { defaultValidator } from 'ember-a11y-refocus';
 export default class ApplicationController extends Controller {
   // https://github.com/ember-a11y/ember-a11y-refocus#customizing-the-definition-of-a-route-change
   transitionValidator(transition) {
+    //this isn't working but it's not breaking either.
     if (transition.to.queryParams) {
       console.log('transition to queryParams...should not reset focus');
       return false;
