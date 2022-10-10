027c76d1e963d4fa0a3dbdf967608aee4f010a84
Melanie Sumner <melanie@hashicorp.com>
2022-10-10 16:19:44 -0500

    removes unused component class

--- packages/components/tests/dummy/app/controllers/application.js
+++ packages/components/tests/dummy/app/controllers/application.js
@@ -5,7 +5,7 @@ export default class ApplicationController extends Controller {
   // https://github.com/ember-a11y/ember-a11y-refocus#customizing-the-definition-of-a-route-change
   transitionValidator(transition) {
     if (transition.to.queryParams) {
-      console.log('sortBy transition to, should not reset focus');
+      console.log('transition to queryParams...should not reset focus');
       return false;
     } else {
       console.log(`second one: ${transition.to.queryParams}`);
