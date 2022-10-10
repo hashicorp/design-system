5d2e58df7314117414cf86816647336558400966
Melanie Sumner <melanie@hashicorp.com>
2022-10-10 16:14:59 -0500

    made progress on the transition definition customization

---
+++ packages/components/tests/dummy/app/controllers/application.js
@@ -0,0 +1,15 @@
+import Controller from '@ember/controller';
+import { defaultValidator } from 'ember-a11y-refocus';
+
+export default class ApplicationController extends Controller {
+  // https://github.com/ember-a11y/ember-a11y-refocus#customizing-the-definition-of-a-route-change
+  transitionValidator(transition) {
+    if (transition.to.queryParams) {
+      console.log('sortBy transition to, should not reset focus');
+      return false;
+    } else {
+      console.log(`second one: ${transition.to.queryParams}`);
+      return defaultValidator(transition);
+    }
+  }
+}
