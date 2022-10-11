7ae39d70ebb08bf0e744e8d179d27d92c4642400
Melanie Sumner <melanie@hashicorp.com>
2022-10-11 08:51:26 -0500

    resolved issue where sorting was causing focus to be reset

--- packages/components/tests/dummy/app/controllers/application.js
+++ packages/components/tests/dummy/app/controllers/application.js
@@ -4,12 +4,9 @@ import { defaultValidator } from 'ember-a11y-refocus';
 export default class ApplicationController extends Controller {
   // https://github.com/ember-a11y/ember-a11y-refocus#customizing-the-definition-of-a-route-change
   transitionValidator(transition) {
-    //this isn't working but it's not breaking either.
-    if (transition.to.queryParams) {
-      console.log('transition to queryParams...should not reset focus');
+    if (transition.to.queryParams.sortBy) {
       return false;
     } else {
-      console.log(`second one: ${transition.to.queryParams}`);
       return defaultValidator(transition);
     }
   }
