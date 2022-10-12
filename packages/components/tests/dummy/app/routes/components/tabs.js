96ad8f061938cbe68ee6bfcf80d6cfe89dd0ed33
Kristin Bradley <kristin.bradley@hashicorp.com>
2022-10-12 15:51:41 -0700

    Update packages/components/tests/dummy/app/routes/components/tabs.js
    Co-authored-by: Cristiano Rastelli <cristiano.rastelli@hashicorp.com>

--- packages/components/tests/dummy/app/routes/components/tabs.js
+++ packages/components/tests/dummy/app/routes/components/tabs.js
@@ -1,9 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class ComponentsTabsRoute extends Route {
-  model() {
-    // these are used only for presentation purpose in the showcase
-    const TAG_STATES = ['default', 'hover', 'focus'];
-    return { TAG_STATES };
-  }
-}
+export default class ComponentsTabsRoute extends Route {}
