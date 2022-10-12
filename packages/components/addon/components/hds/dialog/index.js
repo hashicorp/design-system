2b414adc80f6b719467d535dd0bbee1c80f81439
Alex <alex-ju@users.noreply.github.com>
2022-10-12 22:16:14 +0100

    Add `@type` argument to `Hds::Dialog`

--- packages/components/addon/components/hds/dialog/index.js
+++ packages/components/addon/components/hds/dialog/index.js
@@ -2,14 +2,16 @@ import Component from '@glimmer/component';
 import { assert } from '@ember/debug';
 
 export const DEFAULT_SIZE = 'regular';
+export const DEFAULT_TYPE = 'neutral';
 export const SIZES = ['small', 'regular', 'large'];
+export const TYPES = ['neutral', 'warning', 'critical'];
 
 export default class HdsDialogIndexComponent extends Component {
   /**
    * Sets the size of the dialog
    * Accepted values: small, regular, large
    *
-   * @param type
+   * @param size
    * @type {string}
    * @default 'regular'
    */
@@ -27,6 +29,27 @@ export default class HdsDialogIndexComponent extends Component {
   }
 
   /**
+   * Sets the type of the dialog
+   * Accepted values: neutral, warning, critical
+   *
+   * @param type
+   * @type {string}
+   * @default 'neutral'
+   */
+  get type() {
+    let { type = DEFAULT_TYPE } = this.args;
+
+    assert(
+      `@type for "Hds::Dialog" must be one of the following: ${TYPES.join(
+        ', '
+      )}; received: ${type}`,
+      TYPES.includes(type)
+    );
+
+    return type;
+  }
+
+  /**
    * Get the class names to apply to the component.
    * @method classNames
    * @return {string} The "class" attribute to apply to the component.
@@ -36,6 +59,9 @@ export default class HdsDialogIndexComponent extends Component {
 
     // add a class based on the @size argument
     classes.push(`hds-dialog--size-${this.size}`);
+
+    // add a class based on the @type argument
+    classes.push(`hds-dialog--type-${this.type}`);
 
     return classes.join(' ');
   }
