ee8ee012b1431610b54c351b698e47e110ac6acc
Alex <alex-ju@users.noreply.github.com>
2022-10-12 21:56:08 +0100

    Add `@size` argument to `Hds::Dialog`

--- packages/components/addon/components/hds/dialog/index.js
+++ packages/components/addon/components/hds/dialog/index.js
@@ -1,6 +1,31 @@
 import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+
+export const DEFAULT_SIZE = 'regular';
+export const SIZES = ['small', 'regular', 'large'];
 
 export default class HdsDialogIndexComponent extends Component {
+  /**
+   * Sets the size of the dialog
+   * Accepted values: small, regular, large
+   *
+   * @param type
+   * @type {string}
+   * @default 'regular'
+   */
+  get size() {
+    let { size = DEFAULT_SIZE } = this.args;
+
+    assert(
+      `@size for "Hds::Dialog" must be one of the following: ${SIZES.join(
+        ', '
+      )}; received: ${size}`,
+      SIZES.includes(size)
+    );
+
+    return size;
+  }
+
   /**
    * Get the class names to apply to the component.
    * @method classNames
@@ -9,8 +34,8 @@ export default class HdsDialogIndexComponent extends Component {
   get classNames() {
     let classes = ['hds-dialog'];
 
-    // add a class based on the @xxx argument
-    // classes.push(`hds-dialog--[variant]-${this.xxx}`);
+    // add a class based on the @size argument
+    classes.push(`hds-dialog--size-${this.size}`);
 
     return classes.join(' ');
   }
