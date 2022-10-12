54e78a86997631f99aca7aea4f99057b76336f5f
Melanie Sumner <melanie@hashicorp.com>
2022-10-12 11:06:32 -0500

    fixed failing tests

--- packages/components/tests/integration/components/hds/table/th-sort-test.js
+++ packages/components/tests/integration/components/hds/table/th-sort-test.js
@@ -9,9 +9,17 @@ module('Integration | Component | hds/table/th-sort', function (hooks) {
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('setSortBy', function (text) {
+      return text;
+    });
 
-    await render(hbs`<Hds::Table::ThSort />`);
+    await render(hbs`<Hds::Table::ThSort
+    @text="artist"
+    @sortBy="artist"
+    @sortOrder="asc"
+    @setSortBy={{this.setSortBy}}
+  />`);
 
-    assert.dom(this.element).hasText('');
+    assert.dom(this.element).hasText('artist');
   });
 });
