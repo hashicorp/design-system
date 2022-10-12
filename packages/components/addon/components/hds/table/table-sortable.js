b8b9c7f89e148a6bfda5b66e0d7b258da7cc7733
Melanie Sumner <melanie@hashicorp.com>
2022-10-12 10:31:41 -0500

    updated table code to remove need for app controller

--- packages/components/addon/components/hds/table/table-sortable.js
+++ packages/components/addon/components/hds/table/table-sortable.js
@@ -1,26 +1,33 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
+import { tracked } from '@glimmer/tracking';
 
 export default class HdsTableTableSortableComponent extends Component {
   a11yMessageId = 'table-a11y-message-' + guidFor(this);
 
-  get sort() {
-    return `${this.args.sortBy}:${this.args.sortOrder}`;
+  @tracked sortBy = this.args.sortBy;
+  @tracked sortOrder = this.args.sortOrder || 'asc';
+
+  get getSortCriteria() {
+    return `${this.sortBy}:${this.sortOrder}`;
   }
 
   @action
   setSortBy(column) {
-    if (this.args.sortBy === column) {
+    if (this.sortBy === column) {
       //invert the sort order
-      this.args.setSortOrder(this.args.sortOrder === 'asc' ? 'desc' : 'asc');
+      this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
     } else {
-      this.args.setSortBy(column);
-      this.args.setSortOrder('asc');
+      this.sortBy = column;
+      this.sortOrder = 'asc';
     }
 
+    const currentColHeading = event.target;
+    const currColHeadingText = currentColHeading.textContent;
+
     const liveRegion = document.querySelector(`#${this.a11yMessageId}`);
-    liveRegion.textContent = `Sorted by ${column} ${this.args.sortOrder}ending`;
+    liveRegion.textContent = `Sorted by ${currColHeadingText} ${this.sortOrder}ending`;
     setTimeout(function () {
       liveRegion.textContent = '';
     }, 2000);
