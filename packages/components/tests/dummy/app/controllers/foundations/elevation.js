6220e40f51f914041af39426567bc66b28975166
Cristiano Rastelli <public@didoo.net>
2022-10-13 18:42:30 +0100

    introduced a component to list `vars` strings and copy them + replaced with it the `CodeBlock` elements in “Typography” and “Elevation” documentation pages

--- packages/components/tests/dummy/app/controllers/foundations/elevation.js
+++ packages/components/tests/dummy/app/controllers/foundations/elevation.js
@@ -4,25 +4,23 @@ import { ELEVATIONS, SURFACES } from '../../routes/foundations/elevation';
 
 export default class ElevationController extends Controller {
   get cssVariables() {
-    const cssVariables = [];
+    const cssVariables = { elevations: [], surfaces: [] };
     ELEVATIONS.forEach((elevation) => {
-      cssVariables.push(`--hds-elevation-${elevation}-box-shadow`);
+      cssVariables.elevations.push(`--hds-elevation-${elevation}-box-shadow`);
     });
-    cssVariables.push('');
     SURFACES.forEach((surface) => {
-      cssVariables.push(`--hds-surface-${surface}-box-shadow`);
+      cssVariables.surfaces.push(`--hds-surface-${surface}-box-shadow`);
     });
-    return `${cssVariables.join('\n')}`;
+    return cssVariables;
   }
   get cssHelpers() {
-    const cssHelpers = [];
+    const cssHelpers = { elevations: [], surfaces: [] };
     ELEVATIONS.forEach((elevation) => {
-      cssHelpers.push(`.hds-elevation-${elevation}`);
+      cssHelpers.elevations.push(`.hds-elevation-${elevation}`);
     });
-    cssHelpers.push('');
     SURFACES.forEach((surface) => {
-      cssHelpers.push(`.hds-surface-${surface}`);
+      cssHelpers.surfaces.push(`.hds-surface-${surface}`);
     });
-    return `${cssHelpers.join('\n')}`;
+    return cssHelpers;
   }
 }
