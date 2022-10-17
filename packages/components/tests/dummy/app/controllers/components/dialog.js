ad681d31a46276bff02e8858f48e0e629b9df9af
Alex <alex-ju@users.noreply.github.com>
2022-10-17 11:57:35 +0100

    Add 'Showcase' section (interim)

--- packages/components/tests/dummy/app/controllers/components/dialog.js
+++ packages/components/tests/dummy/app/controllers/components/dialog.js
@@ -1,8 +1,23 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class DialogController extends Controller {
-  // notice: this is used as "noop" function for the onClose callback of the Dialog component
+  @tracked basicDialogActive = false;
+  @tracked formDialogActive = false;
+
+  @action
+  activateModal(modal) {
+    this[modal] = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  @action
+  deactivateModal(modal) {
+    this[modal] = false;
+    document.body.style.overflow = 'auto';
+  }
+
   @action
   noop() {
     console.log('noop');
--- packages/components/tests/dummy/app/routes/components/dialog.js
+++ packages/components/tests/dummy/app/routes/components/dialog.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
 
-export default class ComponentsDialogRoute extends Route {}
+import {
+  COLORS,
+  SIZES,
+} from '@hashicorp/design-system-components/components/hds/dialog';
+
+export default class ComponentsDialogRoute extends Route {
+  model() {
+    return {
+      COLORS,
+      SIZES,
+    };
+  }
+}
