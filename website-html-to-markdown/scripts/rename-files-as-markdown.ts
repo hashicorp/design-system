74be782084a041a8fcdd6163da88130462a35864
Cristiano Rastelli <public@didoo.net>
2022-10-18 09:03:35 +0100

    [RENAME FILES] refactored script to rename HNS to Markdown files in place

---
+++ website-html-to-markdown/scripts/rename-files-as-markdown.ts
@@ -0,0 +1,39 @@
+import { glob } from 'glob';
+import fs from 'fs-extra';
+import path from 'path';
+import chalk from 'chalk';
+
+const sourceFolder = path.resolve(__dirname, '../source/split-files');
+
+(async () => {
+  try {
+    console.log(
+      `\n==========\n${chalk.cyan(
+        'Renaming ".hbs" source files to ".md"...'
+      )}\n`
+    );
+
+    await renameFiles();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
+
+async function renameFiles() {
+  // process the HBS files
+  glob(sourceFolder + '/**/*.hbs', {}, async function (_error, files) {
+    // loop on every HBS file found in the source folder
+    for (const filePath of files) {
+      // DEBUG
+      // console.log(
+      //   `Renaming file "${filePath}" to "${filePath.replace(
+      //     /.hbs$/,
+      //     '.md'
+      //   )}"`
+      // );
+
+      await fs.rename(filePath, filePath.replace(/.hbs$/, '.md'));
+    }
+  });
+}
