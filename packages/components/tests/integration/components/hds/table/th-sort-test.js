49d74eb3ab197ad900263b2c1181c1ad37ac82ad
Melanie Sumner <melanie@hashicorp.com>
2022-10-21 13:07:51 -0500

    updated the th-sort component and test

--- packages/components/tests/integration/components/hds/table/th-sort-test.js
+++ packages/components/tests/integration/components/hds/table/th-sort-test.js
@@ -5,13 +5,18 @@ import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | hds/table/th-sort', function (hooks) {
   setupRenderingTest(hooks);
+  hooks.beforeEach(function () {
+    this.set('sortBy', 'artist');
+    this.set('sortOrder', 'asc');
+    this.set('sortedMessageText', 'Sorted by artist in ascending order');
+    this.set('setSortBy', function (sortBy) {
+      return sortBy || 'artist';
+    });
+  });
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
-    this.set('setSortBy', function (text) {
-      return text;
-    });
 
     await render(hbs`<Hds::Table::ThSort
   @sortBy='artist'
@@ -20,5 +25,15 @@ module('Integration | Component | hds/table/th-sort', function (hooks) {
 >artist</Hds::Table::ThSort>`);
 
     assert.dom(this.element).hasText('artist');
+  });
+
+  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::Table::ThSort
+  @sortBy='artist'
+  @sortOrder='asc'
+  @setSortBy={{this.setSortBy}}
+>artist</Hds::Table::ThSort>`);
+
+    assert.dom('[data-test-table-th-sort]').hasClass('hds-table__th-sort');
   });
 });
