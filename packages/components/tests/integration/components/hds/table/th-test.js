70a4c999df11e06f0b0cbcc4565e4a064dc1801a
Melanie Sumner <melanie@hashicorp.com>
2022-10-24 12:54:38 -0500

    adding regression test for scope

--- packages/components/tests/integration/components/hds/table/th-test.js
+++ packages/components/tests/integration/components/hds/table/th-test.js
@@ -14,6 +14,13 @@ module('Integration | Component | hds/table/th', function (hooks) {
     assert.dom(this.element).hasText('template block text');
   });
 
+  test('it has the scope attribute, and it is set to column', async function (assert) {
+    await render(
+      hbs`<Hds::Table::Th id="data-test-table-th">artist</Hds::Table::Th>`
+    );
+
+    assert.dom('#data-test-table-th').hasAttribute('scope', 'col');
+  });
   test('it should render with a CSS class that matches the component name', async function (assert) {
     await render(hbs`<Hds::Table::Th id="test-th" />`);
     assert.dom('#test-th').hasClass('hds-table__th');
