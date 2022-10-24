b525bb5d5561af5736c2daefbbf81b502c5752da
Melanie Sumner <melanie@hashicorp.com>
2022-10-24 14:11:38 -0500

    added splattribute support tests

--- packages/components/tests/integration/components/hds/table/th-test.js
+++ packages/components/tests/integration/components/hds/table/th-test.js
@@ -7,22 +7,25 @@ module('Integration | Component | hds/table/th', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Template block usage:
     await render(hbs`
       <Hds::Table::Th>template block text</Hds::Table::Th>`);
-
     assert.dom(this.element).hasText('template block text');
   });
 
   test('it has the scope attribute, and it is set to column', async function (assert) {
     await render(
-      hbs`<Hds::Table::Th id="data-test-table-th">artist</Hds::Table::Th>`
+      hbs`<Hds::Table::Th id="test-table-th">artist</Hds::Table::Th>`
     );
-
-    assert.dom('#data-test-table-th').hasAttribute('scope', 'col');
+    assert.dom('#test-table-th').hasAttribute('scope', 'col');
   });
+
   test('it should render with a CSS class that matches the component name', async function (assert) {
-    await render(hbs`<Hds::Table::Th id="test-th" />`);
-    assert.dom('#test-th').hasClass('hds-table__th');
+    await render(hbs`<Hds::Table::Th id="test-table-th" />`);
+    assert.dom('#test-table-th').hasClass('hds-table__th');
+  });
+
+  test('it should support splattributes', async function (assert) {
+    await render(hbs`<Hds::Table::Th id="test-table-th" />`);
+    assert.dom('[data-test-table-th]').hasAttribute('id', 'test-table-th');
   });
 });
