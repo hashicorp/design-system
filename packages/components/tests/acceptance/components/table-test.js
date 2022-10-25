847bb8b735f2d323ced29433f0067d7afe4a8848
Melanie Sumner <melanie@hashicorp.com>
2022-10-25 11:03:36 -0500

    fix and test aria-sort

--- packages/components/tests/acceptance/components/table-test.js
+++ packages/components/tests/acceptance/components/table-test.js
@@ -10,6 +10,7 @@ module('Acceptance | components/table', function (hooks) {
 
     assert.strictEqual(currentURL(), '/components/table');
   });
+
   test('the sortable table exists on the page', async function (assert) {
     await visit('/components/table');
     assert.dom('#showcase-table-sortable-all').exists();
@@ -28,5 +29,52 @@ module('Acceptance | components/table', function (hooks) {
     assert
       .dom('#showcase-table-sortable-all td:nth-of-type(1)')
       .hasText('Bob Dylan');
+  });
+
+  test('it renders the empty caption by default', async function (assert) {
+    await visit('/components/table');
+
+    assert
+      .dom('#showcase-table-sortable-all td:nth-of-type(1)')
+      .hasText('Nick Drake');
+
+    assert.dom('#showcase-table-sortable-all caption').hasText('');
+  });
+
+  test('it updates the caption correctly after a sort has been performed', async function (assert) {
+    await visit('/components/table');
+
+    assert
+      .dom('#showcase-table-sortable-all td:nth-of-type(1)')
+      .hasText('Nick Drake');
+
+    assert.dom('#showcase-table-sortable-all caption').hasText('');
+
+    await click(
+      '#showcase-table-sortable-all .hds-table__th-sort:nth-of-type(1) button'
+    );
+    assert
+      .dom('#showcase-table-sortable-all td:nth-of-type(1)')
+      .hasText('Bob Dylan');
+    assert
+      .dom('#showcase-table-sortable-all caption')
+      .hasText('Sorted by artist ascending');
+  });
+
+  test('it updates the `aria-sort` attribute value when a sort is performed', async function (assert) {
+    await visit('/components/table');
+
+    await click(
+      '#showcase-table-sortable-all .hds-table__th-sort:nth-of-type(1) button'
+    );
+    assert
+      .dom('#showcase-table-sortable-all .hds-table__th-sort:nth-of-type(1)')
+      .hasAttribute('aria-sort', 'ascending');
+    await click(
+      '#showcase-table-sortable-all .hds-table__th-sort:nth-of-type(1) button'
+    );
+    assert
+      .dom('#showcase-table-sortable-all .hds-table__th-sort:nth-of-type(1)')
+      .hasAttribute('aria-sort', 'descending');
   });
 });
