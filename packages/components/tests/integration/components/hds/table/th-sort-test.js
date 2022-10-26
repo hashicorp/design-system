39141c87ec5ea266471c808e11ef41ee14f7915c
Melanie Sumner <melanie@hashicorp.com>
2022-10-26 14:35:38 -0500

    formatting update

--- packages/components/tests/integration/components/hds/table/th-sort-test.js
+++ packages/components/tests/integration/components/hds/table/th-sort-test.js
@@ -8,49 +8,45 @@ module('Integration | Component | hds/table/th-sort', function (hooks) {
   // since we'd have to repeat this for each test, we'll set it up once to be used in all tests
   hooks.beforeEach(function () {
     this.set('setSortBy', function (sortBy) {
-      return sortBy || 'artist';
+      return sortBy?.noop();
     });
   });
 
   test('it renders', async function (assert) {
-    await render(hbs`<Hds::Table::ThSort
-  @setSortBy={{this.setSortBy}}
->Artist</Hds::Table::ThSort>`);
+    await render(
+      hbs`<Hds::Table::ThSort @setSortBy={{this.setSortBy}}>Artist</Hds::Table::ThSort>`
+    );
 
     assert.dom(this.element).hasText('Artist');
   });
   test('it has the scope attribute, and it is set to column', async function (assert) {
-    await render(hbs`<Hds::Table::ThSort
-  @setSortBy={{this.setSortBy}}
->Artist</Hds::Table::ThSort>`);
+    await render(
+      hbs`<Hds::Table::ThSort @setSortBy={{this.setSortBy}}>Artist</Hds::Table::ThSort>`
+    );
 
     assert.dom('[data-test-table-th-sort]').hasAttribute('scope', 'col');
   });
 
   test('it should render the component with a CSS class that matches the component name', async function (assert) {
-    await render(hbs`<Hds::Table::ThSort
-  @setSortBy={{this.setSortBy}}
->Artist</Hds::Table::ThSort>`);
+    await render(
+      hbs`<Hds::Table::ThSort @setSortBy={{this.setSortBy}}>Artist</Hds::Table::ThSort>`
+    );
 
     assert.dom('[data-test-table-th-sort]').hasClass('hds-table__th-sort');
   });
 
   test('if @sortOrder is not defined, the swap-vertical icon should be displayed', async function (assert) {
-    await render(hbs`<Hds::Table::ThSort
-  @sortBy='artist'
-  @setSortBy={{this.setSortBy}}
->artist</Hds::Table::ThSort>`);
+    await render(
+      hbs`<Hds::Table::ThSort @sortBy='artist' @setSortBy={{this.setSortBy}}>Artist</Hds::Table::ThSort>`
+    );
 
     assert.dom('[data-test-icon="swap-vertical"]').exists();
   });
 
   test('if `@sortOrder` is set to asc, the arrow-up icon should be displayed', async function (assert) {
-    await render(hbs`<Hds::Table::ThSort
-  @sortBy='artist'
-  @sortKey='artist'
-  @sortOrder='asc'
-  @setSortBy={{this.setSortBy}}
->Artist</Hds::Table::ThSort>`);
+    await render(
+      hbs`<Hds::Table::ThSort @sortBy='artist' @sortKey='artist' @sortOrder='asc' @setSortBy={{this.setSortBy}}>Artist</Hds::Table::ThSort>`
+    );
 
     assert.dom('[data-test-icon="arrow-up"]').exists();
   });
