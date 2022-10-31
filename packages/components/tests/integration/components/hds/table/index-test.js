9aeaa16f5ec640ab372a3dcd022a41605df737d8
Melanie Sumner <melanie@hashicorp.com>
2022-10-31 16:17:21 -0500

    adds an integration test to check table renders when no data model is defined

--- packages/components/tests/integration/components/hds/table/index-test.js
+++ packages/components/tests/integration/components/hds/table/index-test.js
@@ -20,4 +20,42 @@ module('Integration | Component | hds/table/index', function (hooks) {
     await render(hbs`<Hds::Table id="test-table" />`);
     assert.dom('[data-test-table]').hasAttribute('id', 'test-table');
   });
+
+  test('it should render the table with manual data passed and no model defined', async function (assert) {
+    await render(hbs`
+      <Hds::Table>
+        <:head>
+          <Hds::Table::Tr>
+            <Hds::Table::Th>Cell Header 1</Hds::Table::Th>
+            <Hds::Table::Th>Cell Header 2</Hds::Table::Th>
+            <Hds::Table::Th>Cell Header 3</Hds::Table::Th>
+          </Hds::Table::Tr>
+        </:head>
+        <:body>
+          <Hds::Table::Tr>
+            <td>Cell Content 1 1</td>
+            <td>Cell Content 1 2</td>
+            <td>Cell Content 1 3</td>
+          </Hds::Table::Tr>
+          <Hds::Table::Tr>
+            <td>Cell Content 2 1</td>
+            <td>Cell Content 2 2</td>
+            <td>Cell Content 2 3</td>
+          </Hds::Table::Tr>
+          <Hds::Table::Tr>
+            <td>Cell Content 3 1</td>
+            <td>Cell Content 3 2</td>
+            <td>Cell Content 3 3</td>
+          </Hds::Table::Tr>
+        </:body>
+      </Hds::Table>
+    `);
+
+    assert
+      .dom('[data-test-table] tr th:first-of-type')
+      .hasText('Cell Header 1');
+    assert
+      .dom('[data-test-table] tr td:first-of-type')
+      .hasText('Cell Content 1 1');
+  });
 });
