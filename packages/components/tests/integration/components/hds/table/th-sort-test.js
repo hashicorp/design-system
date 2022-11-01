ab7baf7166f1f2c1099bfb0e713a99c302d3aac2
Melanie Sumner <melanie@hashicorp.com>
2022-11-01 17:23:43 -0500

    correct usage of ids and data-test attributes, tests pass

--- packages/components/tests/integration/components/hds/table/th-sort-test.js
+++ packages/components/tests/integration/components/hds/table/th-sort-test.js
@@ -14,30 +14,30 @@ module('Integration | Component | hds/table/th-sort', function (hooks) {
 
   test('it renders', async function (assert) {
     await render(
-      hbs`<Hds::Table::ThSort @setSortBy={{this.setSortBy}} id="data-test-table-th-sort">Artist</Hds::Table::ThSort>`
+      hbs`<Hds::Table::ThSort @setSortBy={{this.setSortBy}}>Artist</Hds::Table::ThSort>`
     );
 
     assert.dom(this.element).hasText('Artist');
   });
   test('it has the scope attribute, and it is set to column', async function (assert) {
     await render(
-      hbs`<Hds::Table::ThSort @setSortBy={{this.setSortBy}} id="data-test-table-th-sort">Artist</Hds::Table::ThSort>`
+      hbs`<Hds::Table::ThSort @setSortBy={{this.setSortBy}}>Artist</Hds::Table::ThSort>`
     );
 
-    assert.dom('#data-test-table-th-sort').hasAttribute('scope', 'col');
+    assert.dom('[data-test-table-th-sort]').hasAttribute('scope', 'col');
   });
 
   test('it should render the component with a CSS class that matches the component name', async function (assert) {
     await render(
-      hbs`<Hds::Table::ThSort @setSortBy={{this.setSortBy}} id="data-test-table-th-sort">Artist</Hds::Table::ThSort>`
+      hbs`<Hds::Table::ThSort @setSortBy={{this.setSortBy}}>Artist</Hds::Table::ThSort>`
     );
 
-    assert.dom('#data-test-table-th-sort').hasClass('hds-table__th-sort');
+    assert.dom('[data-test-table-th-sort]').hasClass('hds-table__th-sort');
   });
 
   test('if @sortOrder is not defined, the swap-vertical icon should be displayed', async function (assert) {
     await render(
-      hbs`<Hds::Table::ThSort @sortBy='artist' @setSortBy={{this.setSortBy}} id="data-test-table-th-sort">Artist</Hds::Table::ThSort>`
+      hbs`<Hds::Table::ThSort @sortBy='artist' @setSortBy={{this.setSortBy}}>Artist</Hds::Table::ThSort>`
     );
 
     assert.dom('[data-test-icon="swap-vertical"]').exists();
