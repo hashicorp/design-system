2ace190f225c4463f8fa9cf4a792c31f6cda1940
Melanie Sumner <melanie@hashicorp.com>
2022-11-02 12:34:04 -0500

    start refactor by adding API comments

--- packages/components/addon/components/hds/table/th-sort.js
+++ packages/components/addon/components/hds/table/th-sort.js
@@ -1,10 +1,20 @@
 import Component from '@glimmer/component';
 
 export default class HdsTableThSortComponent extends Component {
+  /**
+   * @param isActiveSortColumn
+   * @type {boolean}
+   * @description Determines if the column is the active sort column
+   */
   get isActiveSortColumn() {
     return this.args.sortKey === this.args.sortBy;
   }
 
+  /**
+   * @param ariaSort
+   * @type {string}
+   * @description Sets the aria-sort attribute based on the sort order defined
+   */
   get ariaSort() {
     if (this.isActiveSortColumn) {
       return this.args.sortOrder === 'asc' ? 'ascending' : 'descending';
@@ -13,6 +23,12 @@ export default class HdsTableThSortComponent extends Component {
     }
   }
 
+  /**
+   * @param icon
+   * @type {string}
+   * @default swap-vertical
+   * @description Determines which icon to use based on the sort order defined
+   */
   get icon() {
     if (this.isActiveSortColumn) {
       return this.args.sortOrder === 'asc' ? 'arrow-up' : 'arrow-down';
