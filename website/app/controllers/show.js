337ced884e909bec9b72bcae72cbd3818695d550
Cristiano Rastelli <public@didoo.net>
2022-11-03 19:36:36 +0100

    removed converter in page (not needed?)

--- website/app/controllers/show.js
+++ website/app/controllers/show.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
-import showdown from 'showdown';
+// import showdown from 'showdown';
 
 export default class ShowController extends Controller {
   get renderedContent() {
-    const converter = new showdown.Converter();
-    return converter.makeHtml(this.model.content);
+    console.log(this.model);
+    // const converter = new showdown.Converter();
+    // return converter.makeHtml(this.model.content);
+    // TODO! why a converter here, if the model already returns an HTML???
+    return this.model.html;
   }
 }
