0a4c6a207042d586fc3ae6b8ca1af26b50c022ee
Cristiano Rastelli <public@didoo.net>
2022-11-03 14:17:19 +0100

    moved frontmatter testing files under correct folder

--- website/docs/testing/05-test-frontmatter/file-with-frontmatter.js
+++ website/docs/testing/05-test-frontmatter/file-with-frontmatter.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 
-export default class Test extends Component {
+export default class FileWithFrontmatter extends Component {
   get test() {
     console.log(this, this.attributes);
     return 'this is a test';
