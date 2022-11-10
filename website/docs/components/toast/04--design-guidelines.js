872d0f500621f7fb91f795f1e60543a6110731d9
Jory Tindall <jory.tindall@hashicorp.com>
2022-11-09 16:09:11 -0800

    Appeased prettier

--- website/docs/components/toast/04--design-guidelines.js
+++ website/docs/components/toast/04--design-guidelines.js
@@ -1,7 +1,7 @@
-import Component from '@glimmer/component'
+import Component from '@glimmer/component';
 
 export default class DesignGuidelines extends Component {
-    get noop() {
-        return () => {};
-    }
+  get noop() {
+    return () => {};
+  }
 }
