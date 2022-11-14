bf5167cac95b899c16916ad3e63a2815bf28724b
Melanie Sumner <melanie@hashicorp.com>
2022-11-14 08:19:25 -0600

    updated th-sort per review

--- packages/components/addon/components/hds/table/th-sort.js
+++ packages/components/addon/components/hds/table/th-sort.js
@@ -4,21 +4,13 @@ const NOOP = () => {};
 
 export default class HdsTableThSortComponent extends Component {
   /**
-   * @param isActiveSortColumn
-   * @type {boolean}
-   * @description Determines if the column is the active sort column
-   */
-  get isActiveSortColumn() {
-    return this.args.sortKey === this.args.sortBy;
-  }
-
-  /**
    * @param ariaSort
    * @type {string}
+   * @private
    * @description Sets the aria-sort attribute based on the sort order defined
    */
   get ariaSort() {
-    if (this.isActiveSortColumn) {
+    if (this.args.isSorted) {
       return this.args.sortOrder === 'asc' ? 'ascending' : 'descending';
     } else {
       return undefined;
@@ -28,11 +20,12 @@ export default class HdsTableThSortComponent extends Component {
   /**
    * @param icon
    * @type {string}
+   * @private
    * @default swap-vertical
    * @description Determines which icon to use based on the sort order defined
    */
   get icon() {
-    if (this.isActiveSortColumn) {
+    if (this.args.isSorted && this.args.sortOrder) {
       return this.args.sortOrder === 'asc' ? 'arrow-up' : 'arrow-down';
     } else {
       return 'swap-vertical';
