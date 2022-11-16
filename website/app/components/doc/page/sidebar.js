c5ffa919238f602103cbaa54b275d24a04e0733f
Cristiano Rastelli <public@didoo.net>
2022-11-16 11:26:22 +0000

    updated the sidebar navigation TOC generation to create “bundles” of related sections

--- website/app/components/doc/page/sidebar.js
+++ website/app/components/doc/page/sidebar.js
@@ -1,5 +1,30 @@
 import Component from '@glimmer/component';
 
+// super hacky way to do what we need, but is a stepping stone (things will be done properly in https://hashicorp.atlassian.net/browse/HDS-1059)
+const getTocBundle = (section) => {
+  const ABOUT = ['about', 'overview', 'getting-started', 'updates'];
+  const FOUNDATIONS = ['foundations'];
+  const COMPONENTS = ['components', 'overrides', 'utilities'];
+  const PATTERNS = ['patterns'];
+  // This will be removed later
+  const TESTING = ['testing'];
+
+  if (ABOUT.includes(section)) {
+    return ABOUT;
+  } else if (FOUNDATIONS.includes(section)) {
+    return FOUNDATIONS;
+  } else if (COMPONENTS.includes(section)) {
+    return COMPONENTS;
+  } else if (PATTERNS.includes(section)) {
+    return PATTERNS;
+  } else if (TESTING.includes(section)) {
+    return TESTING;
+  } else {
+    console.log(`ERROR (getTocBundle): section not found for '${section}'`);
+    return [];
+  }
+};
+
 export default class DocPageSidebarComponent extends Component {
   //
   // this is a temporary function used to emulate the functionality
@@ -15,13 +40,32 @@ export default class DocPageSidebarComponent extends Component {
   get navigationTree() {
     const { currentPath, currentRoute } = this.args;
 
+    // eg. currentPath = 'about'
     // console.log('sidebar >>> currentPath', currentPath);
+
+    // eg. currentRoute = {
+    //      name: 'about',
+    //      parent: {
+    //        name: 'application',
+    //        parent: null,
+    //        ...
+    //      },
+    //      localName: 'about',
+    //      attributes: [
+    //        { id: 'components', title: 'components', pages: [Array] },
+    //        { id: 'content', title: 'content', pages: [Array] },
+    //        { id: 'foundations', title: 'foundations', pages: [Array] },
+    //        { id: 'getting-started', title: 'getting-started', pages: [Array] },
+    //        { id: 'overrides', title: 'overrides', pages: [Array] },
+    //        { id: 'overview', title: 'overview', pages: [Array] },
+    //        { id: 'testing', title: 'testing', pages: [Array] },
+    //        { id: 'updates', title: 'updates', pages: [Array] },
+    //        { id: 'utilities', title: 'utilities', pages: [Array] }
+    //      ]
+    //   }
     // console.log('sidebar >>> currentRoute', currentRoute);
-    // console.log('sidebar >>> sortedTocs', this.sortedTocs);
 
     let section;
-    let currentSectionTocs;
-
     if (currentRoute.localName === 'show') {
       // eg. "foundations/tokens/03--how-to-use/"
       section = currentRoute.params.path.split('/')[0];
@@ -30,9 +74,11 @@ export default class DocPageSidebarComponent extends Component {
       section = currentPath;
     }
 
-    currentSectionTocs = this.sortedTocs.filter((item) => item.id === section);
+    const currentPageTocs = this.sortedTocs.filter((item) => {
+      return getTocBundle(item.id).includes(section);
+    });
 
-    return currentSectionTocs.length > 0 ? currentSectionTocs : undefined;
+    return currentPageTocs.length > 0 ? currentPageTocs : undefined;
   }
 
   get sortedTocs() {
