c060f1d0d03d5149f50b82c0e63f038ba0f319b3
Cristiano Rastelli <public@didoo.net>
2022-11-18 17:14:50 +0000

    added rainbow button to the top navigation header to toggle colors visibility

---
+++ website/app/components/doc/page/header.js
@@ -0,0 +1,18 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class DocPageHeaderComponent extends Component {
+  @tracked showLayoutColors = false;
+
+  @action
+  toggleLayoutColors() {
+    if (this.showLayoutColors) {
+      this.showLayoutColors = false;
+      document.body.classList.remove('🌈');
+    } else {
+      this.showLayoutColors = true;
+      document.body.classList.add('🌈');
+    }
+  }
+}
