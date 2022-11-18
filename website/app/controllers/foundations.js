0f6e8cc9f88f096d9565833e3aa04d9a6bedcf05
Cristiano Rastelli <public@didoo.net>
2022-11-18 12:37:18 +0000

    refactored “Foundations” page to use the new `Doc::Cards::Deck` component

--- website/app/controllers/foundations.js
+++ website/app/controllers/foundations.js
@@ -1,30 +1,54 @@
 import Controller from '@ember/controller';
 
 export default class FoundationsController extends Controller {
-  items = [
+  cards = [
     {
-      text: 'Tokens',
-      url: 'tokens/03--how-to-use/',
+      image: 'https://picsum.photos/seed/s{{1}}/232/124',
+      title: 'Tokens',
+      description:
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+      route: 'show',
+      model: 'foundations/tokens/03--how-to-use/',
     },
     {
-      text: 'Colors',
-      url: 'colors/03--how-to-use/',
+      image: 'https://picsum.photos/seed/s{{2}}/232/124',
+      title: 'Colors',
+      description:
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+      route: 'show',
+      model: 'foundations/colors/03--how-to-use/',
     },
     {
-      text: 'Typography',
-      url: 'typography/03--how-to-use/',
+      image: 'https://picsum.photos/seed/s{{3}}/232/124',
+      title: 'Typography',
+      description:
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+      route: 'show',
+      model: 'foundations/typography/03--how-to-use/',
     },
     {
-      text: 'Icons ⛔️',
-      url: 'icons',
+      image: 'https://picsum.photos/seed/s{{4}}/232/124',
+      title: 'Icons ⛔️',
+      description:
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+      route: 'show',
+      model: 'foundations/icons',
     },
     {
-      text: 'Elevation',
-      url: 'elevation/03--how-to-use/',
+      image: 'https://picsum.photos/seed/s{{5}}/232/124',
+      title: 'Elevation',
+      description:
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+      route: 'show',
+      model: 'foundations/elevation/03--how-to-use/',
     },
     {
-      text: 'Focus Ring',
-      url: 'focus-ring/03--how-to-use/',
+      image: 'https://picsum.photos/seed/s{{6}}/232/124',
+      title: 'Focus Ring',
+      description:
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+      route: 'show',
+      model: 'foundations/focus-ring/03--how-to-use/',
     },
   ];
 }
