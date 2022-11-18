be952cdbf010658a7a801cbdce4203c97e454a78
Cristiano Rastelli <public@didoo.net>
2022-11-18 13:32:42 +0000

    further refactoring of the “Foundations” page to generate automatically list using TOC object

--- website/app/controllers/foundations.js
+++ website/app/controllers/foundations.js
@@ -1,54 +1,38 @@
 import Controller from '@ember/controller';
 
 export default class FoundationsController extends Controller {
-  cards = [
-    {
-      image: 'https://picsum.photos/seed/s{{1}}/232/124',
-      title: 'Tokens',
-      description:
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
-      route: 'show',
-      model: 'foundations/tokens/03--how-to-use/',
-    },
-    {
-      image: 'https://picsum.photos/seed/s{{2}}/232/124',
-      title: 'Colors',
-      description:
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
-      route: 'show',
-      model: 'foundations/colors/03--how-to-use/',
-    },
-    {
-      image: 'https://picsum.photos/seed/s{{3}}/232/124',
-      title: 'Typography',
-      description:
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
-      route: 'show',
-      model: 'foundations/typography/03--how-to-use/',
-    },
-    {
-      image: 'https://picsum.photos/seed/s{{4}}/232/124',
-      title: 'Icons ⛔️',
-      description:
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
-      route: 'show',
-      model: 'foundations/icons',
-    },
-    {
-      image: 'https://picsum.photos/seed/s{{5}}/232/124',
-      title: 'Elevation',
-      description:
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
-      route: 'show',
-      model: 'foundations/elevation/03--how-to-use/',
-    },
-    {
-      image: 'https://picsum.photos/seed/s{{6}}/232/124',
-      title: 'Focus Ring',
-      description:
-        'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
-      route: 'show',
-      model: 'foundations/focus-ring/03--how-to-use/',
-    },
-  ];
+  get subTOC() {
+    // NOTICE: the TOC comes direclty from the application "model" (see addons/field-guide/addon/routes/application.js)
+    // TODO! important: instead of spreading the content all over the model, group the TOC under a specific "key"!
+
+    // hacky way to have the subTOC items in the right order
+    const subIDs = ['foundations'];
+    const subTOC = [];
+
+    // TODO! convert this to a recursive function shared with the "Foundations page"
+    this.model
+      .filter((item) => subIDs.includes(item.id))
+      .forEach((item) => {
+        const { id, title, pages } = item;
+        const cards = [];
+        pages.forEach((page) => {
+          if (page.pages && page.pages.length > 0) {
+            cards.push({
+              image: `https://picsum.photos/seed/s${encodeURI(
+                page.title
+              )}/232/124`,
+              title: page.title,
+              description:
+                'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+              route: 'show',
+              model: page.pages[0].id,
+            });
+          } else {
+            console.log('????????????');
+          }
+        });
+        subTOC[id] = { title, cards };
+      });
+    return subTOC;
+  }
 }
