3ea6f7a9daca1294578eb20fc72c66e7659cb5df
Cristiano Rastelli <public@didoo.net>
2022-11-18 13:07:53 +0000

    added comment

--- addons/field-guide/addon/routes/application.js
+++ addons/field-guide/addon/routes/application.js
@@ -6,6 +6,7 @@ import config from 'ember-get-config';
 export default Route.extend({
   model() {
     return fetch(`${config.rootURL}toc.json`)
-      .then(res => res.json())
+    // TODO! important: instead of spreading the content all over the model, group the TOC under a specific "key"!
+    .then(res => res.json())
   }
 })
