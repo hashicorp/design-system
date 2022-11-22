20c05d32ffd13268c986ca32a1358b3a0a57043f
Cristiano Rastelli <public@didoo.net>
2022-11-22 18:44:14 +0000

    node-temp-playground - part #1

---
+++ node-temp-playground/scripts/play.ts
@@ -0,0 +1,160 @@
+// import { glob } from 'glob';
+import fs from 'fs-extra';
+// import path from 'path';
+import _ from 'lodash';
+
+const files: string[] = [
+  'components/alert/01--overview',
+  'components/alert/02--component-api',
+  'components/alert/03--how-to-use',
+  'components/alert/04--design-guidelines',
+  'components/alert/05--accessibility',
+  'components/alert/06--showcase',
+  'components/alert/index',
+  'components/form/checkbox/01--overview',
+  'components/form/checkbox/02--component-api',
+  'components/form/checkbox/03--how-to-use',
+  'components/form/checkbox/04--design-guidelines',
+  'components/form/checkbox/05--accessibility',
+  'components/form/checkbox/06--showcase',
+  'components/form/checkbox/index',
+  'overview/01--about',
+  'overview/02--hds-principles',
+];
+
+// console.log(JSON.stringify({files}, null, 2));
+
+const basicDirectoryTree = getBasicDirectoryTree(files);
+const structuredDirectoryTree = getStructuredDirectoryTree(basicDirectoryTree);
+const populatedDirectoryTree = getPopulatedDirectoryTree(structuredDirectoryTree);
+
+console.log(
+  'populatedDirectoryTree',
+  JSON.stringify(populatedDirectoryTree, null, 2)
+);
+
+// const pages = files
+// .filter(path => path != 'all.json')
+// .map(path => path.replace(/\.json$/, ''))
+// .map(path => path.split('/'));
+
+// console.log('pages', JSON.stringify({pages}, null, 2));
+
+// const toc = clusterPages(pages)
+
+// console.log('toc', JSON.stringify({toc}, null, 2));
+
+// ################################################
+// ################################################
+// ################################################
+
+function getBasicDirectoryTree(files: string[]) {
+  if (!files.length) return;
+
+  const tree: Record<string, unknown> = {};
+
+  files.forEach((file) => {
+    const parts = file.split('/');
+    const filename = parts.pop();
+    if (_.has(tree, parts)) {
+      _.get(tree, parts).push(filename);
+    } else {
+      _.set(tree, parts, [filename]);
+    }
+  });
+
+  return tree;
+}
+
+function getStructuredDirectoryTree(
+  srcTree: Record<string, unknown>,
+  path: string
+) {
+  const tree: Record<string, unknown> = {};
+
+  _.forIn(srcTree, function (subTree, key) {
+    const currPath = path ? `${path}/${key}` : key;
+    if (Array.isArray(subTree)) {
+      if (subTree.includes('index')) {
+        tree[key] = {
+          pages: [
+            {
+              fileName: 'index',
+              parentPath: currPath,
+              siblings: _.pull(subTree, 'index'),
+            },
+          ],
+        };
+      } else {
+        tree[key] = {
+          pages: subTree.map((page) => {
+            return {
+              fileName: page,
+              parentPath: currPath,
+            };
+          }),
+        };
+      }
+    } else {
+      tree[key] = getStructuredDirectoryTree(subTree, currPath);
+    }
+  });
+
+  return tree;
+}
+
+function getPopulatedDirectoryTree(srcTree: Record<string, unknown>) {
+  const tree: Record<string, unknown> = {};
+
+  _.forIn(srcTree, function (subTree: any) {
+    if (_.has(subTree, 'pages') && Array.isArray(subTree?.pages)) {
+      subTree.pages.forEach((page: Record<string, unknown>) => {
+        console.log('found page', page, `${page.parentPath}/${page.fileName}.json`);
+      });
+    } else {
+      getPopulatedDirectoryTree(subTree);
+    }
+  });
+
+  return tree;
+}
+
+function clusterPages(pages: string[][], path?: string) {
+  if (!pages.length) return;
+
+  const outputPages = [];
+
+  const groups = _.groupBy(pages, (item) => item[0]);
+  console.log('clusterPages groups - 1', groups);
+
+  // remove the grouped key
+  _.forEach(groups, (value) => {
+    value.forEach((arr) => arr.shift());
+  });
+  console.log('clusterPages groups - 2', groups);
+
+  _.forEach(groups, (value, key) => {
+    const subPages = value.filter((arr) => arr.length === 1);
+    const subSections = value.filter((arr) => arr.length > 1);
+
+    const newPage = {
+      id: `${path ? path + '/' : ''}${key}`,
+      title: key,
+      pages: subPages.map((page) => {
+        return {
+          id: `${path ? path + '/' : ''}${key}/${page}`,
+          title: page[0],
+          // pages: addPages(subSections, key)
+        };
+      }),
+    };
+
+    if (subSections.length) {
+      newPage.pages = newPage.pages.concat(clusterPages(subSections, key));
+    }
+
+    outputPages.push(newPage);
+  });
+
+  return outputPages;
+}
