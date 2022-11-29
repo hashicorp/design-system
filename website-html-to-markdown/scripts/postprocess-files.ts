4fd08d38fa27c45e058884177b6ab85029844755
Cristiano Rastelli <public@didoo.net>
2022-11-29 21:30:05 +0000

    added a postprocess script that replaces all the `DummyPlaceholder` occurrences with `Doc::Placeholder`
    (notice: the actual regeneration of the markdown files will be done in a follow-up PR)

---
+++ website-html-to-markdown/scripts/postprocess-files.ts
@@ -0,0 +1,42 @@
+import { glob } from 'glob';
+import fs from 'fs-extra';
+import path from 'path';
+import chalk from 'chalk';
+
+const docsFolder = path.resolve(__dirname, '../../website/docs');
+
+(async () => {
+  try {
+    console.log(
+      `\n==========\n${chalk.cyan(
+        'Postprocessing "docs" files...'
+      )}\n`
+    );
+
+    await postprocess();
+
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
+
+async function postprocess() {
+  // process the markdown files
+  glob(docsFolder + '/**/*.md', {}, async function (_error, files) {
+    // loop on every markdown file found in the source folder
+    for (const filePath of files) {
+
+      // we read the markdown source
+      let markdownSource = await fs.readFile(filePath, 'utf8');
+
+      // GLOBAL CHANGES
+      // ----------------------------
+
+      markdownSource = markdownSource.replace(/<DummyPlaceholder/g, '<Doc::Placeholder');
+      markdownSource = markdownSource.replace(/<\/DummyPlaceholder/g, '</Doc::Placeholder');
+
+      await fs.writeFile(filePath, markdownSource);
+    }
+  });
+}
