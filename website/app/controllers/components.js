9d48eaa2f8f18726602ffd0c973eb66717178c68
Cristiano Rastelli <public@didoo.net>
2022-11-29 00:10:20 +0000

    refactored `Foundations` and `Components` pages to use the new TOC structure

--- website/app/controllers/components.js
+++ website/app/controllers/components.js
@@ -1,55 +1,28 @@
 import Controller from '@ember/controller';
 
 export default class ComponentsController extends Controller {
-  get subTOC() {
-    // NOTICE: the TOC comes direclty from the application "model" (see addons/field-guide/addon/routes/application.js)
-    // TODO! important: instead of spreading the content all over the model, group the TOC under a specific "key"!
-
-    // hacky way to have the subTOC items in the right order
-    const subIDs = ['components', 'overrides', 'utilities'];
-    const subTOC = {};
-
-    // TODO! convert this to a recursive function shared with the "Foundations page"
-    this.model
-      .filter((item) => subIDs.includes(item.id))
-      .forEach((item) => {
-        const { id, title, pages } = item;
-        const cards = [];
-        pages.forEach((page) => {
-          if (page.pages && page.pages.length > 0) {
-            if (page.pages[0].pages) {
-              // TODO recursive
-              page.pages.forEach((subpage) => {
-                cards.push({
-                  image: `https://picsum.photos/seed/s${encodeURI(
-                    page.subtitle
-                  )}/232/124`,
-                  title: subpage.title,
-                  description:
-                    'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
-                  route: 'show',
-                  // model: `${subpage.id}/${subpage.pages[0].id}`,
-                  model: subpage.pages[0].id,
-                });
-              });
-            } else {
-              cards.push({
-                image: `https://picsum.photos/seed/s${encodeURI(
-                  page.title
-                )}/232/124`,
-                title: page.title,
-                description:
-                  'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
-                route: 'show',
-                model: page.pages[0].id,
-              });
-            }
-          } else {
-            console.log('????????????');
-          }
+  get cards() {
+    // we want to use a flat tree here...
+    const tocTree = this.model.toc.flat;
+    const sections = ['components', 'overrides', 'utilities'];
+    const cards = {};
+    sections.forEach((section) => {
+      cards[section] = tocTree
+        .filter((page) => page.pageParents[0] === section)
+        .map((page) => {
+          return {
+            image: `https://picsum.photos/seed/s${encodeURI(
+              page.pageURL.replaceAll('/', '-')
+            )}/232/124`,
+            title: page.pageAttributes.title,
+            description:
+              page.pageAttributes.description ||
+              'Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+            route: 'show',
+            model: page.pageURL,
+          };
         });
-        subTOC[id] = { title, cards };
-      });
-    return subTOC;
+    });
+    return cards;
   }
 }
