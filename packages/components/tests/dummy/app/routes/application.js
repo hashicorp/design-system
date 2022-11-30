723a5b55f99f1c1c33e2c24f784c50181cdfa0fe
Melanie Sumner <melanie@hashicorp.com>
2022-11-30 17:03:41 -0600

    Table documentation: Adds internationalized example and showcase sample

--- packages/components/tests/dummy/app/routes/application.js
+++ packages/components/tests/dummy/app/routes/application.js
@@ -1,5 +1,12 @@
 import Route from '@ember/routing/route';
 import { setup } from 'ember-prism';
+import { service } from '@ember/service';
 
 setup();
-export default class ApplicationRoute extends Route {}
+export default class ApplicationRoute extends Route {
+  @service intl;
+
+  beforeModel() {
+    this.intl.setLocale(['en-us']);
+  }
+}
