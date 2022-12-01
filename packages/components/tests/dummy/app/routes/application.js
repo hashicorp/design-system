4de2436f8b2e749ba11af1e4a2d6894c2169cfc7
Melanie Sumner <melanie@hashicorp.com>
2022-12-01 09:53:11 -0600

    remove showcase sample

--- packages/components/tests/dummy/app/routes/application.js
+++ packages/components/tests/dummy/app/routes/application.js
@@ -1,12 +1,5 @@
 import Route from '@ember/routing/route';
 import { setup } from 'ember-prism';
-import { service } from '@ember/service';
 
 setup();
-export default class ApplicationRoute extends Route {
-  @service intl;
-
-  beforeModel() {
-    this.intl.setLocale(['en-us']);
-  }
-}
+export default class ApplicationRoute extends Route {}
