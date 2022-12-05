99d45e71deed001d979c2a5c538388621d1d9784
Kristin Bradley <kristin.bradley@hashicorp.com>
2022-12-05 15:57:58 -0800

    HDS-969 Make buttons with labels the same min-width

--- packages/components/addon/components/hds/pagination-bar/pagination/nav-button.js
+++ packages/components/addon/components/hds/pagination-bar/pagination/nav-button.js
@@ -14,4 +14,23 @@ export default class HdsPaginationBarPaginationIndexComponent extends Component 
       onClick(this.args.direction);
     }
   }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = [
+      'hds-pagination__page-link',
+      'hds-typography-body-100',
+      'hds-font-weight-medium',
+    ];
+
+    if (this.args.type === 'compact') {
+      classes.push(`hds-pagination__page-link--labeled`);
+    }
+
+    return classes.join(' ');
+  }
 }
