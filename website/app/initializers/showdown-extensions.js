8bab4881d0037239f12815d3fc5434496625fbaa
Alex <alex-ju@users.noreply.github.com>
2022-12-06 11:09:05 +0000

    Override showdown's `unhashHTMLSpans` parser

--- website/app/initializers/showdown-extensions.js
+++ website/app/initializers/showdown-extensions.js
@@ -4,6 +4,42 @@
 import showdown from 'showdown';
 
 export function initialize(/* application */) {
+  // Overriding `unhashHTMLSpans` subparser to overcome the 10 levels of nesting limit
+  showdown.subParser('unhashHTMLSpans', function (text, options, globals) {
+    'use strict';
+
+    text = globals.converter._dispatch(
+      'unhashHTMLSpans.before',
+      text,
+      options,
+      globals
+    );
+  
+    for (var i = 0; i < globals.gHtmlSpans.length; ++i) {
+      var repText = globals.gHtmlSpans[i],
+        // limiter to prevent infinite loop (assume 50 as limit for recurse)
+        limit = 0;
+  
+      while (/¨C(\d+)C/.test(repText)) {
+        var num = RegExp.$1;
+        repText = repText.replace('¨C' + num + 'C', globals.gHtmlSpans[num]);
+        if (limit === 50) {
+          console.error('maximum nesting of 50 spans reached!!!');
+          break;
+        }
+        ++limit;
+      }
+      text = text.replace('¨C' + i + 'C', repText);
+    }
+  
+    text = globals.converter._dispatch(
+      'unhashHTMLSpans.after',
+      text,
+      options,
+      globals
+    );
+    return text;
+  });
 
   showdown.subParser('githubCodeBlocks', function (text, options, globals) {
     'use strict';
