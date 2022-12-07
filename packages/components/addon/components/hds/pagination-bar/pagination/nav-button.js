3e2631a1ded494cd3f6bac963313fc0f0f8cbe8e
Cristiano Rastelli <public@didoo.net>
2022-12-07 19:19:50 +0000

    small refactoring of the `PaginationBar::Pagination::NavButton`

--- packages/components/addon/components/hds/pagination-bar/pagination/nav-button.js
+++ packages/components/addon/components/hds/pagination-bar/pagination/nav-button.js
@@ -2,16 +2,34 @@
 
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { assert } from '@ember/debug';
 
 export const DIRECTIONS = ['next', 'previous'];
 
 export default class HdsPaginationBarPaginationIndexComponent extends Component {
-  @action
-  onClick() {
-    let { onClick } = this.args;
+  get content() {
+    let { direction } = this.args;
 
-    if (typeof onClick === 'function') {
-      onClick(this.args.direction);
+    assert(
+      `@direction for "PaginationBar::Pagination::NavButton" must be one of the following: ${DIRECTIONS.join(
+        ', '
+      )}; received: ${direction}`,
+      DIRECTIONS.includes(direction)
+    );
+
+    if (direction === 'previous') {
+      return {
+        label: 'Previous',
+        icon: 'chevron-left',
+        ariaLabel: 'Previous page',
+      };
+    }
+    if (direction === 'next') {
+      return {
+        label: 'Next',
+        icon: 'chevron-right',
+        ariaLabel: 'Next page',
+      };
     }
   }
 
@@ -22,15 +40,29 @@ export default class HdsPaginationBarPaginationIndexComponent extends Component 
    */
   get classNames() {
     let classes = [
+      'hds-pagination__nav-button',
       'hds-pagination__page-link',
       'hds-typography-body-100',
       'hds-font-weight-medium',
     ];
 
+    classes.push(
+      `hds-pagination__nav-button--direction-${this.args.direction}`
+    );
+
     if (this.args.type === 'compact') {
-      classes.push(`hds-pagination__page-link--labeled`);
+      classes.push(`hds-pagination__nav-button--labeled`);
     }
 
     return classes.join(' ');
+  }
+
+  @action
+  onClick() {
+    let { onClick } = this.args;
+
+    if (typeof onClick === 'function') {
+      onClick(this.args.direction);
+    }
   }
 }
