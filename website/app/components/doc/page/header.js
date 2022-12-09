144d1f0c25b661988975ece1063094a469a2f233
Cristiano Rastelli <public@didoo.net>
2022-12-09 18:55:24 +0000

    work-in-progress (not working)

--- website/app/components/doc/page/header.js
+++ website/app/components/doc/page/header.js
@@ -1,18 +1,69 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { schedule } from '@ember/runloop';
+import { inject as service } from '@ember/service';
+
+const MENU = [
+  {
+    label: 'About',
+    route: 'about',
+  },
+  {
+    label: 'Foundations',
+    route: 'foundations',
+  },
+  {
+    label: 'Components',
+    route: 'components',
+  },
+  {
+    label: 'Patterns',
+    route: 'patterns',
+  },
+];
 
 export default class DocPageHeaderComponent extends Component {
+  @service router;
+  @service fastboot;
+
   @tracked showLayoutColors = false;
+  @tracked currentTopRoute = 'foundations';
+
+  constructor() {
+    super(...arguments);
+
+    // schedule nav udpate for after this content is rendered
+    // if (!this.fastboot.isFastBoot) { // TODO! this is not working?
+    schedule('afterRender', () => {
+      console.log('called afterRender');
+      this.updateNavigation();
+    });
+    // }
+  }
+
+  get menu() {
+    let xxx = MENU.map((item) => {
+      console.log('item1', item);
+      item.isCurrent = item.route === this.currentTopRoute;
+      console.log('item2', item);
+      return item;
+    });
+    console.log('called getMenu', xxx);
+    return xxx;
+  }
 
   @action
-  toggleLayoutColors() {
-    if (this.showLayoutColors) {
-      this.showLayoutColors = false;
-      document.body.classList.remove('🌈');
-    } else {
-      this.showLayoutColors = true;
-      document.body.classList.add('🌈');
-    }
+  updateNavigation() {
+    this.currentTopRoute = MENU[Math.floor(Math.random() * MENU.length)];
+    // this.menu.forEach((item) => {
+    //   item.isCurrent = true;
+    // });
+    // console.log('called updateNavigation', this.menu);
+  }
+
+  @action
+  test() {
+    this.updateNavigation();
   }
 }
