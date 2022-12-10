bb751aec22c4473ccb2362308117ab12fc254c5b
Kristin Bradley <kristin.bradley@hashicorp.com>
2022-12-09 16:23:25 -0800

    HDS-970 Add stand alone test for Pagination::Nav, fix API to work as stand alone and update docs

--- packages/components/tests/integration/components/hds/pagination/nav/index-test.js
+++ packages/components/tests/integration/components/hds/pagination/nav/index-test.js
@@ -7,12 +7,43 @@ module('Integration | Component | hds/pagination/nav/index', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders the component', async function (assert) {
-    await render(hbs`<Hds::Pagination::Nav />`);
+    await render(hbs`
+      <Hds::Pagination::Nav @totalPages={{10}} @currentPage={{1}} />
+    `);
     assert.dom(this.element).exists();
   });
 
   test('it should render with a CSS class that matches the component name', async function (assert) {
-    await render(hbs`<Hds::Pagination::Nav id="test-pagination-nav" />`);
+    await render(hbs`
+      <Hds::Pagination::Nav @totalPages={{10}} @currentPage={{1}} id="test-pagination-nav" />
+    `);
     assert.dom('#test-pagination-nav').hasClass('hds-pagination-nav');
+  });
+
+  // Test API:
+
+  test('it has the same numbr of pages displayed as are passed in', async function (assert) {
+    await render(hbs`
+      <Hds::Pagination::Nav @totalPages={{10}} @currentPage={{1}} @type="numbered" />
+    `);
+    assert.dom('.hds-pagination-nav__page-item:nth-child(10)').exists();
+  });
+
+  test('it selects the 1st page by default', async function (assert) {
+    await render(hbs`
+      <Hds::Pagination::Nav @totalPages={{10}} @type="numbered" />
+    `);
+    assert
+      .dom('.hds-pagination-nav__page-item:nth-child(1)')
+      .hasClass('hds-pagination-nav__button-number--is-selected');
+  });
+
+  test('it selects the passed in current page', async function (assert) {
+    await render(hbs`
+      <Hds::Pagination::Nav @totalPages={{10}} @currentPage={{3}} @type="numbered" />
+    `);
+    assert
+      .dom('.hds-pagination-nav__page-item:nth-child(3)')
+      .hasClass('hds-pagination-nav__button-number--is-selected');
   });
 });
