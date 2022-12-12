dee2acfb2b21e1aa0fec46e237bc145fd441a82e
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2022-12-12 14:21:50 -0600

    rename tests

--- website/tests/acceptance/component-query-test.js
+++ website/tests/acceptance/component-query-test.js
@@ -2,10 +2,10 @@ import { module, test } from 'qunit';
 import { click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'website/tests/helpers';
 
-module('Acceptance | tab routing', function (hooks) {
+module('Acceptance | Component Tabs', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting a tabbed page and interacting with tabs by default', async function (assert) {
+  test('visiting a tabbed component page and interacting with tabs by default', async function (assert) {
     await visit('/components/alert');
 
     assert.strictEqual(currentURL(), '/components/alert?tab=guidelines');
@@ -24,7 +24,7 @@ module('Acceptance | tab routing', function (hooks) {
     assert.strictEqual(currentURL(), '/components/alert?tab=code');
   });
 
-  test('visiting a tabbed page and interacting with tabs by specific tab', async function (assert) {
+  test('visiting a tabbed component page and interacting with tabs by specific tab', async function (assert) {
     await visit('/components/alert?tab=code');
 
     assert.strictEqual(currentURL(), '/components/alert?tab=code');
@@ -35,7 +35,7 @@ module('Acceptance | tab routing', function (hooks) {
       .hasClass('doc-page-tabs__tab--is-current');
   });
 
-  test('specifying a tab that does not exist defaults to the first tab', async function (assert) {
+  test('specifying a component tab that does not exist defaults to the first tab', async function (assert) {
     await visit('/components/alert?tab=wubalubadubdub');
 
     assert.strictEqual(currentURL(), '/components/alert?tab=guidelines');
