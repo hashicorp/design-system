4307d4f472accb1d0d5fcc36e5e04f8f4668c6aa
Melanie Sumner <melanie@hashicorp.com>
2022-12-12 15:26:38 -0600

    added width tests

--- packages/components/tests/integration/components/hds/table/th-test.js
+++ packages/components/tests/integration/components/hds/table/th-test.js
@@ -12,16 +12,28 @@ module('Integration | Component | hds/table/th', function (hooks) {
     assert.dom(this.element).hasText('template block text');
   });
 
-  test('it has the scope attribute, and it is set to column', async function (assert) {
+  test('it has the scope attribute set to column by default', async function (assert) {
     await render(
       hbs`<Hds::Table::Th id="data-test-table-th">artist</Hds::Table::Th>`
     );
     assert.dom('#data-test-table-th').hasAttribute('scope', 'col');
   });
 
+  test('it has the scope attribute set to row if inside a tbody', async function (assert) {
+    await render(
+      hbs`<Hds::Table><:body as |B|><B.Tr><B.Th id="data-test-table-th">artist</B.Th></B.Tr></:body></Hds::Table>`
+    );
+    assert.dom('#data-test-table-th').hasAttribute('scope', 'row');
+  });
+
   test('it should render with a CSS class that matches the component name', async function (assert) {
     await render(hbs`<Hds::Table::Th id="data-test-table-th" />`);
     assert.dom('#data-test-table-th').hasClass('hds-table__th');
+  });
+
+  test('it should add inline styles if `@width` is declared', async function (assert) {
+    await render(hbs`<Hds::Table::Th id="data-test-table-th" @width="10%" />`);
+    assert.dom('#data-test-table-th').hasAttribute('style');
   });
 
   test('it should support splattributes', async function (assert) {
