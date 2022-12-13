3667326e24f8d1f0f6084ec2ba192b7bbeb4e138
Cristiano Rastelli <public@didoo.net>
2022-12-13 15:20:59 +0000

    further code simplification (pairing with Brian)

--- website/app/components/doc/page/header/index.js
+++ website/app/components/doc/page/header/index.js
@@ -1,42 +1,14 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { schedule } from '@ember/runloop';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class DocPageHeaderComponent extends Component {
   @service router;
-  @service fastboot;
-
   @tracked burgerMenuOpen = false;
-  @tracked currentTopRoute = 'foundations';
 
-  constructor() {
-    super(...arguments);
-
-    if (!this.fastboot.isFastBoot) {
-      // schedule nav udpate for after this content is rendered
-      schedule('routerTransitions', () => {
-        console.log('called routerTransitions');
-        this.updateNavigation();
-        // should I use addObserver instead?
-        // https://api.emberjs.com/ember/4.8/classes/RouterService/methods?anchor=addObserver
-        this.router.on('routeDidChange', this, this.updateNavigation);
-      });
-    }
-  }
-
-  willDestroy() {
-    super.willDestroy(...arguments);
-    if (!this.fastboot.isFastBoot) {
-      this.router.off('routeDidChange', this, this.updateNavigation);
-    }
-  }
-
-  // @action
-  updateNavigation() {
-    // eg. /foundations/colors/
-    this.currentTopRoute = this.router.currentURL.split('/')[1];
+  get currentTopRoute() {
+    return this.router.currentURL.split('/')[1];
   }
 
   get classNames() {
