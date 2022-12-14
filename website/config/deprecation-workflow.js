6175829b48db90d020005743deb4140dc265f112
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2022-12-14 11:32:15 -0600

    silence ember-modifier.function-based-options

--- website/config/deprecation-workflow.js
+++ website/config/deprecation-workflow.js
@@ -3,6 +3,7 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchId: "remove-owner-inject" }
+    { handler: "silence", matchId: "remove-owner-inject" },
+    { handler: "silence", matchId: "ember-modifier.function-based-options" }
   ]
 };
