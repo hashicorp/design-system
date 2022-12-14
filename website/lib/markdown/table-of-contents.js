8e463b22c69380dff258ec3fbd335c4266967ab5
Cristiano Rastelli <public@didoo.net>
2022-12-14 18:22:32 +0000

    added sorting function to the TOC generation

--- website/lib/markdown/table-of-contents.js
+++ website/lib/markdown/table-of-contents.js
@@ -6,20 +6,70 @@
 const walkSync = require('walk-sync');
 const Plugin = require('broccoli-plugin');
 const fs = require('fs-extra');
-const _ = require('lodash');
+const { pick, set, isEqual } = require('lodash');
 
-// const CATEGORIES = [
-//   'about',
-//   'overview',
-//   'getting-started',
-//   'updates',
-//   'foundations',
-//   'components',
-//   'overrides',
-//   'utilities',
-//   'patterns',
-//   'testing',
-// ];
+const CATEGORIES = [
+  'about',
+  'getting-started',
+  'updates',
+  'foundations',
+  'components',
+  'overrides',
+  'utilities',
+  'patterns',
+  'testing',
+];
+
+const sortPages = (s1, s2) => {
+  if (
+    s1.filePath === 'components/form/checkbox/index' &&
+    s2.filePath === 'components/badge/index'
+  ) {
+    console.log('s1-s2 (form/checkbox vs badge)', s1, s2);
+  }
+  // if they are siblings...
+  if (isEqual(s1.pageParents, s2.pageParents)) {
+    //  we use the weight first...
+    if (s1.pageAttributes.weight < s2.pageAttributes.weight) {
+      return -1;
+    } else if (s1.pageAttributes.weight > s2.pageAttributes.weight) {
+      return 1;
+    } else {
+      // or we fallback to sort alphabethically
+      const p1 = s1.pageName.toLowerCase;
+      const p2 = s2.pageName.toLowerCase;
+      if (p1 < p2) {
+        return 1;
+      } else if (p1 > p2) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  } else {
+    // we try to use the top-level category
+    const c1 = CATEGORIES.indexOf(s1.pageParents[0].toLowerCase());
+    const c2 = CATEGORIES.indexOf(s2.pageParents[0].toLowerCase());
+    if (c1 < c2) {
+      return -1;
+    } else if (c1 > c2) {
+      return 1;
+    } else {
+      // the natural "file system" order works here
+      return 0;
+      // or if it doesn't try to tweak this logic
+      // const p1 = s1.filePath;
+      // const p2 = s2.filePath;
+      // if (p1 < p2) {
+      //   return -1;
+      // } else if (p1 > p2) {
+      //   return 1;
+      // } else {
+      //   return 0;
+      // }
+    }
+  }
+};
 
 class TableOfContents extends Plugin {
   constructor(inputNodes, folder) {
@@ -58,7 +108,7 @@ class TableOfContents extends Plugin {
       if (fs.existsSync(fullFilePath)) {
         const jsonData = fs.readJSONSync(fullFilePath);
         // notice: the page attributes are quite verbose, so we select only a subset (what we really need for the TOC generation)
-        pageAttributes = _.pick(jsonData.data.attributes, [
+        pageAttributes = pick(jsonData.data.attributes, [
           'title',
           'caption',
           'weight',
@@ -85,11 +135,12 @@ class TableOfContents extends Plugin {
     });
 
     // notice: we pre-sort the list so we don't need to do it in the structured tree (much harder!)
-    // flatPageList = flatPageList.sort(sortPages);
+    // const flatSortedPageList = [...flatPageList].sort(sortPages); // use this if you want to maintain the
+    flatPageList.sort(sortPages);
 
     flatPageList.forEach((page) => {
       // we have to rebuild the full structure here
-      _.set(structuredPageTree, [...page.pageParents, page.pageName], page);
+      set(structuredPageTree, [...page.pageParents, page.pageName], page);
     });
 
     const toc = { flat: flatPageList, tree: structuredPageTree };
