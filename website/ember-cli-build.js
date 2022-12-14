255b130a48a78ab9aebbe759e8427589437ea5b2
Alex <alex-ju@users.noreply.github.com>
2022-12-14 23:31:07 +0000

    Add `prism-dracula`-based syntax highlighter

--- website/ember-cli-build.js
+++ website/ember-cli-build.js
@@ -29,7 +29,7 @@ module.exports = function (defaults) {
         'ruby',
         'scss',
       ],
-
+      theme: 'dracula',
       plugins: ['line-numbers', 'normalize-whitespace'],
     },
   });
