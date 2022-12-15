22dc45dbd093df160f80d29365dc3acf79121d57
Cristiano Rastelli <public@didoo.net>
2022-12-15 15:18:47 +0000

    first iteration of styling for the page “sidebar” based on the design specs
    (still to be confirmed with Heather)

--- website/app/components/doc/table-of-contents/index.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { htmlSafe } from '@ember/template';
-
-export default class DummyTableOfContentsComponent extends Component {
-  get leftPadStyle() {
-    return htmlSafe(`padding-left:${this.args.level}em;`);
-  }
-}
