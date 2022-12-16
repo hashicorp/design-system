00bc4b9fb05c1ef7b96465d95e799d025d31b1a5
Kristin Bradley <kristin.bradley@hashicorp.com>
2022-12-15 17:27:14 -0800

    HDS-1174 Add truncation variant type

--- packages/components/addon/components/hds/pagination/nav/index.js
+++ packages/components/addon/components/hds/pagination/nav/index.js
@@ -3,7 +3,7 @@ import { assert } from '@ember/debug';
 import { action } from '@ember/object';
 
 export const DEFAULT_TYPE = 'compact';
-export const TYPES = ['compact', 'numbered'];
+export const TYPES = ['compact', 'numbered', 'truncated'];
 
 export default class HdsPaginationNavIndexComponent extends Component {
   /**
@@ -67,12 +67,60 @@ export default class HdsPaginationNavIndexComponent extends Component {
     return pages;
   }
 
+  get truncatedPages() {
+    console.log(
+      this.elliptize({ pages: this.pages, current: this.currentPage })
+    );
+    console.log(this.currentPage);
+    return this.elliptize({ pages: this.pages, current: this.currentPage });
+  }
+
   get isDisabledPrev() {
     return this.currentPage === 1;
   }
 
   get isDisabledNext() {
     return this.currentPage === this.totalPages;
+  }
+
+  elliptize({ pages, current }) {
+    const array = Array.from(pages);
+    const limit = 7; // limt # of page numbers shown at a time
+    const length = array.length;
+    const ellipsis = '…';
+    let result = [];
+    let start;
+    let end;
+
+    if (current <= length / 2) {
+      start = Math.ceil(limit / 2);
+      end = limit - start;
+    } else {
+      end = Math.ceil(limit / 2);
+      start = limit - end;
+    }
+
+    const sliceStart = array.slice(0, start);
+    const sliceEnd = array.slice(-end);
+    const sliceCurr = [current - 1, current, current + 1];
+
+    if (sliceStart.includes(current) && sliceStart.includes(current + 1)) {
+      sliceEnd.splice(0, 1, ellipsis);
+      result = [].concat(sliceStart, sliceEnd);
+    } else if (sliceEnd.includes(current - 1) && sliceEnd.includes(current)) {
+      sliceStart.splice(-1, 1, ellipsis);
+      result = [].concat(sliceStart, sliceEnd);
+    } else {
+      result = [].concat(
+        sliceStart.shift(),
+        ellipsis,
+        sliceCurr,
+        ellipsis,
+        sliceEnd.pop()
+      );
+    }
+
+    return result;
   }
 
   @action
