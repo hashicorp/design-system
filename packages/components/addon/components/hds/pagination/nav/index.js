7b47758f62fb885edda563dd62a2813f8e74e1a2
Kristin Bradley <kristin.bradley@hashicorp.com>
2022-12-16 10:30:49 -0800

    HDS-1174 Add code to improve algorithm for truncation

--- packages/components/addon/components/hds/pagination/nav/index.js
+++ packages/components/addon/components/hds/pagination/nav/index.js
@@ -80,7 +80,7 @@ export default class HdsPaginationNavIndexComponent extends Component {
   }
 
   elliptize({ pages, current }) {
-    const limit = 7; // limt # of page numbers shown at a time
+    const limit = 7; // limit # of page numbers shown at a time (should always be an even number!)
     const length = pages.length;
     const ellipsis = '…';
     let result = [];
@@ -101,15 +101,23 @@ export default class HdsPaginationNavIndexComponent extends Component {
 
     const sliceStart = pages.slice(0, start);
     const sliceEnd = pages.slice(-end);
-    const sliceCurr = [current - 1, current, current + 1];
 
     if (sliceStart.includes(current) && sliceStart.includes(current + 1)) {
+      // "current" (and its next sibling) is contained within the "sliceStart" block
       sliceEnd.splice(0, 1, ellipsis);
       result = [].concat(sliceStart, sliceEnd);
     } else if (sliceEnd.includes(current - 1) && sliceEnd.includes(current)) {
+      // "current" (and its prev sibling) is contained within the "sliceEnd" block
       sliceStart.splice(-1, 1, ellipsis);
       result = [].concat(sliceStart, sliceEnd);
     } else {
+      // this is a bit more tricky :)
+      // we need to calculate how many items there are before/after the current item
+      // since both the initial and ending blocks are always 2 items long (number + ellipsis)
+      // and there is always the "current" item, we can just subtract 5 from the limit
+      const delta = (limit - 5) / 2; // this is why the limit needs to be an even number
+      // we slice the array starting at the "current" index, minus the delta, minus one because it's an array (zero-based)
+      const sliceCurr = pages.slice(current - delta - 1, current + delta);
       result = [].concat(
         sliceStart.shift(),
         ellipsis,
