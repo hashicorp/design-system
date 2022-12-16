45814e3ca4bcc5a72dbf0f541fa4242b2c1ffbb5
Cristiano Rastelli <public@didoo.net>
2022-12-16 14:21:12 +0000

    Added `Doc::TokensList` to “Tokens” page as “library” and implemented filtering logic

--- website/docs/foundations/tokens/index.js
+++ website/docs/foundations/tokens/index.js
@@ -1,20 +1,61 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { restartableTask, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 import TOKENS_RAW from '@hashicorp/design-system-tokens/dist/docs/products/tokens.json';
 
-export default class Index extends Component {
-  get groupedTokens() {
-    const groupedTokens = {};
+const DEBOUNCE_MS = 250;
 
+export default class Index extends Component {
+  @service router;
+
+  constructor() {
+    super(...arguments);
+    this.groupedTokens = {};
     TOKENS_RAW.forEach((token) => {
       const category = token.attributes.category;
-      if (!groupedTokens[category]) {
-        groupedTokens[category] = [];
+      if (!this.groupedTokens[category]) {
+        this.groupedTokens[category] = [];
       }
-
-      groupedTokens[category].push(token);
+      this.groupedTokens[category].push(token);
     });
+  }
 
-    return groupedTokens;
+  get filteredGroupedTokens() {
+    // query params come from `controllers/show.js` and we access them here because there
+    // is no "controller" for individual component documentation routes
+    const searchQuery = this.router.currentRoute.queryParams['searchQuery'];
+
+    let filteredGroupedTokens = {};
+    if (searchQuery) {
+      Object.keys(this.groupedTokens).forEach((category) => {
+        const filteredTokens = this.groupedTokens[category].filter(
+          (t) => t.name.indexOf(searchQuery) !== -1
+        );
+        filteredGroupedTokens[category] =
+          filteredTokens.length > 0 ? filteredTokens : false;
+      });
+    } else {
+      filteredGroupedTokens = this.groupedTokens;
+    }
+
+    return filteredGroupedTokens;
+  }
+
+  updateQueryParams() {
+    // TODO later when we will have the tabs and sidecar parameters too, we have to preserve them
+    const newQueryParams = { queryParams: {} };
+    if (this.searchQuery) {
+      newQueryParams.queryParams.searchQuery = this.searchQuery;
+    }
+    this.router.transitionTo(newQueryParams);
+  }
+
+  @restartableTask *searchTokens(searchQuery) {
+    yield timeout(DEBOUNCE_MS);
+
+    this.searchQuery = searchQuery;
+    this.updateQueryParams();
   }
 }
