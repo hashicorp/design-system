033ae157e0ec94c425700ad6fa12640ee661d022
Kristin Bradley <kristin.bradley@hashicorp.com>
2022-12-19 14:53:17 -0800

    HDS-1174 Add simpler tests for truncation, edit doc text for clarity and add another example

--- packages/components/tests/integration/components/hds/pagination/nav/index-test.js
+++ packages/components/tests/integration/components/hds/pagination/nav/index-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { click, render } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | hds/pagination/nav/index', function (hooks) {
@@ -31,48 +31,38 @@ module('Integration | Component | hds/pagination/nav/index', function (hooks) {
 
   test('it displays a truncated list of page numbers for the "truncated" type', async function (assert) {
     await render(hbs`
-      <Hds::Pagination::Nav @totalPages={{1000}} @currentPage={{1}} @type="truncated" />
+      <Hds::Pagination::Nav @totalPages={{100}} @currentPage={{1}} @type="truncated" />
     `);
     // displays 7 items (the max number displayed)
     assert.dom('.hds-pagination-nav__page-item').exists({ count: 7 });
     // It displays an ellipsis for the 5th item initially
     assert.dom('.hds-pagination-nav__page-item:nth-child(5)').hasText('...');
-
-    // The ellipsis should stay in the same position when the 2nd & 3rd controls are clicked and made active
-    // NOTE: Not certain these are valid, see commented out test below
-    await click(
-      '.hds-pagination-nav__page-item:nth-child(2) .hds-pagination-nav__button-number'
-    );
-    assert.dom('.hds-pagination-nav__page-item:nth-child(5)').hasText('...');
-
-    await click(
-      '.hds-pagination-nav__page-item:nth-child(3) .hds-pagination-nav__button-number'
-    );
-    assert.dom('.hds-pagination-nav__page-item:nth-child(5)').hasText('...');
+    assert
+      .dom('.hds-pagination-nav__page-item:not(:nth-child(5))')
+      .doesNotIncludeText('...');
   });
 
-  /*
-  DISABLEtest('it should display ellipses for the 2nd and 6th items when a middle page number is selected', async function (assert) {
+  test('it should display an ellipsis for the 2nd and 6th items when a middle page number is selected', async function (assert) {
     await render(hbs`
-      <Hds::Pagination::Nav @totalPages={{10}} @currentPage={{3}} @type="truncated" />
+      <Hds::Pagination::Nav @totalPages={{100}} @currentPage={{4}} @type="truncated" />
     `);
-
-    // Navigate to the 4th page using Next button:
-    await click('.hds-pagination-nav__button-arrow--direction-next'); // move to 4th page
-    // This is failing. It doesn't seem to be waiting for the DOM to be updated before the assertion is called?
-    // (Adding await settled() didn't help)
     assert.dom('.hds-pagination-nav__page-item:nth-child(2)').hasText('...');
     assert.dom('.hds-pagination-nav__page-item:nth-child(6)').hasText('...');
-  });
-  */
-
-  test('it selects the 1st page by default', async function (assert) {
-    await render(hbs`
-      <Hds::Pagination::Nav @totalPages={{10}} @type="numbered" />
-    `);
     assert
-      .dom('.hds-pagination-nav__page-item:nth-child(1)')
-      .hasClass('hds-pagination-nav__button-number--is-selected');
+      .dom(
+        '.hds-pagination-nav__page-item:not(:nth-child(2)):not(:nth-child(6))'
+      )
+      .doesNotIncludeText('...');
+  });
+
+  test('it should display an ellipsis for the 3rd item when the last page number is selected', async function (assert) {
+    await render(hbs`
+      <Hds::Pagination::Nav @totalPages={{100}} @currentPage={{100}} @type="truncated" />
+    `);
+    assert.dom('.hds-pagination-nav__page-item:nth-child(3)').hasText('...');
+    assert
+      .dom('.hds-pagination-nav__page-item:not(:nth-child(3))')
+      .doesNotIncludeText('...');
   });
 
   test('it selects the passed in current page', async function (assert) {
