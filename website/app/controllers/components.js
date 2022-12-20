a8cd739c085443a9a5490654514f22806fb8e4c2
Melanie Sumner <melanie@hashicorp.com>
2022-12-20 16:02:35 -0600

    updates the component controller img url

--- website/app/controllers/components.js
+++ website/app/controllers/components.js
@@ -11,9 +11,10 @@ export default class ComponentsController extends Controller {
         .filter((page) => page.pageParents[0] === section)
         .map((page) => {
           return {
-            image: `https://picsum.photos/seed/s${encodeURI(
-              page.pageURL.replaceAll('/', '-')
-            )}/232/124`,
+            image:
+              `/assets/illustrations/components/` +
+              page.pageAttributes.title.trim().toLowerCase() +
+              `.png`,
             title: page.pageAttributes.title,
             caption:
               page.pageAttributes.caption ||
