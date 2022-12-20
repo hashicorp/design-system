0670ff5e22d166ea73962cbe196180be70b4cae1
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2022-12-20 12:23:10 -0600

    fix query params on icons page
    * Updates search if input is cleared
    * Correctly binds search query params to the form

--- website/docs/foundations/icons/index.js
+++ website/docs/foundations/icons/index.js
@@ -11,8 +11,11 @@ const DEBOUNCE_MS = 250;
 export default class Index extends Component {
   @service router;
 
+  // query params come from `controllers/show.js` and we access them here because there
+  // is no "controller" for individual component documentation routes
   @tracked selectedIconSize =
     this.router.currentRoute.queryParams['selectedIconSize'] || '24';
+  @tracked searchQuery = this.router.currentRoute.queryParams['searchQuery'];
 
   allIcons = catalog.assets.map(({ iconName, fileName, size, description }) => {
     return {
@@ -25,26 +28,26 @@ export default class Index extends Component {
   });
 
   get filteredIcons() {
-    // query params come from `controllers/show.js` and we access them here because there
-    // is no "controller" for individual component documentation routes
-    const searchQuery = this.router.currentRoute.queryParams['searchQuery'];
-    if (searchQuery) {
+    const selectedIconSize = this.selectedIconSize;
+    if (this.searchQuery) {
       return this.allIcons.filter(
         (i) =>
-          i.size === this.selectedIconSize &&
-          i.searchable.indexOf(searchQuery) !== -1
+          i.size === selectedIconSize &&
+          i.searchable.indexOf(this.searchQuery) !== -1
       );
     } else {
-      return this.allIcons.filter((i) => i.size === this.selectedIconSize);
+      return this.allIcons.filter((i) => i.size === selectedIconSize);
     }
   }
 
   updateQueryParams() {
-    // TODO later when we will have the tabs and sidecar parameters too, we have to preserve them
     const newQueryParams = { queryParams: {} };
     if (this.searchQuery) {
       newQueryParams.queryParams.searchQuery = this.searchQuery;
+    } else {
+      newQueryParams.queryParams.searchQuery = null;
     }
+
     if (this.selectedIconSize) {
       newQueryParams.queryParams.selectedIconSize = this.selectedIconSize;
     }
