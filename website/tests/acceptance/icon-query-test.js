2c9111f625a00ae6f2bc57a2c82c30b2f4876e39
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2022-12-20 13:21:24 -0600

    add tests for clearing icon input

--- website/tests/acceptance/icon-query-test.js
+++ website/tests/acceptance/icon-query-test.js
@@ -28,6 +28,15 @@ module('Acceptance | Icon Search', function (hooks) {
     assert.dom('.doc-icons-list-grid-item').exists({ count: 1 });
   });
 
+  test('visiting a icons page with a query param and clearing the search results', async function (assert) {
+    await visit('/foundations/icons?searchQuery=loading&selectedIconSize=16');
+
+    await fillIn('.doc-icons-list-filter input[type="search"]', '');
+
+    // naive way of seeing if all the results come back
+    assert.dom('[data-test-icon="activity"]').exists();
+  });
+
   test('visiting icons page with no results', async function (assert) {
     await visit('/foundations/icons?searchQuery=wubalubadubdub');
 
