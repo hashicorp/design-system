b0cc44cfdf44684ca7da2f80a9550e72a9b3715f
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2022-12-23 08:40:16 -0600

    update toast content (#936)

--- website/docs/components/toast/partials/specifications/design-guidelines.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class DesignGuidelines extends Component {
-  @action
-  noop() {
-    //
-  }
-}
