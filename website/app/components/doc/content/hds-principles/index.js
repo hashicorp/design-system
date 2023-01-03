52dbeddf92c6abc82396d9b4d1bae052cb37bbe9
Cristiano Rastelli <public@didoo.net>
2023-01-03 18:01:03 +0000

    styled “Principles” page according to designs

--- website/app/components/doc/content/hds-principles/index.js
+++ website/app/components/doc/content/hds-principles/index.js
@@ -3,36 +3,42 @@ import Component from '@glimmer/component';
 export default class DocBadgeComponent extends Component {
   principles = [
     {
+      number: '01',
       title: 'Rooted in reality',
       description:
         'We ground our work and our decisions in reality through data and observations.',
       image: '/assets/illustrations/principles/rooted-in-reality.png',
     },
     {
+      number: '02',
       title: 'Guidance over control',
       description:
         'We provide balance between configurability and composability while driving consistency.',
       image: '/assets/illustrations/principles/guidance-over-control.png',
     },
     {
+      number: '03',
       title: 'Quality by default',
       description:
         'We recognize that we are providing a service and commit to a baseline of quality to provide value and leverage for our consumers. We iterate on features, not quality.',
       image: '/assets/illustrations/principles/quality-by-default.png',
     },
     {
+      number: '04',
       title: 'Design in context',
       description:
         'We meet consumers where they are and consider both the current and future context in which they use the system.',
       image: '/assets/illustrations/principles/design-in-context.png',
     },
     {
+      number: '05',
       title: 'Consider everyone',
       description:
         'We take an inclusive approach to our work from the start, considering the context and range of abilities for all customers.',
       image: '/assets/illustrations/principles/consider-everyone.png',
     },
     {
+      number: '06',
       title: 'Invite feedback',
       description:
         'We take time to have the right conversations with the appropriate stakeholders, to validate and create trust in the outcome.',
