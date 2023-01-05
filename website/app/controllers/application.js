90f264dc076ecc01b59cd9c0ce15ad88d8caa20d
Cristiano Rastelli <public@didoo.net>
2023-01-05 14:16:20 +0000

    refactored logic for `showSidebarOnSmallViewport`

--- website/app/controllers/application.js
+++ website/app/controllers/application.js
@@ -1,15 +1,44 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { scheduleOnce } from '@ember/runloop';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
-  @tracked showSidebarOnSmallViewport = false;
+  @service router;
+  @service fastboot;
 
-  // TODO add a reset for the `showSidebarOnSmallViewport` on route change
+  // notice: don't set it to false, otherwise the sidebar menu will appear open on page load
+  @tracked showSidebarOnSmallViewport;
+
+  constructor() {
+    super(...arguments);
+    this.router.on('routeDidChange', this, 'routeDidChange');
+  }
+
+  routeDidChange() {
+    scheduleOnce('afterRender', this, this.onToggleBurgerMenu);
+  }
+
+  willDestroy() {
+    this.router.off('routeDidChange', this, 'routeDidChange');
+  }
+
+  get currentTopRoute() {
+    return this.router.currentURL.split('/')[1];
+  }
 
   @action
-  onToggleBurgerMenu(isOpen) {
-    this.showSidebarOnSmallViewport = isOpen;
-    document.body.style.overflow = isOpen ? 'hidden' : 'auto';
+  onToggleBurgerMenu() {
+    if (this.showSidebarOnSmallViewport === false) {
+      this.showSidebarOnSmallViewport = true;
+    } else {
+      this.showSidebarOnSmallViewport = false;
+    }
+    if (!this.fastboot.isFastBoot) {
+      document.body.style.overflow = this.showSidebarOnSmallViewport
+        ? 'hidden'
+        : 'auto';
+    }
   }
 }
