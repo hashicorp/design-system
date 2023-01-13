c76f31494b21c11ad507935504b488b1b6171805
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2023-01-13 06:53:53 -0600

    add google site verification tag (#1061)
    Co-authored-by: Alex <alex-ju@users.noreply.github.com>

--- website/lib/analytics/index.js
+++ website/lib/analytics/index.js
@@ -5,7 +5,9 @@ module.exports = {
 
   contentFor(type) {
     if (type === 'head-footer' && process.env.VERCEL_ENV === 'production') {
-      return `<script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="PPQLJJKK" data-excluded-domains="localhost" defer></script>`;
+      return `<script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="PPQLJJKK" data-excluded-domains="vercel.app,localhost" defer></script>
+      
+      <meta name="google-site-verification" content="mx7oZncj-AnNjefq1rOLXEdtbrLbsrYP_vax-ZZWiOI" />`;
     }
   },
 };
