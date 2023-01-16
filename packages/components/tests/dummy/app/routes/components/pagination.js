f5516f04840c6af0898100a982f90bd1a2c0d68a
Cristiano Rastelli <public@didoo.net>
2023-01-16 14:40:11 +0000

    Updated documentation - Added “mock” data for demo of paginated table

--- packages/components/tests/dummy/app/routes/components/pagination.js
+++ packages/components/tests/dummy/app/routes/components/pagination.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
 
-export default class ComponentsPaginationRoute extends Route {}
+export default class ComponentsPaginationRoute extends Route {
+  async model() {
+    let response = await fetch('/api/mock-users.json');
+    let records = await response.json();
+    return { records };
+  }
+}
