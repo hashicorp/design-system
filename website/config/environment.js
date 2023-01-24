69879b5d91230e4a12e9704f842de17d637f67b0
Melanie Sumner <melanie@hashicorp.com>
2023-01-24 08:33:11 -0600

    Update website/config/environment.js

--- website/config/environment.js
+++ website/config/environment.js
@@ -42,7 +42,7 @@ module.exports = function (environment) {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
