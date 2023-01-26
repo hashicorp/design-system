884ffd0adfbf0e9dcc371dc0f0a4c0f6efc05ed3
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2023-01-26 15:59:32 -0600

    Apply suggestions from code review

--- website/config/environment.js
+++ website/config/environment.js
@@ -30,7 +30,7 @@ module.exports = function (environment) {
 
     'ember-meta': {
       description:
-        'The Helios Design System from HashiCorp provides the open source building blocks to design and implement consistent, accessible, and delightful product experiences',
+        'The Helios Design System from HashiCorp provides the open source building blocks to design and implement consistent, accessible, and delightful product experiences.',
       siteName: 'Helios Design System',
       title: 'Helios Design System',
       imgSrc: '/assets/logos/share-card.jpg',
