d2019f7637a223a92aa38532cf60102051b7f6a3
Melanie Sumner <melanie@hashicorp.com>
2023-02-06 11:35:54 -0600

    WIP
    Co-authored-by: Cristiano Rastelli <didoo@users.noreply.github.com>

--- packages/components/tests/dummy/app/controllers/components/table.js
+++ packages/components/tests/dummy/app/controllers/components/table.js
@@ -1,9 +1,31 @@
 import Controller from '@ember/controller';
 
 export default class ComponentsTableController extends Controller {
+  @tracked
   get customSort() {
-    return () => {
-      console.log(`a custom sort function was applied.`);
+    let myCustomDataArray = [
+      'critical',
+      'warning',
+      'success',
+      'highlight',
+      'neutral',
+    ];
+    return (a, b) => {
+      const aIndex = myCustomDataArray.indexOf(a['badge-color']);
+      const bIndex = myCustomDataArray.indexOf(b['badge-color']);
+      if (aIndex < bIndex) {
+        return -1;
+      } else if (aIndex > bIndex) {
+        return 1;
+      } else {
+        return 0;
+      }
+      console.log(
+        `a custom sort function was applied.`,
+        aIndex,
+        bIndex,
+        ...arguments
+      );
     };
   }
 }
