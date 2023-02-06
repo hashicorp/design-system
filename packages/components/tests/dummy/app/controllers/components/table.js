9db3d096800aa2e94a00e7174b478a35041f5358
Melanie Sumner <melanie@hashicorp.com>
2023-02-06 10:14:12 -0600

    update examples

--- packages/components/tests/dummy/app/controllers/components/table.js
+++ packages/components/tests/dummy/app/controllers/components/table.js
@@ -2,8 +2,8 @@ import Controller from '@ember/controller';
 
 export default class ComponentsTableController extends Controller {
   get customSort() {
-    return (sortBy, sortOrder) => {
-      console.log(`sortBy: ${sortBy}, sortOrder: ${sortOrder}`);
+    return () => {
+      console.log(`a custom sort function was applied.`);
     };
   }
 }
