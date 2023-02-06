bdcb15512ced5b397398c07e10aa37e96b178f23
Melanie Sumner <melanie@hashicorp.com>
2023-02-06 14:16:58 -0600

    more updates to be squashed later

--- packages/components/tests/dummy/app/controllers/components/table.js
+++ packages/components/tests/dummy/app/controllers/components/table.js
@@ -1,34 +1,9 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 
 export default class ComponentsTableController extends Controller {
-  @tracked mySortBy;
-  @tracked mySortOrder;
-
   get customSort() {
-    let myCustomDataArray = [
-      'critical',
-      'warning',
-      'success',
-      'highlight',
-      'neutral',
-    ];
-    return (a, b) => {
-      const aIndex = myCustomDataArray.indexOf(a['badge-color']);
-      const bIndex = myCustomDataArray.indexOf(b['badge-color']);
-      if (aIndex < bIndex) {
-        return -1;
-      } else if (aIndex > bIndex) {
-        return 1;
-      } else {
-        return 0;
-      }
-      console.log(
-        `a custom sort function was applied.`,
-        aIndex,
-        bIndex,
-        ...arguments
-      );
+    return () => {
+      console.log('custom sort');
     };
   }
 }
