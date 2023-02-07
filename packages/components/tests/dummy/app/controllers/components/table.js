c34e58b0cb0ffdea0e31ff42090443f00c47d739
Melanie Sumner <melanie@hashicorp.com>
2023-02-07 12:41:00 -0600

    yay it works

--- packages/components/tests/dummy/app/controllers/components/table.js
+++ packages/components/tests/dummy/app/controllers/components/table.js
@@ -7,8 +7,8 @@ export default class ComponentsTableController extends Controller {
   // queryParams = ['sortBy', 'sortOrder'];
 
   // these are already tracked in the component, do they also need to be tracked here? Will that cause problems if there are other sortable tables?
-  // @tracked sortBy = 'color';
-  // @tracked sortOrder = 'asc';
+  @tracked sortBy = 'color'; // this doesn't work, what should this value be?
+  @tracked sortOrder = 'asc';
 
   get customSortMethod() {
     let myCustomDataArray = [
@@ -19,22 +19,22 @@ export default class ComponentsTableController extends Controller {
       'neutral',
     ];
 
-    const sortFn = (a, b) => {
+    return (a, b) => {
       const aIndex = myCustomDataArray.indexOf(a['color']);
       const bIndex = myCustomDataArray.indexOf(b['color']);
       if (aIndex < bIndex) {
-        return -1;
+        return this.sortOrder === 'asc' ? -1 : 1;
       } else if (aIndex > bIndex) {
-        return 1;
+        return this.sortOrder === 'asc' ? 1 : -1;
       } else {
         return 0;
       }
     };
-    return myCustomDataArray.sort(sortFn);
   }
 
   @action
   customOnSort(sortBy, sortOrder) {
+    console.log('customOnSort', sortBy, sortOrder);
     this.sortBy = sortBy;
     this.sortOrder = sortOrder;
   }
