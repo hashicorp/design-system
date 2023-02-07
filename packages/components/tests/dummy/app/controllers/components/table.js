71bc0caf1ccf66b6ca643042bc8d8df1a2577717
Melanie Sumner <melanie@hashicorp.com>
2023-02-07 10:53:57 -0600

    updating example in controller

--- packages/components/tests/dummy/app/controllers/components/table.js
+++ packages/components/tests/dummy/app/controllers/components/table.js
@@ -1,9 +1,36 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ComponentsTableController extends Controller {
+  @tracked sortBy = '';
+  @tracked sortOrder = '';
+
   get customSortMethod() {
-    return () => {
-      console.log('custom sort');
+    let myCustomDataArray = [
+      'critical',
+      'warning',
+      'success',
+      'highlight',
+      'neutral',
+    ];
+
+    return (a, b) => {
+      const aIndex = myCustomDataArray.indexOf(a['badge-color']);
+      const bIndex = myCustomDataArray.indexOf(b['badge-color']);
+      if (aIndex < bIndex) {
+        return -1;
+      } else if (aIndex > bIndex) {
+        return 1;
+      } else {
+        return 0;
+      }
     };
+  }
+
+  @action
+  customOnSort(sortBy, sortOrder) {
+    this.sortBy = sortBy;
+    this.sortOrder = sortOrder;
   }
 }
