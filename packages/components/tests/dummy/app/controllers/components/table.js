a291fc1f8b795f9e0ccf6b99b7625dc04e4e8c31
Cristiano Rastelli <public@didoo.net>
2023-02-08 15:25:14 +0000

    added example in the showcase of table with custom sorting without the use of `@model` automatic sorting

--- packages/components/tests/dummy/app/controllers/components/table.js
+++ packages/components/tests/dummy/app/controllers/components/table.js
@@ -3,16 +3,10 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ComponentsTableController extends Controller {
-  // TODO think about query param support;
-  // how do you reset query params??
   queryParams = ['sortBy', 'sortOrder'];
 
-  // Will this cause problems if there are other sortable tables?
-  @tracked sortBy = 'color';
-  @tracked sortOrder = 'asc';
-
-  // @tracked sortBy;
-  // @tracked sortOrder;
+  @tracked sortBy;
+  @tracked sortOrder;
 
   get customSortMethod() {
     if (this.sortBy === 'color') {
@@ -42,8 +36,13 @@ export default class ComponentsTableController extends Controller {
 
   @action
   customOnSort(sortBy, sortOrder) {
-    // console.log('customOnSort', sortBy, sortOrder);
     this.sortBy = sortBy;
     this.sortOrder = sortOrder;
+  }
+
+  @action
+  onClickThCustomSort(column) {
+    this.sortBy = column;
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
   }
 }
