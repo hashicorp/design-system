11bf885188da39556ec59c4e38aabe2667aba926
Cristiano Rastelli <public@didoo.net>
2023-02-13 15:32:22 +0000

    refactored data retrieval for `Table` component so that I can add a new type of data structure to the model

--- packages/components/tests/dummy/app/routes/components/table.js
+++ packages/components/tests/dummy/app/routes/components/table.js
@@ -1,22 +1,15 @@
 import Route from '@ember/routing/route';
-import { capitalize } from '@ember/string';
+
+const STATES = ['active', 'default', 'hover', 'focus'];
 
 export default class ComponentsTableRoute extends Route {
   async model() {
-    let response = await fetch('/api/folk.json');
-    let { data } = await response.json();
-    const STATES = ['active', 'default', 'hover', 'focus'];
+    let responseMusic = await fetch('/api/folk.json');
+    let { data: music } = await responseMusic.json();
 
-    // make sure the variable is declared outside of the loop
-    // so we can return it in the model response
-    let dataResponse = data.map((record) => {
-      let { id, attributes } = record;
-      return { id, ...attributes };
-    });
-    const keys = Object.keys(data[0].attributes);
-    const columns = keys.map((key) => {
-      return { key, label: capitalize(key) };
-    });
-    return { data: dataResponse, columns, STATES };
+    return {
+      music: music.map((record) => ({ id: record.id, ...record.attributes })),
+      STATES,
+    };
   }
 }
