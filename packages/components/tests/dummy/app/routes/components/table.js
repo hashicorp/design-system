d184ba767e62745027ce38af054919acb4594f5d
Cristiano Rastelli <public@didoo.net>
2023-02-13 15:41:15 +0000

    added a “Clusters” type of data structure to the `Table` model

--- packages/components/tests/dummy/app/routes/components/table.js
+++ packages/components/tests/dummy/app/routes/components/table.js
@@ -5,10 +5,13 @@ const STATES = ['active', 'default', 'hover', 'focus'];
 export default class ComponentsTableRoute extends Route {
   async model() {
     let responseMusic = await fetch('/api/folk.json');
+    let responseClusters = await fetch('/api/mock-clusters-with-status.json');
     let { data: music } = await responseMusic.json();
+    let clusters = await responseClusters.json();
 
     return {
       music: music.map((record) => ({ id: record.id, ...record.attributes })),
+      clusters,
       STATES,
     };
   }
