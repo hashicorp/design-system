41341598f55650b7ef22a0ad481e3bdd452cc67e
Jory Tindall <jory.tindall@hashicorp.com>
2023-02-21 14:28:28 -0800

    Added controller for pattern cards

---
+++ website/app/controllers/patterns.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Controller from '@ember/controller';
+
+export default class PatternsController extends Controller {
+  get cards() {
+    // we want to use a flat tree here...
+    const tocTree = this.model.toc.flat;
+    const sections = ['patterns'];
+    const cards = {};
+    sections.forEach((section) => {
+      cards[section] = tocTree
+        .filter(
+          (page) =>
+            page.pageParents[0] === section &&
+            !page.pageAttributes?.navigation?.hidden
+        )
+        .map((page) => {
+          return {
+            image: page.pageAttributes.previewImage,
+            title:
+              page.pageAttributes?.navigation?.label ||
+              page.pageAttributes.title,
+            caption: page.pageAttributes.caption,
+            route: 'show',
+            model: page.pageURL,
+          };
+        });
+    });
+    return cards;
+  }
+}
