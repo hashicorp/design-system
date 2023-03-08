c62191e76ed85ca1b82558ad2ddd06d3c5c3060b
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-03-07 16:49:57 -0800

    HDS-1143 Update BackLink tests

--- packages/components/tests/integration/components/hds/side-nav/back-link-test.js
+++ packages/components/tests/integration/components/hds/side-nav/back-link-test.js
@@ -34,4 +34,17 @@ module('Integration | Component | hds/side-nav/index', function (hooks) {
       .dom('.hds-side-nav__list-item-link--back-link')
       .hasAttribute('href', 'https://www.hashicorp.com/');
   });
+
+  // ATTRIBUTES
+
+  test('it should spread all the attributes passed to the component on the element', async function (assert) {
+    await render(
+      hbs`<Hds::SideNav::List::BackLink @text="Back to parent page" @href="https://www.hashicorp.com/" class="my-class" data-test1 data-test2="test" />`
+    );
+    assert
+      .dom('.hds-side-nav__list-item-link--back-link')
+      .hasAttribute('data-test1')
+      .hasAttribute('data-test1')
+      .hasAttribute('data-test2', 'test');
+  });
 });
