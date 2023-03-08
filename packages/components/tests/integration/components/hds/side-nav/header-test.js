3f90830c6e5af9ae03a4a02a0330db282dea115c
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-03-08 08:24:27 -0800

    HDS-1143 Spread attributes on header, add tests

---
+++ packages/components/tests/integration/components/hds/side-nav/header-test.js
@@ -0,0 +1,62 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | hds/side-nav/index', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // Basic
+
+  test('it renders the component', async function (assert) {
+    await render(hbs`<Hds::SideNav::Header />`);
+    assert.dom(this.element).exists();
+  });
+
+  test('it should render with a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::SideNav::Header />`);
+    assert.dom('.hds-side-nav-header').exists();
+  });
+
+  // Test Content / Args
+
+  test('it renders passed in content', async function (assert) {
+    await render(hbs`
+      <Hds::SideNav::Header>
+        <:logo>
+          <Hds::SideNav::HomeLink @icon="hashicorp" @text="HashiCorp" @href="#" />
+        </:logo>
+        <:actions>
+          <Hds::Dropdown class="hds-side-nav__dropdown" as |dd|>
+            <dd.ToggleIcon @icon="help" @text="settings menu" />
+            <dd.Title @text="Signed In" />
+            <dd.Description @text="email@domain.com" />
+            <dd.Separator />
+            <dd.Interactive @href="#" @text="Settings and Preferences" />
+          </Hds::Dropdown>
+          <Hds::Dropdown class="hds-side-nav__dropdown" as |dd|>
+            <dd.ToggleIcon @icon="user" @text="user menu" />
+            <dd.Title @text="Signed In" />
+            <dd.Description @text="email@domain.com" />
+            <dd.Interactive @href="#" @text="Account Settings" />
+          </Hds::Dropdown>
+        </:actions>
+      </Hds::SideNav::Header>
+    `);
+    assert.dom('.hds-side-nav-header__logo').exists();
+    assert.dom('.hds-disclosure').exists({ count: 2 });
+  });
+
+  // ATTRIBUTES
+
+  test('it should spread all the attributes passed to the component on the element', async function (assert) {
+    await render(
+      hbs`<Hds::SideNav::Header class="my-class" data-test1 data-test2="test" />`
+    );
+    assert
+      .dom('.hds-side-nav-header')
+      .hasAttribute('data-test1')
+      .hasAttribute('data-test1')
+      .hasAttribute('data-test2', 'test');
+  });
+});
