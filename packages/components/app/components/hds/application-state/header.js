3850aae29dd9839402784ddbf288052f28f24c00
Melanie Sumner <melanie@hashicorp.com>
2023-03-20 15:19:58 -0500

    Added missing copyright comments

--- packages/components/app/components/hds/application-state/header.js
+++ packages/components/app/components/hds/application-state/header.js
@@ -1,1 +1,6 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 export { default } from '@hashicorp/design-system-components/components/hds/application-state/header';
