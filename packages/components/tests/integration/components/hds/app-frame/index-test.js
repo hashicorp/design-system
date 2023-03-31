2362f2b97af226ffb03852a26dc60e651c825742
Cristiano Rastelli <public@didoo.net>
2023-03-31 11:42:17 +0100

    linting

--- packages/components/tests/integration/components/hds/app-frame/index-test.js
+++ packages/components/tests/integration/components/hds/app-frame/index-test.js
@@ -5,7 +5,7 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, pauseTest } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | hds/app-frame/index', function (hooks) {
