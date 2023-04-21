da641a57087d185b8eaf256b808ca2dfa5458a30
Alex <alex-ju@users.noreply.github.com>
2023-04-21 16:31:47 +0100

    Add `Hds::SegmentedGroup` component

---
+++ packages/components/tests/integration/components/hds/segmented-group/index-test.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | hds/segmented-group/index', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::SegmentedGroup id="test-segmented-group" />`);
+    assert.dom('#test-segmented-group').hasClass('hds-segmented-group');
+  });
+
+  // CONTEXTUAL COMPONENTS
+
+  test('it renders the contextual components with CSS modifier classes', async function (assert) {
+    await render(
+      hbs`<Hds::SegmentedGroup as |S|>
+            <S.Button id="segmented-button" @color="secondary" @text="Button" />
+            <S.Dropdown id="segmented-dropdown" as |DD|>
+              <DD.ToggleButton @color="secondary" @text="Toggle" />
+              <DD.Interactive @href="#" @text="Dropdown Item" />
+            </S.Dropdown>
+            <S.Select id="segmented-select"/>
+            <S.TextInput id="segmented-input" />
+          </Hds::SegmentedGroup>`
+    );
+    assert.dom('#segmented-button').hasClass('hds-button--segmented');
+    assert.dom('#segmented-dropdown').hasClass('hds-dropdown--segmented');
+    assert.dom('#segmented-select').hasClass('hds-form-select--segmented');
+    assert.dom('#segmented-input').hasClass('hds-form-text-input--segmented');
+  });
+});
