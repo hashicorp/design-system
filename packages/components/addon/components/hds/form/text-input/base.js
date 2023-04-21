5ce7e2a50b5351bfb1cde8167eef4f3501db7439
Alex <alex-ju@users.noreply.github.com>
2023-04-21 16:30:33 +0100

    Allow `contextualClass` on segments
    Allow `contextualClass` on Button, Dropdown, Select, TextInput to be able to pass in modifier classes

--- packages/components/addon/components/hds/form/text-input/base.js
+++ packages/components/addon/components/hds/form/text-input/base.js
@@ -56,6 +56,13 @@ export default class HdsFormTextInputBaseComponent extends Component {
       classes.push(`hds-form-text-input--is-invalid`);
     }
 
+    // add a class based on the @contextualClass argument
+    // notice: this will *not* be documented for public use
+    // the reason for this is that the contextual component declarations don't pass attributes to the component
+    if (this.args.contextualClass) {
+      classes.push(this.args.contextualClass);
+    }
+
     return classes.join(' ');
   }
 }
