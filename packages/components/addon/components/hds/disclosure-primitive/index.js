21d7eae3dddf7c5ccde46ee6b202bc56d90fa11a
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-05-23 11:00:57 -0700

    HDS-1932 Add back mistakenly deleted logic

--- packages/components/addon/components/hds/disclosure-primitive/index.js
+++ packages/components/addon/components/hds/disclosure-primitive/index.js
@@ -12,6 +12,11 @@ export default class HdsDisclosurePrimitiveComponent extends Component {
   @tracked isOpen; // notice: if in the future we need to add a "@isOpen" prop to control the status from outside (eg to have the DisclosurePrimitive opened on render) just add  "this.args.isOpen" here to initalize the variable
 
   @action
+  onClickToggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  @action
   close() {
     // we schedule this afterRender to avoid an error in tests caused by updating `isOpen` multiple times in the same computation
     schedule('afterRender', () => {
