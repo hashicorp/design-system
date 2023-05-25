89917ac5ec0bfa16beedcb1295a5223db8ac8603
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-05-25 15:22:23 -0700

    HDS-1911 create preliminary Reveal component and showcase

---
+++ packages/components/addon/components/hds/reveal/toggle/button.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+
+const NOOP = () => {};
+
+export default class HdsDropdownToggleButtonComponent extends Component {
+  /**
+   * @param text
+   * @type {string}
+   * @description The text of the button. If no text value is defined an error will be thrown.
+   */
+  get text() {
+    let { text } = this.args;
+
+    assert(
+      '@text for "Hds::Dropdown::Toggle::Button" must have a valid value',
+      text !== undefined
+    );
+
+    return text;
+  }
+
+  /**
+   * @param onClick
+   * @type {function}
+   * @default () => {}
+   */
+  get onClick() {
+    let { onClick } = this.args;
+
+    // notice: this is a guard used in case the toggle is used as standalone element (eg. in the showcase)
+    // in reality it's always used inside the Reveal main component as yielded component, so the onClick handler is always defined
+    if (typeof onClick === 'function') {
+      return onClick;
+    } else {
+      return NOOP;
+    }
+  }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method ToggleButton#classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-reveal__button'];
+
+    // add a class based on the @isOpen argument
+    if (this.args.isOpen) {
+      classes.push('hds-reveal__button--is-open');
+    }
+
+    return classes.join(' ');
+  }
+}
