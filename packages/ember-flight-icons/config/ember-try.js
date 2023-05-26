542cfd21453a874a9427fd1cc6b110c8c656379a
Alex <alex-ju@users.noreply.github.com>
2023-05-26 12:25:27 +0100

    Upgrade Ember to 4.12 in `components`
    - ember-cli-update
    - shifted node version as 12 is end of life
    
    Update package.json
    
    Update package.json
    
    fix

--- packages/ember-flight-icons/config/ember-try.js
+++ packages/ember-flight-icons/config/ember-try.js
@@ -19,18 +19,18 @@ module.exports = async function () {
     },
     scenarios: [
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
           },
         },
       },
@@ -55,24 +55,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-          ember: {
-            edition: 'classic',
           },
         },
       },
