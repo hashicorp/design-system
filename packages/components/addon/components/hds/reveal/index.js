8df2f412215c56f64ef9ba09784c2e6c45cd981e
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-05-30 11:38:22 -0700

    HDS-1911 Add aria-controls and id

--- packages/components/addon/components/hds/reveal/index.js
+++ packages/components/addon/components/hds/reveal/index.js
@@ -4,8 +4,16 @@
  */
 
 import Component from '@glimmer/component';
+import { guidFor } from '@ember/object/internals';
 
 export default class HdsRevealIndexComponent extends Component {
+  /**
+   * Generates a unique ID for the Content
+   *
+   * @param contentId
+   */
+  contentId = 'content-' + guidFor(this);
+
   /**
    * Get the class names to apply to the component.
    * @method classNames
