1db1eb7c83316c3e08d9387339f714565e5f04be
Alex <alex-ju@users.noreply.github.com>
2023-06-13 12:08:14 +0100

    Update ember test matrix
     - add latest LTS (currently 4.8)
     - remove ember-release (current version in the repo that we already test with)

--- packages/components/config/ember-try.js
+++ packages/components/config/ember-try.js
@@ -19,18 +19,18 @@ module.exports = async function () {
     },
     scenarios: [
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
           },
         },
       },
