d16b5f5197b44cd727d1946f60b0a914ac5d699f
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-06-16 15:30:34 -0700

    HDS-2034 Add styling and examples for states

--- packages/components/tests/dummy/app/routes/components/accordion.js
+++ packages/components/tests/dummy/app/routes/components/accordion.js
@@ -5,4 +5,10 @@
 
 import Route from '@ember/routing/route';
 
-export default class ComponentsAccordionRoute extends Route {}
+export default class ComponentsAccordionRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    const STATES = ['default', 'hover', 'focus'];
+    return { STATES };
+  }
+}
