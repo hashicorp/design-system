80f9123a726856eec77fe89d48a48abca6a0f3da
Melanie Sumner <melanie@hashicorp.com>
2023-07-05 15:18:04 -0500

    tidied up the interactive component tests for a11yAudit

--- packages/components/tests/integration/components/hds/interactive/index-test.js
+++ packages/components/tests/integration/components/hds/interactive/index-test.js
@@ -14,24 +14,28 @@ module('Integration | Component | hds/interactive/index', function (hooks) {
   // notice: since this element can generate different HTML element, to make the tests even more solid, in the DOM selectors we prefix the #ID of the element with the tag name
 
   test('it renders the interactive container', async function (assert) {
-    await render(hbs`<Hds::Interactive />`);
+    await render(hbs`<Hds::Interactive>Interactive text</Hds::Interactive>`);
     assert.dom(this.element).exists();
   });
 
   // GENERATED ELEMENTS
 
   test('it should render a <button> if no @href or @route is passed (default)', async function (assert) {
-    await render(hbs`<Hds::Interactive id="test-interactive" />`);
+    await render(
+      hbs`<Hds::Interactive id="test-interactive">Interactive text</Hds::Interactive>`
+    );
     assert.dom('#test-interactive').hasTagName('button');
   });
   test('it should render a <a> link if @href is passed', async function (assert) {
-    await render(hbs`<Hds::Interactive @href="#" id="test-interactive" />`);
+    await render(
+      hbs`<Hds::Interactive @href="#" id="test-interactive">Interactive text</Hds::Interactive>`
+    );
     assert.dom('#test-interactive').hasTagName('a');
     assert.dom('#test-interactive').hasAttribute('href', '#');
   });
   test('it should render a <a> link if @route is passed', async function (assert) {
     await render(
-      hbs`<Hds::Interactive @route="utilities.interactive" id="test-interactive" />`
+      hbs`<Hds::Interactive @route="utilities.interactive" id="test-interactive">Link text</Hds::Interactive>`
     );
     assert.dom('#test-interactive').hasTagName('a');
     assert
@@ -42,20 +46,22 @@ module('Integration | Component | hds/interactive/index', function (hooks) {
   // TARGET/REL ATTRIBUTES
 
   test('it should render a <a> link with the right "target" and "rel" attributes if @href is passed', async function (assert) {
-    await render(hbs`<Hds::Interactive @href="#" id="test-interactive" />`);
+    await render(
+      hbs`<Hds::Interactive @href="#" id="test-interactive">Link text</Hds::Interactive>`
+    );
     assert.dom('#test-interactive').hasAttribute('target', '_blank');
     assert.dom('#test-interactive').hasAttribute('rel', 'noopener noreferrer');
   });
   test('it should render a <a> link with custom "target" and "rel" attributes if they are passed as attributes', async function (assert) {
     await render(
-      hbs`<Hds::Interactive @href="#" id="test-interactive" target="test-target" rel="test-rel" />`
+      hbs`<Hds::Interactive @href="#" id="test-interactive" target="test-target" rel="test-rel">Interactive text</Hds::Interactive>`
     );
     assert.dom('#test-interactive').hasAttribute('target', 'test-target');
     assert.dom('#test-interactive').hasAttribute('rel', 'test-rel');
   });
-  test('it should render a <a> link withhout "target" and "rel" attributes if @isHrefExternal is false', async function (assert) {
+  test('it should render a <a> link without "target" and "rel" attributes if @isHrefExternal is false', async function (assert) {
     await render(
-      hbs`<Hds::Interactive @href="#" @isHrefExternal={{false}} id="test-interactive" />`
+      hbs`<Hds::Interactive @href="#" @isHrefExternal={{false}} id="test-interactive">Interactive text</Hds::Interactive>`
     );
     assert.dom('#test-interactive').doesNotHaveAttribute('target');
     assert.dom('#test-interactive').doesNotHaveAttribute('rel');
@@ -65,7 +71,7 @@ module('Integration | Component | hds/interactive/index', function (hooks) {
 
   test('it should spread all the attributes passed to the <button> element', async function (assert) {
     await render(
-      hbs`<Hds::Interactive id="test-interactive" class="my-class" data-test1 data-test2="test" />`
+      hbs`<Hds::Interactive id="test-interactive" class="my-class" data-test1 data-test2="test">Interactive text</Hds::Interactive>`
     );
     assert.dom('button#test-interactive').hasClass('my-class');
     assert.dom('button#test-interactive').hasAttribute('data-test1');
@@ -73,7 +79,7 @@ module('Integration | Component | hds/interactive/index', function (hooks) {
   });
   test('it should spread all the attributes passed to the <a> element', async function (assert) {
     await render(
-      hbs`<Hds::Interactive @href="#" id="test-interactive" class="my-class" data-test1 data-test2="test" />`
+      hbs`<Hds::Interactive @href="#" id="test-interactive" class="my-class" data-test1 data-test2="test">Interactive text</Hds::Interactive>`
     );
     assert.dom('a#test-interactive').hasClass('my-class');
     assert.dom('a#test-interactive').hasAttribute('data-test1');
@@ -81,7 +87,7 @@ module('Integration | Component | hds/interactive/index', function (hooks) {
   });
   test('it should spread all the attributes passed to the <LinkTo> element', async function (assert) {
     await render(
-      hbs`<Hds::Interactive @route="index" id="test-interactive" class="my-class" data-test1 data-test2="test" />`
+      hbs`<Hds::Interactive @route="index" id="test-interactive" class="my-class" data-test1 data-test2="test">Interactive text</Hds::Interactive>`
     );
     assert.dom('a#test-interactive').hasClass('my-class');
     assert.dom('a#test-interactive').hasAttribute('data-test1');
@@ -92,41 +98,46 @@ module('Integration | Component | hds/interactive/index', function (hooks) {
 
   test('it should yield the children of the <button> element', async function (assert) {
     await render(
-      hbs`<Hds::Interactive id="test-interactive"><pre>test</pre></Hds::Interactive>`
+      hbs`<Hds::Interactive id="test-interactive"><span>Interactive text</span></Hds::Interactive>`
     );
-    assert.dom('button#test-interactive > pre').exists();
-    assert.dom('button#test-interactive > pre').hasText('test');
+    assert.dom('button#test-interactive > span').exists();
+    assert.dom('button#test-interactive > span').hasText('Interactive text');
   });
   test('it should yield the children of the <a> element', async function (assert) {
     await render(
-      hbs`<Hds::Interactive @href="#" id="test-interactive"><pre>test</pre></Hds::Interactive>`
+      hbs`<Hds::Interactive @href="#" id="test-interactive"><span>Interactive text</span></Hds::Interactive>`
     );
-    assert.dom('a#test-interactive > pre').exists();
-    assert.dom('a#test-interactive > pre').hasText('test');
+    assert.dom('a#test-interactive > span').exists();
+    assert.dom('a#test-interactive > span').hasText('Interactive text');
   });
   test('it should yield the children of the <LinkTo> element', async function (assert) {
     await render(
-      hbs`<Hds::Interactive @route="index" id="test-interactive"><pre>test</pre></Hds::Interactive>`
+      hbs`<Hds::Interactive @route="index" id="test-interactive"><span>Interactive text</span></Hds::Interactive>`
     );
-    assert.dom('a#test-interactive > pre').exists();
-    assert.dom('a#test-interactive > pre').hasText('test');
+    assert.dom('a#test-interactive > span').exists();
+    assert.dom('a#test-interactive > span').hasText('Interactive text');
   });
 
   // A11Y
 
   test('it should render with the correct button "type" by default', async function (assert) {
-    await render(hbs`<Hds::Interactive id="test-interactive" />`);
+    await render(
+      hbs`<Hds::Interactive id="test-interactive">Interactive text</Hds::Interactive>`
+    );
     assert.dom('button#test-interactive').hasAttribute('type', 'button');
   });
-  test('it should have a custom type if @type is set', async function (assert) {
-    await render(hbs`<Hds::Interactive id="test-interactive" type="submit" />`);
+  test('it should have a custom type if type attribute is set', async function (assert) {
+    await render(
+      hbs`<Hds::Interactive id="test-interactive" type="submit" >Interactive text</Hds::Interactive>`
+    );
     assert.dom('button#test-interactive').hasAttribute('type', 'submit');
   });
+
   test('it should dispatch a click event when pressing space key on a link', async function (assert) {
     let clicked = false;
     this.set('clickHandler', () => (clicked = true));
     await render(
-      hbs`<div {{on "click" this.clickHandler}}><Hds::Interactive @href="javascript:;" id="test-interactive"/></div>`
+      hbs`<div {{on "click" this.clickHandler}}><Hds::Interactive @href="javascript:;" id="test-interactive">Interactive text</Hds::Interactive></div>`
     );
     await triggerKeyEvent('#test-interactive', 'keyup', ' ');
     assert.ok(clicked);
