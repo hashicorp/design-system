f1bf3c73fd285934bf6de2c38f1c4c1e30c044b7
Cristiano Rastelli <public@didoo.net>
2023-07-07 13:45:21 +0100

    spike styling `input[type=file]`

---
+++ packages/components/addon/components/hds/form/file-input/index.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+export default class HdsFormFileInputIndexComponent extends Component {
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-form-file-input'];
+
+    // add a class based on the @extraClass argument
+    // classes.push(`hds-form-file-input--variant-${this.args.extraClass}`);
+
+    return classes.join(' ');
+  }
+}
