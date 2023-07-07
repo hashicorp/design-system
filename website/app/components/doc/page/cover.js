6bde524441cad2ad4763a68b81817b8cc8b78e19
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-07-07 13:37:50 -0700

    HDS-1357 Undelete file

---
+++ website/app/components/doc/page/cover.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+const STATUSES = {
+  'on-roadmap': {
+    badgeType: 'neutral',
+    badgeText: 'On roadmap',
+  },
+  released: {
+    badgeType: 'success',
+    badgeText: 'Released',
+  },
+  experimental: {
+    badgeType: 'warning',
+    badgeText: 'Experimental',
+  },
+  'code-only': {
+    badgeType: 'information',
+    badgeText: 'Code only',
+  },
+  'design-only': {
+    badgeType: 'information',
+    badgeText: 'Design only',
+  },
+  deprecated: {
+    badgeType: 'critical',
+    badgeText: 'Deprecated',
+  },
+};
+
+export default class DocPageContentComponent extends Component {
+  get status() {
+    let { extra } = this.args;
+
+    if (extra?.status) {
+      if (extra.status in STATUSES) {
+        return STATUSES[extra.status];
+      } else {
+        console.error(
+          `Error: you have provided an unexpected status for the page cover element (${
+            extra.status
+          }) - Possible values: "${Object.keys(STATUSES).join('", "')}".`
+        );
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  get links() {
+    let { extra } = this.args;
+
+    if (extra?.links) {
+      return extra?.links;
+    } else {
+      return false;
+    }
+  }
+}
