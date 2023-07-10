37552603a52fbb311f1c68ae43631b088d1ae736
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-07-10 14:18:25 -0700

    HDS-1757 Create new 'text' child component and apply conditional class names, delete css custom properties and apply as class names instead

---
+++ packages/components/addon/components/hds/alert/text.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+export default class HdsAlertTextComponent extends Component {
+  /**
+   * Get the class names to apply to the component.
+   * @method Alert#classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-alert__text'];
+
+    // Add typography classes based on @type argument
+    if (this.args.type === 'compact') {
+      classes.push('hds-typography-body-100');
+    } else {
+      classes.push('hds-typography-body-200');
+    }
+
+    return classes.join(' ');
+  }
+}
