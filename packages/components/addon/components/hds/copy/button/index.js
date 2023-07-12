90aa57aa3bf8aecee74be39f3ca51897b5203a83
Melanie Sumner <melanie@hashicorp.com>
2023-07-12 11:28:14 -0500

    initial update to use hds-button inside copy-button

--- packages/components/addon/components/hds/copy/button/index.js
+++ packages/components/addon/components/hds/copy/button/index.js
@@ -9,29 +9,11 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export const DEFAULT_SIZE = 'medium';
-export const DEFAULT_COLOR = 'secondary';
 export const SIZES = ['small', 'medium'];
-export const COLORS = ['secondary'];
 
 export default class HdsCopyButtonComponent extends Component {
   @tracked isSuccess = false;
   @tracked isError = false;
-
-  /**
-   * @param text
-   * @type {string}
-   * @description The text to be used for the accessible name of the button. If no text value is defined an error will be thrown
-   */
-  get text() {
-    let { text } = this.args;
-
-    assert(
-      '@text for "Hds::Copy::Button" must have a valid value',
-      text !== undefined
-    );
-
-    return text;
-  }
 
   /**
    * @param clipboardText
@@ -63,45 +45,6 @@ export default class HdsCopyButtonComponent extends Component {
   }
 
   /**
-   * @param color
-   * @type {string}
-   * @default secondary
-   * @description Determines the color of copy/button to be used; the only acceptable value is `secondary`
-   */
-  get color() {
-    let { color = DEFAULT_COLOR } = this.args;
-
-    assert(
-      `@color for "Hds::Copy::Button" must be one of the following: ${COLORS.join(
-        ', '
-      )}; received: ${color}`,
-      COLORS.includes(color)
-    );
-
-    return color;
-  }
-
-  /**
-   * @param isIconOnly
-   * @type {boolean}
-   * @default false
-   * @description Indicates if the button will only contain an icon; component will also ensure that accessible text is still applied to the component.
-   */
-  get isIconOnly() {
-    return this.args.isIconOnly ?? false;
-  }
-
-  /**
-   * @param isFullWidth
-   * @type {boolean}
-   * @default false
-   * @description Indicates that a button should take up the full width of the parent container. The default is false.
-   */
-  get isFullWidth() {
-    return this.args.isFullWidth ?? false;
-  }
-
-  /**
    * Get the class names to apply to the component.
    * @method classNames
    * @return {string} The "class" attribute to apply to the component.
@@ -111,14 +54,6 @@ export default class HdsCopyButtonComponent extends Component {
 
     // add a class based on the @size argument
     classes.push(`hds-copy-button--size-${this.size}`);
-
-    // add a class based on the @color argument
-    classes.push(`hds-copy-button--color-${this.color}`);
-
-    // add a class based on the @isFullWidth argument
-    if (this.isFullWidth) {
-      classes.push('hds-copy-button--width-full');
-    }
 
     return classes.join(' ');
   }
--- packages/components/tests/dummy/app/routes/components/copy/button.js
+++ packages/components/tests/dummy/app/routes/components/copy/button.js
@@ -4,15 +4,12 @@
  */
 
 import Route from '@ember/routing/route';
-import {
-  SIZES,
-  COLORS,
-} from '@hashicorp/design-system-components/components/hds/copy/button';
+import { SIZES } from '@hashicorp/design-system-components/components/hds/copy/button';
 
 export default class ComponentsCopyButtonRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
     const STATES = ['default', 'hover', 'active', 'focus', 'success'];
-    return { SIZES, COLORS, STATES };
+    return { SIZES, STATES };
   }
 }
