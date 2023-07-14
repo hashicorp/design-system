6e292a42d37d5368dbaa93245f44e9a48c21c3db
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-07-14 13:58:18 -0700

    HDS-2246 Delete JS added to docs for removed Showcase

--- website/docs/components/form/radio-card/index.js
+++ website/docs/components/form/radio-card/index.js
@@ -6,11 +6,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 export default class Index extends Component {
-  get STATES() {
-    // these are used only for presentation purpose in the showcase
-    return ['default', 'hover', 'focus', 'disabled'];
-  }
-
   get RADIOCARDS() {
     return [
       {
