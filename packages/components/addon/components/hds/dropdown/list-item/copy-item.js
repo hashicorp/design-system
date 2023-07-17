e9b4b3a128b23ddb6018b0953e43e61d3a052896
Melanie Sumner <melanie@hashicorp.com>
2023-07-17 16:37:47 -0500

    Puts copy/snippet into copy-item

--- packages/components/addon/components/hds/dropdown/list-item/copy-item.js
+++ packages/components/addon/components/hds/dropdown/list-item/copy-item.js
@@ -4,29 +4,11 @@
  */
 
 import Component from '@glimmer/component';
-import { assert } from '@ember/debug';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
 export default class HdsDropdownListItemCopyItemComponent extends Component {
-  @tracked isSuccess = false;
-
-  /**
-   * @param text
-   * @type {string}
-   * @description The text of the item. If no text value is defined an error will be thrown
-   */
   get text() {
-    let { text } = this.args;
-
-    assert(
-      '@text for "Hds::Dropdown::ListItem::CopyItem" must have a valid value',
-      text !== undefined
-    );
-
-    return text;
+    return this.args.text;
   }
-
   /**
    * Get the class names to apply to the component.
    * @method classNames
@@ -39,30 +21,5 @@ export default class HdsDropdownListItemCopyItemComponent extends Component {
     ];
 
     return classes.join(' ');
-  }
-
-  @action
-  async copyCode() {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard
-    await navigator.clipboard.writeText(this.args.text);
-
-    if (navigator.clipboard.readText) {
-      const result = await navigator.clipboard.readText();
-
-      if (result === this.args.text) {
-        this.isSuccess = true;
-      }
-    } else {
-      // assume that it works so Firefox can show the success state
-      // doesn't confirm that you'll get the correct pasted text
-      // but we accept this as a reasonable tradeoff
-      // since users can always copy/paste manually.
-      this.isSuccess = true;
-    }
-
-    // make it fade back to the default state
-    setTimeout(() => {
-      this.isSuccess = false;
-    }, 1000);
   }
 }
