09d3f2845e3efa09f407ae7bcd716bc4b36924e2
Melanie Sumner <melanie@hashicorp.com>
2023-07-17 12:25:15 -0500

    initial add of tests

--- packages/components/tests/integration/components/hds/copy/snippet/index-test.js
+++ packages/components/tests/integration/components/hds/copy/snippet/index-test.js
@@ -11,10 +11,19 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | hds/copy/snippet/index', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+  test('it should render the default component variation with a CSS class that matches the component name', async function (assert) {
     await render(
-      hbs`<Hds::Copy::Snippet id="test-copy-snippet" @text="3423g-234525-h345346-f34rtf4" />`
+      hbs`<Hds::Copy::Snippet id="test-copy-snippet" @textToCopy="3423g-234525-h345346-f34rtf4" />`
     );
     assert.dom('#test-copy-snippet').hasClass('hds-copy-snippet');
+  });
+
+  test('it should render the secondary color if defined', async function (assert) {
+    await render(
+      hbs`<Hds::Copy::Snippet id="test-copy-snippet" @textToCopy="3423g-234525-h345346-f34rtf4" @color="secondary" />`
+    );
+    assert
+      .dom('#test-copy-snippet')
+      .hasClass('hds-copy-snippet--color-secondary');
   });
 });
