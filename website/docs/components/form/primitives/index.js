67d6384b09e36ba2a62d419879aab7428c5996bf
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-07-17 08:55:48 -0700

    HDS-2246 Delete unused variable

--- website/docs/components/form/primitives/index.js
+++ website/docs/components/form/primitives/index.js
@@ -4,7 +4,6 @@
  */
 
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class Index extends Component {
