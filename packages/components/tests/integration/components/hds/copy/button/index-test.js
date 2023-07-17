7fb9294e38907fe473b4e79e59931b745e891de6
Melanie Sumner <melanie@hashicorp.com>
2023-07-17 12:25:15 -0500

    initial add of tests

--- packages/components/tests/integration/components/hds/copy/button/index-test.js
+++ packages/components/tests/integration/components/hds/copy/button/index-test.js
@@ -11,10 +11,44 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | hds/copy/button/index', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+  test('it should render the default component variation with a CSS class that matches the component name', async function (assert) {
     await render(
       hbs`<Hds::Copy::Button id="test-copy-button" @text="copy" @clipboardText="#clipboardText" />`
     );
     assert.dom('#test-copy-button').hasClass('hds-copy-button');
+  });
+
+  test('it should only render an icon and also render an aria-label if isIconOnly is set to true', async function (assert) {
+    await render(
+      hbs`<p id="clipboardTarget2">
+      The button will copy the text in this paragraph element.
+    </p>
+    <Hds::Copy::Button @text="Copy" @isIconOnly={{true}}
+    @targetToCopy="#clipboardTarget2" id="test-copy-button" />`
+    );
+    assert.dom('#test-copy-button').doesNotIncludeText('Copy');
+    assert.dom('#test-copy-button').hasAria('label', 'Copy');
+  });
+
+  test('it should render the medium size if no size is declared', async function (assert) {
+    await render(
+      hbs`<Hds::Copy::Button id="test-copy-button" @text="copy" @clipboardText="#clipboardText" />`
+    );
+    assert.dom('#test-copy-button').hasClass('hds-button--size-medium');
+  });
+
+  test('it should render the small size if @size small is defined', async function (assert) {
+    await render(
+      hbs`<Hds::Copy::Button id="test-copy-button" @text="copy" @clipboardText="#clipboardText" @size="small" />`
+    );
+    assert.dom('#test-copy-button').hasClass('hds-button--size-small');
+  });
+
+  test('it render the text value even if textToCopy is defined', async function (assert) {
+    await render(
+      hbs`<Hds::Copy::Button id="test-copy-button" @text="Copy your secret key"
+      @textToCopy="someSecretThingGoesHere" />`
+    );
+    assert.dom('#test-copy-button').hasText('Copy your secret key');
   });
 });
