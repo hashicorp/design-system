a5985aebf431ebf410e22996ce583f6074e2cbe3
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-07-18 09:38:00 -0700

    HDS-2246 Add back dummy action needed by dropdown

---
+++ website/docs/components/dropdown/index.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class Index extends Component {
+  // dummy action, do not remove
+  @action
+  myAction() {
+    //
+  }
+}
