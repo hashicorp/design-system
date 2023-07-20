7ab7df3bbdbeed1f76282f4966f84b7e6eeaf77c
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2023-07-20 12:51:22 -0500

    implement component(s)

--- packages/components/addon/components/hds/copy/snippet/index.js
+++ packages/components/addon/components/hds/copy/snippet/index.js
@@ -8,64 +8,44 @@ import { assert } from '@ember/debug';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export const DEFAULT_SIZE = 'medium';
 export const DEFAULT_COLOR = 'tertiary';
-export const SIZES = ['small', 'medium'];
-export const COLORS = ['tertiary'];
+export const COLORS = ['tertiary', 'secondary'];
+export const DEFAULT_ICON = 'clipboard-copy';
+export const SUCCESS_ICON = 'clipboard-checked';
+export const ERROR_ICON = 'clipboard';
+export const DEFAULT_STATUS = 'idle';
 
 export default class HdsCopySnippetIndexComponent extends Component {
-  @tracked isSuccess = false;
-  @tracked isError = false;
+  @tracked status = DEFAULT_STATUS;
+  @tracked timer;
 
   /**
-   * @param text
+   * @param icon
    * @type {string}
-   * @description The text of the button. If no text value is defined an error will be thrown
+   * @default DEFAULT_ICON
+   * @description Determines the icon to be used, based on the success state. Note that this is auto-tracked because it depends on a tracked property (status).
    */
-  get text() {
-    // this doesn't do what I want really, it just returns the ID value which isn't the same as the target id's text value. But we'll use it for the assertion for now, until I figure out how to do the right thing.
-    let validText = this.args.clipboardText ?? this.args.text;
-    // for now this still is what's returned, necessary for the use case where we have an icon-only button and need the text value for the aria-label
-    // ideally, this would be either the text arg or the value of the clipboardText but even more ideally, something unique in case there were multiple icon-only copy buttons on the same page
-    let text = this.args.text ?? 'give me an accessible name';
-
-    assert(
-      'Either @clipboardText or @text for "Hds::CopySnippet" must have a valid value',
-      validText !== undefined
-    );
-
-    return text;
-  }
-  /**
-   * @param size
-   * @type {string}
-   * @default medium
-   * @description The size of the button; acceptable values are `small`, `medium`, and `large`
-   */
-  get size() {
-    let { size = DEFAULT_SIZE } = this.args;
-
-    assert(
-      `@size for "Hds::CopySnippet" must be one of the following: ${SIZES.join(
-        ', '
-      )}; received: ${size}`,
-      SIZES.includes(size)
-    );
-
-    return size;
+  get icon() {
+    let icon = DEFAULT_ICON;
+    if (this.status === 'success') {
+      icon = SUCCESS_ICON;
+    } else if (this.status === 'error') {
+      icon = ERROR_ICON;
+    }
+    return icon;
   }
 
   /**
    * @param color
    * @type {string}
-   * @default secondary
-   * @description Determines the color of button to be used; acceptable values are `secondary`, `tertiary`
+   * @default tertiary
+   * @description Determines the color of button to be used; acceptable values are `secondary` and `tertiary`
    */
   get color() {
     let { color = DEFAULT_COLOR } = this.args;
 
     assert(
-      `@color for "Hds::CopySnippet" must be one of the following: ${COLORS.join(
+      `@color for "Hds::Copy::Snippet" must be one of the following: ${COLORS.join(
         ', '
       )}; received: ${color}`,
       COLORS.includes(color)
@@ -74,77 +54,49 @@ export default class HdsCopySnippetIndexComponent extends Component {
     return color;
   }
 
-  /**
-   * @param isFullWidth
-   * @type {boolean}
-   * @default false
-   * @description Indicates that a button should take up the full width of the parent container. The default is false.
-   */
-  get isFullWidth() {
-    return this.args.isFullWidth ?? false;
+  get textToCopy() {
+    return this.args.encoded
+      ? decodeURI(this.args.textToCopy)
+      : this.args.textToCopy;
+  }
+
+  get targetToCopy() {
+    return this.args.targetToCopy;
   }
 
   /**
    * Get the class names to apply to the component.
-   * @method classNames
+   * @method CopySnippet#classNames
    * @return {string} The "class" attribute to apply to the component.
    */
   get classNames() {
     let classes = ['hds-copy-snippet'];
 
-    // add a class based on the @size argument
-    classes.push(`hds-copy-snippet--size-${this.size}`);
-
     // add a class based on the @color argument
-    classes.push(`hds-copy-snippet--color-${this.color}`);
+    classes.push(`hds-button--color-${this.color}`);
 
-    // add a class based on the @isFullWidth argument
-    if (this.isFullWidth) {
-      classes.push('hds-copy-snippet--width-full');
-    }
+    classes.push(`hds-copy-snippet--${this.status}`);
 
     return classes.join(' ');
   }
 
   @action
-  async copyCode() {
-    let textToCopy;
+  onSuccess() {
+    this.status = 'success';
+    this.resetStatusDelayed();
+  }
 
-    // this could probably be made more elegant.
-    if (this.args.clipboardText) {
-      let clipboardTextContent = document
-        .querySelector(this.args.clipboardText)
-        .innerHTML.trim();
-      textToCopy = clipboardTextContent;
-      // leaving this in while dev mode
-      console.log(`textToCopy is @clipboardText arg value: ${textToCopy}`);
-    } else if (this.args.text) {
-      textToCopy = this.args.text;
-      // leaving this in while dev mode
-      console.log(`textToCopy is @text arg value: ${textToCopy}`);
-    } else {
-      this.isError = true;
-      console.log(`something went wrong, @isError is ${this.isError}`);
-    }
-    // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard
-    await navigator.clipboard.writeText(textToCopy);
+  @action
+  onError() {
+    this.status = 'error';
+    this.resetStatusDelayed();
+  }
 
-    if (navigator.clipboard.readText) {
-      const result = await navigator.clipboard.readText();
-
-      if (result === textToCopy) {
-        this.isSuccess = true;
-      }
-    } else {
-      this.isError = true;
-      window.alert(
-        'the copy was not successful, the browser requires your permission'
-      );
-    }
-
+  resetStatusDelayed() {
+    clearTimeout(this.timer);
     // make it fade back to the default state
-    setTimeout(() => {
-      this.isSuccess = false;
+    this.timer = setTimeout(() => {
+      this.status = DEFAULT_STATUS;
     }, 1500);
   }
 }
--- packages/components/tests/integration/components/hds/copy/snippet/index-test.js
+++ packages/components/tests/integration/components/hds/copy/snippet/index-test.js
@@ -8,18 +8,13 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module(
-  'Integration | Component | hds/copy/snippet/index',
-  function (hooks) {
-    setupRenderingTest(hooks);
+module('Integration | Component | hds/copy/snippet/index', function (hooks) {
+  setupRenderingTest(hooks);
 
-    test('it should render the component with a CSS class that matches the component name', async function (assert) {
-      await render(
-        hbs`<Hds::Copy::Snippet id="test-copy-snippet" />`
-      );
-      assert
-        .dom('#test-copy-snippet')
-        .hasClass('hds-copy-snippet');
-    });
-  }
-);
+  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+    await render(
+      hbs`<Hds::Copy::Snippet id="test-copy-snippet" @text="3423g-234525-h345346-f34rtf4" />`
+    );
+    assert.dom('#test-copy-snippet').hasClass('hds-copy-snippet');
+  });
+});
