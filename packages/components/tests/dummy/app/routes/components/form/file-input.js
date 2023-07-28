356ea9f5e010b931397922ffa5e38e0ad0cf1d65
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-07-27 17:20:38 -0700

    HDS-2278 Update Showcase to include state, modify styles to make button shadow visible and apply focus style to button, not entire input

--- packages/components/tests/dummy/app/routes/components/form/file-input.js
+++ packages/components/tests/dummy/app/routes/components/form/file-input.js
@@ -5,4 +5,10 @@
 
 import Route from '@ember/routing/route';
 
-export default class ComponentsFormFileInputRoute extends Route {}
+export default class ComponentsFormFileInputRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    const STATES = ['default', 'hover', 'active', 'focus'];
+    return { STATES };
+  }
+}
