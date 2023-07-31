35fa160ba66ae38fd8676bb324808a3d277168aa
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-07-31 10:59:02 -0700

    HDS-2278 Fix linter errors

--- packages/components/tests/integration/components/hds/form/file-input/index-test.js
+++ packages/components/tests/integration/components/hds/form/file-input/index-test.js
@@ -8,18 +8,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module(
-  'Integration | Component | hds/form/file-input/index',
-  function (hooks) {
-    setupRenderingTest(hooks);
+module('Integration | Component | hds/form/file-input/index', function (hooks) {
+  setupRenderingTest(hooks);
 
-    test('it should render the component with a CSS class that matches the component name', async function (assert) {
-      await render(
-        hbs`<Hds::Form::FileInput id="test-form-file-input" />`
-      );
-      assert
-        .dom('#test-form-file-input')
-        .hasClass('hds-form-file-input');
-    });
-  }
-);
+  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::Form::FileInput id="test-form-file-input" />`);
+    assert.dom('#test-form-file-input').hasClass('hds-form-file-input');
+  });
+});
