4aa5bc11fcd09c4b9059f6a66122783a0111a130
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-08-02 10:39:24 -0700

    HDS-2278 Refactor FileInput to add  base and field sub-components

--- packages/components/tests/integration/components/hds/form/file-input/base-test.js
+++ packages/components/tests/integration/components/hds/form/file-input/base-test.js
@@ -8,11 +8,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | hds/form/file-input/index', function (hooks) {
+module('Integration | Component | hds/form/file-input/base', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it should render the component with a CSS class that matches the component name', async function (assert) {
-    await render(hbs`<Hds::Form::FileInput id="test-form-file-input" />`);
+    await render(hbs`<Hds::Form::FileInput::Base id="test-form-file-input" />`);
     assert.dom('#test-form-file-input').hasClass('hds-form-file-input');
   });
 });
