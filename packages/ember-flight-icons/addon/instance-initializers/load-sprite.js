04aefd9501df1012d3658ee0a005d44047be0514
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2023-08-08 16:36:23 -0500

    load sprite in a different place in testing environment

--- packages/ember-flight-icons/addon/instance-initializers/load-sprite.js
+++ packages/ember-flight-icons/addon/instance-initializers/load-sprite.js
@@ -3,7 +3,15 @@ import config from 'ember-get-config';
 
 export function initialize() {
   if (config.emberFlightIcons?.lazyEmbed) {
-    window.document.body.insertAdjacentHTML('beforeend', svgSprite);
+    // in test environments we can inject the sprite directly into the ember testing container
+    // to avoid issues with tools like Percy that only consider content inside that element
+    if (config.environment === 'test') {
+      window.document
+        .getElementById('ember-testing')
+        .insertAdjacentHTML('afterbegin', svgSprite);
+    } else {
+      window.document.body.insertAdjacentHTML('beforeend', svgSprite);
+    }
   }
 }
 
