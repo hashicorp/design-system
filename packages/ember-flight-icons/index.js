e7d02fff16db733f4172e0e053cc818d2ba9e8ab
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2023-08-09 08:24:10 -0500

    remove custom percy logic
    this is no longer needed now that we can load the sprite via the new initializer

--- packages/ember-flight-icons/index.js
+++ packages/ember-flight-icons/index.js
@@ -11,15 +11,7 @@ module.exports = {
   name: require('./package').name,
 
   contentFor(type, config) {
-    // notice: the "body-footer" is used in the normal app, while the "ember-testing-sprite-embed" is used
-    // in the testing environment to inject the sprite in the #ember-testing "app root element" (ENV.APP.rootElement)
-    // otherwise the @percy/ember package ignores everything else and the SVG sprite is not added to the DOM
-    // see thread: https://hashicorp.slack.com/archives/C11JCBJTW/p1633978558343000
-    // see: https://github.com/percy/percy-ember/blob/ab3b8ebb272fb6479e1185b8ef6e11dab3d6d9b0/addon-test-support/%40percy/ember/index.js#L53
-    if (
-      !config.emberFlightIcons?.lazyEmbed &&
-      (type === 'body-footer' || type === 'ember-testing-sprite-embed')
-    ) {
+    if (!config.emberFlightIcons?.lazyEmbed && type === 'body-footer') {
       return flightIconSprite;
     }
   },
