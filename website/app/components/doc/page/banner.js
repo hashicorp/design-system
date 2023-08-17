9a86d234ba5dd421fb165ab658d11d06a65252a9
Alex <alex-ju@users.noreply.github.com>
2023-08-17 09:23:49 +0100

    Add safeguarding to document.cookie operations
    As in server side rendering `ember-fastboot` doesn't have a `document` to operate on

--- website/app/components/doc/page/banner.js
+++ website/app/components/doc/page/banner.js
@@ -13,17 +13,24 @@ export default class DocPageBannerComponent extends Component {
   @tracked isVisible = !this.isDismissed;
 
   get isDismissed() {
-    const hasCookie =
-      document.cookie
-        .split('; ')
-        .findIndex((cookie) => cookie.startsWith(COOKIE_NAME)) >= 0;
+    let hasCookie = false;
+    // safeguarding as in SSR `ember-fastboot` doesn't have a `document` to operate on
+    if (typeof document !== 'undefined') {
+      hasCookie =
+        document.cookie
+          .split('; ')
+          .findIndex((cookie) => cookie.startsWith(COOKIE_NAME)) >= 0;
+    }
     return hasCookie;
   }
 
   @action
   onClose() {
     this.isVisible = false;
-    // we let it expire in two days (we want more impressions over two week's time)
-    document.cookie = `${COOKIE_NAME}=true; max-age=${60 * 60 * 24 * 2};`;
+    // safeguarding as in SSR `ember-fastboot` doesn't have a `document` to operate on
+    if (typeof document !== 'undefined') {
+      // we let it expire in two days (we want more impressions over two week's time)
+      document.cookie = `${COOKIE_NAME}=true; max-age=${60 * 60 * 24 * 2};`;
+    }
   }
 }
