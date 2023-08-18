bff4ccbc413a80dad28f8a09900feda35cae24b8
Alex <alex-ju@users.noreply.github.com>
2023-08-18 12:30:33 +0100

    Add Button to template registry

---
+++ packages/components/addon/template-registry.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import type HdsButtonIndexComponent from '@hashicorp/design-system-components/components/hds/button';
+
+export default interface HdsComponentsRegistry {
+  HdsButtonComponent: typeof HdsButtonIndexComponent;
+}
