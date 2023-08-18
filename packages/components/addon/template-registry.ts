1ca9d518543f76a9f61a89559994d08a04d44771
Alex <alex-ju@users.noreply.github.com>
2023-08-18 14:35:30 +0100

    Add signature and types for `interactive/index.ts`

--- packages/components/addon/template-registry.ts
+++ packages/components/addon/template-registry.ts
@@ -4,7 +4,9 @@
  */
 
 import type HdsButtonIndexComponent from '@hashicorp/design-system-components/components/hds/button';
+import type HdsInteractiveIndexComponent from '@hashicorp/design-system-components/components/hds/interactive';
 
 export default interface HdsComponentsRegistry {
   HdsButtonComponent: typeof HdsButtonIndexComponent;
+  HdsInteractiveComponent: typeof HdsInteractiveIndexComponent;
 }
