f23157452786249454afd4fe9a0725ac0be19842
heatherlarsen <heatherlarsen@users.noreply.github.com>
2023-08-21 23:24:26 +0000

    sync & build of flight icons

---
+++ packages/flight-icons/svg-react/vercel-color-24.tsx
@@ -0,0 +1,30 @@
+import { forwardRef, useMemo } from 'react';
+import { IconProps } from './types';
+
+export const IconVercelColor24 = forwardRef<SVGSVGElement, IconProps>(
+    ({ color = 'currentColor', title, ...props }, svgRef) => {
+        const titleId = useMemo(
+            () =>
+                title
+                    ? 'title-' + Math.random().toString(36).substr(2, 9)
+                    : undefined,
+            [title]
+        );
+        return (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+                fill="none"
+                viewBox="0 0 24 24"
+                aria-hidden={!title}
+                ref={svgRef}
+                aria-labelledby={titleId}
+                {...props}
+            >
+                {title ? <title id={titleId}>{title}</title> : null}
+                <path fill="#000" d="M12 3.75l9.25 15.417H2.75L12 3.75z" />
+            </svg>
+        );
+    }
+);
