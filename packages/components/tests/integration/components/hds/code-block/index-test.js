2b72d6a8da7dcc03adf37fc52a47624821d2dc47
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-08-25 14:53:25 -0700

    HDS-2277 Add component scaffolding

---
+++ packages/components/tests/integration/components/hds/code-block/index-test.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | hds/code-block/index', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::CodeBlock id="test-code-block" />`);
+    assert.dom('#test-code-block').hasClass('hds-code-block');
+  });
+});
