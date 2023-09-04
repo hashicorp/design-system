760d3f51b7466913fa34171ff281b60f89b7b86e
Alex <alex-ju@users.noreply.github.com>
2023-09-04 16:22:00 +0100

    Rename `TextInput` arguments

--- packages/components/addon/components/hds/form/text-input/field.js
+++ packages/components/addon/components/hds/form/text-input/field.js
@@ -8,12 +8,52 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class HdsFormTextInputFieldComponent extends Component {
-  @tracked isMasked = this.args.isMasked ?? true;
+  @tracked isPasswordReadable = this.args.isPasswordReadable ?? false;
+  @tracked hasVisibilityToggle = this.args.hasVisibilityToggle ?? true;
   @tracked type = this.args.type ?? 'text';
 
+  /**
+   * @param showVisibilityToggle
+   * @type {boolean}
+   * @default false
+   */
+  get showVisibilityToggle() {
+    return this.args.type === 'password' && this.hasVisibilityToggle;
+  }
+
+  /**
+   * @param ariaLabel
+   * @type {string}
+   * @default 'Show password'
+   */
+  get ariaLabel() {
+    if (this.args.ariaLabel) {
+      return this.args.ariaLabel;
+    } else if (this.isPasswordReadable) {
+      return 'Hide password';
+    } else {
+      return 'Show password';
+    }
+  }
+
+  /**
+   * @param ariaMessageText
+   * @type {string}
+   * @default 'Password is now hidden'
+   */
+  get ariaMessageText() {
+    if (this.args.ariaMessageText) {
+      return this.args.ariaMessageText;
+    } else if (this.isPasswordReadable) {
+      return 'Password is now visible';
+    } else {
+      return 'Password is now hidden';
+    }
+  }
+
   @action
-  onClickToggle() {
-    this.isMasked = !this.isMasked;
-    this.type = this.isMasked ? 'password' : 'text';
+  onClickTogglePasswordReadability() {
+    this.isPasswordReadable = !this.isPasswordReadable;
+    this.type = this.isPasswordReadable ? 'text' : 'password';
   }
 }
