5d4a781059bbb515b3882643f8871a3c3ece0ec3
Cristiano Rastelli <public@didoo.net>
2023-09-05 13:35:40 +0100

    Working MVP - Part 2

--- packages/components/addon/modifiers/hds-clipboard.js
+++ packages/components/addon/modifiers/hds-clipboard.js
@@ -4,111 +4,139 @@
  */
 
 import Modifier from 'ember-modifier';
+import { assert } from '@ember/debug';
 
-const copyToClipboard = (text, target) => {
+async function copyToClipboard(text, target) {
   let textToCopy;
 
-  if (text && typeof text === 'string') {
-    textToCopy = text;
+  if (text) {
+    if (typeof text === 'string') {
+      textToCopy = text;
+    } else if (
+      // context: https://github.com/hashicorp/design-system/pull/1564
+      typeof text === 'number' ||
+      typeof text === 'bigint'
+    ) {
+      textToCopy = text.toString();
+    } else {
+      assert(
+        `\`hds-clipboard\` modifier - \`text\` argument must be a string - provided: ${typeof text}`
+      );
+    }
   } else if (target) {
-    // todo! check that the node exists
-    const targetElement = document.querySelector(target);
+    let targetElement;
+    if (typeof target === 'string') {
+      targetElement = document.querySelector(target);
+
+      assert(
+        '`hds-clipboard` modifier - `target` selector provided does not point to an existing DOM node, check your selector string',
+        targetElement
+      );
+    } else if (target instanceof Node && target.nodeType === 1) {
+      targetElement = target;
+    } else if (target instanceof NodeList) {
+      assert(
+        '`hds-clipboard` modifier - `target` argument must be a string or a DOM node - provided: a list of DOM nodes'
+      );
+    } else {
+      assert(
+        `\`hds-clipboard\` modifier - \`target\` argument must be a string or a DOM node - provided: ${typeof target}`
+      );
+    }
+
     if (
-      // TODO! improve handling of target elements here!
-      // target instanceof HTMLInputElement && !['text', 'search', 'url', 'tel', 'password'].includes(target?.type)
-      targetElement.nodeName === 'INPUT' ||
-      targetElement.nodeName === 'SELECT' ||
-      targetElement.nodeName === 'TEXTAREA'
+      targetElement instanceof HTMLInputElement || // targetElement.nodeName === 'INPUT' ||
+      targetElement instanceof HTMLTextAreaElement || // targetElement.nodeName === 'TEXTAREA' ||
+      targetElement instanceof HTMLSelectElement // targetElement.nodeName === 'SELECT'
     ) {
       textToCopy = targetElement.value;
     } else {
-      // TODO! add handling of `innerText` for other elements
-      if (targetElement.hasAttribute('contenteditable')) {
-        targetElement.focus();
-      }
+      // simplest approach
+      textToCopy = targetElement.innerText;
 
-      var selection = window.getSelection();
-      var range = document.createRange();
-
-      selection.removeAllRanges();
-      range.selectNodeContents(targetElement);
-      selection.addRange(range);
-
-      textToCopy = selection.toString();
-
-      selection.removeAllRanges();
+      // approach based on text selection (left for backup just in case)
+      // var selection = window.getSelection();
+      // var range = document.createRange();
+      // selection.removeAllRanges();
+      // range.selectNodeContents(targetElement);
+      // selection.addRange(range);
+      // textToCopy = selection.toString();
+      // selection.removeAllRanges();
     }
   } else {
-    // TODO! console an error message here
+    assert(
+      '`hds-clipboard` modifier - either a `text` or a `target` argument is required'
+    );
   }
 
-  // finally copy the text
-
+  // finally copy the text to the clipboard using the Clipboard API
+  // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
   if (textToCopy) {
-    navigator.clipboard.writeText(textToCopy).then(
-      () => {
-        /* clipboard successfully set */
-        console.log('success', textToCopy);
-      },
-      (error) => {
-        /* clipboard write failed */
-        console.log('error', textToCopy, error);
-      }
-    );
-    // TODO! add handling of success/error
-    return true;
+    try {
+      // notice: the "clipboard-write" permission is granted automatically to pages when they are in the active tab
+      // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/write
+      navigator.clipboard.writeText(textToCopy);
+      console.log('success', textToCopy);
+      return true;
+    } catch (error) {
+      // clipboard write failed
+      console.log(
+        '`hds-clipboard` modifier - an error occurred while writing to the clipboard - check your browser permissions',
+        textToCopy,
+        error
+      );
+      return false;
+    }
   } else {
     return false;
   }
-};
+}
 
 export default class HdsClipboardModifier extends Modifier {
-  // constructor(trigger, text, target, onSuccess, onError) {
-  //   super();
-  //   this.text = text;
-  //   this.target = target;
-  //   this.onSuccess = onSuccess;
-  //   this.onError = onError;
-  //   this.listenClick(trigger);
-  // }
   didSetup = false;
-  listener = null;
 
   modify(element, positional, named) {
+    assert(
+      '`hds-clipboard` modifier - the modifier must be applied to an element',
+      element
+    );
+
     const { text, target, onSuccess, onError } = named;
     this.text = text;
     this.target = target;
     this.onSuccess = onSuccess;
     this.onError = onError;
-    this.#listenClick(element);
+
+    if (!this.didSetup) {
+      this.#listenClick(element);
+      this.didSetup = true;
+    }
   }
 
-  /**
-   * Adds a click event listener to the passed trigger.
-   * @param {String|HTMLElement|HTMLCollection|NodeList} trigger
-   */
-  #listenClick(trigger) {
-    this.listener = trigger.addEventListener('click', (e) => this.#onClick(e));
+  // add a "click" event listener to the element
+  #listenClick(element) {
+    this.listener = element.addEventListener('click', (e) => this.#onClick(e));
   }
 
-  /**
-   * Defines a new `ClipboardAction` on each click event.
-   * @param {Event} e
-   */
-  #onClick(e) {
+  // defines a new `copyToClipboard` action on each click event
+  async #onClick(e) {
     const trigger = e.delegateTarget || e.currentTarget;
-    const success = copyToClipboard(this.text, this.target);
+    const success = await copyToClipboard(this.text, this.target);
 
-    // Fires an event based on the copy operation result.
+    // fire the `onSuccess/onError` callbacks (if provided)
     const args = {
       triggger: trigger,
       text: this.text,
       target: this.target,
     };
     if (success) {
-      this.onSuccess(args);
+      if (typeof this.onSuccess === 'function') {
+        this.onSuccess(args);
+      }
     } else {
-      this.onError(args);
+      if (typeof this.onError === 'function') {
+        this.onError(args);
+      }
     }
   }
 
