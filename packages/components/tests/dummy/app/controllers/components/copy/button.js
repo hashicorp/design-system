c8fe2ab0e1cd91f38558d2a1d50f0641624c8342
Cristiano Rastelli <public@didoo.net>
2023-09-05 15:16:56 +0100

    extended the showcase for the `Copy::Button` component to cover more use cases - part 3

--- packages/components/tests/dummy/app/controllers/components/copy/button.js
+++ packages/components/tests/dummy/app/controllers/components/copy/button.js
@@ -4,6 +4,8 @@
  */
 
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { scheduleOnce } from '@ember/runloop';
 
@@ -26,6 +28,7 @@ function replaceMockCopyStatus() {
 
 export default class CopyButtonController extends Controller {
   @service router;
+  @tracked isModalActive = false;
 
   constructor() {
     super(...arguments);
@@ -47,5 +50,15 @@ export default class CopyButtonController extends Controller {
 
   get targetMultipleNodeElements() {
     return document.querySelectorAll('#test-target-node-elements');
+  }
+
+  @action
+  activateModal() {
+    this.isModalActive = true;
+  }
+
+  @action
+  deactivateModal() {
+    this.isModalActive = false;
   }
 }
