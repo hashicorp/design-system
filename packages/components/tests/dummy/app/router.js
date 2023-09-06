91b390459fc9f107236c1ba12e3d280139ac0d01
Alex <alex-ju@users.noreply.github.com>
2023-09-06 11:53:03 +0100

    Apply suggestions from code review
    Co-authored-by: Cristiano Rastelli <cristiano.rastelli@hashicorp.com>

--- packages/components/tests/dummy/app/router.js
+++ packages/components/tests/dummy/app/router.js
@@ -76,7 +76,6 @@ Router.map(function () {
     this.route('dismiss-button');
     this.route('interactive');
     this.route('menu-primitive');
-    this.route('visibility-toggle');
   });
   this.route('overrides', function () {
     this.route('power-select');
