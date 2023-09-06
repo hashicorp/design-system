5d3a9d0bd5604d732e14b0945a6a4e651e349160
Kristin Bradley <kristin.bradley@hashicorp.com>
2023-09-06 15:12:27 -0700

    HDS-2277 Fix failing test, clean up CSS to prevent accidental override of whitespace value

--- packages/components/tests/integration/components/hds/code-block/index-test.js
+++ packages/components/tests/integration/components/hds/code-block/index-test.js
@@ -12,7 +12,9 @@ module('Integration | Component | hds/code-block/index', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it should render the component with a CSS class that matches the component name', async function (assert) {
-    await render(hbs`<Hds::CodeBlock id="test-code-block" />`);
+    await render(hbs`
+      <Hds::CodeBlock @value="console.log('Hello world');" id="test-code-block" />
+    `);
     assert.dom('#test-code-block').hasClass('hds-code-block');
   });
 });
