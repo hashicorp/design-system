10afded3d8728faea567a3fb533213b733cf9288
Melanie Sumner <melanie@hashicorp.com>
2023-10-05 10:53:40 -0500

    adds the remainder of the component a11y acceptance tests

---
+++ packages/components/tests/acceptance/components/hds/text-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'dummy/tests/helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Acceptance | Component | hds/text', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('Components/hds/text page passes automated a11y checks', async function (assert) {
+    await visit('/components/text');
+    await a11yAudit();
+
+    assert.ok(true, 'a11y automation audit passed');
+  });
+});
