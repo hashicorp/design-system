a06b3e8e781a1b6199ba6a1f27212bfa5646ac0f
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2023-10-06 08:33:43 -0500

    Remove url test from component acceptance test blueprint

--- packages/components/blueprints/hds-component-test/files/tests/acceptance/components/hds/__name__.js
+++ packages/components/blueprints/hds-component-test/files/tests/acceptance/components/hds/__name__.js
@@ -4,18 +4,12 @@
  */
 
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'dummy/tests/helpers';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 
 module('Acceptance | components/<%= dasherizedModuleName %>', function (hooks) {
   setupApplicationTest(hooks);
-
-  test('visiting /components/<%= dasherizedModuleName %>', async function (assert) {
-    await visit('/components/<%= dasherizedModuleName %>');
-
-    assert.strictEqual(currentURL(), '/components/<%= dasherizedModuleName %>');
-  });
 
   test('Components/<%= dasherizedModuleName %> page passes automated a11y checks', async function (assert) {
     await visit('/components/<%= dasherizedModuleName %>');
