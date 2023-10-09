6366bdc6392185137dda4b8480b82bdd4c9fa067
Melanie Sumner <melanie@hashicorp.com>
2023-10-09 16:41:18 -0500

    removed extraneous tests from link components

--- packages/components/tests/acceptance/components/hds/link/inline-test.js
+++ packages/components/tests/acceptance/components/hds/link/inline-test.js
@@ -1,16 +1,11 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'dummy/tests/helpers';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 
 module('Acceptance | Component | hds/link/inline', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /components/link/inline', async function (assert) {
-    await visit('/components/link/inline');
-
-    assert.strictEqual(currentURL(), '/components/link/inline');
-  });
   test('Components/hds/link/inline page passes automated a11y checks', async function (assert) {
     await visit('/components/link/inline');
     await a11yAudit();
--- packages/components/tests/acceptance/components/hds/link/standalone-test.js
+++ packages/components/tests/acceptance/components/hds/link/standalone-test.js
@@ -1,16 +1,11 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'dummy/tests/helpers';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 
 module('Acceptance | Component | hds/link/standalone', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /components/link/standalone', async function (assert) {
-    await visit('/components/link/standalone');
-
-    assert.strictEqual(currentURL(), '/components/link/standalone');
-  });
   test('Components/hds/link/standalone page passes automated a11y checks', async function (assert) {
     await visit('/components/link/standalone');
     await a11yAudit();
