69f9fb322e534a7b7b1fdd285564c632a296bf68
Alex <alex-ju@users.noreply.github.com>
2023-10-11 20:24:51 +0100

    Enforce resolutions for ember-lts-3.28

--- packages/components/config/ember-try.js
+++ packages/components/config/ember-try.js
@@ -22,6 +22,11 @@ module.exports = async function () {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
+            '@ember/test-helpers': '^2.0.0',
+            'ember-a11y-testing': '^5.2.1',
+            'ember-cli': '~3.28.0',
+            'ember-qunit': '^6.0.0',
+            'ember-resolver': '^8.1.0',
             'ember-source': '~3.28.11',
           },
         },
