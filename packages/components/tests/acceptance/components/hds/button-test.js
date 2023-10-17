4a7fb7f227e1acf2e3932bde4884beccbb590783
Melanie Sumner <melanie@hashicorp.com>
2023-10-17 10:58:31 -0500

    Tweaks to button test

--- packages/components/tests/acceptance/components/hds/button-test.js
+++ packages/components/tests/acceptance/components/hds/button-test.js
@@ -7,8 +7,16 @@ module('Acceptance | Component | hds/button', function (hooks) {
   setupApplicationTest(hooks);
 
   test('Components/button passes a11y automated checks', async function (assert) {
+    // if making any changes to this component, enable the rule and run it to check that changes are still compliant
+    let axeOptions = {
+      rules: {
+        'color-contrast': {
+          enabled: false,
+        },
+      },
+    };
     await visit('/components/button');
-    await a11yAudit();
+    await a11yAudit(axeOptions);
 
     assert.ok(true, 'a11y automation audit passed');
   });
