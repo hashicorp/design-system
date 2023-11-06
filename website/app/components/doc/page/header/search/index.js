88977cca6554ddc69b3548d64fc4fb1efff9fedf
Cristiano Rastelli <public@didoo.net>
2023-11-06 16:20:41 +0000

    split logic and templating for search results

--- website/app/components/doc/page/header/search/index.js
+++ website/app/components/doc/page/header/search/index.js
@@ -6,21 +6,13 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
-import ENV from 'website/config/environment';
+import { autocomplete } from '@algolia/autocomplete-js';
 
-import algoliasearch from 'algoliasearch/lite';
-import { autocomplete, getAlgoliaResults } from '@algolia/autocomplete-js';
-// import { createLocalStorageRecentSearchesPlugin } from '@algolia/autocomplete-plugin-recent-searches';
-
-const searchClient = algoliasearch(
-  ENV.APP.ALGOLIA_APPLICATION_ID,
-  ENV.APP.ALGOLIA_API_KEY_SEARCH
-);
-
-// const algoliaRecentSearchesPlugin = createLocalStorageRecentSearchesPlugin({
-//   key: 'RECENT_SEARCH',
-//   limit: 5,
-// });
+import { getItemsFunction } from './parts/getItems';
+import { templatesHeaderFunction } from './parts/templatesHeaderFunction';
+import { templatesFooterFunction } from './parts/templatesFooterFunction';
+import { templatesNoResultsFunction } from './parts/templatesNoResultsFunction';
+import { templatesItemFunction } from './parts/templatesItemFunction';
 
 export default class DocAlgoliaSearchComponent extends Component {
   @action
@@ -41,84 +33,30 @@ export default class DocAlgoliaSearchComponent extends Component {
       // GET SOURCES
       // see: https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-js/autocomplete/#param-getsources
       getSources({ query }) {
+        let searchQuery = query;
+        let searchFilters = {};
+        let searchType = 'generic';
+        if (query?.match(/^token:/)) {
+          searchQuery = query.replace(/^token:/, '');
+          searchFilters = { filters: 'type:token' };
+          searchType = 'token';
+        } else if (query?.match(/^icon:/)) {
+          searchQuery = query.replace(/^icon:/, '');
+          searchFilters = { filters: 'type:icon' };
+          searchType = 'icon';
+        }
         return [
           {
             sourceId: 'content',
-            getItems() {
-              return getAlgoliaResults({
-                searchClient,
-                queries: [
-                  {
-                    indexName: ENV.APP.ALGOLIA_INDEX_ID,
-                    query,
-                    params: {
-                      hitsPerPage: 5,
-                      attributesToSnippet: ['title:10', 'caption:35'],
-                      snippetEllipsisText: '…',
-                    },
-                  },
-                ],
-              });
-            },
-            getItemUrl({ item }) {
+            getItems: getItemsFunction({ searchQuery, searchFilters }),
+            getItemUrl: ({ item }) => {
               return item.pageURL;
             },
             templates: {
-              header() {
-                return 'Suggestions';
-              },
-              footer() {
-                return 'Footer';
-              },
-              noResults() {
-                return 'No results.';
-              },
-              item({ item, components, html }) {
-                return html`<div class="aa-ItemWrapper">
-                  <div class="aa-ItemContent">
-                    <div class="aa-ItemIcon aa-ItemIcon--alignTop">
-                      <img
-                        src="${item.image}"
-                        alt="${item.name}"
-                        width="40"
-                        height="40"
-                      />
-                    </div>
-                    <div class="aa-ItemContentBody">
-                      <div class="aa-ItemContentTitle">
-                        ${components.Highlight({
-                          hit: item,
-                          attribute: 'title',
-                        })}
-                      </div>
-                      <div class="aa-ItemContentDescription">
-                        ${components.Snippet({
-                          hit: item,
-                          attribute: 'description',
-                        })}
-                      </div>
-                    </div>
-                    <div class="aa-ItemActions">
-                      <button
-                        class="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly"
-                        type="button"
-                        title="Select"
-                      >
-                        <svg
-                          viewBox="0 0 24 24"
-                          width="20"
-                          height="20"
-                          fill="currentColor"
-                        >
-                          <path
-                            d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                </div>`;
-              },
+              header: templatesHeaderFunction,
+              footer: templatesFooterFunction,
+              noResults: templatesNoResultsFunction({ searchType }),
+              item: templatesItemFunction({ searchType }),
             },
           },
         ];
