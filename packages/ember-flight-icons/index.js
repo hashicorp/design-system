734811d8348b14f6387d93549c5f3a8924c2075b
Matthew Irish <matthew@hashicorp.com>
2023-11-13 14:40:56 -0600

    enable dynamic import in ember-auto-import

--- packages/ember-flight-icons/index.js
+++ packages/ember-flight-icons/index.js
@@ -10,6 +10,12 @@ const flightIconSprite = require('@hashicorp/flight-icons/svg-sprite/svg-sprite-
 module.exports = {
   name: require('./package').name,
 
+  options: {
+    babel: {
+      plugins: [require.resolve('ember-auto-import/babel-plugin')],
+    },
+  },
+
   contentFor(type, config) {
     if (!config.emberFlightIcons?.lazyEmbed && type === 'body-footer') {
       return flightIconSprite;
