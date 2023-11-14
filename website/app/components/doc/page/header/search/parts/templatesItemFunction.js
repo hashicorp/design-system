4aa961f2ac49a07e1fb64b3d900229819515dbdd
Cristiano Rastelli <public@didoo.net>
2023-11-14 15:22:23 +0000

    more styling of the search results

--- website/app/components/doc/page/header/search/parts/templatesItemFunction.js
+++ website/app/components/doc/page/header/search/parts/templatesItemFunction.js
@@ -7,19 +7,28 @@ const itemPreview = ({ item }) => {
   let content = '';
   switch (item.type) {
     case 'icon':
-      content += `<div class="doc-algolia-search__aa-preview doc-algolia-search__aa-preview--icon">`;
+      content += `<div class="aa-ItemPreview aa-ItemPreview--icon">`;
       content += `  <svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">`;
       content += `    <use href="#flight-${item['icon-name']}-24"></use>`;
       content += `  </svg>`;
       content += `</div>`;
       break;
     case 'token':
-      content += `<div class="doc-algolia-search__aa-preview doc-algolia-search__aa-preview--token">`;
-      content += `  T`;
-      content += `</div>`;
+      if (item['token-name'].match(/^token-typography/)) {
+        content += `<div class="aa-ItemPreview aa-ItemPreview--token">`;
+        content += `  Aa`;
+        content += `</div>`;
+      } else if (item['token-name'].match(/^token-color/)) {
+        content += `<div class="aa-ItemPreview aa-ItemPreview--token" style="background-color: ${item['token-value']}">`;
+        content += `</div>`;
+      } else {
+        content += `<div class="aa-ItemPreview aa-ItemPreview--token">`;
+        content += `  T`;
+        content += `</div>`;
+      }
       break;
     default:
-      content += `<div class="doc-algolia-search__aa-preview doc-algolia-search__aa-preview--illustration">`;
+      content += `<div class="aa-ItemPreview aa-ItemPreview--illustration">`;
       if (item.previewImage) {
         content += `  <img src="/${item.previewImage}" alt="" role="presentation" />`;
       } else {
@@ -41,7 +50,7 @@ const itemBody = ({ item, html, components }) => {
       content += `    Icon: ${item['icon-name']}\n`;
       content += `  </div>\n`;
       content += `  <div class="aa-ItemContentDescription">\n`;
-      content += `    Aliases: ${item['icon-aliases']}\n`;
+      content += `    Alias: ${item['icon-aliases']}\n`;
       content += `  </div>\n`;
       break;
     case 'token':
@@ -51,9 +60,6 @@ const itemBody = ({ item, html, components }) => {
       if (item['token-value']) {
         content += `  <div class="aa-ItemContentDescription">\n`;
         content += `    Value: <code>${item['token-value']}</code>\n`;
-        if (item['token-type']) {
-          content += `    (${item['token-value']})\n`;
-        }
         content += `  </div>\n`;
       }
       break;
@@ -76,14 +82,12 @@ const itemBody = ({ item, html, components }) => {
   return content;
 };
 
-const itemActions = () => {
+const itemExtra = () => {
   let content = '';
-  content += `<div class="aa-ItemActions">`;
-  content += `  <button type="button" class="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly">`;
-  content += `    <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">`;
-  content += `      <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z" />`;
-  content += `    </svg>`;
-  content += `  </button>`;
+  content += `<div class="aa-ItemExtra">`;
+  content += `  <svg class="aa-ItemExtra__ReturnIcon" viewBox="0 0 24 24" width="20" height="20" fill="currentColor">`;
+  content += `    <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z" />`;
+  content += `  </svg>`;
   content += `</div>`;
   return content;
 };
@@ -94,15 +98,11 @@ export const templatesItemFunction = () => {
     console.log(`ITEM / ${itemType.toUpperCase()}`, item);
 
     let content = '';
-    content += `<div class="aa-ItemWrapper doc-algolia-search__aa-item-wrapper">`;
-    content += `  <div class="aa-ItemContent doc-algolia-search__aa-item-content">`;
-    content += `    <a class="aa-ItemLink doc-algolia-search__aa-item-link" href="/${item.pageURL}">`;
-    content += `      ${itemPreview({ item })}`;
-    content += `      ${itemBody({ item, html, components })}`;
-    content += `      ${itemActions()}`;
-    content += `    </a>`;
-    content += `  </div>`;
-    content += `</div>`;
+    content += `<a class="aa-ItemLinkWrapper" href="/${item.pageURL}">`;
+    content += `  ${itemPreview({ item })}`;
+    content += `  ${itemBody({ item, html, components })}`;
+    content += `  ${itemExtra()}`;
+    content += `</a>`;
 
     // IMPORTANT! you can't do html`${content}` or it will render HTML as a string
     // see: https://www.algolia.com/doc/ui-libraries/autocomplete/core-concepts/templates/#with-a-shim
