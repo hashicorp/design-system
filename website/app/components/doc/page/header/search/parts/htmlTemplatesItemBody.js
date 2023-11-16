b738e0d1b3963bc701f6b6b7411b42e0637f4da3
Cristiano Rastelli <public@didoo.net>
2023-11-16 15:32:28 +0000

    improved snipppeting/highliting with custom method

--- website/app/components/doc/page/header/search/parts/htmlTemplatesItemBody.js
+++ website/app/components/doc/page/header/search/parts/htmlTemplatesItemBody.js
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import { parseAlgoliaHitHighlight } from '@algolia/autocomplete-preset-algolia';
+
 const htmlItemContentTitle = ({ html, title }) => html`
   <div class="aa-ItemContentTitle">${title}</div>
 `;
@@ -10,6 +12,25 @@ const htmlItemContentTitle = ({ html, title }) => html`
 const htmlItemContentDescription = ({ html, description }) => html`
   <div class="aa-ItemContentDescription">${description}</div>
 `;
+
+const snippetInitialText = (text) => {
+  var splitted = text.split(' ');
+  var reduced = splitted.slice(-5);
+  if (splitted.length === reduced.length) {
+    return text;
+  } else {
+    return `… ${reduced.join(' ')}`;
+  }
+};
+
+function concatPartsAsHtml({ html, parts }) {
+  return parts.reduce((acc, current) => {
+    return html`${acc}
+    ${current.isHighlighted
+      ? html`<mark>${current.value}</mark>`
+      : current.value} `;
+  }, html``);
+}
 
 export const htmlTemplatesItemBody = ({ item, html, components }) => {
   let title;
@@ -28,11 +49,21 @@ export const htmlTemplatesItemBody = ({ item, html, components }) => {
       });
       break;
     default:
+      // title (full)
       title = item.pageTitle;
-      description = components.Snippet({
+      // description (snippeted)
+      var snippetedParts = parseAlgoliaHitHighlight({
         hit: item,
-        attribute: 'content',
+        attribute: ['content'],
       });
+      // overwrite the initial text part (if it's not highlighted) with a shorter version (if needed)
+      if (snippetedParts.length > 0 && !snippetedParts[0].isHighlighted) {
+        snippetedParts[0].value = snippetInitialText(snippetedParts[0].value);
+        description = concatPartsAsHtml({ html, parts: snippetedParts });
+      } else {
+        // we fallback to the normal highlighting/snippeting, that may not always make the highlighted word(s) visible in the description, because of the elliptization via CSS
+        description = components.Snippet({ hit: item, attribute: ['content'] });
+      }
   }
 
   return html`
