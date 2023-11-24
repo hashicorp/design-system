bbadc6b8a8c4e3246fa17f414265bc300e079fa2
Alex <alex-ju@users.noreply.github.com>
2023-11-24 15:54:18 +0000

    Add dummy character count validation to `TextInput`

--- packages/components/tests/dummy/app/controllers/components/form/text-input.js
+++ packages/components/tests/dummy/app/controllers/components/form/text-input.js
@@ -5,8 +5,21 @@
 
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class FormTextInputController extends Controller {
+  @tracked fieldIsInvalid;
   @action
   noop() {}
+
+  @action onFieldInput(
+    control,
+    maxLength,
+    minLength,
+    remaining,
+    shortfall,
+    currentLength
+  ) {
+    this.fieldIsInvalid = currentLength > maxLength;
+  }
 }
