3ffea2e3368f51c6d0c2193f566cd98bea527763
Alex <alex-ju@users.noreply.github.com>
2023-11-24 15:54:35 +0000

    Add dummy character count validation to `MaskedInput`

---
+++ packages/components/tests/dummy/app/controllers/components/form/masked-input.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class FormMaskedInputController extends Controller {
+  @tracked textInputFieldIsInvalid;
+  @tracked textareaFieldIsInvalid;
+  @action onFieldInput(
+    control,
+    maxLength,
+    minLength,
+    remaining,
+    shortfall,
+    currentLength
+  ) {
+    if (control.localName === 'input') {
+      this.textInputFieldIsInvalid = currentLength > maxLength;
+    } else if (control.localName === 'textarea') {
+      this.textareaFieldIsInvalid = currentLength > maxLength;
+    }
+  }
+}
