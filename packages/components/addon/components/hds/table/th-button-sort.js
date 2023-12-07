1a402a0bcc6eb37ab44ffa124f887750057d16cf
Cristiano Rastelli <public@didoo.net>
2023-12-07 19:09:44 +0000

    added `Table::ThButtonSort` and `Table::ThButtonTooltip` as standalone subcomponents

---
+++ packages/components/addon/components/hds/table/th-button-sort.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+const NOOP = () => {};
+
+export default class HdsTableThButtonSortComponent extends Component {
+  /**
+   * @param onClick
+   * @type {function}
+   * @default () => {}
+   */
+  get onClick() {
+    let { onClick } = this.args;
+    return onClick || NOOP;
+  }
+}
