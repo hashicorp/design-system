f7205815616649231537ee071774b17f04bd5c77
Cristiano Rastelli <public@didoo.net>
2023-12-13 21:12:18 +0000

    updated (and added missing) integration tests

--- packages/components/tests/integration/components/hds/table/th-sort-test.js
+++ packages/components/tests/integration/components/hds/table/th-sort-test.js
@@ -5,7 +5,7 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, focus } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | hds/table/th-sort', function (hooks) {
@@ -16,13 +16,19 @@ module('Integration | Component | hds/table/th-sort', function (hooks) {
     );
 
     assert.dom('#data-test-table-th-sort').hasClass('hds-table__th--sort');
+    assert.dom('#data-test-table-th-sort .hds-table__th-button--sort').exists();
   });
 
-  test('it has the scope attribute, and it is set to column', async function (assert) {
-    await render(hbs`<Hds::Table::ThSort>Artist</Hds::Table::ThSort>`);
+  // CONTENT
 
-    assert.dom('.hds-table__th--sort').hasAttribute('scope', 'col');
+  test('it renders text content yielded within the cell', async function (assert) {
+    await render(
+      hbs`<Hds::Table::ThSort id="data-test-table-th-sort">Artist</Hds::Table::ThSort>`
+    );
+    assert.dom('#data-test-table-th-sort').hasText('Artist');
   });
+
+  // ALIGNMENT
 
   test('it should render with the appropriate `@align` CSS class', async function (assert) {
     await render(
@@ -33,6 +39,8 @@ module('Integration | Component | hds/table/th-sort', function (hooks) {
       .hasClass('hds-table__th--align-right');
   });
 
+  // WIDTH
+
   test('it should add inline styles if `@width` is declared', async function (assert) {
     await render(
       hbs`<Hds::Table::ThSort id="data-test-table-th-sort" @width="10%" />`
@@ -41,6 +49,8 @@ module('Integration | Component | hds/table/th-sort', function (hooks) {
       .dom('#data-test-table-th-sort')
       .hasAttribute('style', 'width: 10%; min-width: 10%;');
   });
+
+  // SORT ICON
 
   test('if @sortOrder is not defined, the swap-vertical icon should be displayed', async function (assert) {
     await render(
@@ -63,6 +73,29 @@ module('Integration | Component | hds/table/th-sort', function (hooks) {
 
     assert.dom('[data-test-icon="arrow-down"]').exists();
   });
+
+  // ATTRIBUTES
+
+  test('it should support splattributes', async function (assert) {
+    await render(
+      hbs`<Hds::Table::ThSort id="data-test-table-th" lang="es">Artist</Hds::Table::ThSort>`
+    );
+    assert.dom('#data-test-table-th').hasAttribute('lang', 'es');
+  });
+
+  test('it has the `scope` attribute, and it is set to column by default', async function (assert) {
+    await render(hbs`<Hds::Table::ThSort>Artist</Hds::Table::ThSort>`);
+
+    assert.dom('.hds-table__th--sort').hasAttribute('scope', 'col');
+  });
+  test('the default `scope` attribute can not be overwritten', async function (assert) {
+    await render(
+      hbs`<Hds::Table::ThSort scope="row">Artist</Hds::Table::ThSort>`
+    );
+
+    assert.dom('.hds-table__th--sort').hasAttribute('scope', 'col');
+  });
+
   test('if unsorted, the aria-sort attribute value should be set to none', async function (assert) {
     await render(
       hbs`<Hds::Table::ThSort @sortBy='artist' id="data-test-table-th-sort">Artist</Hds::Table::ThSort>`
@@ -78,5 +111,54 @@ module('Integration | Component | hds/table/th-sort', function (hooks) {
     assert
       .dom('#data-test-table-th-sort')
       .hasAttribute('aria-sort', 'descending');
+  });
+
+  test('it renders the expected aria attributes for the sorting and tooltip buttons', async function (assert) {
+    await render(
+      hbs`<Hds::Table::ThSort id="data-test-table-th-sort" @tooltip="More info.">Artist</Hds::Table::ThSort>`
+    );
+    assert
+      .dom('#data-test-table-th-sort .hds-table__th-button--tooltip')
+      .hasAria('label', 'more information');
+    assert
+      .dom('#data-test-table-th-sort .hds-table__th-button--sort')
+      .hasAria('label', 'sort');
+  });
+
+  // ONCLICK
+
+  test('it should call the `@onClick` function if provided', async function (assert) {
+    let isClicked = false;
+    this.set('onClick', () => (isClicked = true));
+    await render(
+      hbs`<Hds::Table::ThSort id="data-test-table-th-sort" @onClick={{this.onClick}}>Artist</Hds::Table::ThSort>`
+    );
+    await click('#data-test-table-th-sort .hds-table__th-button--sort');
+    assert.ok(isClicked);
+  });
+
+  // TOOLTIP
+
+  test('if @tooltip is undefined a tooltip button toggle should not be present', async function (assert) {
+    await render(
+      hbs`<Hds::Table::ThSort id="data-test-table-th-sort">Artist</Hds::Table::ThSort>`
+    );
+
+    assert
+      .dom('#data-test-table-th-sort .hds-table__th-button--tooltip')
+      .doesNotExist();
+  });
+  test('if @tooltip is defined a tooltip should be added to the table cell header', async function (assert) {
+    await render(
+      hbs`<Hds::Table::ThSort @tooltip="More info." id="data-test-table-th-sort">Artist</Hds::Table::ThSort>`
+    );
+
+    assert
+      .dom('#data-test-table-th-sort .hds-table__th-button--tooltip')
+      .exists();
+    // activate the tooltip:
+    await focus('#data-test-table-th-sort .hds-table__th-button--tooltip');
+    // test that the tooltip exists and has the passed in content:
+    assert.dom('.tippy-content').hasText('More info.');
   });
 });
--- packages/components/tests/integration/components/hds/table/th-test.js
+++ packages/components/tests/integration/components/hds/table/th-test.js
@@ -5,7 +5,7 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, focus } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | hds/table/th', function (hooks) {
@@ -18,19 +18,16 @@ module('Integration | Component | hds/table/th', function (hooks) {
     assert.dom('#data-test-table-th').hasClass('hds-table__th');
   });
 
-  test('it has the scope attribute set to column by default', async function (assert) {
+  // CONTENT
+
+  test('it renders text content yielded within the cell', async function (assert) {
     await render(
-      hbs`<Hds::Table::Th id="data-test-table-th">artist</Hds::Table::Th>`
+      hbs`<Hds::Table::Th id="data-test-table-th">Artist</Hds::Table::Th>`
     );
-    assert.dom('#data-test-table-th').hasAttribute('scope', 'col');
+    assert.dom('#data-test-table-th').hasText('Artist');
   });
 
-  test('it has the scope attribute set to row if inside a tbody', async function (assert) {
-    await render(
-      hbs`<Hds::Table><:body as |B|><B.Tr><B.Th id="data-test-table-th">artist</B.Th></B.Tr></:body></Hds::Table>`
-    );
-    assert.dom('#data-test-table-th').hasAttribute('scope', 'row');
-  });
+  // ALIGNMENT
 
   test('it should render with the appropriate `@align` CSS class', async function (assert) {
     await render(
@@ -38,6 +35,8 @@ module('Integration | Component | hds/table/th', function (hooks) {
     );
     assert.dom('#data-test-table-th').hasClass('hds-table__th--align-right');
   });
+
+  // WIDTH
 
   test('it should add inline styles if `@width` is declared', async function (assert) {
     await render(
@@ -48,10 +47,57 @@ module('Integration | Component | hds/table/th', function (hooks) {
       .hasAttribute('style', 'width: 10%; min-width: 10%;');
   });
 
+  // ATTRIBUTES
+
   test('it should support splattributes', async function (assert) {
     await render(
       hbs`<Hds::Table::Th id="data-test-table-th" lang="es">Artist</Hds::Table::Th>`
     );
     assert.dom('#data-test-table-th').hasAttribute('lang', 'es');
+  });
+
+  test('it has the scope attribute set to column by default', async function (assert) {
+    await render(
+      hbs`<Hds::Table::Th id="data-test-table-th">Artist</Hds::Table::Th>`
+    );
+    assert.dom('#data-test-table-th').hasAttribute('scope', 'col');
+  });
+  test('it has the scope attribute set to row if inside a tbody', async function (assert) {
+    await render(
+      hbs`<Hds::Table><:body as |B|><B.Tr><B.Th id="data-test-table-th">Artist</B.Th></B.Tr></:body></Hds::Table>`
+    );
+    assert.dom('#data-test-table-th').hasAttribute('scope', 'row');
+  });
+
+  test('it renders the expected aria attribute for the tooltip button', async function (assert) {
+    await render(
+      hbs`<Hds::Table::Th id="data-test-table-th" @tooltip="More info.">Artist</Hds::Table::Th>`
+    );
+    assert
+      .dom('#data-test-table-th .hds-table__th-button--tooltip')
+      .hasAria('label', 'more information');
+  });
+
+  // TOOLTIP
+
+  test('if @tooltip is undefined a tooltip button toggle should not be present', async function (assert) {
+    await render(
+      hbs`<Hds::Table::Th id="data-test-table-th">Artist</Hds::Table::Th>`
+    );
+
+    assert
+      .dom('#data-test-table-th .hds-table__th-button--tooltip')
+      .doesNotExist();
+  });
+  test('if @tooltip is defined a tooltip should be added to the table cell header', async function (assert) {
+    await render(
+      hbs`<Hds::Table::Th @tooltip="More info." id="data-test-table-th">Artist</Hds::Table::Th>`
+    );
+
+    assert.dom('#data-test-table-th .hds-table__th-button--tooltip').exists();
+    // activate the tooltip:
+    await focus('#data-test-table-th .hds-table__th-button--tooltip');
+    // test that the tooltip exists and has the passed in content:
+    assert.dom('.tippy-content').hasText('More info.');
   });
 });
