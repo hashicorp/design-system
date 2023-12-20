f6b38202f151e4132f4ef973a27827d605bc3ecb
Alex <alex-ju@users.noreply.github.com>
2023-12-20 12:01:25 +0000

    Fix template-registry paths

--- packages/components/src/template-registry.ts
+++ packages/components/src/template-registry.ts
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import type HdsButtonIndexComponent from '@hashicorp/design-system-components/components/hds/button';
-import type HdsInteractiveIndexComponent from '@hashicorp/design-system-components/components/hds/interactive';
+import type HdsButtonIndexComponent from './components/hds/button';
+import type HdsInteractiveIndexComponent from './components/hds/interactive';
 
 export default interface HdsComponentsRegistry {
   HdsButtonComponent: typeof HdsButtonIndexComponent;
