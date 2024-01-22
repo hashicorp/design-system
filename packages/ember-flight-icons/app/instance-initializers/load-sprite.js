7f959b240de84e9615a71a66d18ed382a444387d
Alex <alex-ju@users.noreply.github.com>
2024-01-22 18:12:11 +0000

    Rename `addon` to `src` in `ember-flight-icons`

--- packages/ember-flight-icons/app/instance-initializers/load-sprite.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
-export {
-  default,
-  initialize,
-} from '@hashicorp/ember-flight-icons/instance-initializers/load-sprite';
