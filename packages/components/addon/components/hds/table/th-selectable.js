7b7b157cecba2120625f22d88034f172d27feb55
Cristiano Rastelli <public@didoo.net>
2024-01-23 14:41:35 +0000

    possible fix for the `aria-label` values on rows’ checkboxes not being updated on “select all” operations

--- packages/components/addon/components/hds/table/th-selectable.js
+++ packages/components/addon/components/hds/table/th-selectable.js
@@ -32,15 +32,30 @@ export default class HdsTableThSelectableComponent extends Component {
     let { didInsert } = this.args;
     if (typeof didInsert === 'function') {
       didInsert(checkbox, this.args.selectionKey);
+      // we need to use a custom event listener here because changing the `checked` value via JS
+      // (and this happens with the "select all") doesn't trigger the `change` event
+      // and consequently the `aria-label` won't be automatically updated (and so we have to force it)
+      checkbox.addEventListener(
+        'toggle',
+        this.updateAriaLabel.bind(this),
+        true
+      );
     }
   }
 
   @action
-  willDestroy() {
+  willDestroy(checkbox) {
     super.willDestroy(...arguments);
     let { willDestroy } = this.args;
     if (typeof willDestroy === 'function') {
       willDestroy(this.args.selectionKey);
+      if (checkbox) {
+        checkbox.removeEventListener(
+          'toggle',
+          this.updateAriaLabel.bind(this),
+          true
+        );
+      }
     }
   }
 
@@ -51,5 +66,10 @@ export default class HdsTableThSelectableComponent extends Component {
     if (typeof onSelectionChange === 'function') {
       onSelectionChange(event.target, this.args.selectionKey);
     }
+  }
+
+  updateAriaLabel(event) {
+    // updating the `isSelected` value will trigger the update of the `aria-label` value via the `ariaLabel` getter
+    this.isSelected = event.target.checked;
   }
 }
