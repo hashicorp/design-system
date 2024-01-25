258848a16e0f329b37962fe9cd5a47a0d97e3e44
Alex <alex-ju@users.noreply.github.com>
2024-01-25 11:42:28 +0000

    chore(showcase): update showcase to use on modifier for updating masked input values

--- packages/components/tests/dummy/app/controllers/components/form/masked-input.js
+++ packages/components/tests/dummy/app/controllers/components/form/masked-input.js
@@ -8,19 +8,27 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class FormMaskedInputController extends Controller {
-  @tracked textInputFieldIsInvalid;
-  @tracked textareaFieldIsInvalid;
+  @tracked defaultText = 'Lorem ipsum dolor';
+  @tracked customText = 'Lorem ipsum dolor';
+  @tracked withErrorMessage = 'Lorem ipsum dolor sit amet';
+  @tracked multilineDefaultText = 'Lorem ipsum dolor';
+  @tracked multilineCustomText = 'Lorem ipsum dolor';
+  @tracked multilineWithErrorMessage = 'Lorem ipsum dolor sit amet';
 
   multilineText1 = 'Lorem\nipsum\ndolor';
   multilineText2 = `Lorem
 ipsum
 dolor`;
 
-  @action onFieldInput(args) {
-    if (args.inputControl.localName === 'input') {
-      this.textInputFieldIsInvalid = args.currentLength > args.maxLength;
-    } else if (args.inputControl.localName === 'textarea') {
-      this.textareaFieldIsInvalid = args.currentLength > args.maxLength;
-    }
+  get textInputFieldIsInvalid() {
+    return this.withErrorMessage.length > 20;
+  }
+
+  get textareaFieldIsInvalid() {
+    return this.multilineWithErrorMessage.length > 20;
+  }
+
+  @action updateValue(propName, event) {
+    this[propName] = event.target.value;
   }
 }
