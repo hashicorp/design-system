a0bd10a4ab58cbd07f757ee1cdddd0e9451da6ee
Jory Tindall <jory.tindall@hashicorp.com>
2024-02-13 12:14:22 -0800

    Add documentation for using multiple `Alerts` (#1908)
    Co-authored-by: Cristiano Rastelli <public@didoo.net>

---
+++ showcase/dist/assets/chunk.tests.0dae70ead7719fa95931.js
@@ -0,0 +1,338 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+var __ember_auto_import__;
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "@ember/component":
+/*!***********************************!*\
+  !*** external "@ember/component" ***!
+  \***********************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@ember/component");
+
+/***/ }),
+
+/***/ "@ember/debug":
+/*!*******************************!*\
+  !*** external "@ember/debug" ***!
+  \*******************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@ember/debug");
+
+/***/ }),
+
+/***/ "@ember/object":
+/*!********************************!*\
+  !*** external "@ember/object" ***!
+  \********************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@ember/object");
+
+/***/ }),
+
+/***/ "@ember/runloop":
+/*!*********************************!*\
+  !*** external "@ember/runloop" ***!
+  \*********************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@ember/runloop");
+
+/***/ }),
+
+/***/ "@ember/service":
+/*!*********************************!*\
+  !*** external "@ember/service" ***!
+  \*********************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@ember/service");
+
+/***/ }),
+
+/***/ "@ember/template-factory":
+/*!******************************************!*\
+  !*** external "@ember/template-factory" ***!
+  \******************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@ember/template-factory");
+
+/***/ }),
+
+/***/ "@ember/test-helpers":
+/*!**************************************!*\
+  !*** external "@ember/test-helpers" ***!
+  \**************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@ember/test-helpers");
+
+/***/ }),
+
+/***/ "@ember/test/adapter":
+/*!**************************************!*\
+  !*** external "@ember/test/adapter" ***!
+  \**************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@ember/test/adapter");
+
+/***/ }),
+
+/***/ "@glimmer/component":
+/*!*************************************!*\
+  !*** external "@glimmer/component" ***!
+  \*************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@glimmer/component");
+
+/***/ }),
+
+/***/ "@glimmer/tracking":
+/*!************************************!*\
+  !*** external "@glimmer/tracking" ***!
+  \************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("@glimmer/tracking");
+
+/***/ }),
+
+/***/ "ember":
+/*!************************!*\
+  !*** external "ember" ***!
+  \************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("ember");
+
+/***/ }),
+
+/***/ "ember-cli-test-loader/test-support/index":
+/*!***********************************************************!*\
+  !*** external "ember-cli-test-loader/test-support/index" ***!
+  \***********************************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("ember-cli-test-loader/test-support/index");
+
+/***/ }),
+
+/***/ "../../../../../../private/var/folders/dr/m5szrsjn2qg3mhmds1xpshch0000gq/T/broccoli-84892WyvHMw5vGW2f/cache-349-webpack_bundler_ember_auto_import_webpack/tests.cjs":
+/*!**************************************************************************************************************************************************************************!*\
+  !*** ../../../../../../private/var/folders/dr/m5szrsjn2qg3mhmds1xpshch0000gq/T/broccoli-84892WyvHMw5vGW2f/cache-349-webpack_bundler_ember_auto_import_webpack/tests.cjs ***!
+  \**************************************************************************************************************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("\nmodule.exports = (function(){\n  var d = _eai_d;\n  var r = _eai_r;\n  window.emberAutoImportDynamic = function(specifier) {\n    if (arguments.length === 1) {\n      return r('_eai_dyn_' + specifier);\n    } else {\n      return r('_eai_dynt_' + specifier)(Array.prototype.slice.call(arguments, 1))\n    }\n  };\n  window.emberAutoImportSync = function(specifier) {\n    return r('_eai_sync_' + specifier)(Array.prototype.slice.call(arguments, 1))\n  };\n    d('@hashicorp/design-system-components/components/hds/pagination/numbered', ['@glimmer/component','@glimmer/tracking','@ember/object','@ember/debug','@ember/service','@ember/component','@ember/template-factory'], function() { return __webpack_require__(/*! @hashicorp/design-system-components/components/hds/pagination/numbered */ \"../packages/components/dist/components/hds/pagination/numbered/index.js\"); });\n    d('axe-core', [], function() { return __webpack_require__(/*! axe-core */ \"../node_modules/axe-core/axe.js\"); });\n    d('ember-qunit', ['@ember/test/adapter','ember-cli-test-loader/test-support/index','@ember/runloop','@ember/test-helpers','ember'], function() { return __webpack_require__(/*! ember-qunit */ \"../node_modules/ember-qunit/dist/index.js\"); });\n    d('qunit', [], function() { return __webpack_require__(/*! qunit */ \"../node_modules/qunit/qunit/qunit.js\"); });\n    d('qunit-dom', [], function() { return __webpack_require__(/*! qunit-dom */ \"../node_modules/qunit-dom/dist/es/index.js\"); });\n    d('sinon', [], function() { return __webpack_require__(/*! sinon */ \"../node_modules/sinon/pkg/sinon-esm.js\"); });\n})();\n\n\n//# sourceURL=webpack://__ember_auto_import__/../../../../../../private/var/folders/dr/m5szrsjn2qg3mhmds1xpshch0000gq/T/broccoli-84892WyvHMw5vGW2f/cache-349-webpack_bundler_ember_auto_import_webpack/tests.cjs?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			id: moduleId,
+/******/ 			loaded: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/node module decorator */
+/******/ 	(() => {
+/******/ 		__webpack_require__.nmd = (module) => {
+/******/ 			module.paths = [];
+/******/ 			if (!module.children) module.children = [];
+/******/ 			return module;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"tests": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = globalThis["webpackChunk_ember_auto_import_"] = globalThis["webpackChunk_ember_auto_import_"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	(() => {
+/******/ 		__webpack_require__.nc = undefined;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["vendors-node_modules_axe-core_axe_js-node_modules_ember-qunit_dist_index_js-node_modules_quni-9b87bf","packages_components_dist_components_hds_pagination_numbered_index_js-private_var_folders_dr_m-040d6f"], () => (__webpack_require__("../../../../../../private/var/folders/dr/m5szrsjn2qg3mhmds1xpshch0000gq/T/broccoli-84892WyvHMw5vGW2f/cache-349-webpack_bundler_ember_auto_import_webpack/l.cjs")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["vendors-node_modules_axe-core_axe_js-node_modules_ember-qunit_dist_index_js-node_modules_quni-9b87bf","packages_components_dist_components_hds_pagination_numbered_index_js-private_var_folders_dr_m-040d6f"], () => (__webpack_require__("../../../../../../private/var/folders/dr/m5szrsjn2qg3mhmds1xpshch0000gq/T/broccoli-84892WyvHMw5vGW2f/cache-349-webpack_bundler_ember_auto_import_webpack/tests.cjs")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	__ember_auto_import__ = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
--- website/docs/components/alert/partials/guidelines/guidelines.md
+++ website/docs/components/alert/partials/guidelines/guidelines.md
@@ -26,6 +26,12 @@ Use inline alerts when the message is contextual or specific to the section it�
 
 Use compact alerts when a less prominent alert is needed.
 
+### Ordering Alerts by type
+
+Alert types have an implied hierarchy and should be ordered starting from `page` and descending in hierarchy to `compact`.
+
+![Ordering alerts of different types](/assets/components/alert/ordering-alerts-type.png)
+
 ## Color
 
 <Doc::Layout @spacing="12px">
@@ -56,24 +62,24 @@ Use color logically.
 - **Neutral** to provide general information to the user regarding the current context or relevant actions.
 - **Highlight** to provide general or promotional information to the user prominently.
 - **Success** to indicate a successful action.
-    - Use the success variant sparingly. To communicate success after a user action is performed, use [Toast](/components/toast).
+  - Use the success variant sparingly. To communicate success after a user action is performed, use [Toast](/components/toast).
 - **Warning** to help users avoid an issue. Provide guidance and actions, if possible.
 - **Critical** to indicate critical errors that need immediate action.
 
 ### Critical alerts
 
-Use critical alerts to convey crucial information or errors, such as validation errors that require user attention or action. 
+Use critical alerts to convey crucial information or errors, such as validation errors that require user attention or action.
 
 !!! Do
 
 Use the Alert for more intrusive message communication about errors or critical disruptions at an application, page, or section level where users need to take immediate action.
 
 <Hds::Alert @type="inline" @color="critical" as |A|>
-    <A.Title>You have exceeded 50 applies this month</A.Title>
-    <A.Description>You may only invoke applies that destroy managed resources. Upgrade now and access additional product features, unlimited applies, and increased concurrency.</A.Description>
-    <A.Button @text="Upgrade" @color="secondary" @onClick={{this.noop}} />
-    <A.LinkStandalone @color="secondary" @icon="arrow-right" @iconPosition="trailing" @text="View usage" @href="#" />
-  </Hds::Alert>
+  <A.Title>You have exceeded 50 applies this month</A.Title>
+  <A.Description>You may only invoke applies that destroy managed resources. Upgrade now and access additional product features, unlimited applies, and increased concurrency.</A.Description>
+  <A.Button @text="Upgrade" @color="secondary" @onClick={{this.noop}} />
+  <A.LinkStandalone @color="secondary" @icon="arrow-right" @iconPosition="trailing" @text="View usage" @href="#" />
+</Hds::Alert>
 !!!
 
 !!! Do
@@ -81,13 +87,13 @@ Use the Alert for more intrusive message communication about errors or critical 
 Use the Alert to communicate validation errors. For more details, refer to the [form validation patterns](/patterns/form-patterns?tab=validation).
 
 <Hds::Alert @type="inline" @color="critical" as |A|>
-    <A.Title>Form submission error</A.Title>
-    <A.Description>Correct the formatting of the following fields to update your user profile:
-    </A.Description>
-    <A.Description>
-    <Hds::Link::Inline @href="..." @color="secondary">Email address</Hds::Link::Inline>
-    </A.Description>
-  </Hds::Alert>
+  <A.Title>Form submission error</A.Title>
+  <A.Description>Correct the formatting of the following fields to update your user profile:
+  </A.Description>
+  <A.Description>
+  <Hds::Link::Inline @href="..." @color="secondary">Email address</Hds::Link::Inline>
+  </A.Description>
+</Hds::Alert>
 !!!
 
 !!! Dont
@@ -95,10 +101,10 @@ Use the Alert to communicate validation errors. For more details, refer to the [
 Don't use alerts to provide non-intrusive feedback to users about the failure of an ongoing task or request. For example, a failure while deleting a cluster. Use the [Toast](/components/toast) instead.
 
 <Hds::Alert @type="inline" @color="critical" as |A|>
-    <A.Title>Module error</A.Title>
-    <A.Description>This module encountered an error during publishing. You may need to republish.
-    </A.Description>
-  </Hds::Alert>
+  <A.Title>Module error</A.Title>
+  <A.Description>This module encountered an error during publishing. You may need to republish.
+  </A.Description>
+</Hds::Alert>
 !!!
 
 !!! Dont
@@ -106,9 +112,113 @@ Don't use alerts to provide non-intrusive feedback to users about the failure of
 Don't use alerts to communicate error messages that are not caused by the user. For example, an unsuccessful Vault cluster creation due to a failure while validating the deployment. Use the [Toast](/components/toast) instead.
 
 <Hds::Alert @type="inline" @color="critical" as |A|>
-    <A.Title>Vault cluster update failed</A.Title>
-    <A.Description>**test-cluster_1** configuration update failed.</A.Description>
-  </Hds::Alert>
+  <A.Title>Vault cluster update failed</A.Title>
+  <A.Description>**test-cluster_1** configuration update failed.</A.Description>
+</Hds::Alert>
+!!!
+
+### Ordering Alerts by color
+
+Order multiple Alerts by their importance and impact on the user, starting from `critical`, to communicate an Alert that must be resolved immediately, and descending in order of perceived impact to `neutral`.
+
+![Ordering alerts](/assets/components/alert/ordering-alerts-color.png)
+
+## Placing Alerts
+
+`Inline` and `compact` Alerts can have more meaning if they are placed within the element that is responsible for the Alert. This can help when it’s necessary to have more than one Alert on the page and is relevant for pages that aggregate content like dashboards, or where a specific localized message is necessary.
+
+![Example of multiple Alerts placed to different parts of the UI](/assets/components/alert/combining-contextualized-alerts.png)
+
+### Placement by type
+
+Where an Alert is placed is dependent on the type.
+
+#### Page
+
+Page alerts are placed between the global header navigation and the breadcrumb, next to the left navigation.
+
+![Placement of page alert](/assets/components/alert/alert-placement-page.png =600x*)
+
+#### Inline
+
+Inline alerts can be added to a section or component or inline with content.
+
+![Placement of inline alert](/assets/components/alert/alert-placement-inline.png =600x*)
+
+#### Compact
+
+Compact alerts can be added to a section or component or inline with content.
+
+![Placement of compact Alert](/assets/components/alert/alert-placement-compact.png =600x*)
+
+#### Mixing Alert types
+
+We recommend against mixing different types of Alerts in the same context, as this can unintentionally convey that different Alert types have the same hierarchical impact.
+
+!!! Dont
+
+Don’t mix different Alert types in the same context.
+
+![Example of different Alert types in the same context](/assets/components/alert/mixing-alert-types-dont.png)
+!!!
+
+!!! Do
+
+Instead, be more specific with how the Alerts are placed depending on what the message applies to.
+
+![Example of different Alert type placements](/assets/components/alert/mixing-alert-types-do.png)
+!!!
+
+### Ordering Alerts by color
+
+Order multiple Alerts by their importance and impact on the user, starting from `critical`, to communicate an Alert that must be resolved immediately, and descending in order of perceived impact to `neutral`.
+
+![Ordering alerts](/assets/components/alert/ordering-alerts-color.png)
+
+## Placing Alerts
+
+`Inline` and `compact` Alerts can have more meaning if they are placed within the element that is responsible for the Alert. This can help when it’s necessary to have more than one Alert on the page and is relevant for pages that aggregate content like dashboards, or where a specific localized message is necessary.
+
+![Example of multiple Alerts placed to different parts of the UI](/assets/components/alert/combining-contextualized-alerts.png)
+
+### Placement by type
+
+Where an Alert is placed is dependent on the type.
+
+#### Page
+
+Page alerts are placed between the global header navigation and the breadcrumb, next to the left navigation.
+
+![Placement of page alert](/assets/components/alert/alert-placement-page.png =600x*)
+
+#### Inline
+
+Inline alerts can be added to a section or component or inline with content.
+
+![Placement of inline alert](/assets/components/alert/alert-placement-inline.png =600x*)
+
+#### Compact
+
+Compact alerts can be added to a section or component or inline with content.
+
+![Placement of compact Alert](/assets/components/alert/alert-placement-compact.png =600x*)
+
+#### Mixing Alert types
+
+We recommend against mixing different types of Alerts in the same context, as this can unintentionally convey that different Alert types have the same hierarchical impact.
+
+!!! Dont
+
+Don’t mix different Alert types in the same context.
+
+![Example of different Alert types in the same context](/assets/components/alert/mixing-alert-types-dont.png)
+!!!
+
+!!! Do
+
+Instead, be more specific with how the Alerts are placed depending on what the message applies to.
+
+![Example of different Alert type placements](/assets/components/alert/mixing-alert-types-do.png)
 !!!
 
 ## Icons
@@ -158,22 +268,39 @@ We recommend keeping `critical` alerts non-dismissible as they are essential to 
 </Hds::Alert>
 !!!
 
-## Placement
+## Multiple alerts
 
-### Page
+In general, displaying multiple alerts on the same page is discouraged as it can negatively impact the page hierarchy and unnecessarily shift emphasis away from the main content on the page.
 
-Page alerts are placed between the global header navigation and the breadcrumb, next to the left navigation.
+However, depending on the logic within the application and the type of content being expressed to the user, displaying more than one Alert on a page may be necessary.
 
-![Placement of page alert](/assets/components/alert/alert-placement-page.png =600x*)
+### Alerts of the same color
 
-### Inline
+While it might not always be possible depending on the type of content and intended action, try to refrain from displaying more than one of the same `color` Alert on the same page. This adds unnecessary visual weight to the page and may result in the user being confused about which Alert is more important or which might require attention first.
 
-Inline alerts can be added to a section or component or inline with content.
+![Example of multiple Alerts of the same color](/assets/components/alert/multiple-color-alerts-dont.png)
 
-![Placement of inline alert](/assets/components/alert/alert-placement-inline.png =600x*)
+To avoid this, try combining relevant or overlapping content into a single Alert with a more concise narrative, or by translating the related messages into a list.
 
-### Compact
+![Example of multiple Alerts combined into one](/assets/components/alert/multiple-color-alerts-do.png)
 
-Compact alerts can be added to a section or component or inline with content.
+![Multiple Alert messages combined in a bulleted list](/assets/components/alert/multiple-color-alerts-do-bulleted-list.png)
 
-![Placement of compact Alert](/assets/components/alert/alert-placement-compact.png =600x*)
+### Alerts of the same type
+
+While not recommended for `page` type Alerts, it may be necessary to have more than one `inline` or `compact` Alert on a page. If this is the case, ensure that the Alerts are [ordered](#ordering-of-alerts) by impact and [placed](#placing-alerts) in areas where they provide the most relevance.
+
+### Alerts within a Reveal
+
+**As a last resort**, when multiple Alerts of the same type are necessary, display the Alerts that require immediate attention or have the most impact to the user within the page while obfuscating additional Alerts within a [Reveal](/components/reveal). We recommend against hiding `critical` and `warning` Alerts in this manner as they generally must be addressed quickly to avoid a negative outcome.
+
+!!! Warning
+
+This method should be **rare and infrequent**, and should only be used if the number of Alerts of the same type exceeds two. Instead, effort should be put into placing Alerts that matter most to the user and in ways that make them easy to resolve.
+!!!
+
+This pattern is somewhat experimental, if you find a use case for it we recommend performing a usability test and collecting user feedback as a means to validate the pattern. The HDS team would love to discuss your findings through our normal [support channels](/about/support).
+
+![Multiple alerts when a Reveal is closed](/assets/components/alert/alerts-within-reveal-closed.png)
+
+![Multiple alerts when a Reveal is open](/assets/components/alert/alerts-within-reveal-open.png)
