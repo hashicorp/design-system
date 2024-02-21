120fb7871980d33fe9a3af2eb99e6ee512e076c6
Cristiano Rastelli <public@didoo.net>
2024-02-21 22:09:08 +0000

    converted `hds-float-element` to use Popper instead of Floating UI

--- packages/components/src/modifiers/hds-float-element.js
+++ packages/components/src/modifiers/hds-float-element.js
@@ -2,204 +2,125 @@
 // code: https://github.com/CrowdStrike/ember-velcro/blob/main/ember-velcro/src/modifiers/velcro.ts
 // repo: https://github.com/CrowdStrike/ember-velcro
 
-import Modifier from 'ember-modifier';
+import { modifier } from 'ember-modifier';
 import { assert } from '@ember/debug';
-import { registerDestructor } from '@ember/destroyable';
 
-import {
-  autoUpdate,
-  computePosition,
-  flip,
-  // hide,
-  offset,
-  shift,
-  // size,
-} from '@floating-ui/dom';
+import { createPopper } from '@popperjs/core';
 
-export default class HdsFloatElementModifier extends Modifier {
-  didSetup = false;
-  // isDialogOpen = false;
-  // isDialogForcedOpen = false;
+function getPopperOptions(namedOptions) {
+  const {
+    floatingElementPlacement = 'bottom',
+    floatingElementPositionStrategy = 'absolute', // if we use `fixed` then the overscroll of the body makes the dialog look weird when the page is overscrolled
+    floatingElementZIndex,
+    floatingElementOffsetOptions,
+    floatingElementEnableCollisionDetection,
+  } = namedOptions;
 
-  modify(
-    // the element that "floats" next to the anchor (whose position is calculated in relation to the anchor)
-    // notice: this is the element the Ember modifier is attached to
-    floatingElement,
-    // the element that acts as an "anchor" for the "floating" element
-    // notice: it's expressed as argument for modifier and it can be a DOM node direclty, or a string (CSS selector, will be converted below to an actual DOM node)
-    // (positional arguments of the modifier)
-    [_anchorTarget],
-    // (named arguments of the modifier)
-    {
-      floatingElementPlacement = 'bottom',
-      floatingElementPositionStrategy = 'absolute', // if we use `fixed` then the overscroll of the body makes the dialog look weird when the page is overscrolled
-      floatingElementZIndex = 0,
-      floatingElementOffsetOptions = 4,
-      floatingLogicFlipOptions,
-      floatingLogicShiftOptions,
-      floatingElementEnableCollisionDetection = false,
-      floatingLogicMiddlewareExtra = [],
-    }
-  ) {
-    const anchorElement =
-      typeof _anchorTarget === 'string'
-        ? document.querySelector(_anchorTarget)
-        : _anchorTarget;
+  const options = { modifiers: [] };
 
-    assert(
-      '`hds-float-element` modifier - no anchoring element defined',
-      anchorElement instanceof HTMLElement ||
-        anchorElement instanceof SVGElement
-    );
+  options.placement = floatingElementPlacement;
+  options.strategy = floatingElementPositionStrategy;
 
-    assert(
-      '`hds-float-element` modifier -  no floating element defined',
-      floatingElement instanceof HTMLElement ||
-        floatingElement instanceof SVGElement
-    );
-
-    assert(
-      '`hds-float-element` modifier - anchoring and floating elements cannot be the same element',
-      floatingElement !== anchorElement
-    );
-
-    assert(
-      '@floatingLogicMiddlewareExtra must be an array of one or more objects',
-      Array.isArray(floatingLogicMiddlewareExtra)
-    );
-
-    this.anchor = anchorElement;
-    this.floating = floatingElement;
-
-    Object.assign(this.floating.style, {
-      position: floatingElementPositionStrategy,
-      zIndex: floatingElementZIndex,
-      top: '0',
-      left: '0',
+  if (floatingElementOffsetOptions) {
+    options.modifiers.push({
+      // https://popper.js.org/docs/v2/modifiers/offset/
+      name: 'offset',
+      options: {
+        offset: floatingElementOffsetOptions,
+      },
     });
-
-    if (!this.didSetup) {
-      // this.#addEventsToAnchorElement();
-      this.didSetup = true;
-    }
-
-    let update = async () => {
-      let { middlewareData, x, y } = await computePosition(
-        this.anchor,
-        this.floating,
-        {
-          // we build dynamically the list of middleware functions to invoke, depending on the options provided
-          middleware: [
-            offset(floatingElementOffsetOptions),
-            // we use this trick to conditionally add the `flip()` and `shift()` middleware only if specifically enabled
-            ...(floatingElementEnableCollisionDetection
-              ? [
-                  flip(floatingLogicFlipOptions),
-                  shift(floatingLogicShiftOptions),
-                ]
-              : []),
-            // TODO! commenting this for now, will need to make this conditional to some argument (and understand how this relates to the `@height` argument)
-            // size({
-            //   apply: ({ availableWidth, availableHeight, middlewareData }) => {
-            //     middlewareData.size = { availableWidth, availableHeight };
-            //   },
-            // }),
-            ...floatingLogicMiddlewareExtra,
-            // TODO! decide if we want to expose this with some property/argument
-            // hide({ strategy: 'referenceHidden' }),
-            // TODO! probably this is not needed because we handle the `esc` key in the underlying `MenuPrimitive` sub-component
-            // hide({ strategy: 'escaped' }),
-          ],
-          placement: floatingElementPlacement,
-          strategy: floatingElementPositionStrategy,
-        }
-      );
-
-      // see above, probably we dont' want this
-      // const { referenceHidden } = middlewareData.hide;
-
-      Object.assign(this.floating.style, {
-        top: `${y}px`,
-        left: `${x}px`,
-        // TODO! commenting this for now, will need to make this conditional to some argument (and understand how this relates to the `@height` argument)
-        // maxHeight: `${middlewareData.size.availableHeight - 10}px`,
-        margin: 0,
-        // see above, probably we dont' want this
-        // visibility: referenceHidden ? 'hidden' : 'visible',
-      });
-    };
-
-    update();
-
-    let cleanup = autoUpdate(this.anchor, this.floating, update);
-
-    registerDestructor(this, cleanup);
   }
 
-  // #addEventsToAnchorElement() {
-  //   this.anchor.addEventListener(
-  //     'click',
-  //     this.#toggleForcedShowDialog.bind(this)
-  //   );
-  //   this.anchor.addEventListener('focus', this.#showDialog.bind(this));
-  //   this.anchor.addEventListener('mouseenter', this.#showDialog.bind(this));
-  //   this.anchor.addEventListener('blur', this.#hideDialog.bind(this));
-  //   this.anchor.addEventListener('mouseleave', this.#hideDialog.bind(this));
-  // }
+  if (floatingElementEnableCollisionDetection) {
+    options.modifiers.push(
+      {
+        // https://popper.js.org/docs/v2/modifiers/flip/
+        name: 'flip',
+        enabled: true,
+      },
+      {
+        // https://popper.js.org/docs/v2/modifiers/prevent-overflow/
+        name: 'preventOverflow',
+        options: {
+          padding: 8,
+        },
+      }
+    );
+  }
 
-  // because of how the modifier is implemented (and there's no alternative) the element may be already destroyed
-  // when this function is invoked if we use a `registerDestructor`
-  // so unfortunately we have to comment this out
+  if (floatingElementZIndex) {
+    options.modifiers.push({
+      // https://popper.js.org/docs/v2/modifiers/#custom-modifiers
+      name: 'floatingElementZIndex',
+      enabled: true,
+      phase: 'main',
+      fn: ({ state }) => {
+        state.styles.popper.zIndex = floatingElementZIndex;
+      },
+    });
+  }
+
+  // TODO add custom modifier (conditional option?) to resize vertically the popper if the height is larger than the available space
+  // https://popper.js.org/docs/v2/utils/detect-overflow/#example
   //
-  // #removeEventsToTriggerElement() {
-  //   debugger;
-  //   this.anchor.removeEventListener(
-  //     'click',
-  //     this.#toggleForcedShowDialog.bind(this)
-  //   );
-  //   this.anchor.removeEventListener('focus', this.#showDialog.bind(this));
-  //   this.anchor.removeEventListener('mouseenter', this.#showDialog.bind(this));
-  //   this.anchor.removeEventListener('blur', this.#hideDialog.bind(this));
-  //   this.anchor.removeEventListener('mouseleave', this.#hideDialog.bind(this));
-  // }
+  // ➔ we can use this community package:
+  // https://www.npmjs.com/package/popper-max-size-modifier
+  //
 
-  // #setOpenStatus() {
-  //   if (this.isDialogForcedOpen || this.isDialogOpen) {
-  //     this.floating.setAttribute('open', true);
-  //   } else {
-  //     this.floating.removeAttribute('open');
-  //   }
-  // }
+  // uncomment to debug first rendering [NOTICE: doesn't seem to work]
+  // options.modifiers.push({
+  //   // https://popper.js.org/docs/v2/lifecycle/#hook-into-the-lifecycle
+  //   onFirstUpdate: (state) => {
+  //     console.log('Popper `onFirstUpdate` invoked with state', state);
+  //   },
+  // });
 
-  // #showDialog() {
-  //   this.isDialogOpen = true;
-  //   this.#setOpenStatus();
-  // }
-
-  // #hideDialog() {
-  //   this.isDialogOpen = false;
-  //   this.#setOpenStatus();
-  // }
-
-  // #toggleForcedShowDialog(event) {
-  //   event.preventDefault();
-  //   if (this.isDialogForcedOpen) {
-  //     this.isDialogForcedOpen = false;
-  //     this.isDialogOpen = false;
-  //   } else {
-  //     this.isDialogForcedOpen = true;
-  //   }
-  //   this.#setOpenStatus();
-  // }
-
-  // In case we want to change the behaviour and have only the "click" even to open the dialog we may want to add a handler for click outside.
-  // see https://github.com/hashicorp/cloud-ui/blob/main/addons/core/addon/components/menu/index.js#L60-L66
-  // @action
-  // setupClickOutside(el) {
-  //   document.addEventListener(
-  //     'click',
-  //     (evt) => this.clickOutside(evt, el),
-  //     true
-  //   );
-  // }
+  return options;
 }
+
+// Notice: we use a function-based modifier here instead of a class-based one
+// because it's quite simple in its logic, and doesn't require injecting services
+// see: https://github.com/ember-modifier/ember-modifier#function-based-modifiers
+
+export default modifier((element, positional, named) => {
+  // the element that "floats" next to the anchor (whose position is calculated in relation to the anchor)
+  // notice: this is the element the Ember modifier is attached to
+  const floatingElement = element;
+  // the element that acts as an "anchor" for the "floating" element
+  // notice: it's expressed as argument for modifier and it can be a DOM node direclty, or a string (CSS selector, will be converted below to an actual DOM node)
+  // (positional arguments of the modifier)
+  const [_anchorTarget] = positional;
+  const popperOptions = getPopperOptions(named);
+
+  const anchorElement =
+    typeof _anchorTarget === 'string'
+      ? document.querySelector(_anchorTarget)
+      : _anchorTarget;
+
+  assert(
+    '`hds-float-element` modifier - no anchoring element defined',
+    anchorElement instanceof HTMLElement || anchorElement instanceof SVGElement
+  );
+
+  assert(
+    '`hds-float-element` modifier -  no floating element defined',
+    floatingElement instanceof HTMLElement ||
+      floatingElement instanceof SVGElement
+  );
+
+  assert(
+    '`hds-float-element` modifier - anchoring and floating elements cannot be the same element',
+    floatingElement !== anchorElement
+  );
+
+  const popperInstance = createPopper(
+    anchorElement,
+    floatingElement,
+    popperOptions
+  );
+
+  // this (teardown) function is run when the element is removed
+  return () => {
+    popperInstance?.destroy();
+  };
+});
