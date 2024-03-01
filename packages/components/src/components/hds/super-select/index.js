12e28fb555e3432899209859899147dcdb04ef9c
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-02-29 17:15:12 -0800

    HDS-3143 Add PowerSelectMultiple variant to spike component, update Showcase, fix list item styles to remove stray bullet

--- packages/components/src/components/hds/super-select/index.js
+++ packages/components/src/components/hds/super-select/index.js
@@ -4,10 +4,9 @@
  */
 
 import Component from '@glimmer/component';
-// import Prism from 'prismjs';
-// import PowerSelect from 'ember-power-select/components/power-select';
+import { assert } from '@ember/debug';
 
-// import PowerSelect from 'ember-power-select/components/power-select';
+export const SELECTION_TYPES = ['single', 'multiple'];
 
 export default class HdsSuperSelectIndexComponent extends Component {
   // UNCOMMENT THIS IF YOU NEED A CONSTRUCTOR
@@ -15,6 +14,24 @@ export default class HdsSuperSelectIndexComponent extends Component {
   //   super(...arguments);
   //   // ADD YOUR ASSERTIONS HERE
   // }
+
+  /**
+   * Get selection type
+   * @method selectionType
+   * @return {string} The selection type.
+   */
+  get selectionType() {
+    let { selectionType = 'single' } = this.args;
+
+    assert(
+      `@selectionType for "Hds::SuperSelect" must be one of the following: ${SELECTION_TYPES.join(
+        ', '
+      )}; received: ${selectionType}`,
+      SELECTION_TYPES.includes(selectionType)
+    );
+
+    return selectionType;
+  }
 
   /**
    * Get the class names to apply to the component.
