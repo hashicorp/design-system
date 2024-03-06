85516f2e6f4c0eb81d8166268a3a6f4beff14e4f
Natalie Smith <nsmith@hashicorp.com>
2024-03-06 12:23:11 -0800

    chore: add ember-truth-helper and LinkToExternal types to glint registry

--- packages/components/src/types/global.d.ts
+++ packages/components/src/types/global.d.ts
@@ -1,1 +1,14 @@
 import '@glint/environment-ember-loose';
+
+declare module 'ember-truth-helpers';
+
+import { LinkTo } from "@ember/routing";
+
+import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry extends EmberTruthRegistry /*, other addon registries */ {
+    // local entries
+    "LinkToExternal": typeof LinkTo;
+  }
+}
