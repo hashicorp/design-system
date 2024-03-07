2b028e096faa8fbe830fe1ebb4f0860db0c6832a
wenincode <tyler.wendlandt@hashicorp.com>
2024-03-07 16:38:59 -0700

    refactor: convert  to typescript

---
+++ packages/components/src/components/hds/dismiss-button/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+export interface HdsDismissButtonSignature {
+  Args: {
+    ariaLabel?: string;
+  },
+  Element: HTMLButtonElement;
+}
+
+export default class HdsDismissButtonIndexComponent extends Component<HdsDismissButtonSignature> {
+  /**
+   * @param ariaLabel
+   * @type {string}
+   * @default 'Dismiss'
+   */
+  get ariaLabel() {
+    return this.args.ariaLabel ?? 'Dismiss';
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'Hds::DismissButton': typeof HdsDismissButtonIndexComponent;
+    'hds/dismiss-button': typeof HdsDismissButtonIndexComponent;
+  }
+}
