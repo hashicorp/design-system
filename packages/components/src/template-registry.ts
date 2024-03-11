94e2e6759787f8e52f764b0a04e2450d0a4e1cb4
Natalie Smith <nsmith@hashicorp.com>
2024-03-11 16:09:17 -0700

    refactor: put all component registry declarations in template-registry

--- packages/components/src/template-registry.ts
+++ packages/components/src/template-registry.ts
@@ -3,12 +3,30 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import type HdsLinkToModelsHelper from './helpers/hds-link-to-models';
 import type HdsButtonIndexComponent from './components/hds/button';
 import type HdsDismissButtonIndexComponent from './components/hds/dismiss-button';
 import type HdsInteractiveIndexComponent from './components/hds/interactive';
+import type HdsLinkToQueryHelper from './helpers/hds-link-to-query';
 
 export default interface HdsComponentsRegistry {
-  HdsButtonComponent: typeof HdsButtonIndexComponent;
-  HdsDismissComponent: typeof HdsDismissButtonIndexComponent;
   HdsInteractiveComponent: typeof HdsInteractiveIndexComponent;
+  // Button
+  'Hds::Button': typeof HdsButtonIndexComponent;
+  'hds/button': typeof HdsButtonIndexComponent;
+  HdsButton: typeof HdsButtonIndexComponent;
+
+  // Dismiss button
+  'Hds::DismissButton': typeof HdsDismissButtonIndexComponent;
+  'hds/dismiss-button': typeof HdsDismissButtonIndexComponent;
+  HdsDismissButton: typeof HdsDismissButtonIndexComponent;
+
+  // Interactive
+  'Hds::Interactive': typeof HdsInteractiveIndexComponent;
+  'hds/interactive': typeof HdsInteractiveIndexComponent;
+  HdsInteractive: typeof HdsInteractiveIndexComponent;
+
+  // Helpers
+  'hds-link-to-models': typeof HdsLinkToModelsHelper;
+  'hds-link-to-query': typeof HdsLinkToQueryHelper;
 }
--- packages/components/src/types/global.d.ts
+++ packages/components/src/types/global.d.ts
@@ -5,9 +5,10 @@ declare module 'ember-truth-helpers';
 import { LinkTo } from "@ember/routing";
 
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
+import type HdsComponentsRegistry from '../template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry extends EmberTruthRegistry /*, other addon registries */ {
+  export default interface Registry extends EmberTruthRegistry, HdsComponentsRegistry /*, other addon registries */ {
     // local entries
     "LinkToExternal": typeof LinkTo;
   }
