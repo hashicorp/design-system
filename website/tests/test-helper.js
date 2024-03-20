00e50273c734df645d5ce09dc48811d552c15043
Melanie Sumner <melanie@hashicorp.com>
2024-03-20 18:58:50 -0500

    update test helper to include a11y run options

--- website/tests/test-helper.js
+++ website/tests/test-helper.js
@@ -10,11 +10,25 @@ import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import { setRunOptions } from 'ember-a11y-testing/test-support';
-import { globalAxeOptions } from 'website/tests/a11y-helper';
 
 setApplication(Application.create(config.APP));
 
-setRunOptions(globalAxeOptions);
+setRunOptions({
+  runOnly: {
+    type: 'tag',
+    values: [
+      'wcag2a',
+      'wcag2aa',
+      'wcag21a',
+      'wcag21aa',
+      'wcag22aa',
+      'best-practice',
+      'EN-301-549',
+      'ACT',
+    ],
+  },
+  include: [['#ember-testing-container']],
+});
 
 setup(QUnit.assert);
 
