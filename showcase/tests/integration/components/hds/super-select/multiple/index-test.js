c851c4d4bf08ae66dd8da87ab584239dc8bb0cf5
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-03-25 16:38:20 -0700

    HDS-3221 Stub out files for SuperSelect components

---
+++ showcase/tests/integration/components/hds/super-select/multiple/index-test.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module(
+  'Integration | Component | hds/super-select/multiple/index',
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    test('it should render the component with a CSS class that matches the component name', async function (assert) {
+      await render(
+        hbs`<Hds::SuperSelect::Multiple id="test-super-select-multiple" />`
+      );
+      assert
+        .dom('#test-super-select-multiple')
+        .hasClass('hds-super-select-multiple');
+    });
+  }
+);
