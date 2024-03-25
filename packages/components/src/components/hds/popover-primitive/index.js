33eb547d15e8151868e04980befbdcc2e4b0f86e
Cristiano Rastelli <public@didoo.net>
2024-03-25 18:25:50 +0000

    WIP `PopoverPrimitive`
    sq

---
+++ packages/components/src/components/hds/popover-primitive/index.js
@@ -0,0 +1,315 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { warn } from '@ember/debug';
+import { guidFor } from '@ember/object/internals';
+import { modifier } from 'ember-modifier';
+
+import registerEvent from '../../../modifiers/hds-register-event';
+import floatPopoverModifier from '../../../modifiers/hds-float-popover';
+
+// https://github.com/oddbird/popover-polyfill?tab=readme-ov-file#with-npm
+// this is needed until Firefox officially supports the Popover API
+// see: https://wiki.mozilla.org/Release_Management/Release_owners
+import {
+  isSupported as isPopoverApiSupported,
+  apply as applyPopoverApiPolyfill,
+} from '@oddbird/popover-polyfill/fn';
+
+export default class HdsPopoverPrimitiveComponent extends Component {
+  @tracked toggleElement;
+  @tracked popoverElement;
+  @tracked isOpen = this.args.isOpen ?? false;
+  @tracked isForcedOpen = false;
+  @tracked isClosing = false;
+  // this will enable "click" events for the toggle
+  @tracked enableClickEvents = this.args.enableClickEvents ?? false;
+  // this will enable "soft" events for the toggle ("hover" and "focus")
+  @tracked enableSoftEvents = this.args.enableSoftEvents ?? false;
+  // we'll use this flag to overwrite the popover positioning strategy
+  // this is specifically done for Firefox: currently it doesn't support it, but will soon (we need Firefox 127 to support the last 2 versions)
+  // see: https://whattrainisitnow.com/release/?version=127
+  // see: https://github.com/oddbird/popover-polyfill/blob/main/src/popover.ts#L15
+  @tracked isPopoverApiSupported = isPopoverApiSupported();
+  @tracked isPopoverApiPolyfilled = HTMLElement.prototype.popoverIsPolyfilled;
+
+  /**
+   * Generates a unique ID for the "popover" (will be used in the `popovertarget` attribute of the toggle button)
+   *
+   * @param popoverId
+   */
+  popoverId = 'popover-' + guidFor(this);
+
+  setupMenuPrimitiveContainer = modifier(
+    (element) => {
+      this.containerElement = element;
+
+      // if the Popover API is not supported we need to polyfill it
+      if (!this.isPopoverApiSupported) {
+        warn(
+          "The browser used does not support the Popover API so it's been emulated and some functionalities may not work as expected. Please check the minimum browser requirements.",
+          {
+            id: 'hds-popover.no-popover-api-support.polyfill-applied',
+          }
+        );
+        // this function polyfills quite a few DOM methods and adds emulation for the Popover API
+        // see: https://github.com/oddbird/popover-polyfill/blob/main/src/popover.ts#L123
+        applyPopoverApiPolyfill();
+
+        // CONTENT WARNING: TERRIBLE HACK BELOW!
+        // we need to assign a special flag to be able to determine (after the polyfill is applied)
+        // if the Popover API has been polyfilled or not, to change position strategi for Firefox (see below)
+        // see: https://github.com/oddbird/popover-polyfill/issues/189
+        Object.defineProperties(HTMLElement.prototype, {
+          popoverIsPolyfilled: {
+            value: true,
+            writable: false,
+            enumerable: false,
+            configurable: true,
+          },
+        });
+      }
+
+      // we register the "soft" events
+      if (this.enableSoftEvents) {
+        registerEvent(this.containerElement, ['mouseenter', this.onMouseEnter]);
+        registerEvent(this.containerElement, ['mouseleave', this.onMouseLeave]);
+        registerEvent(this.containerElement, ['focusin', this.onFocusIn]);
+      }
+      // we always want the focusOut event
+      registerEvent(this.containerElement, ['focusout', this.onFocusOut]);
+    },
+    { eager: false }
+  );
+
+  setupMenuPrimitiveToggle = modifier(
+    (element) => {
+      this.toggleElement = element;
+
+      if (this.args.toggleAriaLabel) {
+        this.toggleElement.setAttribute(
+          'aria-label',
+          this.args.toggleAriaLabel
+        );
+      }
+
+      this.toggleElement.setAttribute(
+        'aria-expanded',
+        this.isOpen ? 'true' : 'false'
+      );
+
+      // for the click events we don't have `onclick` event listeners,
+      // we rely on the `popovertarget` attribute provided by the Popover API
+      // which does all the magic for us without needing JS code
+      // (notice: to work it needs to be applied to a button)
+      if (this.enableClickEvents) {
+        this.toggleElement.setAttribute('popovertarget', this.popoverId);
+      }
+
+      // when the toggle supports both soft events and click events
+      // we want the popover to be explicitly dismissed when the toggle was clicked
+      // (so mouseout is disabled)
+      if (this.enableClickEvents && this.enableSoftEvents) {
+        registerEvent(this.toggleElement, ['click', this.toggleForcedOpen]);
+      }
+    },
+    { eager: false }
+  );
+
+  setupMenuPrimitivePopover = modifier(
+    (element) => {
+      this.popoverElement = element;
+
+      this.popoverElement.id = this.popoverId;
+
+      // this should be an extremely edge case, but in the case the popover needs to be initially forced to be open
+      // we need to use the "manual" state to support the case of multiple "menus" opened at the same time
+      // IMPORTANT! if a "popover" is set to "open" with a "manual" state, then it can't be closed via `esc` and `click outside`
+      if (this.args.isOpen) {
+        this.popoverElement.popover = 'manual';
+        this.popoverElement.showPopover();
+      } else {
+        this.popoverElement.popover = 'auto';
+      }
+
+      // Register "onBeforeToggle" + "onToggle" callback functions to be called when a native 'toggle' event is dispatched
+      registerEvent(this.popoverElement, [
+        'beforetoggle',
+        this.onBeforeTogglePopover,
+      ]);
+      registerEvent(this.popoverElement, ['toggle', this.onTogglePopover]);
+
+      // apply the "float-popover" modifier to the "popover" element
+      // (notice: this function runs the first time when the element the modifier was applied to is inserted into the DOM, and it autotracks while running. Any tracked values that it accesses will be tracked, including the arguments it receives, and if any of them changes, the function will run again)
+      // this modifiers uses the Floating UI library to provide:
+      // - positioning of the "popover" in relation to the "toggle"
+      // - collision detection (optional)
+      floatPopoverModifier(
+        this.popoverElement, // element the modifier is attached to
+        [this.toggleElement], // positional arguments
+        { popoverOptions: this.popoverOptions } // named arguments
+      );
+    },
+    { eager: false }
+  );
+
+  // this is used to intercept the arguments and overwrihe the "strategy" if the Popover API has been polyfilled
+  // this is specifically done for Firefox: currently it doesn't support it, but will soon (we need Firefox 127 to support the last 2 versions)
+  // see: https://wiki.mozilla.org/Release_Management/Release_owners
+  get popoverOptions() {
+    const popoverOptions = this.args.popoverOptions;
+    // if the Popover API is not supported (polyfill applied for the first time) of it's already been polyfilled (see above)
+    if (!this.isPopoverApiSupported || this.isPopoverApiPolyfilled) {
+      // when using the "absolute" strategy, the presence of a parent with "relative" position leads to wrong layout rendering (known issue in the polyfill library)
+      // see: https://github.com/oddbird/popover-polyfill/tree/main?tab=readme-ov-file#caveats
+      popoverOptions.popoverPositionStrategy = 'fixed';
+    }
+    return popoverOptions;
+  }
+
+  @action
+  showPopover() {
+    console.log('showPopover invoked');
+    this.popoverElement.showPopover();
+  }
+
+  @action
+  hidePopover() {
+    console.log('hidePopover invoked');
+    this.popoverElement.hidePopover();
+  }
+
+  @action
+  togglePopover() {
+    console.log('togglePopover invoked', this.isOpen);
+    this.popoverElement.togglePopover();
+  }
+
+  // fired just _before_ the "popover" is shown or hidden
+  @action
+  onBeforeTogglePopover(event) {
+    console.log('onBeforeTogglePopover invoked', event);
+    if (event.newState === 'closed') {
+      // we need this flag to check if it's in the "closing" process,
+      // because the browser automatically returns the focus to the "trigger" button
+      // and this would re-open immediately the popover because of the `focusin` event
+      this.isClosing = true;
+    }
+  }
+
+  // fired just _after_ the "popover" is shown or hidden
+  @action
+  onTogglePopover(event) {
+    console.log('onTogglePopover invoked', event, event.newState, this.isOpen);
+
+    if (event.newState === 'open') {
+      console.log('Popover has been shown');
+      this.isOpen = true;
+      this.toggleElement.setAttribute('aria-expanded', 'true');
+
+      // we call the "onOpen" callback if it exists (and is a function)
+      let { onOpen } = this.args;
+      if (typeof onOpen === 'function') {
+        onOpen();
+      }
+    } else {
+      console.log('Popover has been hidden');
+      // if the popover was initially forced to be open (using the "manual" state) then revert its status to `auto` once the user interacts with it
+      if (this.args.isOpen) {
+        this.popoverElement.popover = 'auto';
+      }
+      this.isOpen = false;
+      this.isForcedOpen = false;
+      // reset the "isClosing" flag (the `toggle` event is fired _after_ the popover is closed)
+      this.isClosing = false;
+      this.toggleElement.setAttribute('aria-expanded', 'false');
+      // we call the "onClose" callback if it exists (and is a function)
+      let { onClose } = this.args;
+      if (typeof onClose === 'function') {
+        onClose();
+      }
+    }
+  }
+
+  @action
+  onMouseEnter() {
+    console.log('onMouseEnter invoked');
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    this.showPopover();
+  }
+
+  @action
+  onFocusIn() {
+    console.log('onFocusIn invoked');
+    // don't re-open the popover if the focus is returned because the closing
+    if (!this.isClosing) {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      this.showPopover();
+    }
+  }
+
+  @action
+  onMouseLeave() {
+    console.log('onMouseLeave invoked');
+    if (!this.isForcedOpen) {
+      this.timer = setTimeout(this.hidePopover.bind(this), 500);
+    }
+  }
+
+  @action
+  onFocusOut(event) {
+    console.log('onFocusOut invoked');
+    // TODO! discuss with Alex if/why we need this check here
+    // due to inconsistent implementation of relatedTarget across browsers we use the activeElement as a fallback
+    // if the related target is not part of the disclosed content we close the disclosed container
+    if (
+      !this.containerElement.contains(
+        event.relatedTarget || document.activeElement
+      )
+    ) {
+      this.hidePopover();
+    }
+  }
+
+  @action
+  toggleForcedOpen(event) {
+    console.log(
+      'toggleForcedOpen invoked',
+      event,
+      this.isOpen,
+      this.isForcedOpen
+    );
+
+    // we want to have "forced" opening only if the trigger has been explicitly "clicked"
+    // (an "enter" key pressed when the toggle is focus would trigger the click but it would be strange for the consumer to have the toggle re-open the popover)
+    // this doesn't work in Firefox
+    // const isPointerEvent = event.pointerType && event.pointerType !== '';
+    // this works, using event.details (see: https://developer.mozilla.org/en-US/docs/Web/API/UIEvent/detail)
+    const isPointerEvent = event.detail && event.detail > 0;
+
+    if (isPointerEvent && this.isOpen && !this.isForcedOpen) {
+      this.isForcedOpen = true;
+      // we need to prevent the native popover "toggle" to happen
+      event.preventDefault();
+    } else {
+      this.isForcedOpen = false;
+    }
+  }
+
+  // this is exposed to the consumers to programmatically "close" the popover
+  @action
+  close() {
+    console.log('close invoked');
+
+    this.hidePopover();
+  }
+}
