4f8b8a6aa3ad58e14f7957e441bb07474bf47465
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-03-26 17:53:09 -0700

    HDS-3221 Add PowerSelect components and forward arguments, add basic component examples to Showcase and fix basic tests

---
+++ showcase/app/controllers/components/super-select.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
+export default class SuperSelectController extends Controller {
+  @action
+  noop() {}
+
+  @action
+  onInput() {
+    console.log('onInput activated!');
+  }
+
+  // WIP
+  @action
+  resultCountMessage(number) {
+    console.log(`${number} results found`);
+    return `${number} results found`;
+  }
+}
