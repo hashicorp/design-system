7a5e255715583be126e64710e9a4c42fabf180bf
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-04-03 16:08:02 -0700

    HDS-3221 Move SuperSelect to be under Form components, move all related files and update component name in Showcase and tests

--- showcase/tests/acceptance/components/hds/super-select/multiple.js
+++ showcase/tests/acceptance/components/hds/super-select/multiple.js
@@ -8,11 +8,11 @@ import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'dummy/tests/helpers';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 
-module('Acceptance | components/super-select/multiple', function (hooks) {
+module('Acceptance | components/form/super-select/multiple', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('Components/super-select/multiple page passes automated a11y checks', async function (assert) {
-    await visit('/components/super-select/multiple');
+  test('Components/form/super-select/multiple page passes automated a11y checks', async function (assert) {
+    await visit('/components/form/super-select/multiple');
 
     await a11yAudit();
 
