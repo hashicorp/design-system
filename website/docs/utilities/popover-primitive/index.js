2e85496ab3f86eb21f96e60283103ee48fb2fa22
Cristiano Rastelli <public@didoo.net>
2024-04-08 12:37:03 +0100

    added “How to use” documentation

---
+++ website/docs/utilities/popover-primitive/index.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class Index extends Component {
+  @action
+  onClickButton() {
+    window.alert('Button clicked');
+  }
+}
