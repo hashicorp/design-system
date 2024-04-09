8edf385ab93dfb058fd671529aca3960cc632b4c
Alex <alex-ju@users.noreply.github.com>
2024-04-09 15:02:54 +0100

    Update tests after Base/Field changes

---
+++ showcase/tests/integration/components/hds/form/super-select/multiple/field-test.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module(
+  'Integration | Component | hds/form/super-select/multiple/field',
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    test('it should render the component with a CSS class that matches the component name', async function (assert) {
+      this.set('NOOP', () => {});
+      await render(
+        hbs`<Hds::Form::SuperSelect::Multiple::Field @ariaLabel="Label" @onChange={{this.NOOP}} id="test-super-select-multiple" />`
+      );
+      assert
+        .dom('.hds-form-super-select-multiple #test-super-select-multiple')
+        .exists();
+    });
+  }
+);
