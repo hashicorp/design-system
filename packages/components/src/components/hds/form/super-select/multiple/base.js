0ee35b3c9be453cae6d1fe471cea20751a5197e7
Alex <alex-ju@users.noreply.github.com>
2024-04-10 12:06:57 +0100

    Add after-options to SuperSelect::Multiple

--- packages/components/src/components/hds/form/super-select/multiple/base.js
+++ packages/components/src/components/hds/form/super-select/multiple/base.js
@@ -4,8 +4,43 @@
  */
 
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class HdsSuperSelectMultipleBaseComponent extends Component {
+  @tracked powerSelectAPI;
+  @tracked showOnlySelected = false;
+
+  @action
+  setPowerSelectAPI(powerSelectAPI) {
+    if (typeof this.args.registerAPI === 'function') {
+      this.args.registerAPI(powerSelectAPI);
+    }
+    this.powerSelectAPI = powerSelectAPI;
+  }
+
+  get selectedCount() {
+    return this.powerSelectAPI?.selected?.length || '0';
+  }
+
+  get optionsCount() {
+    return this.powerSelectAPI?.options?.length || '0';
+  }
+
+  get resultCountMessage() {
+    return `${this.selectedCount} selected of ${this.optionsCount} total`;
+  }
+
+  @action showSelected() {
+    this.showOnlySelected = true;
+  }
+
+  @action clearSelected() {
+    this.powerSelectAPI.actions.select(null);
+    // show all options after clearing all selection
+    this.showOnlySelected = false;
+  }
+
   // NOTE: The searchPlaceholder doesn't currently work for the multiple select
   /**
    * Get the search placeholder text
@@ -37,6 +72,11 @@ export default class HdsSuperSelectMultipleBaseComponent extends Component {
     // add a class based on the @isInvalid argument
     if (this.args.isInvalid) {
       classes.push(`hds-form-super-select--is-invalid`);
+    }
+
+    // add a class based on the @isInvalid argument
+    if (this.showOnlySelected) {
+      classes.push(`hds-form-super-select--show-only-selected`);
     }
 
     return classes.join(' ');
