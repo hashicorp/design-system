90004859fc5afee24c2e65b3aa67c15f864a5896
Cristiano Rastelli <public@didoo.net>
2024-04-11 14:11:43 +0100

    generalized autoscroll to `Shw::Autoscrollable` showcase component so it can be reused

--- showcase/app/controllers/utilities/popover-primitive.js
+++ showcase/app/controllers/utilities/popover-primitive.js
@@ -5,37 +5,8 @@
 
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
-import { scheduleOnce } from '@ember/runloop';
-
-function centerScrollableArea() {
-  document.querySelectorAll('[center-scrollable-area]').forEach((element) => {
-    const direction =
-      element.attributes['center-scrollable-area'].value ?? 'both';
-    if (direction === 'both' || direction === 'x') {
-      element.scrollLeft = (element.scrollWidth - element.offsetWidth) / 2;
-    }
-    if (direction === 'both' || direction === 'y') {
-      element.scrollTop =
-        20 + (element.scrollHeight - element.offsetHeight) / 2;
-    }
-  });
-}
 
 export default class PopoverPrimitiveController extends Controller {
-  @service router;
-
-  constructor() {
-    super(...arguments);
-    this.router.on('routeDidChange', this, 'routeDidChange');
-  }
-
-  routeDidChange() {
-    if (this.router.currentRoute.name === 'utilities.popover-primitive') {
-      scheduleOnce('afterRender', this, centerScrollableArea);
-    }
-  }
-
   @action
   noop() {}
 
