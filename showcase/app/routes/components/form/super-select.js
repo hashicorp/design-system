2bf9e29d58dd74a1576f18933430616212c7c32f
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-04-10 18:12:53 -0700

    HDS-3221 Refactor naming of options data for showcase to be more consistent, add example of options with rich content

--- showcase/app/routes/components/form/super-select.js
+++ showcase/app/routes/components/form/super-select.js
@@ -9,9 +9,11 @@ export default class ComponentsSuperSelectRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
     const STATES = ['default', 'hover', 'focus'];
+
+    // Options data
     const OPTIONS = ['Option 1', 'Option 2', 'Option 3'];
-    const SELECTED = ['Option 1'];
-    const SELECTED_MULTIPLE = ['Option 1', 'Option 2'];
+    const SELECTED_OPTION = ['Option 1'];
+    const SELECTED_OPTIONS = ['Option 1', 'Option 2'];
 
     const GROUPED_OPTIONS = [
       { groupName: 'Smalls', options: ['one', 'two', 'three'] },
@@ -37,23 +39,42 @@ export default class ComponentsSuperSelectRoute extends Route {
       'London(eu-west-2)',
       'Frankfurt (eu-central-1)',
     ];
-    const PLACES_SELECTED = ['Oregon (us-west-2)'];
-    const PLACES_SELECTED_MULTIPLE = [
+    const SELECTED_PLACES_OPTION = ['Oregon (us-west-2)'];
+    const SELECTED_PLACES_OPTIONS = [
       'Oregon (us-west-2)',
       'N. Virginia (us-east-1)',
       'Ireland (eu-west-1)',
     ];
 
+    const CLUSTER_SIZE_OPTIONS = [
+      {
+        size: 'Extra Small',
+        description: '2 vCPU | 1 GiB RAM',
+        price: '$0.02',
+      },
+      { size: 'Small', description: '2 vCPU | 2 GiB RAM', price: '$0.04' },
+      { size: 'Medium', description: '4 vCPU | 4 GiB RAM', price: '$0.08' },
+      { size: 'Large', description: '8 vCPU | 8 GiB RAM', price: '$0.16' },
+      {
+        size: 'Extra Large',
+        description: '16 vCPU | 16 GiB RAM',
+        price: '$0.32',
+      },
+    ];
+    const SELECTED_CLUSTER_SIZE_OPTION = CLUSTER_SIZE_OPTIONS[1];
+
     return {
       STATES,
       OPTIONS,
-      SELECTED,
-      SELECTED_MULTIPLE,
+      SELECTED_OPTION,
+      SELECTED_OPTIONS,
       GROUPED_OPTIONS,
       SELECTED_GROUPED_OPTION,
       PLACES_OPTIONS,
-      PLACES_SELECTED,
-      PLACES_SELECTED_MULTIPLE,
+      SELECTED_PLACES_OPTION,
+      SELECTED_PLACES_OPTIONS,
+      CLUSTER_SIZE_OPTIONS,
+      SELECTED_CLUSTER_SIZE_OPTION,
     };
   }
 }
