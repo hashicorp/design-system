a2db2513b07047f41d73ca50fa3fc96d131c6641
Cristiano Rastelli <cristiano.rastelli@hashicorp.com>
2024-04-15 14:39:23 +0100

    Update tests per review suggestion
    Co-authored-by: Alex <alex-ju@users.noreply.github.com>

--- showcase/tests/integration/modifiers/hds-register-event-test.js
+++ showcase/tests/integration/modifiers/hds-register-event-test.js
@@ -5,19 +5,41 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { click, render } from '@ember/test-helpers';
+import { click, render, triggerEvent } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Modifier | hds-register-event', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it calls the callback associated with the `click` event assigned via the modifier', async function (assert) {
-    let clicked;
-    this.set('onClick', () => (clicked = true));
+  test('it adds an event listener to the element', async function (assert) {
+    assert.expect(1);
+
+    this.set('eventHandler', () => {
+      assert.ok(true, 'event handler was called');
+    });
+
     await render(
-      hbs`<button id="test-button" {{hds-register-event 'click' this.onClick}}>Test</button>`
+      hbs`<button id="test-button" {{hds-register-event 'click' this.eventHandler}}>Test</button>`
     );
-    await click('button#test-button');
-    assert.true(clicked);
+
+    await click('button');
+  });
+
+  test('it passes the `useCapture` option to the event listener', async function (assert) {
+    assert.expect(1);
+
+    this.set('eventHandler', (event) => {
+      assert.strictEqual(
+        event.eventPhase,
+        Event.CAPTURING_PHASE,
+        'event was captured'
+      );
+    });
+
+    await render(
+      hbs`<button id="test-button" {{hds-register-event 'click' this.eventHandler useCapture=true}}><span>Test</span></button>`
+    );
+
+    await triggerEvent('span', 'click', { bubbles: true });
   });
 });
