25f8326136be32acb6c358bb49983bcceb2fa726
Cristiano Rastelli <public@didoo.net>
2024-04-18 12:15:54 +0100

    added the `RichTooltip` component + `RichTooltip::Toggle::InfoText` sub-component

---
+++ packages/components/src/components/hds/rich-tooltip/index.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+
+import { PLACEMENTS as PRIMITIVE_PLACEMENTS } from '../popover-primitive/index';
+
+export const DEFAULT_PLACEMENT = 'bottom';
+export const PLACEMENTS = [...PRIMITIVE_PLACEMENTS];
+
+export default class HdsRichTooltipIndexComponent extends Component {
+  /**
+   * @param placement
+   * @type {string}
+   * @description Determines the position of the "popover"
+   */
+  get placement() {
+    let { placement = DEFAULT_PLACEMENT } = this.args;
+
+    assert(
+      `@placement for "Hds::Popover" must be one of the following: ${PLACEMENTS.join(
+        ', '
+      )}; received: ${placement}`,
+      PLACEMENTS.includes(placement)
+    );
+
+    return placement;
+  }
+
+  get enableSoftEvents() {
+    return this.args.enableSoftEvents ?? true;
+  }
+
+  get enableClickEvents() {
+    return this.args.enableClickEvents ?? false;
+  }
+
+  get popoverOptions() {
+    return {
+      placement: this.placement,
+      offsetOptions: this.args.offset || 12,
+      enableCollisionDetection: this.args.enableCollisionDetection || false,
+      arrowOptions: { padding: 12 },
+    };
+  }
+}
