9b9fd1f3d7f84131e0a5ee0c0dd50f51b9beb8c2
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-04-17 23:18:56 -0700

    HDS-3222 Add example of rich content to docs

--- website/docs/components/form/super-select/index.js
+++ website/docs/components/form/super-select/index.js
@@ -44,6 +44,32 @@ export default class Index extends Component {
     return ['AWS', 'CloudWise'];
   }
 
+  get CLUSTER_SIZE_OPTIONS() {
+    return [
+      {
+        size: 'Extra Small',
+        description: '2 vCPU | 1 GiB RAM',
+        price: '$0.02',
+      },
+      { size: 'Small', description: '2 vCPU | 2 GiB RAM', price: '$0.04' },
+      { size: 'Medium', description: '4 vCPU | 4 GiB RAM', price: '$0.08' },
+      { size: 'Large', description: '8 vCPU | 8 GiB RAM', price: '$0.16' },
+      {
+        size: 'Extra Large',
+        description: '16 vCPU | 16 GiB RAM',
+        price: '$0.32',
+      },
+    ];
+  }
+
+  get SELECTED_CLUSTER_SIZE_OPTION() {
+    return this.CLUSTER_SIZE_OPTIONS[1];
+  }
+
+  get SELECTED_CLUSTER_SIZE_OPTIONS() {
+    return [this.CLUSTER_SIZE_OPTIONS[1], this.CLUSTER_SIZE_OPTIONS[2]];
+  }
+
   // notice: this is used as "noop" function for the onDismiss callback of the SuperSelect component
   @action
   noop() {
