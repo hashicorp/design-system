1535cd1f4f3b1e599832d5ec648fde0bf261b3e6
Cristiano Rastelli <public@didoo.net>
2024-04-29 14:43:18 +0100

    refactored `RichTooltip` to be compliant with accessibility requirements and new `PopoverPrimitive` APIs

--- packages/components/src/components/hds/rich-tooltip/index.js
+++ packages/components/src/components/hds/rich-tooltip/index.js
@@ -4,31 +4,12 @@
  */
 
 import Component from '@glimmer/component';
-import { assert } from '@ember/debug';
-
-import { PLACEMENTS as PRIMITIVE_PLACEMENTS } from '../popover-primitive/index';
-
-export const DEFAULT_PLACEMENT = 'bottom';
-export const PLACEMENTS = [...PRIMITIVE_PLACEMENTS];
+import { getElementId } from '../../../utils/hds-get-element-id';
 
 export default class HdsRichTooltipIndexComponent extends Component {
-  /**
-   * @param placement
-   * @type {string}
-   * @description Determines the position of the "popover"
-   */
-  get popoverPlacement() {
-    let { popoverPlacement = DEFAULT_PLACEMENT } = this.args;
-
-    assert(
-      `@popoverPlacement for "Hds::RichTooltip" must be one of the following: ${PLACEMENTS.join(
-        ', '
-      )}; received: ${popoverPlacement}`,
-      PLACEMENTS.includes(popoverPlacement)
-    );
-
-    return popoverPlacement;
-  }
+  elementId = getElementId(this);
+  arrowId = `arrow-${this.elementId}`;
+  popoverId = `popover-${this.elementId}`;
 
   get enableSoftEvents() {
     return this.args.enableClickEvents !== true;
@@ -36,16 +17,5 @@ export default class HdsRichTooltipIndexComponent extends Component {
 
   get enableClickEvents() {
     return this.args.enableClickEvents ?? false;
-  }
-
-  get anchoredPositionOptions() {
-    // custom options specific for the `RichTooltip` component
-    // for details see the `hds-anchored-position` modifier
-    return {
-      placement: this.popoverPlacement,
-      offsetOptions: this.args.popoverOffset ?? 12,
-      enableCollisionDetection: this.args.enableCollisionDetection ?? true,
-      arrowOptions: { padding: 12 },
-    };
   }
 }
