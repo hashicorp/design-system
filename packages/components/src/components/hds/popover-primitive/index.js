ca9d8151d9166153f3e46f0329070be1e8411c91
Cristiano Rastelli <public@didoo.net>
2024-04-29 14:35:52 +0100

    refactored `PopoverPrimitive` to be a headless component and provide only the Popover API functionality

--- packages/components/src/components/hds/popover-primitive/index.js
+++ packages/components/src/components/hds/popover-primitive/index.js
@@ -33,13 +33,8 @@ export {
 } from '../../../modifiers/hds-anchored-position';
 
 export default class HdsPopoverPrimitiveComponent extends Component {
-  @tracked toggleElement;
-  @tracked popoverElement;
-  @tracked arrowElement;
   @tracked isOpen = this.args.isOpen ?? false;
   @tracked isClosing = false;
-  @tracked toggleContainsInteractive =
-    this.args.toggleContainsInteractive ?? false;
   // this will enable "soft" events for the toggle ("hover" and "focus")
   @tracked enableSoftEvents = this.args.enableSoftEvents ?? false;
   // this will enable "click" events for the toggle
@@ -62,13 +57,6 @@ export default class HdsPopoverPrimitiveComponent extends Component {
     }
   }
 
-  /**
-   * Generates a unique ID for the "popover" (will be used in the `popovertarget` attribute of the toggle button)
-   *
-   * @param popoverId
-   */
-  popoverId = 'popover-' + guidFor(this);
-
   setupPrimitiveContainer = modifier(
     (element) => {
       this.containerElement = element;
@@ -89,41 +77,32 @@ export default class HdsPopoverPrimitiveComponent extends Component {
     (element) => {
       this.toggleElement = element;
 
-      if (this.args.toggleAriaLabel) {
-        this.toggleElement.setAttribute(
-          'aria-label',
-          this.args.toggleAriaLabel
-        );
-      }
-
-      // for the click events we don't use `onclick` event listeners, but we rely on the `popovertarget` attribute
-      // provided by the Popover API which does all the magic for us without needing JS code
-      // (important: to work it needs to be applied to a button, so we can't safely support if the toggle contains interactive elements)
-      if (this.enableClickEvents) {
-        if (this.toggleContainsInteractive) {
-          assert(
-            'Hds::PopoverPrimitive - You have enabled `click` events to the "toggle" element (via `@enableClickEvents`), but it contains interactive elements: this may result in unexpected behaviours or non accessible code'
-          );
-        } else {
-          this.toggleElement.setAttribute('popovertarget', this.popoverId);
-        }
-      }
-    },
-    { eager: true }
-  );
-
-  setupPrimitiveArrow = modifier(
-    (element) => {
-      this.arrowElement = element;
+      assert(
+        `The toggle element of "Hds::PopoverPrimitive" must be a <button>; element received: <${element.tagName.toLowerCase()}>`,
+        element instanceof HTMLButtonElement
+      );
     },
     { eager: true }
   );
 
   setupPrimitivePopover = modifier(
-    (element) => {
+    (element, _positional, named = {}) => {
       this.popoverElement = element;
 
-      this.popoverElement.id = this.popoverId;
+      // for the click events we don't use `onclick` event listeners, but we rely on the `popovertarget` attribute
+      // provided by the Popover API which does all the magic for us without needing JS code
+      // (important: to work it needs to be applied to a button)
+      if (this.enableClickEvents) {
+        let popoverId;
+        if (this.popoverElement.id) {
+          popoverId = this.popoverElement.id;
+        } else {
+          // we need a DOM id for the `popovertarget` attribute
+          popoverId = guidFor(this);
+          this.popoverElement.id = popoverId;
+        }
+        this.toggleElement.setAttribute('popovertarget', popoverId);
+      }
 
       // this should be an extremely edge case, but in the case the popover needs to be initially forced to be open
       // we need to use the "manual" state to support the case of multiple "menus" opened at the same time
@@ -135,36 +114,24 @@ export default class HdsPopoverPrimitiveComponent extends Component {
         this.popoverElement.popover = 'auto';
       }
 
-      if (this.args.popoverWidth) {
-        // we overwrite the values of the CSS variables that set the width/max-width of the "content"
-        element.style.setProperty(
-          '--hds-popover-primitive-content-width',
-          this.args.popoverWidth
-        );
-        element.style.setProperty(
-          '--hds-popover-primitive-content-max-width',
-          'none'
-        );
-      }
-
-      if (this.args.popoverHeight) {
-        // we overwrite the values of the CSS variables that set the height/max-height of the "content"
-        element.style.setProperty(
-          '--hds-popover-primitive-content-height',
-          this.args.popoverHeight
-        );
-        element.style.setProperty(
-          '--hds-popover-primitive-content-max-height',
-          'none'
-        );
-      }
-
       // Register "onBeforeToggle" + "onToggle" callback functions to be called when a native 'toggle' event is dispatched
       registerEvent(this.popoverElement, [
         'beforetoggle',
         this.onBeforeTogglePopover,
       ]);
       registerEvent(this.popoverElement, ['toggle', this.onTogglePopover]);
+
+      // we need to spread the argument because if it's set via `{{ hash … }}` Ember complains when we overwrite one of its values
+      const anchoredPositionOptions = { ...named.anchoredPositionOptions };
+
+      // we overwrite the "strategy" if the Popover API is not supported (polyfill applied for the first time) of it's already been polyfilled (see above)
+      // this is specifically done for Firefox: currently it doesn't support it, but will soon (we need Firefox 127 to support the last 2 versions)
+      // see: https://wiki.mozilla.org/Release_Management/Release_owners
+      if (!isPopoverApiSupported() || isPopoverApiPolyfilled()) {
+        // when using the "absolute" strategy, the presence of a parent with "relative" position leads to wrong layout rendering (known issue in the polyfill library)
+        // see: https://github.com/oddbird/popover-polyfill/tree/main?tab=readme-ov-file#caveats
+        anchoredPositionOptions.strategy = 'fixed';
+      }
 
       // Apply the `hds-anchored-position` modifier to the "popover" element
       // (notice: this function runs the first time when the element the modifier was applied to is inserted into the DOM, and it autotracks while running.
@@ -176,68 +143,12 @@ export default class HdsPopoverPrimitiveComponent extends Component {
         anchoredPositionModifier(
           this.popoverElement, // element the modifier is attached to
           [this.toggleElement], // positional arguments
-          this.anchoredPositionOptions // named arguments
+          anchoredPositionOptions // named arguments
         );
       });
     },
     { eager: true }
   );
-
-  get anchoredPositionOptions() {
-    // we need to spread the argument because if it's set via `{{ hash … }}` Ember complains when we overwrite one of its values
-    const anchoredPositionOptions = this.args.anchoredPositionOptions
-      ? { ...this.args.anchoredPositionOptions }
-      : {};
-
-    // we overwrite the "strategy" if the Popover API is not supported (polyfill applied for the first time) of it's already been polyfilled (see above)
-    // this is specifically done for Firefox: currently it doesn't support it, but will soon (we need Firefox 127 to support the last 2 versions)
-    // see: https://wiki.mozilla.org/Release_Management/Release_owners
-    if (!isPopoverApiSupported() || isPopoverApiPolyfilled()) {
-      // when using the "absolute" strategy, the presence of a parent with "relative" position leads to wrong layout rendering (known issue in the polyfill library)
-      // see: https://github.com/oddbird/popover-polyfill/tree/main?tab=readme-ov-file#caveats
-      anchoredPositionOptions.strategy = 'fixed';
-    }
-
-    if (this.arrowElement) {
-      anchoredPositionOptions.arrowOptions = {
-        // we assign the `arrowElement` to the `element` key
-        element: this.arrowElement,
-        // we use the padding value from the provided options (if assigned) or use a default
-        padding: anchoredPositionOptions?.arrowOptions?.padding ?? 0,
-      };
-    }
-
-    return anchoredPositionOptions;
-  }
-
-  /**
-   * @param isInline
-   * @type {boolean}
-   * @default true
-   * @description sets display for the container and toggle elements
-   */
-  get isInline() {
-    let { isInline = false } = this.args;
-    return isInline;
-  }
-
-  /**
-   * Get the class names to apply to the element
-   * @method classNames
-   * @return {string} The "class" attribute to apply to the root element
-   */
-  get classNames() {
-    let classes = ['hds-popover-primitive'];
-
-    // add a class based on the @isInline argument
-    if (this.isInline) {
-      classes.push('hds-popover-primitive--is-inline');
-    } else {
-      classes.push('hds-popover-primitive--is-block');
-    }
-
-    return classes.join(' ');
-  }
 
   @action
   showPopover() {
@@ -278,6 +189,7 @@ export default class HdsPopoverPrimitiveComponent extends Component {
       }
     } else {
       this.isOpen = false;
+
       // reset the "isClosing" flag (the `toggle` event is fired _after_ the popover is closed)
       this.isClosing = false;
 
@@ -320,7 +232,6 @@ export default class HdsPopoverPrimitiveComponent extends Component {
 
   @action
   onFocusOut(event) {
-    // TODO! discuss with Alex if/why we need this check here
     // due to inconsistent implementation of relatedTarget across browsers we use the activeElement as a fallback
     // if the related target is not part of the disclosed content we close the disclosed container
     if (
