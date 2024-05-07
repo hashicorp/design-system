2c9ce7f8db18dff9843623cb96182d28005c8628
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2024-05-07 09:03:49 -0500

    DROP

--- website/lib/analytics/index.js
+++ website/lib/analytics/index.js
@@ -9,9 +9,9 @@ module.exports = {
   name: require('./package').name,
 
   contentFor(type) {
-    if (type === 'head-footer' && process.env.VERCEL_ENV === 'production') {
-      return `<script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="PPQLJJKK" data-excluded-domains="vercel.app,localhost" defer></script>
-      
+    if (type === 'head-footer') {
+      return `<script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="PJPASXHG" defer></script>
+
       <meta name="google-site-verification" content="mx7oZncj-AnNjefq1rOLXEdtbrLbsrYP_vax-ZZWiOI" />`;
     }
   },
