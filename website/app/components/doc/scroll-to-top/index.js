7981bec0c73e09393a282cd14a7be9c4bba283a7
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2024-05-08 06:04:21 -0500

    only show button after user has scrolled the page

--- website/app/components/doc/scroll-to-top/index.js
+++ website/app/components/doc/scroll-to-top/index.js
@@ -5,12 +5,54 @@
 
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class DocScrollToTopComponent extends Component {
-  @action scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
+  @service fastboot;
+  @tracked isButtonVisible = false;
+
+  constructor() {
+    super(...arguments);
+    this.addScrollListener();
+  }
+
+  willDestroy() {
+    super.willDestroy(...arguments);
+    this.removeScrollListener();
+  }
+
+  @action
+  scrollToTop() {
+    if (!this.fastboot.isFastBoot) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+  }
+
+  @action
+  checkScroll() {
+    if (window.scrollY > 200) {
+      this.isButtonVisible = true;
+    } else {
+      this.isButtonVisible = false;
+    }
+  }
+
+  @action
+  addScrollListener() {
+    if (!this.fastboot.isFastBoot) {
+      this.scrollFunction = () => this.checkScroll();
+      window.addEventListener('scroll', this.scrollFunction);
+    }
+  }
+
+  @action
+  removeScrollListener() {
+    if (!this.fastboot.isFastBoot) {
+      window.removeEventListener('scroll', this.scrollFunction);
+    }
   }
 }
