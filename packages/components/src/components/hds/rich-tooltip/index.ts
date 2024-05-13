eca8d60de492c9b1bcee711787e0a9a5a075609f
Cristiano Rastelli <public@didoo.net>
2024-05-13 11:11:10 +0100

    run the command `npx ember-codemod-args-to-signature --src src/components/hds/ rich-tooltip`

--- packages/components/src/components/hds/rich-tooltip/index.ts
+++ packages/components/src/components/hds/rich-tooltip/index.ts
@@ -6,7 +6,20 @@
 import Component from '@glimmer/component';
 import { getElementId } from '../../../utils/hds-get-element-id.js';
 
-export default class HdsRichTooltipIndexComponent extends Component {
+interface IndexSignature {
+  Args: {
+    enableClickEvents: unknown;
+    isOpen: unknown;
+    onClose: unknown;
+    onOpen: unknown;
+  };
+  Blocks: {
+    default: [unknown];
+  };
+  Element: HTMLDivElement;
+}
+
+export default class IndexComponent extends Component<IndexSignature> {
   elementId = getElementId(this);
   arrowId = `arrow-${this.elementId}`;
   popoverId = `popover-${this.elementId}`;
@@ -17,5 +30,12 @@ export default class HdsRichTooltipIndexComponent extends Component {
 
   get enableClickEvents() {
     return this.args.enableClickEvents ?? false;
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'Index': typeof IndexComponent;
+    'index': typeof IndexComponent;
   }
 }
