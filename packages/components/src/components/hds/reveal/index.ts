a97d599435a06e91821dd303a9055c182018dc5b
Cristiano Rastelli <public@didoo.net>
2024-05-16 11:13:54 +0100

    simplified arguments for the `getText` local helper per code review suggestion

--- packages/components/src/components/hds/reveal/index.ts
+++ packages/components/src/components/hds/reveal/index.ts
@@ -35,16 +35,12 @@ export default class HdsRevealComponent extends Component<HdsRevealSignature> {
    * @type {string}
    * @description A local function that emulates a getter to compute the value of the `@text` argument for the button (mainly to make TypeScript happy)
    */
-  getText = (
-    isOpen: boolean,
-    text: string,
-    textWhenOpen: string | undefined
-  ): string => {
-    if (isOpen && textWhenOpen !== undefined) {
-      return textWhenOpen;
+  getText = (isOpen: boolean): string => {
+    if (isOpen && this.args.textWhenOpen !== undefined) {
+      return this.args.textWhenOpen;
     } else {
-      if (text !== undefined) {
-        return text;
+      if (this.args.text !== undefined) {
+        return this.args.text;
       } else {
         assert('@text for "Hds::Reveal" must have a valid value');
       }
