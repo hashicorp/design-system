0eae3a7a87060ad36df4da61ddc7aca5c553fdc5
Cristiano Rastelli <public@didoo.net>
2024-05-17 17:29:02 +0100

    updated backing class to use the new `TemplateOnlyComponent` with signature

--- packages/components/src/components/hds/segmented-group/index.ts
+++ packages/components/src/components/hds/segmented-group/index.ts
@@ -1,5 +1,26 @@
-import templateOnlyComponent from '@ember/component/template-only';
+import TemplateOnlyComponent from '@ember/component/template-only';
 
-const HdsSegmentedGroupComponent = templateOnlyComponent();
+import type { ComponentLike } from '@glint/template';
+import type { HdsButtonSignature } from '../button';
+import type { HdsYieldSignature } from '../yield';
+
+interface HdsSegmentedGroupSignature {
+  Blocks: {
+    default: [
+      {
+        Button: ComponentLike<HdsButtonSignature>;
+        // TODO add here the missing signatures
+        // Dropdown
+        // Select
+        // TextInput
+        Generic: ComponentLike<HdsYieldSignature>;
+      }
+    ];
+  };
+  Element: HTMLDivElement;
+}
+
+const HdsSegmentedGroupComponent =
+  TemplateOnlyComponent<HdsSegmentedGroupSignature>();
 
 export default HdsSegmentedGroupComponent;
