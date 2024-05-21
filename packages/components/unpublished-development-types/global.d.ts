7568262aec1b2a9ac35862668e7c3a724319befa
Cristiano Rastelli <public@didoo.net>
2024-05-21 22:26:57 +0100

    re-ordered imports in `packages/components/unpublished-development-types/global.d.ts`

--- packages/components/unpublished-development-types/global.d.ts
+++ packages/components/unpublished-development-types/global.d.ts
@@ -2,22 +2,22 @@ import '@glint/environment-ember-loose';
 
 import { LinkTo } from '@ember/routing';
 
-import type RenderModifiersRegistry from '@ember/render-modifiers/template-registry';
-import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
-import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
-import type EmberStyleModifierRegistry from 'ember-style-modifier/template-registry';
-import type EmberStargate from 'ember-stargate/template-registry';
-
 import type HdsComponentsRegistry from '../src/template-registry';
+
+import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
+import type EmberStargate from 'ember-stargate/template-registry';
+import type EmberStyleModifierRegistry from 'ember-style-modifier/template-registry';
+import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
+import type RenderModifiersRegistry from '@ember/render-modifiers/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry
-    extends EmberTruthRegistry,
-      HdsComponentsRegistry,
+    extends HdsComponentsRegistry,
       EmberElementHelperRegistry,
+      EmberStargate,
       EmberStyleModifierRegistry,
-      RenderModifiersRegistry,
-      EmberStargate /*, other addon registries */ {
+      EmberTruthRegistry,
+      RenderModifiersRegistry /*, other addon registries */ {
     // local entries
     LinkToExternal: typeof LinkTo;
   }
