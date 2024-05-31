b0be760ccf6ba9e805759a990e4e2afc14806401
Alex <alex-ju@users.noreply.github.com>
2024-05-31 12:06:28 +0100

    Add freshly converted components and sort

--- packages/components/src/components.ts
+++ packages/components/src/components.ts
@@ -2,12 +2,14 @@
 import HdsAccordion from './components/hds/accordion/index.ts';
 import HdsAlert from './components/hds/alert/index.ts';
 import HdsAppFooter from './components/hds/app-footer/index.ts';
+import HdsAppFrame from './components/hds/app-frame/index.ts';
 import HdsBadge from './components/hds/badge/index.ts';
 import HdsBadgeCount from './components/hds/badge-count/index.ts';
 import HdsButton from './components/hds/button/index.ts';
 import HdsButtonSet from './components/hds/button-set/index.ts';
-import HdsAppFrame from './components/hds/app-frame/index.ts';
-import HdsCardContainer from './components/hds/card/container.ts';
+import HdsCard from './components/hds/card/container.ts';
+import HdsCopyButton from './components/hds/copy/button/index.ts';
+import HdsCopySnippet from './components/hds/copy/snippet/index.ts';
 import HdsDisclosurePrimitive from './components/hds/disclosure-primitive/index.ts';
 import HdsDismissButton from './components/hds/dismiss-button/index.ts';
 import HdsIconTile from './components/hds/icon-tile/index.ts';
@@ -16,23 +18,27 @@ import HdsLinkInline from './components/hds/link/inline.ts';
 import HdsLinkStandalone from './components/hds/link/standalone.ts';
 import HdsReveal from './components/hds/reveal/index.ts';
 import HdsSeparator from './components/hds/separator/index.ts';
+import HdsStepperStepIndicator from './components/hds/stepper/step/indicator.ts';
+import HdsStepperTaskIndicator from './components/hds/stepper/task/indicator.ts';
+import HdsTag from './components/hds/tag/index.ts';
 import HdsText from './components/hds/text/index.ts';
 import HdsTextBody from './components/hds/text/body.ts';
-import HdsTextDisplay from './components/hds/text/display.ts';
-import HdsTag from './components/hds/tag/index.ts';
-import HdsToast from './components/hds/toast/index.ts';
 import HdsTextCode from './components/hds/text/code.ts';
+import HdsTextDisplay from './components/hds/text/display.ts';
+import HdsToast from './components/hds/toast/index.ts';
 
 export {
   HdsAccordion,
   HdsAlert,
   HdsAppFooter,
+  HdsAppFrame,
   HdsBadge,
   HdsBadgeCount,
   HdsButton,
   HdsButtonSet,
-  HdsAppFrame,
-  HdsCardContainer,
+  HdsCard,
+  HdsCopyButton,
+  HdsCopySnippet,
   HdsDisclosurePrimitive,
   HdsDismissButton,
   HdsIconTile,
@@ -41,10 +47,12 @@ export {
   HdsLinkStandalone,
   HdsReveal,
   HdsSeparator,
+  HdsStepperStepIndicator,
+  HdsStepperTaskIndicator,
+  HdsTag,
   HdsText,
   HdsTextBody,
-  HdsTextDisplay,
-  HdsTag,
-  HdsToast,
   HdsTextCode,
+  HdsTextDisplay,
+  HdsToast,
 };
