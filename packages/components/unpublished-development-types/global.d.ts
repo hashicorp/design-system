48f1c3b3501f58b2370049be1d058d32ef43a9bc
Alexey Kulakov <uni_que@me.com>
2024-06-03 21:11:37 +0100

    feat(ember-flight-icons): update blueprint

--- packages/components/unpublished-development-types/global.d.ts
+++ packages/components/unpublished-development-types/global.d.ts
@@ -1,11 +1,11 @@
 import '@glint/environment-ember-loose';
 
 import { LinkTo } from '@ember/routing';
-
 import DidInsertModifier from '@ember/render-modifiers/modifiers/did-insert';
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
 import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
 import type EmberStyleModifier from 'ember-style-modifier';
+import type FlightIconsRegistry from '@hashicorp/ember-flight-icons/template-registry';
 
 export default interface EmberStyleModifierRegistry {
   style: typeof EmberStyleModifier;
@@ -17,7 +17,8 @@ declare module '@glint/environment-ember-loose/registry' {
     extends EmberTruthRegistry,
       HdsComponentsRegistry,
       EmberElementHelperRegistry,
-      EmberStyleModifierRegistry /*, other addon registries */ {
+      EmberStyleModifierRegistry,
+      FlightIconsRegistry /*, other addon registries */ {
     // local entries
     'did-insert': typeof DidInsertModifier;
     LinkToExternal: typeof LinkTo;
---
+++ packages/ember-flight-icons/.eslintrc.cjs
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['ember', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {},
+  overrides: [
+    // ts files
+    {
+      files: ['**/*.ts', '**/*.gts'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        // Add any custom rules here
+      },
+    },
+    // require relative imports use full extensions
+    {
+      files: ['src/**/*.{js,ts,gjs,gts}'],
+      rules: {
+        'import/extensions': ['error', 'always', { ignorePackages: true }],
+      },
+    },
+    // node files
+    {
+      files: [
+        './.eslintrc.cjs',
+        './.prettierrc.cjs',
+        './.template-lintrc.cjs',
+        './addon-main.cjs',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ['n'],
+      extends: ['plugin:n/recommended'],
+    },
+  ],
+};
