4ddca78369514fbb01fad75eba55f422ff9c6a03
Alex <alex-ju@users.noreply.github.com>
2024-06-04 11:55:05 +0100

    Use generic types for `SideNav::Base` subcomponent

--- packages/components/src/components/hds/side-nav/base.ts
+++ packages/components/src/components/hds/side-nav/base.ts
@@ -3,9 +3,12 @@ import TemplateOnlyComponent from '@ember/component/template-only';
 export interface HdsSideNavBaseSignature {
   Blocks: {
     root?: [];
-    header?: [];
-    body?: [];
-    footer?: [];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    header?: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    body?: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    footer?: any;
   };
   Element: HTMLDivElement;
 }
