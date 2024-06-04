41aecb9b02e3abf4c2ce7f79c7beca7c4b091375
Cristiano Rastelli <public@didoo.net>
2024-06-04 19:23:59 +0100

    added `Portal/PortalTarget` as local entries in the `unpublished-development-types/global.d.ts` file as backstop solution

--- packages/components/unpublished-development-types/global.d.ts
+++ packages/components/unpublished-development-types/global.d.ts
@@ -1,6 +1,7 @@
 import '@glint/environment-ember-loose';
 
 import { LinkTo } from '@ember/routing';
+import { Portal, PortalTarget } from 'ember-stargate';
 
 import type HdsComponentsRegistry from '../src/template-registry';
 
@@ -20,5 +21,8 @@ declare module '@glint/environment-ember-loose/registry' {
       RenderModifiersRegistry /*, other addon registries */ {
     // local entries
     LinkToExternal: typeof LinkTo;
+    // we have to use `ember-stargate` version `0.4.3` because version `0.5.0` causes a break in the tests for `ember-lts-3.28`
+    Portal: typeof Portal;
+    PortalTarget: typeof PortalTarget;
   }
 }
