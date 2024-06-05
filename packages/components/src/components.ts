245af8c0635dae2c20c411158642487a61bdc3be
Alexey Kulakov <uni_que@me.com>
2024-06-05 15:58:23 +0100

    feat(components): reexport all components that have backing class

--- packages/components/src/components.ts
+++ packages/components/src/components.ts
@@ -11,28 +11,39 @@ import HdsAppFrame from './components/hds/app-frame/index.ts';
 import HdsApplicationState from './components/hds/application-state/index.ts';
 import HdsBadge from './components/hds/badge/index.ts';
 import HdsBadgeCount from './components/hds/badge-count/index.ts';
+import HdsBreadcrumb from './components/hds/breadcrumb/index.js';
+import HdsBreadcrumbItem from './components/hds/breadcrumb/item.js';
 import HdsButton from './components/hds/button/index.ts';
 import HdsButtonSet from './components/hds/button-set/index.ts';
 import HdsCard from './components/hds/card/container.ts';
+import HdsCodeBlock from './components/hds/code-block/index.js';
 import HdsCopyButton from './components/hds/copy/button/index.ts';
 import HdsCopySnippet from './components/hds/copy/snippet/index.ts';
 import HdsDisclosurePrimitive from './components/hds/disclosure-primitive/index.ts';
 import HdsDismissButton from './components/hds/dismiss-button/index.ts';
+import HdsDropdown from './components/hds/dropdown/index.ts';
+import HdsFlyout from './components/hds/flyout/index.ts';
 import HdsIconTile from './components/hds/icon-tile/index.ts';
 import HdsInteractive from './components/hds/interactive/index.ts';
 import HdsLinkInline from './components/hds/link/inline.ts';
 import HdsLinkStandalone from './components/hds/link/standalone.ts';
 import HdsPageHeader from './components/hds/page-header/index.ts';
 import HdsReveal from './components/hds/reveal/index.ts';
+import HdsRichTooltip from './components/hds/rich-tooltip/index.js';
+// import HdsSegmentedGroup from './components/hds/segmented-group/index.js';
 import HdsSeparator from './components/hds/separator/index.ts';
+import HdsSideNav from './components/hds/side-nav/index.js';
 import HdsStepperStepIndicator from './components/hds/stepper/step/indicator.ts';
 import HdsStepperTaskIndicator from './components/hds/stepper/task/indicator.ts';
+import HdsTable from './components/hds/table/index.js';
+import HdsTabs from './components/hds/tabs/index.js';
 import HdsTag from './components/hds/tag/index.ts';
 import HdsText from './components/hds/text/index.ts';
 import HdsTextBody from './components/hds/text/body.ts';
 import HdsTextCode from './components/hds/text/code.ts';
 import HdsTextDisplay from './components/hds/text/display.ts';
 import HdsToast from './components/hds/toast/index.ts';
+import HdsTooltipButton from './components/hds/tooltip-button/index.js';
 
 export {
   HdsAccordion,
@@ -42,26 +53,36 @@ export {
   HdsApplicationState,
   HdsBadge,
   HdsBadgeCount,
+  HdsBreadcrumb,
+  HdsBreadcrumbItem,
   HdsButton,
   HdsButtonSet,
   HdsCard,
+  HdsCodeBlock,
   HdsCopyButton,
   HdsCopySnippet,
   HdsDisclosurePrimitive,
   HdsDismissButton,
+  HdsDropdown,
+  HdsFlyout,
   HdsIconTile,
   HdsInteractive,
   HdsLinkInline,
   HdsLinkStandalone,
   HdsPageHeader,
   HdsReveal,
+  HdsRichTooltip,
   HdsSeparator,
+  HdsSideNav,
   HdsStepperStepIndicator,
   HdsStepperTaskIndicator,
+  HdsTable,
+  HdsTabs,
   HdsTag,
   HdsText,
   HdsTextBody,
   HdsTextCode,
   HdsTextDisplay,
   HdsToast,
+  HdsTooltipButton,
 };
