85ee1c72f35b6cf69eb7d9cf13d7f4e331aef71b
Zack Moore <zack.moore@hashicorp.com>
2024-06-07 17:35:36 -0400

    added a doc-track-event modifier and accompanying test. adds sinon as dev dep

---
+++ website/tests/integration/modifiers/doc-track-event-test.js
@@ -0,0 +1,32 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { click } from '@ember/test-helpers';
+import sinon from 'sinon';
+
+module('Integration | Modifier | doc-track-event', function (hooks) {
+  setupRenderingTest(hooks);
+
+  let originalFathom;
+  let trackEventSpy;
+
+  hooks.beforeEach(function () {
+    originalFathom = window.fathom;
+    trackEventSpy = sinon.spy();
+    window.fathom = {
+      trackEvent: trackEventSpy,
+    };
+  });
+
+  hooks.afterEach(function () {
+    window.fathom = originalFathom;
+  });
+
+  test('it adds click event listener and calls handleClick', async function (assert) {
+    await render(hbs`<div {{doc-track-event 'testEvent'}}></div>`);
+    await click('div');
+
+    assert.ok(trackEventSpy.calledOnceWith('testEvent'));
+  });
+});
