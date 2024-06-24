1bec001819e7975bcfcf3d25d34581b74ca015f8
Cristiano Rastelli <public@didoo.net>
2024-06-24 15:18:56 +0100

    updated `Shw:Flex` and `Shw::Grid` components to support `@gap` argument

--- showcase/app/components/shw/flex/index.js
+++ showcase/app/components/shw/flex/index.js
@@ -4,9 +4,19 @@
  */
 
 import Component from '@glimmer/component';
+import { htmlSafe } from '@ember/template';
 
 export default class FlexIndexComponent extends Component {
   direction = this.args.direction ?? 'row';
+
+  get itemsStyle() {
+    let styles = [];
+    if (this.args.gap) {
+      styles.push(`gap: ${this.args.gap}`);
+    }
+
+    return styles.length > 0 ? htmlSafe(styles.join('; ')) : undefined;
+  }
 
   get classNames() {
     let classes = ['shw-flex'];
