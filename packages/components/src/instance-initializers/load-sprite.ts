52dc369d4c27f27d6c1b90affc4bf3df4cbc918d
Zack Moore <zack.moore@hashicorp.com>
2024-06-28 11:40:37 -0400

    load sprites if they have not been loaded in another initializer

---
+++ packages/components/src/instance-initializers/load-sprite.ts
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import config from 'ember-get-config';
+import type ApplicationInstance from '@ember/application/instance';
+
+export async function initialize(
+  appInstance: ApplicationInstance & {
+    __flightIconsSpriteLoaded?: boolean;
+  }
+) {
+  if (
+    config?.emberFlightIcons?.lazyEmbed &&
+    appInstance.__flightIconsSpriteLoaded !== true
+  ) {
+    const { default: svgSprite } = await import(
+      '@hashicorp/flight-icons/svg-sprite/svg-sprite-module'
+    );
+
+    // in test environments we can inject the sprite directly into the ember testing container
+    // to avoid issues with tools like Percy that only consider content inside that element
+    if (config.environment === 'test') {
+      window.document
+        ?.getElementById('ember-testing')
+        ?.insertAdjacentHTML('afterbegin', svgSprite);
+    } else {
+      window.document?.body?.insertAdjacentHTML('beforeend', svgSprite);
+    }
+
+    appInstance.__flightIconsSpriteLoaded = true;
+  }
+}
+
+export default {
+  initialize,
+};
