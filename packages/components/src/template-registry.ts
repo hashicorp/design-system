fe2bc5a0f46c2defe8af86e85057876dc62a2728
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-06-28 21:16:19 -0700

    HDS-3373 Add preliminary functionality for responsive menu, remove AppHeader box-shadowm fix button focus styles

--- packages/components/src/template-registry.ts
+++ packages/components/src/template-registry.ts
@@ -11,6 +11,7 @@ import type HdsAlertDescriptionComponent from './components/hds/alert/descriptio
 import type HdsAlertTitleComponent from './components/hds/alert/title';
 import type HdsAppHeaderComponent from './components/hds/app-header';
 import type HdsAppHeaderHomeLinkComponent from './components/hds/app-header/home-link';
+import type HdsAppHeaderMenuButtonComponent from './components/hds/app-header/menu-button';
 import type HdsAppFooterComponent from './components/hds/app-footer';
 import type HdsAppFooterCopyrightComponent from './components/hds/app-footer/copyright';
 import type HdsAppFooterItemComponent from './components/hds/app-footer/item';
@@ -107,6 +108,9 @@ export default interface HdsComponentsRegistry {
 
   'Hds::AppHeader::HomeLink': typeof HdsAppHeaderHomeLinkComponent;
   'hds/app-header/home-link': typeof HdsAppHeaderHomeLinkComponent;
+
+  'Hds::AppHeader::MenuButton': typeof HdsAppHeaderMenuButtonComponent;
+  'hds/app-header/menu-button': typeof HdsAppHeaderMenuButtonComponent;
 
   // AppFooter
   'Hds::AppFooter': typeof HdsAppFooterComponent;
