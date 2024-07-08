2b6be057332a82f2596cdf75fb32ed2715594f55
Curt Puckett <curt.puckett@hashicorp.com>
2024-07-08 12:00:36 -0400

    Update regex to account for self-closing tags
    like <Doc::WcagList />, <Doc::A11ySupport /> etc..

--- website/app/shared/showdown-extensions/remove-auto-p-tags.js
+++ website/app/shared/showdown-extensions/remove-auto-p-tags.js
@@ -4,8 +4,7 @@ export const removeAutoPTags = () => {
       filter: (text) => {
         // Define a regex pattern for matching a block of Doc::Anything or HDS:: elements wrapped in <p class="doc-markdown-p">
         // This pattern attempts to match more complex nested structures by looking for the opening and closing tags of custom elements.
-        const blockCustomElementRegex = /<p class="doc-markdown-p">\s*((<(Doc::|Hds::)[^>]+>[\s\S]*?<\/(Doc::|Hds::)[^>]+>\s*)+)<\/p>/g;
-  
+        const blockCustomElementRegex = /<p class="doc-markdown-p">\s*(<((Doc::|Hds::)[^>\/]+)(?:\/>|>[\s\S]*?<\/\3[^>]*>))\s*<\/p>/g;
         // Replace the <p class="doc-markdown-p"> tag around the block of Doc:: or HDS:: elements, preserving the block structure
         text = text.replace(blockCustomElementRegex, '$1');
   
