22642bc5ef0761042d487eff4af06b4369c48c16
Curt Puckett <curt.puckett@hashicorp.com>
2024-07-08 16:11:53 -0400

    Create a new doc component test page to QA new showdown extension

---
+++ website/docs/testing/markdown/doc-components.js
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+import TOKENS_RAW from '@hashicorp/design-system-tokens/dist/docs/products/tokens.json';
+
+const ELEVATIONS = ['inset', 'low', 'mid', 'high', 'higher', 'overlay'];
+
+export default class Index extends Component {
+
+  get colors() {
+    const colors = {
+      semantic: {},
+    };
+
+    TOKENS_RAW.forEach((token) => {
+      if (token.attributes.category === 'color' && !token.deprecated) {
+        if (token.group) {
+          if (token.group === 'semantic') {
+            const context = token.path[1];
+            if (!colors['semantic'][context]) {
+              colors['semantic'][context] = [];
+            }
+            const tokenObj = {
+              colorName: `${token.path[1]}-${token.path[2]}`,
+              cssVariable: `--${token.name}`,
+              value: token.value,
+            };
+            if (['foreground', 'page', 'surface', 'border'].includes(context)) {
+              const name = token.path[2];
+              tokenObj.cssHelper = `hds-${context}-${name}`;
+            }
+            colors['semantic'][context].push(tokenObj);
+          }
+        } else {
+          console.log(
+            `Notice: the color ${token.name} is missing the 'group' attribute in the JSON definition.`
+          );
+        }
+      }
+    });
+
+    return colors;
+  }
+  
+  get cssHelpers() {
+    const cssHelpers = { elevations: [], };
+    ELEVATIONS.forEach((elevation) => {
+      cssHelpers.elevations.push(`.hds-elevation-${elevation}`);
+    });
+    return cssHelpers;
+  }
+
+  get tokens() {
+    return { elevations: [
+        {
+            "value": "0px 2px 3px 0px #656a7626, 0px 16px 16px -10px #656a7633",
+            "original": {
+                "value": "{elevation.high.box-shadow-01.value}, {elevation.high.box-shadow-02.value}"
+            },
+            "name": "token-elevation-high-box-shadow",
+            "attributes": {
+                "category": "elevation",
+                "type": "high",
+                "item": "box-shadow"
+            },
+            "path": [
+                "elevation",
+                "high",
+                "box-shadow"
+            ]
+        },
+        {
+            "value": "0px 2px 3px 0px #656a761a, 0px 12px 28px 0px #656a7640",
+            "original": {
+                "value": "{elevation.higher.box-shadow-01.value}, {elevation.higher.box-shadow-02.value}"
+            },
+            "name": "token-elevation-higher-box-shadow",
+            "attributes": {
+                "category": "elevation",
+                "type": "higher",
+                "item": "box-shadow"
+            },
+            "path": [
+                "elevation",
+                "higher",
+                "box-shadow"
+            ]
+        },
+        {
+            "value": "inset 0px 1px 2px 1px #656a761a",
+            "original": {
+                "value": "{elevation.inset.box-shadow-01.value}"
+            },
+            "name": "token-elevation-inset-box-shadow",
+            "attributes": {
+                "category": "elevation",
+                "type": "inset",
+                "item": "box-shadow"
+            },
+            "path": [
+                "elevation",
+                "inset",
+                "box-shadow"
+            ]
+        },
+      ]
+    }
+    
+  }
+
+}
---
+++ website/docs/testing/markdown/doc-components.md
@@ -0,0 +1,74 @@
+---
+title: DOC Components
+---
+
+## Doc::Badge
+<Doc::Badge @type="neutral">Spacebar</Doc::Badge>
+
+## Doc::Banner
+<Doc::Banner @type="info"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultrices, sapien vel rutrum tristique, dolor quam convallis sapien, dignissim tincidunt libero turpis sit amet urna.</p></Doc::Banner>
+
+## Doc::Content
+<Doc::Content::HdsPrinciples />
+
+## Doc::CopyButton
+For components that are inline elements we can add to an exclusion array in 'website/app/shared/showdown-extensions/remove-auto-p-tags.js' so that the default showdown wrapping  p is not omitted.
+
+<Doc::CopyButton @textToCopy="Yay!" @type="solid" />
+
+If we omit a line return after our markdowon paragraph text the inline element will roll-up into our paragraph like so.
+<Doc::CopyButton @textToCopy="Yay!" @type="solid" />
+
+## Doc::ColorSwatch
+<!-- algolia-ignore-start -->
+<div>
+  {{#each this.colors.semantic.focus as |color|}}
+    <Doc::ColorSwatch @color={{color}} />
+  {{else}}
+    <p>No tokens found for “semantic/focus” colors.</p>
+  {{/each}}
+</div>
+<!-- algolia-ignore-end -->
+
+## Doc::WcagList
+<Doc::WcagList @criteriaList={{array "1.1.1" "1.2.3"}} />
+
+## Doc::A11ySupport
+<Doc::A11ySupport />
+
+## Doc::VarsList
+
+<!-- algolia-ignore-start -->
+<Doc::VarsList @items={{this.cssHelpers.elevations}} />
+<!-- algolia-ignore-end -->
+
+## Doc::TokensList
+  <!-- algolia-ignore-start -->
+  <Doc::TokensList @groupedTokens={{this.tokens}} />
+  <!-- algolia-ignore-end -->
+
+## Doc::ComponentApi
+<Doc::ComponentApi as |C|>
+  <C.Property @name="<:toggle>" @type="named block">
+    A named block that works as “toggle” for the `AccordionItem`.
+  </C.Property>
+  <C.Property @name="type" @required="true" @type="enum" @values={{array "page" "inline" "compact"}}>
+    Sets the type of alert.
+  </C.Property>
+  <C.Property @name="color" @type="enum" @values={{array "neutral" "highlight" "success" "warning" "critical"}} @default="neutral">
+    Sets the color scheme for `background`, `border`, `title`, and `description`, which **cannot** be overridden.<br/><br/>`color` results in a default `icon`, which **can** be overridden.
+  </C.Property>
+  <C.Property @name="...attributes">
+    This component supports use of [`...attributes`](https://guides.emberjs.com/release/in-depth-topics/patterns-for-components/#toc_attribute-ordering).
+  </C.Property>
+  <C.Property @name="<[G].Error>" @type="yielded component">
+    Container that yields its content inside the “error” block at group level. The content can be a simple string or a more complex/structured string, in which case it inherits the text style. For details about its API check the [`Form::Error`](/components/form/primitives) component.
+    <br/><br/>
+    The `id` attribute of the `Error` element is automatically generated.
+    <Doc::ComponentApi as |C|>
+      <C.Property @name="<[E].Message>" @type="yielded component">
+        If the error is made of multiple messages, you can iterate over a collection of error messages yielding individual items using `Error.Message`.
+      </C.Property>
+    </Doc::ComponentApi>
+  </C.Property>
+</Doc::ComponentApi>
