b8a59c885fcf00742958a801a4b9af554980aacd
Alex <alex-ju@users.noreply.github.com>
2024-07-11 23:06:09 +0100

    Convert `Form::FileInput::Field` to TypeScript

---
+++ packages/components/src/components/hds/form/file-input/field.ts
@@ -0,0 +1,25 @@
+import templateOnlyComponent from '@ember/component/template-only';
+import type { ComponentLike } from '@glint/template';
+import type { HdsFormFieldSignature } from '../field';
+import type { HdsFormLabelSignature } from '../label';
+import type { HdsFormHelperTextSignature } from '../helper-text';
+import type { HdsFormErrorSignature } from '../error';
+
+interface HdsFormFileInputFieldSignature {
+  Args: Omit<HdsFormFieldSignature['Args'], 'contextualClass' | 'layout'>;
+  Blocks: {
+    default: [
+      {
+        Label?: ComponentLike<HdsFormLabelSignature>;
+        HelperText?: ComponentLike<HdsFormHelperTextSignature>;
+        Error?: ComponentLike<HdsFormErrorSignature>;
+      },
+    ];
+  };
+  Element: HdsFormFieldSignature['Element'];
+}
+
+const HdsFormFileInputFieldComponent =
+  templateOnlyComponent<HdsFormFileInputFieldSignature>();
+
+export default HdsFormFileInputFieldComponent;
