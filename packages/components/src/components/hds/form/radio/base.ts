354e7f3cf7dda44d78c8607392c4440e316fe58a
Alex <alex-ju@users.noreply.github.com>
2024-07-11 22:21:24 +0100

    Convert `Form::RadioCard:` to TypeScript

--- packages/components/src/components/hds/form/radio/base.ts
+++ packages/components/src/components/hds/form/radio/base.ts
@@ -1,6 +1,6 @@
 import templateOnlyComponent from '@ember/component/template-only';
 
-interface HdsFormRadioBaseSignature {
+export interface HdsFormRadioBaseSignature {
   Args: {
     value?: string;
   };
