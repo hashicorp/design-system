0745239751ae4c55a896d9c5d0647b9656e9795b
Alex <alex-ju@users.noreply.github.com>
2024-07-11 21:21:59 +0100

    Convert `Form::Radio::Group` to TypeScript

---
+++ packages/components/src/components/hds/form/radio/group.ts
@@ -0,0 +1,32 @@
+import templateOnlyComponent from '@ember/component/template-only';
+import type { HdsFormFieldsetSignature } from '../fieldset';
+import type { ComponentLike } from '@glint/template';
+import type { HdsFormLegendSignature } from '../legend';
+import type { HdsFormHelperTextSignature } from '../helper-text';
+import type { HdsFormRadioFieldSignature } from './field';
+import type { HdsFormErrorSignature } from '../error';
+
+interface HdsFormRadioGroupSignature {
+  Args: {
+    layout?: HdsFormFieldsetSignature['Args']['layout'];
+    name?: string;
+    isRequired?: boolean;
+    isOptional?: boolean;
+  };
+  Blocks: {
+    default: [
+      {
+        Legend?: ComponentLike<HdsFormLegendSignature>;
+        HelperText?: ComponentLike<HdsFormHelperTextSignature>;
+        RadioField?: ComponentLike<HdsFormRadioFieldSignature>;
+        Error?: ComponentLike<HdsFormErrorSignature>;
+      },
+    ];
+  };
+  Element: HdsFormFieldsetSignature['Element'];
+}
+
+const HdsFormRadioGroupComponent =
+  templateOnlyComponent<HdsFormRadioGroupSignature>();
+
+export default HdsFormRadioGroupComponent;
