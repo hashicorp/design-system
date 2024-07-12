428328688e85c131d99abf7646613658653c4999
Alex <alex-ju@users.noreply.github.com>
2024-07-12 23:57:35 +0100

    Convert `Form::MaskedInput::Field` to TypeScript

---
+++ packages/components/src/components/hds/form/masked-input/field.ts
@@ -0,0 +1,32 @@
+import templateOnlyComponent from '@ember/component/template-only';
+import type { ComponentLike, WithBoundArgs } from '@glint/template';
+import type HdsFormCharacterCountComponent from '../character-count';
+import type { HdsFormErrorSignature } from '../error';
+import type { HdsFormFieldSignature } from '../field';
+import type { HdsFormHelperTextSignature } from '../helper-text';
+import type { HdsFormLabelSignature } from '../label';
+import type { HdsMaskedInputBaseSignature } from './base';
+
+interface HdsMaskedInputFieldSignature {
+  Args: Omit<HdsFormFieldSignature['Args'], 'contextualClass' | 'layout'> &
+    HdsMaskedInputBaseSignature['Args'];
+  Blocks: {
+    default: [
+      {
+        Label?: ComponentLike<HdsFormLabelSignature>;
+        HelperText?: ComponentLike<HdsFormHelperTextSignature>;
+        Error?: ComponentLike<HdsFormErrorSignature>;
+        CharacterCount?: WithBoundArgs<
+          typeof HdsFormCharacterCountComponent,
+          'value'
+        >;
+      },
+    ];
+  };
+  Element: HdsFormFieldSignature['Element'];
+}
+
+const HdsMaskedInputFieldComponent =
+  templateOnlyComponent<HdsMaskedInputFieldSignature>();
+
+export default HdsMaskedInputFieldComponent;
