afe951e975b95b13f8e98d5a4fe25d9654575a30
shleewhite <lee.white@hahicorp.com>
2024-07-15 12:35:09 -0400

    chore: take out disclosure primitive

--- website/app/components/doc/table-of-contents/collapsible-item.js
+++ website/app/components/doc/table-of-contents/collapsible-item.js
@@ -5,6 +5,21 @@
 
 import Component from '@glimmer/component';
 import { guidFor } from '@ember/object/internals';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 export default class HdsRevealComponent extends Component {
+  @tracked isOpen = this.args.item.isOpen;
+  @action toggleIsOpen() {
+    this.isOpen = !this.isOpen;
+  }
+
   contentId = 'content-' + guidFor(this);
+
+  get classNames() {
+    const classes = ['doc-table-of-contents__button'];
+    if (this.isOpen) {
+      classes.push('open');
+    }
+    return classes.join(' ');
+  }
 }
