87a22bc9de88e4d747659d933c64c166465b2894
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-07-22 13:42:49 -0700

    HDS-3373 Create AppHeader component, add related examples to Showcase
    # Conflicts:
    #	showcase/app/templates/layouts/app-frame/index.hbs

---
+++ packages/components/src/components/hds/app-header/index.ts
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+export interface HdsAppHeaderSignature {
+  Blocks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    globalBefore?: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    logo: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    globalAfter?: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    utility?: any;
+  };
+  Element: HTMLDivElement;
+}
+// More info on types and signatures: https://github.com/hashicorp/design-system/blob/main/wiki/TypeScript-Migration.md
+
+export default class HdsAppHeaderComponent extends Component<HdsAppHeaderSignature> {
+  // UNCOMMENT THIS IF YOU NEED A CONSTRUCTOR
+  // constructor() {
+  //   super(...arguments);
+  //   // ADD YOUR ASSERTIONS HERE
+  // }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    const classes = ['hds-app-header'];
+
+    // add a class based on the @xxx argument
+    // classes.push(`hds-app-header--[variant]-${this.xxx}`);
+
+    return classes.join(' ');
+  }
+}
---
+++ showcase/tests/integration/components/hds/app-header/index-test.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | hds/app-header/index', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::AppHeader id="test-app-header" />`);
+    assert.dom('#test-app-header').hasClass('hds-app-header');
+  });
+});
