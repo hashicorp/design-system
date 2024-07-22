934cf531ae7c05001f1ee1a84c41c2021731df0d
Curt Puckett <curt.puckett@hashicorp.com>
2024-07-22 13:38:15 -0400

    TS Migration end

--- packages/components/src/components.ts
+++ packages/components/src/components.ts
@@ -97,6 +97,7 @@ import HdsSideNavPortal from './components/hds/side-nav/portal/index.ts';
 import HdsSideNavList from './components/hds/side-nav/list/index.ts';
 import HdsStepperStepIndicator from './components/hds/stepper/step/indicator.ts';
 import HdsStepperTaskIndicator from './components/hds/stepper/task/indicator.ts';
+import HdsTableTd from './components/hds/table/td.ts';
 import HdsTabs from './components/hds/tabs/index.ts';
 import HdsTag from './components/hds/tag/index.ts';
 import HdsText from './components/hds/text/index.ts';
@@ -200,6 +201,7 @@ export {
   HdsSideNavList,
   HdsStepperStepIndicator,
   HdsStepperTaskIndicator,
+  HdsTableTd,
   HdsTabs,
   HdsTag,
   HdsText,
---
+++ packages/components/src/components/hds/table/types.ts
@@ -0,0 +1,5 @@
+export enum HdsTableHorizontalAlignment {
+  Left = 'left',
+  Center = 'center',
+  Right = 'right',
+}
