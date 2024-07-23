345f693dfbbf289f8fe15c01385acf01544db891
Zack Moore <zack.moore@hashicorp.com>
2024-07-23 16:14:29 -0400

    `Hds::Icon` Groundwork (#2198)

---
+++ .changeset/fuzzy-suits-call.md
@@ -0,0 +1,5 @@
+---
+"@hashicorp/design-system-components": minor
+---
+
+Loads the `hds-icon` sprite in the `components` package, and ensures it is only loaded once.
--- packages/components/addon-main.cjs
+++ packages/components/addon-main.cjs
@@ -4,4 +4,19 @@
  */
 
 const { addonV1Shim } = require('@embroider/addon-shim');
-module.exports = addonV1Shim(__dirname);
+const flightIconSprite = require('@hashicorp/flight-icons/svg-sprite/svg-sprite-module');
+
+module.exports = {
+  ...addonV1Shim(__dirname),
+  contentFor(type, config) {
+    if (
+      !config.emberFlightIcons?.lazyEmbed &&
+      !config.__flightIconsSpriteLoaded &&
+      type === 'body-footer'
+    ) {
+      config.__flightIconsSpriteLoaded = true;
+
+      return flightIconSprite;
+    }
+  },
+};
