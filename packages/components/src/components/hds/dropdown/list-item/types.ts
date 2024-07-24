91dd3cd1ea5406d01540399dd8aede464b3ae86f
Alex <alex-ju@users.noreply.github.com>
2024-07-24 11:34:16 +0100

    Convert `ListItem::Interactive` to TypeScript

---
+++ packages/components/src/components/hds/dropdown/list-item/types.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+export enum HdsDropdownListItemInteractiveColorValues {
+  Action = 'action',
+  Critical = 'critical',
+}
+export type HdsDropdownListItemInteractiveColors =
+  `${HdsDropdownListItemInteractiveColorValues}`;
