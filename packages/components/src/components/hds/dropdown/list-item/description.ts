f3e9eda58b98c721a8ed0faf591841d507a48817
Alex <alex-ju@users.noreply.github.com>
2024-07-24 11:52:52 +0100

    Convert `ListItem::Description` to TypeScript

--- packages/components/src/components/hds/dropdown/list-item/description.ts
+++ packages/components/src/components/hds/dropdown/list-item/description.ts
@@ -5,15 +5,23 @@
 
 import Component from '@glimmer/component';
 import { assert } from '@ember/debug';
+import type { HdsTextBodySignature } from '../../text/body';
 
-export default class HdsDropdownListItemDescriptionComponent extends Component {
+interface HdsDropdownListItemDescriptionSignature {
+  Args: {
+    text: string;
+  };
+  Element: HdsTextBodySignature['Element'];
+}
+
+export default class HdsDropdownListItemDescriptionComponent extends Component<HdsDropdownListItemDescriptionSignature> {
   /**
    * @param text
    * @type {string}
    * @description The text of the item. If no text value is defined an error will be thrown
    */
-  get text() {
-    let { text } = this.args;
+  get text(): string {
+    const { text } = this.args;
 
     assert(
       '@text for "Hds::Dropdown::ListItem::Description" must have a valid value',
