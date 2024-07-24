1ce02b016925612f69c5a5bc9966225b17b17df3
Alex <alex-ju@users.noreply.github.com>
2024-07-24 12:19:44 +0100

    Convert `Toggle::Icon` to TypeScript

---
+++ packages/components/src/components/hds/dropdown/toggle/types.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+export enum HdsDropdownToggleIconSizeValues {
+  Small = 'small',
+  Medium = 'medium',
+}
+export type HdsDropdownToggleIconSizes = `${HdsDropdownToggleIconSizeValues}`;
