22e952355f4ea16a4f69e79a6aa3dc4a80c3a8e1
Zack Moore <zack.moore@hashicorp.com>
2024-07-26 13:38:42 -0400

    fixing codemod bugs

--- packages/codemods/transforms/v4/icon/index.js
+++ packages/codemods/transforms/v4/icon/index.js
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-const { preserveLayout } = JSON.parse(process.env.CODEMOD_CLI_ARGS);
-
 module.exports = function ({ source /*, path*/ }, { parse, visit }) {
   const ast = parse(source);
+
+  const { preserveLayout } = JSON.parse(process.env.CODEMOD_CLI_ARGS);
 
   return visit(ast, (env) => {
     let { builders: b } = env.syntax;
@@ -19,15 +19,16 @@ module.exports = function ({ source /*, path*/ }, { parse, visit }) {
 
           // look up for `@isInlineBlock`
           const attr = node.attributes.find((a) => a.name === '@isInlineBlock');
-          const { value } = attr;
 
           // @isInlineBlock attr has been set on the element
-          if (attr && value) {
-            if (value === true) {
+          if (attr && attr.value) {
+            if (attr.value === true) {
               // rename attribute as @isInline, keep value
-              const updatedAttr = b.attr('@isInline', value);
+              const updatedAttr = b.attr('@isInline', attr.value);
               outputAttrs.push(updatedAttr);
             }
+
+            // if the value is false, we don't need to add the attribute
           } else if (preserveLayout) {
             // FlightIcon has a default display of inline-block
             // Hds::Icon has a default display of block
@@ -38,7 +39,7 @@ module.exports = function ({ source /*, path*/ }, { parse, visit }) {
 
           return [
             b.element(
-              { name: node.tag, selfClosing: true },
+              { name: 'Hds::Icon', selfClosing: true },
               {
                 attrs: outputAttrs,
                 children: node.children,
