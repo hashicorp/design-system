ef3f0b94ec1fde486587f77781822effacc179c5
Cristiano Rastelli <public@didoo.net>
2024-07-30 13:18:48 +0200

    added support for predefined “foreground” colors to the @color argument of the `Hds::Icon`

--- packages/components/src/components/hds/icon/types.ts
+++ packages/components/src/components/hds/icon/types.ts
@@ -4,3 +4,27 @@ export enum HdsIconSizeValues {
 }
 
 export type HdsIconSizes = `${HdsIconSizeValues}`;
+
+export enum HdsIconColorValues {
+  Primary = 'primary',
+  Strong = 'strong',
+  Faint = 'faint',
+  Disabled = 'disabled',
+  HighContrast = 'high-contrast',
+  Action = 'action',
+  ActionHover = 'action-hover',
+  ActionActive = 'action-active',
+  Highlight = 'highlight',
+  HighlightOnSurface = 'highlight-on-surface',
+  HighlightHighContrast = 'highlight-high-contrast',
+  Success = 'success',
+  SuccessOnSurface = 'success-on-surface',
+  SuccessHighContrast = 'success-high-contrast',
+  Warning = 'warning',
+  WarningOnSurface = 'warning-on-surface',
+  WarningHighContrast = 'warning-high-contrast',
+  Critical = 'critical',
+  CriticalOnSurface = 'critical-on-surface',
+  CriticalHighContrast = 'critical-high-contrast',
+}
+export type HdsIconColors = `${HdsIconColorValues}`;
