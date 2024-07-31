8c40c9962c49f0c46979a4d22a299a4c141eb166
Zack Moore <zack.moore@hashicorp.com>
2024-07-31 14:56:31 -0400

    added custom title tag option

--- packages/components/src/components/hds/application-state/header.ts
+++ packages/components/src/components/hds/application-state/header.ts
@@ -3,20 +3,22 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import TemplateOnlyComponent from '@ember/component/template-only';
+import Component from '@glimmer/component';
 
 import type { FlightIconSignature } from '@hashicorp/ember-flight-icons/components/flight-icon';
-
+import type { HdsTextDisplaySignature } from '../text/display';
 export interface HdsApplicationStateHeaderSignature {
   Args: {
     title?: string;
+    titleTag?: HdsTextDisplaySignature['Args']['tag'];
     errorCode?: string;
     icon?: FlightIconSignature['Args']['name'];
   };
   Element: HTMLDivElement;
 }
 
-const HdsApplicationStateHeaderComponent =
-  TemplateOnlyComponent<HdsApplicationStateHeaderSignature>();
-
-export default HdsApplicationStateHeaderComponent;
+export default class HdsApplicationStateHeaderComponent extends Component<HdsApplicationStateHeaderSignature> {
+  get titleTag() {
+    return this.args.titleTag ?? 'div';
+  }
+}
