9074fc1b74c2e396dec0ba35dab189da6e2f8293
Zack Moore <zack.moore@hashicorp.com>
2024-08-06 18:02:10 -0400

    added option for yielded text

--- packages/components/src/components/hds/dropdown/list-item/interactive.ts
+++ packages/components/src/components/hds/dropdown/list-item/interactive.ts
@@ -25,15 +25,13 @@ export interface HdsDropdownListItemInteractiveSignature {
     text: string;
     trailingIcon?: FlightIconSignature['Args']['name'];
   };
+  Blocks: {
+    default?: [];
+  };
   Element: HTMLDivElement | HdsInteractiveSignature['Element'];
 }
 
 export default class HdsDropdownListItemInteractiveComponent extends Component<HdsDropdownListItemInteractiveSignature> {
-  /**
-   * @param text
-   * @type {string}
-   * @description The text of the item. If no text value is defined an error will be thrown
-   */
   get text(): string {
     const { text } = this.args;
 
@@ -45,12 +43,6 @@ export default class HdsDropdownListItemInteractiveComponent extends Component<H
     return text;
   }
 
-  /**
-   * @param color
-   * @type {string}
-   * @default primary
-   * @description Determines the color of the item (when item is set to interactive)
-   */
   get color(): HdsDropdownListItemInteractiveColors {
     const { color = DEFAULT_COLOR } = this.args;
 
@@ -64,11 +56,6 @@ export default class HdsDropdownListItemInteractiveComponent extends Component<H
     return color;
   }
 
-  /**
-   * Get the class names to apply to the component.
-   * @method classNames
-   * @return {string} The "class" attribute to apply to the component.
-   */
   get classNames(): string {
     const classes = [
       'hds-dropdown-list-item',
