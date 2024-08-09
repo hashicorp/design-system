a8f6c021f04164455053c17a1847c40dd5af1b97
shleewhite <lee.white@hahicorp.com>
2024-08-09 09:08:10 -0400

    feat: move showcase template registry to separate file

--- showcase/types/global.d.ts
+++ showcase/types/global.d.ts
@@ -7,25 +7,7 @@ import '@glint/environment-ember-loose';
 import type PageTitle from 'ember-page-title/template-registry';
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
 import type RenderModifiersRegistry from '@ember/render-modifiers/template-registry';
-
-import ShwAutoscrollable from '../app/components/shw/autoscrollable';
-import ShwDivider from '../app/components/shw/divider';
-import ShwFlex from '../app/components/shw/flex';
-import ShwFlexItem from '../app/components/shw/flex/item';
-import ShwFrame from '../app/components/shw/frame';
-import ShwGrid from '../app/components/shw/grid';
-import ShwGridItem from '../app/components/shw/grid/item';
-import ShwLabel from '../app/components/shw/label';
-import ShwLogoDesignSystem from '../app/components/shw/logo/design-system';
-import ShwOutliner from '../app/components/shw/outliner';
-import ShwPlaceholder from '../app/components/shw/placeholder';
-import ShwText from '../app/components/shw/text';
-import ShwTextBody from '../app/components/shw/text/body';
-import ShwTextH1 from '../app/components/shw/text/h1';
-import ShwTextH2 from '../app/components/shw/text/h2';
-import ShwTextH3 from '../app/components/shw/text/h3';
-import ShwTextH4 from '../app/components/shw/text/h4';
-
+import type ShowcaseTemplateRegistry from './template-registry';
 import type HdsComponentsRegistry from '@hashicorp/design-system-components/template-registry';
 import type FlightIconsRegistry from '@hashicorp/ember-flight-icons/template-registry';
 import type EmberStyleModifier from 'ember-style-modifier';
@@ -41,40 +23,6 @@ declare module '@glint/environment-ember-loose/registry' {
       HdsComponentsRegistry,
       RenderModifiersRegistry,
       FlightIconsRegistry,
-      PageTitle {
-    'Shw::Autoscrollable': typeof ShwAutoscrollable;
-    'shw/autoscrollable': typeof ShwAutoscrollable;
-    'Shw::Divider': typeof ShwDivider;
-    'shw/divider': typeof ShwDivider;
-    'Shw::Flex': typeof ShwFlex;
-    'shw/flex': typeof ShwFlex;
-    'Shw::Flex::Item': typeof ShwFlexItem;
-    'shw/flex/item': typeof ShwFlexItem;
-    'Shw::Frame': typeof ShwFrame;
-    'shw/frame': typeof ShwFrame;
-    'Shw::Grid': typeof ShwGrid;
-    'shw/grid': typeof ShwGrid;
-    'Shw::Grid::Item': typeof ShwGridItem;
-    'shw/grid/item': typeof ShwGridItem;
-    'Shw::Label': typeof ShwLabel;
-    'shw/label': typeof ShwLabel;
-    'Shw::Logo::DesignSystem': typeof ShwLogoDesignSystem;
-    'shw/logo/design-system': typeof ShwLogoDesignSystem;
-    'Shw::Outliner': typeof ShwOutliner;
-    'shw/outliner': typeof ShwOutliner;
-    'Shw::Placeholder': typeof ShwPlaceholder;
-    'shw/placeholder': typeof ShwPlaceholder;
-    'Shw::Text': typeof ShwText;
-    'shw/text': typeof ShwText;
-    'Shw::Text::H1': typeof ShwTextH1;
-    'shw/text/h1': typeof ShwTextH1;
-    'Shw::Text::H2': typeof ShwTextH2;
-    'shw/text/h2': typeof ShwTextH2;
-    'Shw::Text::H3': typeof ShwTextH3;
-    'shw/text/h3': typeof ShwTextH3;
-    'Shw::Text::H4': typeof ShwTextH4;
-    'shw/text/h4': typeof ShwTextH4;
-    'Shw::Text::Body': typeof ShwTextBody;
-    'shw/text/body': typeof ShwTextBody;
-  }
+      PageTitle,
+      ShowcaseTemplateRegistry {}
 }
