10145588eb44f185c835868d68fd208124a77207
Jory Tindall <jory.tindall@hashicorp.com>
2024-08-13 14:52:46 -0700

    Added content for filter overflow and updated the holistic in-context example

--- website/docs/patterns/filter-patterns/index.js
+++ website/docs/patterns/filter-patterns/index.js
@@ -8,6 +8,8 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class Index extends Component {
+  @tracked filterFlyoutActive = false;
+  // Mock API
   @tracked demoFilters = [
     {
       id: 'role',
@@ -559,17 +561,210 @@ export default class Index extends Component {
     'juxtapose',
     'vivid',
   ];
+  @tracked demoOverflowFilters = [
+    {
+      id: 1,
+      name: 'fruits',
+      label: 'Fruits',
+      values: [
+        {
+          id: 'apple',
+          name: 'Apple',
+        },
+        {
+          id: 'banana',
+          name: 'Banana',
+        },
+        {
+          id: 'cherry',
+          name: 'Cherry',
+        },
+        {
+          id: 'date',
+          name: 'Date',
+        },
+        {
+          id: 'elderberry',
+          name: 'Elderberry',
+        },
+        {
+          id: 'fig',
+          name: 'Fig',
+        },
+        {
+          id: 'grape',
+          name: 'Grape',
+        },
+        {
+          id: 'honeydew',
+          name: 'Honeydew',
+        },
+        {
+          id: 'kiwi',
+          name: 'Kiwi',
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'vegetables',
+      label: 'Vegetables',
+      values: [
+        {
+          id: 'artichoke',
+          name: 'Artichoke',
+        },
+        {
+          id: 'beet',
+          name: 'Beet',
+        },
+        {
+          id: 'carrot',
+          name: 'Carrot',
+        },
+        {
+          id: 'daikon',
+          name: 'Daikon',
+        },
+        {
+          id: 'eggplant',
+          name: 'Eggplant',
+        },
+        {
+          id: 'fennel',
+          name: 'Fennel',
+        },
+        {
+          id: 'garlic',
+          name: 'Garlic',
+        },
+        {
+          id: 'horseradish',
+          name: 'Horseradish',
+        },
+        {
+          id: 'iceberg',
+          name: 'Iceberg',
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'grains',
+      label: 'Grains',
+      values: [
+        {
+          id: 'amaranth',
+          name: 'Amaranth',
+        },
+        {
+          id: 'barley',
+          name: 'Barley',
+        },
+        {
+          id: 'corn',
+          name: 'Corn',
+        },
+        {
+          id: 'durum',
+          name: 'Durum',
+        },
+        {
+          id: 'einkorn',
+          name: 'Einkorn',
+        },
+        {
+          id: 'farro',
+          name: 'Farro',
+        },
+        {
+          id: 'grits',
+          name: 'Grits',
+        },
+        {
+          id: 'hemp',
+          name: 'Hemp',
+        },
+        {
+          id: 'indica',
+          name: 'Indica',
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: 'dairy',
+      label: 'Dairy',
+      values: [
+        {
+          id: 'butter',
+          name: 'Butter',
+        },
+        {
+          id: 'cheese',
+          name: 'Cheese',
+        },
+        {
+          id: 'cream',
+          name: 'Cream',
+        },
+        {
+          id: 'dulce',
+          name: 'Dulce',
+        },
+        {
+          id: 'eggnog',
+          name: 'Eggnog',
+        },
+        {
+          id: 'feta',
+          name: 'Feta',
+        },
+        {
+          id: 'ghee',
+          name: 'Ghee',
+        },
+        {
+          id: 'havarti',
+          name: 'Havarti',
+        },
+        {
+          id: 'ice cream',
+          name: 'Ice Cream',
+        },
+      ],
+    },
+  ];
 
+  // Get the model using the mock API
   get model() {
     return {
       demoFilters: this.demoFilters,
       demoUsers: this.demoUsers,
       demoAppliedFilters: this.demoAppliedFilters,
+      demoOverflowFilters: this.demoOverflowFilters,
     };
   }
 
+  // Empty function to pass to the Tag component
   @action
   filterDismissFunction() {
     //
+  }
+
+  // Show browser alert with the Clear all filters button is clicked
+  @action
+  clearAllFilters() {
+    alert('You cleared all of the filters!');
+  }
+
+  @action
+  activateFlyout(flyout) {
+    this[flyout] = true;
+  }
+
+  @action
+  deactivateFlyout(flyout) {
+    this[flyout] = false;
   }
 }
