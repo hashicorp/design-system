25cac768c43595429f59a347f948e50a45242d9d
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-08-21 16:03:03 -0700

    HDS-3770 Update SideNav Showcase to replace deprecated IconButton & add examples of nested interactive components

--- showcase/app/routes/components/side-nav.js
+++ showcase/app/routes/components/side-nav.js
@@ -5,4 +5,19 @@
 
 import Route from '@ember/routing/route';
 
-export default class ComponentsSideNavRoute extends Route {}
+import { COLORS as TOGGLE_BUTTON_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/toggle/button';
+import { COLORS as ITEM_INTERACTIVE_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/list-item/interactive';
+
+export default class ComponentsSideNavRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    const TOGGLE_STATES = ['default', 'hover', 'active', 'focus', 'disabled'];
+    const ITEM_STATES = ['default', 'hover', 'active', 'focus'];
+    return {
+      TOGGLE_BUTTON_COLORS,
+      TOGGLE_STATES,
+      ITEM_INTERACTIVE_COLORS,
+      ITEM_STATES,
+    };
+  }
+}
