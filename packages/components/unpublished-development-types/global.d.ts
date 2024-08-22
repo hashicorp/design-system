07fdab3f2b60113aab85a265dccd8f69fc27a995
Alex <alex-ju@users.noreply.github.com>
2024-08-22 11:09:01 +0100

    Add `PowerSelectRegistry` to global types

--- packages/components/unpublished-development-types/global.d.ts
+++ packages/components/unpublished-development-types/global.d.ts
@@ -9,6 +9,7 @@ import type EmberElementHelperRegistry from 'ember-element-helper/template-regis
 import type EmberStargateRegistry from 'ember-stargate/template-registry';
 import type EmberStyleModifierRegistry from 'ember-style-modifier/template-registry';
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
+import type PowerSelectRegistry from 'ember-power-select/template-registry';
 import type RenderModifiersRegistry from '@ember/render-modifiers/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
@@ -18,6 +19,7 @@ declare module '@glint/environment-ember-loose/registry' {
       EmberStargateRegistry,
       EmberStyleModifierRegistry,
       EmberTruthRegistry,
+      PowerSelectRegistry,
       RenderModifiersRegistry,
       FlightIconsRegistry /*, other addon registries */ {
     // local entries
