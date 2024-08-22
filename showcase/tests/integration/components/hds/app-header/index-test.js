7e8fbba986625d4c68cd650207fbdf1b56cb38d3
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-08-22 15:54:30 -0700

    HDS-3693 Remove aria-hidden as it's not longer needed together with style refactors

--- showcase/tests/integration/components/hds/app-header/index-test.js
+++ showcase/tests/integration/components/hds/app-header/index-test.js
@@ -78,7 +78,6 @@ module('Integration | Component | hds/app-header/index', function (hooks) {
       <Hds::AppHeader id="test-app-header" />
     `);
     assert.dom('#test-app-header').hasClass('hds-app-header--menu-is-closed');
-    assert.dom('.hds-app-header__actions').hasAttribute('aria-hidden', 'true');
   });
 
   test(`the actions show/hide when the menu button is pressed on narrow viewports`, async function (assert) {
@@ -90,11 +89,9 @@ module('Integration | Component | hds/app-header/index', function (hooks) {
 
     await click('.hds-app-header__menu-button');
     assert.dom('#test-app-header').hasClass('hds-app-header--menu-is-open');
-    assert.dom('.hds-app-header__actions').doesNotHaveAttribute('aria-hidden');
 
     await click('.hds-app-header__menu-button');
     assert.dom('#test-app-header').hasClass('hds-app-header--menu-is-closed');
-    assert.dom('.hds-app-header__actions').hasAttribute('aria-hidden', 'true');
   });
 
   // OPTIONS
