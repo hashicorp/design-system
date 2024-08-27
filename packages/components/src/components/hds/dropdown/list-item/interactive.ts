1a6cf43c82a64d5a4ea3a5a403bc3a0260b9f2e9
Zack Moore <zack.moore@hashicorp.com>
2024-08-27 15:33:19 -0400

    responding to pr feedback

--- packages/components/src/components/hds/dropdown/list-item/interactive.ts
+++ packages/components/src/components/hds/dropdown/list-item/interactive.ts
@@ -51,7 +51,7 @@ export default class HdsDropdownListItemInteractiveComponent extends Component<H
         {
           id: 'hds.dropdown.list-item.interactive',
           until: '5.0.0',
-          url: '',
+          url: 'https://helios.hashicorp.design/components/dropdown?tab=version%20history#deprecated',
           for: '@hashicorp/design-system-components',
           since: {
             available: '4.10.0',
