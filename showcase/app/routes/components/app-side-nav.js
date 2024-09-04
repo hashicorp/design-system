fc8ca6710033f94ef1388db579f7e89a501efb2a
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-09-04 11:54:54 -0700

    HDS-3800 Update JS & CSS for Showcase display

--- showcase/app/routes/components/app-side-nav.js
+++ showcase/app/routes/components/app-side-nav.js
@@ -5,4 +5,26 @@
 
 import Route from '@ember/routing/route';
 
-export default class ComponentsAppSideNavRoute extends Route {}
+import { COLORS as DROPDOWN_TOGGLE_BUTTON_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/toggle/button';
+import { COLORS as DROPDOWN_ITEM_INTERACTIVE_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/list-item/interactive';
+
+export default class ComponentsAppSideNavRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    const DROPDOWN_TOGGLE_STATES = [
+      'default',
+      'hover',
+      'active',
+      'focus',
+      'disabled',
+    ];
+    const DROPDOWN_ITEM_STATES = ['default', 'hover', 'active', 'focus'];
+
+    return {
+      DROPDOWN_TOGGLE_BUTTON_COLORS,
+      DROPDOWN_TOGGLE_STATES,
+      DROPDOWN_ITEM_INTERACTIVE_COLORS,
+      DROPDOWN_ITEM_STATES,
+    };
+  }
+}
