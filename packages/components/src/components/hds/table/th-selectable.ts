324189e11a38a27ad9b2d7d2933e8dc697327d65
Zack Moore <zack.moore@hashicorp.com>
2024-09-10 17:21:52 -0400

    removed dynamic classname

--- packages/components/src/components/hds/table/th-selectable.ts
+++ packages/components/src/components/hds/table/th-selectable.ts
@@ -53,15 +53,6 @@ export default class HdsTableThSelectable extends Component<HdsTableThSelectable
     }
   }
 
-  get innerWrapperClassNames(): string {
-    const classes = [
-      'hds-table__th--is-selectable__inner-wrapper',
-      'hds-table__th-content',
-    ];
-
-    return classes.join(' ');
-  }
-
   @action
   didInsert(checkbox: HdsFormCheckboxBaseSignature['Element']): void {
     const { didInsert } = this.args;
