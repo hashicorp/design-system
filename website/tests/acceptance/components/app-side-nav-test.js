dd75a41ee3307850925ac738440db3942f65269c
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-09-10 17:26:38 -0700

    HDS-3800 Fix failing tests, comment out web tests until web docs are added

--- website/tests/acceptance/components/app-side-nav-test.js
+++ website/tests/acceptance/components/app-side-nav-test.js
@@ -3,31 +3,35 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { module } from 'qunit';
+// import { module, test } from 'qunit';
+// import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'website/tests/helpers';
-import { a11yAudit, setRunOptions } from 'ember-a11y-testing/test-support';
+// import { a11yAudit, setRunOptions } from 'ember-a11y-testing/test-support';
 
 module('Acceptance | components/app-side-nav', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /components/app-side-nav', async function (assert) {
-    await visit('/components/app-side-nav');
+  // TODO: Add AppSideNav Web docs then reenable tests
 
-    assert.strictEqual(currentURL(), '/components/app-side-nav');
-  });
-  test('Components/app-side-nav page passes automated a11y checks', async function (assert) {
-    setRunOptions({
-      rules: {
-        'duplicate-id-aria': { enabled: false },
-        'duplicate-id-active': { enabled: false },
-      },
-    });
+  // DISABLEtest('visiting /components/app-side-nav', async function (assert) {
+  //   await visit('/components/app-side-nav');
 
-    await visit('/components/app-side-nav');
+  //   assert.strictEqual(currentURL(), '/components/app-side-nav');
+  // });
 
-    await a11yAudit();
+  // DISABLEtest('Components/app-side-nav page passes automated a11y checks', async function (assert) {
+  //   setRunOptions({
+  //     rules: {
+  //       'duplicate-id-aria': { enabled: false },
+  //       'duplicate-id-active': { enabled: false },
+  //     },
+  //   });
 
-    assert.ok(true, 'a11y automation audit passed');
-  });
+  //   await visit('/components/app-side-nav');
+
+  //   await a11yAudit();
+
+  //   assert.ok(true, 'a11y automation audit passed');
+  // });
 });
