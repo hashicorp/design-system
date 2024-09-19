2d4173da6e4d1073952f3135b7d0a1f9f232ef06
Alex <alex-ju@users.noreply.github.com>
2024-09-19 12:19:07 +0100

    Generate `hds-code-editor` modifier

---
+++ showcase/tests/integration/modifiers/hds-code-editor-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Modifier | hds-code-editor', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function (assert) {
+    await render(hbs`<div {{hds-code-editor}}></div>`);
+
+    assert.ok(true);
+  });
+});
