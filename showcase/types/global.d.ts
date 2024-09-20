d7e935f76af20cbcbed6f6f82935af2c963eaada
Alex <alex-ju@users.noreply.github.com>
2024-09-20 16:17:34 +0100

    Remove `ember-flight-icons` from `showcase`

--- showcase/types/global.d.ts
+++ showcase/types/global.d.ts
@@ -6,7 +6,6 @@ import '@glint/environment-ember-loose';
 
 import type EmberStyleModifierRegistry from 'ember-style-modifier/template-registry';
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
-import type FlightIconsRegistry from '@hashicorp/ember-flight-icons/template-registry';
 import type HdsComponentsRegistry from '@hashicorp/design-system-components/template-registry';
 import type PageTitle from 'ember-page-title/template-registry';
 import type RenderModifiersRegistry from '@ember/render-modifiers/template-registry';
@@ -16,7 +15,6 @@ declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry
     extends EmberStyleModifierRegistry,
       EmberTruthRegistry,
-      FlightIconsRegistry,
       HdsComponentsRegistry,
       PageTitle,
       RenderModifiersRegistry,
