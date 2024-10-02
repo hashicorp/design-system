6a5faf8d3d42c12bc6bbb4018232ba5c6549127f
Alex <alex-ju@users.noreply.github.com>
2024-10-02 13:10:52 +0100

    Fix line numbers always shown when `highlightLines` is set

--- packages/components/src/components/hds/code-block/index.ts
+++ packages/components/src/components/hds/code-block/index.ts
@@ -192,7 +192,7 @@ export default class HdsCodeBlock extends Component<HdsCodeBlockSignature> {
     }
 
     // Note: Prism.js is using the specific class name "line-numbers" to determine implementation of line numbers in the UI
-    if (this.hasLineNumbers || this.args.highlightLines) {
+    if (this.hasLineNumbers) {
       classes.push('line-numbers');
     }
 
