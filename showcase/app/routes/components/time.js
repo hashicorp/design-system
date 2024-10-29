dee7500c1a44c2120bf811707c55157cb89dfb1f
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-10-29 14:54:09 -0700

    HDS-3945 Fix import of displays for Showcase example

--- showcase/app/routes/components/time.js
+++ showcase/app/routes/components/time.js
@@ -5,9 +5,9 @@
 
 import Route from '@ember/routing/route';
 
-import { DISPLAYS } from '@hashicorp/design-system-components/components/hds/time';
+import { DISPLAYS } from '@hashicorp/design-system-components/services/hds-time';
 
-export default class ComponentsTooltipRoute extends Route {
+export default class ComponentsTimeRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
     // const STATES = ['default', 'hover', 'focus']; TODO: Do we need to design states?
