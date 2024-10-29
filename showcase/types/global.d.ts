51b71b71fefd7eec848c717139397dc070c0212c
Cristiano Rastelli <public@didoo.net>
2024-10-29 11:57:05 +0000

    added Ember Stargate to template registry

--- showcase/types/global.d.ts
+++ showcase/types/global.d.ts
@@ -7,9 +7,11 @@ import 'ember-source/types';
 export {};
 
 import '@glint/environment-ember-loose';
+import { Portal, PortalTarget } from 'ember-stargate';
 
 import type EmberStyleModifierRegistry from 'ember-style-modifier/template-registry';
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
+import type EmberStargateRegistry from 'ember-stargate/template-registry';
 import type HdsComponentsRegistry from '@hashicorp/design-system-components/template-registry';
 import type PageTitle from 'ember-page-title/template-registry';
 // eslint-disable-next-line ember/no-at-ember-render-modifiers
@@ -20,8 +22,13 @@ declare module '@glint/environment-ember-loose/registry' {
   export default interface Registry
     extends EmberStyleModifierRegistry,
       EmberTruthRegistry,
+      EmberStargateRegistry,
       HdsComponentsRegistry,
       PageTitle,
       RenderModifiersRegistry,
-      ShowcaseTemplateRegistry {}
+      ShowcaseTemplateRegistry {
+    // we have to use `ember-stargate` version `0.4.3` because version `0.5.0` causes a break in the tests for `ember-lts-3.28`
+    Portal: typeof Portal;
+    PortalTarget: typeof PortalTarget;
+  }
 }
