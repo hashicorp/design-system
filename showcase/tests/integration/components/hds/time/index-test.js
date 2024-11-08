661b224ae44edc1d1aeae609d3c1ed3d5a573fbb
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-11-08 15:59:11 -0800

    HDS-3945 Add tests for various date options

--- showcase/tests/integration/components/hds/time/index-test.js
+++ showcase/tests/integration/components/hds/time/index-test.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
@@ -11,12 +11,11 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | hds/time/index', function (hooks) {
   setupRenderingTest(hooks);
 
-  // let service;
+  let service;
 
-  // hooks.beforeEach(function () {
-  //   service = this.owner.lookup('service:intl');
-  //   service.setLocale('en-us');
-  // });
+  hooks.beforeEach(function () {
+    service = this.owner.lookup('service:hdsTime');
+  });
 
   test('it should render the component with a CSS class that matches the component name', async function (assert) {
     await render(hbs`
@@ -27,7 +26,8 @@ module('Integration | Component | hds/time/index', function (hooks) {
 
   // OPTIONS
 
-  // hasTooltip
+  // OPTION: hasTooltip
+
   test('it should render the component with a tooltip by default', async function (assert) {
     await render(hbs`
       <Hds::Time @date="05 September 2018 14:48" />
@@ -42,6 +42,234 @@ module('Integration | Component | hds/time/index', function (hooks) {
     assert.dom('.hds-tooltip-button').doesNotExist();
   });
 
-  // display
-  // TODO: Add tests for the different display options
+  // OPTION: isOpen (tooltip)
+
+  test('it should render the component with the tooltip closed by default', async function (assert) {
+    await render(hbs`
+      <Hds::Time @date="05 September 2018 14:48" />
+    `);
+    assert.dom('.tippy-box').doesNotExist();
+  });
+
+  test('it should render the component with the tooltip open', async function (assert) {
+    await render(hbs`
+      <Hds::Time @date="05 September 2018 14:48" @isOpen={{true}} />
+    `);
+    assert.dom('.tippy-box').exists();
+  });
+
+  // OPTION: display
+
+  // Default display type
+
+  // default using Date object
+  test('it should render the correct string in the default format when passing a Date object', async function (assert) {
+    this.set('defaultDate', new Date('05 September 2018 14:07:32'));
+    await render(hbs`
+      <Hds::Time @date={{this.defaultDate}} @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Sep 5, 2018, 2:07:32 PM');
+    // test tooltip content
+    assert.dom('.tippy-content').hasText('2018-09-05T21:07:32.000Z');
+  });
+
+  // default using ISO date string
+  test('it should render the correct string in the default format when passing an ISO date string', async function (assert) {
+    await render(hbs`
+      <Hds::Time @date="05 September 2018 14:07:32" @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Sep 5, 2018, 2:07:32 PM');
+    // test tooltip content
+    assert.dom('.tippy-content').hasText('2018-09-05T21:07:32.000Z');
+  });
+
+  // Friendly display types
+
+  // friendly local using Date object
+  test('it should render the correct string in friendly local format when passing a Date object', async function (assert) {
+    this.set('friendlyLocalDate', new Date('05 September 2018 14:07:32'));
+    await render(hbs`
+      <Hds::Time @date={{this.friendlyLocalDate}} @display="friendly-local" @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Sep 5, 2018, 2:07:32 PM');
+    // test tooltip content
+    assert.dom('.tippy-content').hasText('2018-09-05T21:07:32.000Z');
+  });
+
+  // friendly local using ISO date string
+  test('it should render the correct string in friendly local format when passing an ISO date string', async function (assert) {
+    await render(hbs`
+      <Hds::Time @date="05 September 2018 14:07:32" @display="friendly-local" @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Sep 5, 2018, 2:07:32 PM');
+    // test tooltip content
+    assert.dom('.tippy-content').hasText('2018-09-05T21:07:32.000Z');
+  });
+
+  // friendly only using Date object
+  test('it should render the correct string in friendly only format when passing a Date object', async function (assert) {
+    this.set('friendlyOnlyDate', new Date('05 September 2018 14:07:32'));
+
+    await render(hbs`
+      <Hds::Time @date={{this.friendlyOnlyDate}} @display="friendly-only" @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Sep 5, 2018');
+    // test tooltip content
+    assert.dom('.tippy-content').hasText('2018-09-05T21:07:32.000Z');
+  });
+
+  // friendly only using ISO date string
+  test('it should render the correct string in friendly only format when passing an ISO date string', async function (assert) {
+    await render(hbs`
+      <Hds::Time @date="05 September 2018 14:07:32" @display="friendly-only" @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Sep 5, 2018');
+    // test tooltip content
+    assert.dom('.tippy-content').hasText('2018-09-05T21:07:32.000Z');
+  });
+
+  // UTC display type
+
+  // UTC using Date object
+  test('it should render the correct string in UTC format when passing a Date object', async function (assert) {
+    this.set('utcDate', new Date('05 September 2018 14:07:32'));
+
+    await render(hbs`
+      <Hds::Time @date={{this.utcDate}} @display="utc" @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('2018-09-05T21:07:32.000Z');
+    // test tooltip content
+    assert.dom('.tippy-content').hasText('2018-09-05T21:07:32.000Z');
+  });
+
+  // UTC using ISO date string
+  test('it should render the correct string in UTC format when passing an ISO date string', async function (assert) {
+    await render(hbs`
+      <Hds::Time @date="05 September 2018 14:07:32" @display="utc" @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('2018-09-05T21:07:32.000Z');
+    // test tooltip content
+    assert.dom('.tippy-content').hasText('2018-09-05T21:07:32.000Z');
+  });
+
+  // Relative display types
+
+  // TODO: Tests for times in the near future are failing (descriptions are off by 1)
+
+  // near times in the future
+  skip('it should render the correct string for a date that is five minutes from now', async function (assert) {
+    this.set('fiveMinutesFromNow', new Date(service.now + 1000 * 60 * 5));
+
+    await render(hbs`
+      <Hds::Time @date={{this.fiveMinutesFromNow}} @display="relative" />
+    `);
+    assert.dom('.hds-time').hasText('in 5 minutes');
+  });
+
+  skip('it should render the correct string for a date that is two days from now', async function (assert) {
+    this.set('twoDaysFromNow', new Date(service.now + 1000 * 60 * 60 * 24 * 2));
+
+    await render(hbs`
+      <Hds::Time @date={{this.twoDaysFromNow}} @display="relative" />
+    `);
+    assert.dom('.hds-time').hasText('in 2 days');
+  });
+
+  skip('it should render the correct string for a date that is one week from now', async function (assert) {
+    this.set('oneWeekFromNow', new Date(service.now + 1000 * 60 * 60 * 24 * 7));
+
+    await render(hbs`
+      <Hds::Time @date={{this.oneWeekFromNow}} @display="relative" />
+    `);
+    assert.dom('.hds-time').hasText('in 7 days');
+  });
+
+  // near times in the past
+
+  test('it should render the correct string for a date that is five minutes ago', async function (assert) {
+    this.set('fiveMinutesAgo', new Date(service.now - 1000 * 60 * 5));
+
+    await render(hbs`
+      <Hds::Time @date={{this.fiveMinutesAgo}} @display="relative" />
+    `);
+    assert.dom('.hds-time').hasText('5 minutes ago');
+  });
+
+  test('it should render the correct string for a date that is two days ago', async function (assert) {
+    this.set('twoDaysAgo', new Date(service.now - 1000 * 60 * 60 * 24 * 2));
+
+    await render(hbs`
+      <Hds::Time @date={{this.twoDaysAgo}} @display="relative" />
+    `);
+    assert.dom('.hds-time').hasText('2 days ago');
+  });
+
+  test('it should render the correct string for a date that is one week ago', async function (assert) {
+    this.set('oneWeekAgo', new Date(service.now - 1000 * 60 * 60 * 24 * 7));
+
+    await render(hbs`
+      <Hds::Time @date={{this.oneWeekAgo}} @display="relative" />
+    `);
+    assert.dom('.hds-time').hasText('7 days ago');
+  });
+
+  // DATE RANGE
+
+  // Date range with the same year
+
+  // same year using Date objects
+  test('it should render the correct string for a date range with the same year using Date objects', async function (assert) {
+    this.set('startDate', new Date('20 September 2024 14:07:32'));
+    this.set('endDate', new Date('25 September 2024 02:07:32'));
+
+    await render(hbs`
+      <Hds::Time @startDate={{this.startDate}} @endDate={{this.endDate}} @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Sep 20 – Sep 25, 2024');
+    // test tooltip content
+    assert
+      .dom('.tippy-content')
+      .hasText('2024-09-20T21:07:32.000Z - 2024-09-25T09:07:32.000Z');
+  });
+
+  // same year using ISO date strings
+  test('it should render the correct string for a date range with the same year using ISO date strings', async function (assert) {
+    await render(hbs`
+      <Hds::Time @startDate="20 September 2024 14:07:32" @endDate="25 September 2024 02:07:32" @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Sep 20 – Sep 25, 2024');
+    // test tooltip content
+    assert
+      .dom('.tippy-content')
+      .hasText('2024-09-20T21:07:32.000Z - 2024-09-25T09:07:32.000Z');
+  });
+
+  // Date range with different years
+
+  // different years using Date objects
+  test('it should render the correct string for a date range with different years using Date objects', async function (assert) {
+    this.set('startDate', new Date('8 November 2024 14:07:32'));
+    this.set('endDate', new Date('20 January 2025 02:07:32'));
+
+    await render(hbs`
+      <Hds::Time @startDate={{this.startDate}} @endDate={{this.endDate}} @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Nov 8, 2024 – Jan 20, 2025');
+    // test tooltip content
+    assert
+      .dom('.tippy-content')
+      .hasText('2024-11-08T22:07:32.000Z - 2025-01-20T10:07:32.000Z');
+  });
+
+  // different years using ISO date strings
+  test('it should render the correct string for a date range with different years using ISO date strings', async function (assert) {
+    await render(hbs`
+      <Hds::Time @startDate="8 November 2024 14:07:32" @endDate="20 January 2025 02:07:32" @isOpen={{true}} />
+    `);
+    assert.dom('.hds-time').hasText('Nov 8, 2024 – Jan 20, 2025');
+    // test tooltip content
+    assert
+      .dom('.tippy-content')
+      .hasText('2024-11-08T22:07:32.000Z - 2025-01-20T10:07:32.000Z');
+  });
 });
