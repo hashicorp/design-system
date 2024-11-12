a470b6fc1ffe6c438ad41021f698dea5e03eb388
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-11-12 15:30:18 -0800

    HDS-3945 Clean up comments on helpers with examples

--- packages/components/src/helpers/hds-format-date.ts
+++ packages/components/src/helpers/hds-format-date.ts
@@ -10,19 +10,10 @@ import { helper } from '@ember/component/helper';
  * This helper can be used to format a date with the provided options. The result of this helper is a date string.
  *
  * Example:
- * 
- * TODO: Below was revised in actual usage, update example later
+ *
  * ```hbs
- * {{hds-format-date
-      @date
-      month=@display.options.displayFormat.month
-      day=@display.options.displayFormat.day
-      year=@display.options.displayFormat.year
-      hour=@display.options.displayFormat.hour
-      minute=@display.options.displayFormat.minute
-      second=@display.options.displayFormat.second
-    }}
-  * ```
+ * {{hds-format-date @date @displayInner.options.displayFormat}}
+ * ```
  */
 
 export function hdsFormatDate([date, options]: [
