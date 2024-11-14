7c3ea787f435a875f6d098f324ab6d282a83f12d
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-11-14 12:05:14 -0800

    HDS-3945 Add time showcase to percy testing

--- showcase/tests/acceptance/percy-test.js
+++ showcase/tests/acceptance/percy-test.js
@@ -162,6 +162,9 @@ module('Acceptance | Percy test', function (hooks) {
     await visit('/components/text');
     await percySnapshot('Text');
 
+    await visit('/components/time');
+    await percySnapshot('Time');
+
     await visit('/components/toast');
     await percySnapshot('Toast');
 
