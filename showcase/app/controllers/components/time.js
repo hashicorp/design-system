202b7c187e6c29fd6f888475fb588c1bf6202f74
Kristin Bradley <kristin.bradley@hashicorp.com>
2024-11-15 16:52:20 -0800

    Apply suggestions from code review
    Co-authored-by: Lee White <leewhite128@gmail.com>

--- showcase/app/controllers/components/time.js
+++ showcase/app/controllers/components/time.js
@@ -5,7 +5,6 @@
 
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-// import { action } from '@ember/object';
 
 export default class TimeController extends Controller {
   @service hdsTime;
