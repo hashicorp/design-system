d31b5a2ad74abaccfc29ee28e9f384972eb4bd35
Zack Moore <zack.moore@hashicorp.com>
2024-11-18 18:01:06 -0500

    writing tests and cleaning up code

--- showcase/tests/integration/components/hds/code-editor/index-test.js
+++ showcase/tests/integration/components/hds/code-editor/index-test.js
@@ -7,6 +7,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import codeEditorPage from './page';
 
 module('Integration | Component | hds/code-editor/index', function (hooks) {
   setupRenderingTest(hooks);
@@ -14,5 +15,59 @@ module('Integration | Component | hds/code-editor/index', function (hooks) {
   test('it should render the component with a CSS class that matches the component name', async function (assert) {
     await render(hbs`<Hds::CodeEditor id="test-code-editor" />`);
     assert.dom('#test-code-editor').hasClass('hds-code-editor');
+  });
+
+  // @title
+  test('it should render the component with a title', async function (assert) {
+    await render(hbs`<Hds::CodeEditor @title="Test Title" />`);
+    assert.dom(codeEditorPage.selectors.title).hasText('Test Title');
+  });
+  test('it should not render the component with a title when the `@title` argument is not provided', async function (assert) {
+    await render(hbs`<Hds::CodeEditor />`);
+    assert.dom(codeEditorPage.selectors.title).doesNotExist();
+  });
+
+  // @description
+  test('it should render the component with a description', async function (assert) {
+    await render(hbs`<Hds::CodeEditor @description="Test Description" />`);
+    assert.dom(codeEditorPage.selectors.description).hasText('Test Description');
+  });
+  test('it should not render the component with a description when the `@description` argument is not provided', async function (assert) {
+    await render(hbs`<Hds::CodeEditor />`);
+    assert.dom(codeEditorPage.selectors.description).doesNotExist();
+  });
+
+  // toolbar
+  test('it should render custom content in the toolbar when provided', async function (assert) {
+    await render(hbs`
+      <Hds::CodeEditor as |toolbar|>
+        <button id="test-toolbar-button">Test Button</button>
+      </Hds::CodeEditor>
+    `);
+    assert.dom(`${codeEditorPage.selectors.toolbar} #test-toolbar-button`).exists();
+  });
+  // @canCopy
+  test('it should render a copy button when the `@canCopy` argument is true', async function (assert) {
+    await render(hbs`<Hds::CodeEditor @canCopy={{true}} />`);
+    assert.dom(codeEditorPage.selectors.copyButton).exists();
+  });
+  test('it should not render a copy button when the `@canCopy` argument is not provided', async function (assert) {
+    await render(hbs`<Hds::CodeEditor />`);
+    assert.dom(codeEditorPage.selectors.copyButton).doesNotExist();
+  });
+  // @canExpand
+  test('it should render a toggle fullscreen button when the `@canExpand` argument is true', async function (assert) {
+    await render(hbs`<Hds::CodeEditor @canExpand={{true}} />`);
+    assert.dom(codeEditorPage.selectors.expandButton).exists();
+  });
+  test('it should not render a toggle fullscreen button when the `@canExpand` argument is not provided', async function (assert) {
+    await render(hbs`<Hds::CodeEditor />`);
+    assert.dom(codeEditorPage.selectors.expandButton).doesNotExist();
+  });
+
+  // @value & editing
+  test('it should render the component with the provided value', async function (assert) {
+    await render(hbs`<Hds::CodeEditor @value="Test Code" />`);
+    assert.dom(codeEditorPage.selectors.codeEditor).includesText('Test Code');
   });
 });
