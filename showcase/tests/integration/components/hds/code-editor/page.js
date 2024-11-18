6302eea78409f0b8236fe8dababe25523cc973f8
Zack Moore <zack.moore@hashicorp.com>
2024-11-18 18:01:06 -0500

    writing tests and cleaning up code

---
+++ showcase/tests/integration/components/hds/code-editor/page.js
@@ -0,0 +1,12 @@
+const codeEditorPage = {
+  selectors: {
+    title: '.hds-code-editor__title',
+    description: '.hds-code-editor__description',
+    codeEditor: '.hds-code-editor__editor',
+    toolbar: '.hds-code-editor__header-toolbar',
+    copyButton: '.hds-code-editor__copy-button',
+    expandButton: '.hds-code-editor__expand-button',
+  }, 
+}
+
+export default codeEditorPage;
