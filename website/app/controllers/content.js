c3ef067ca05362195bb9dfd88ab01f1487eaeae0
shleewhite <lee.white@hahicorp.com>
2024-11-19 09:54:35 -0500

    feat: website create new content section

---
+++ website/app/controllers/content.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Controller from '@ember/controller';
+
+export default class ContentController extends Controller {
+  get cards() {
+    // we want to use a flat tree here...
+    const tocTree = this.model.toc.flat;
+    const sections = ['content'];
+    const cards = {};
+    sections.forEach((section) => {
+      cards[section] = tocTree
+        .filter(
+          (page) =>
+            page.pageParents[0] === section &&
+            !page.pageAttributes?.navigation?.hidden
+        )
+        .map((page) => {
+          return {
+            image: page.pageAttributes.previewImage,
+            title:
+              page.pageAttributes?.navigation?.label ||
+              page.pageAttributes.title,
+            caption: page.pageAttributes.caption,
+            status: page.pageAttributes.status,
+            route: 'show',
+            model: page.pageURL,
+          };
+        });
+    });
+    return cards;
+  }
+}
