3234733f9963f5321139ed28976c6668642c09fa
Zack Moore <zack.moore@hashicorp.com>
2024-11-19 14:20:53 -0500

    added examples to the showcase

--- showcase/tests/integration/modifiers/hds-code-editor-test.js
+++ showcase/tests/integration/modifiers/hds-code-editor-test.js
@@ -6,10 +6,8 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Modifier | hds-code-editor', function (hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function (assert) {
-    await render(hbs`<div {{hds-code-editor}}></div>`);
-
-    assert.ok(true);
+  test('it converts the element it is applied to into a CodeMirror editor', async function (assert) {
+    await render(hbs`<div id="code-editor-wrapper" {{hds-code-editor}}></div>`);
+    assert.dom('#code-editor-wrapper .cm-editor').exists('code editor is rendered');
   });
 });
