31cf5d9e6c340f0b0783acd6aba2cd9fd8302dfb
Zack Moore <zack.moore@hashicorp.com>
2024-11-20 12:49:06 -0500

    now works with conditionals inside of the dropdown

--- packages/codemods/transforms/v4/dropdown-list-item-interactive/index.js
+++ packages/codemods/transforms/v4/dropdown-list-item-interactive/index.js
@@ -5,6 +5,81 @@
 
 const CODEMOD_ANALYSIS = process.env.CODEMOD_ANALYSIS;
 
+function processChildren(children, asPrefix, b) {
+  let hasUpdatedChildren = false;
+  let processedChildren = [];
+
+  children.forEach((child) => {
+    let updatedChild;
+    let isProcessed = false;
+
+    if (child.type === 'ElementNode' && child.tag === `${asPrefix}.Interactive`) {
+      const textAttr = child.attributes.find((a) => a.name === '@text');
+
+      if (textAttr) {
+        const childOutputAttributes = child.attributes.filter((a) => a.name !== '@text');
+
+        const isHandlebarsAttr = textAttr.value.type === 'MustacheStatement';
+
+        let children = [];
+
+        if (isHandlebarsAttr) {
+          if (textAttr.value.path.type === 'NumberLiteral') {
+            children = [b.mustache(b.number(textAttr.value.path.value))];
+          } else if (textAttr.value.path.type === 'StringLiteral') {
+            children = [b.mustache(b.string(textAttr.value.path.value))];
+          } else {
+            children = [
+              b.mustache(
+                textAttr.value.path.original,
+                [...textAttr.value.params],
+                textAttr.value.hash
+              ),
+            ];
+          }
+        } else {
+          children = [b.text(textAttr.value.chars)];
+        }
+
+        updatedChild = b.element(
+          { name: child.tag, selfClosing: false },
+          {
+            children,
+            attrs: childOutputAttributes,
+            modifiers: child.modifiers,
+            blockParams: child.blockParams,
+          }
+        );
+
+        isProcessed = true;
+      } else {
+        updatedChild = child;
+      }
+    } else if (child.type === 'BlockStatement') {
+      const { hasUpdatedChildren: nestedHasUpdated, processedChildren: nestedProcessed } =
+        processChildren(child.program.body, asPrefix, b);
+
+      if (nestedHasUpdated) {
+        updatedChild = b.block(
+          child.path,
+          child.params,
+          child.hash,
+          b.program(nestedProcessed, child.program.blockParams),
+          child.inverse
+        );
+        isProcessed = true;
+      } else {
+        updatedChild = child;
+      }
+    }
+
+    processedChildren.push(updatedChild || child);
+    hasUpdatedChildren = hasUpdatedChildren || isProcessed;
+  });
+
+  return { hasUpdatedChildren, processedChildren };
+}
+
 module.exports = function ({ source /*, path*/ }, { parse, visit }) {
   const ast = parse(source);
 
@@ -14,83 +89,28 @@ module.exports = function ({ source /*, path*/ }, { parse, visit }) {
     return {
       ElementNode(node) {
         if (node.type === 'ElementNode' && node.tag === 'Hds::Dropdown') {
-          let hasUpdatedChildren = false;
-          let processedChildren;
-
           if (node.blockParams && node.blockParams.length > 0) {
             const asPrefix = node.blockParams[0];
 
-            processedChildren = [];
+            const { hasUpdatedChildren, processedChildren } = processChildren(
+              node.children,
+              asPrefix,
+              b
+            );
 
-            if (node.children) {
-              node.children.forEach((child) => {
-                let updatedChild;
-                let isProcessed = false;
-
-                if (child.type === 'ElementNode' && child.tag === `${asPrefix}.Interactive`) {
-                  const textAttr = child.attributes.find((a) => a.name === '@text');
-
-                  if (textAttr) {
-                    const childOutputAttributes = child.attributes.filter(
-                      (a) => a.name !== '@text'
-                    );
-
-                    const isHandlebarsAttr = textAttr.value.type === 'MustacheStatement';
-
-                    let children = [];
-
-                    if (isHandlebarsAttr) {
-                      if (textAttr.value.path.type === 'NumberLiteral') {
-                        children = [b.mustache(b.number(textAttr.value.path.value))];
-                      } else if (textAttr.value.path.type === 'StringLiteral') {
-                        children = [b.mustache(b.string(textAttr.value.path.value))];
-                      } else {
-                        children = [
-                          b.mustache(
-                            textAttr.value.path.original,
-                            [...textAttr.value.params],
-                            textAttr.value.hash
-                          ),
-                        ];
-                      }
-                    } else {
-                      children = [b.text(textAttr.value.chars)];
-                    }
-
-                    updatedChild = b.element(
-                      { name: child.tag, selfClosing: false },
-                      {
-                        children,
-                        attrs: childOutputAttributes,
-                        modifiers: child.modifiers,
-                        blockParams: child.blockParams,
-                      }
-                    );
-
-                    isProcessed = true;
-                  } else {
-                    updatedChild = child;
+            if (hasUpdatedChildren && !CODEMOD_ANALYSIS) {
+              return [
+                b.element(
+                  { name: node.tag, selfClosing: false },
+                  {
+                    attrs: node.attributes,
+                    children: processedChildren,
+                    modifiers: node.modifiers,
+                    blockParams: node.blockParams,
                   }
-                }
-
-                processedChildren.push(updatedChild || child);
-                hasUpdatedChildren = hasUpdatedChildren || isProcessed;
-              });
+                ),
+              ];
             }
-          }
-
-          if (hasUpdatedChildren && !CODEMOD_ANALYSIS) {
-            return [
-              b.element(
-                { name: node.tag, selfClosing: false },
-                {
-                  attrs: node.attributes,
-                  children: processedChildren || node.children,
-                  modifiers: node.modifiers,
-                  blockParams: node.blockParams,
-                }
-              ),
-            ];
           }
         }
       },
