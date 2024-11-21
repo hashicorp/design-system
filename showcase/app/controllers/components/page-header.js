b57bdbf771567655c3e643d4a51369193bb0bd62
Dylan Hyun <dylan.hyun@hashicorp.com>
2024-11-21 16:08:46 -0500

    HDS-4154 remove unneeded route from page header showcase

--- showcase/app/controllers/components/page-header.js
+++ showcase/app/controllers/components/page-header.js
@@ -6,11 +6,8 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { inject as service } from '@ember/service';
 
 export default class PageHeaderController extends Controller {
-  @service router;
-
   @tracked showHighlight = false;
 
   // =============================
