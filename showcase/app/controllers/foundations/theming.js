748e34cf352629c0540c526484f2dc72e8fa5530
Cristiano Rastelli <public@didoo.net>
2024-12-03 21:56:28 +0000

    WIP 6

---
+++ showcase/app/controllers/foundations/theming.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default class ThemingController extends Controller {
+  @service theming;
+
+  get currentTheme() {
+    return this.theming.getTheme();
+  }
+}
