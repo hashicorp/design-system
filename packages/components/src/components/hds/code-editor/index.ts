38fbf1739c323a2b05a1cf463b6c47eab3274a16
Zack Moore <zack.moore@hashicorp.com>
2024-12-06 16:52:07 -0500

    improving keybopard navigation

--- packages/components/src/components/hds/code-editor/index.ts
+++ packages/components/src/components/hds/code-editor/index.ts
@@ -13,13 +13,6 @@ import type { HdsButtonSignature } from 'src/components/hds/button';
 import type { HdsCodeEditorDescriptionSignature } from './description';
 import type { HdsCodeEditorTitleSignature } from './title';
 
-type HTMLInteractiveElement =
-  | HTMLButtonElement
-  | HTMLAnchorElement
-  | HTMLInputElement
-  | HTMLSelectElement
-  | HTMLTextAreaElement;
-
 export interface HdsCodeEditorSignature {
   Args: {
     hasCopyButton?: boolean;
@@ -45,8 +38,6 @@ export interface HdsCodeEditorSignature {
 
 export default class HdsCodeEditor extends Component<HdsCodeEditorSignature> {
   @tracked isFullscreen = false;
-  @tracked fullscreenButton: HdsButtonSignature['Element'] | null = null;
-  @tracked codeEditorHeader: HTMLElement | null = null;
 
   get classNames(): string {
     const classes = ['hds-code-editor'];
@@ -67,45 +58,7 @@ export default class HdsCodeEditor extends Component<HdsCodeEditorSignature> {
   }
 
   @action
-  registerCodeEditorHeader(element: HTMLElement): void {
-    this.codeEditorHeader = element;
-  }
-
-  @action
-  registerFullscreenButton(element: HdsButtonSignature['Element']): void {
-    this.fullscreenButton = element;
-  }
-
-  @action
   toggleFullscreen(): void {
     this.isFullscreen = !this.isFullscreen;
-  }
-
-  @action
-  handleKeyDown(event: KeyboardEvent): void {
-    if (event.key === 'Escape') {
-      if (this.isFullscreen) {
-        this.toggleFullscreen();
-      }
-
-      // if the fullscreen button is present, focus it
-      if (this.fullscreenButton != null) {
-        return this.fullscreenButton.focus();
-      }
-
-      // otherwise, focus the first interactive element in the header
-      const interactiveHeaderElements = this.codeEditorHeader?.querySelectorAll(
-        'button, a, input, select, textarea'
-      );
-
-      if (
-        interactiveHeaderElements != null &&
-        interactiveHeaderElements.length > 0
-      ) {
-        return (interactiveHeaderElements[0] as HTMLInteractiveElement).focus();
-      } else {
-        document.querySelector('body')?.focus();
-      }
-    }
   }
 }
