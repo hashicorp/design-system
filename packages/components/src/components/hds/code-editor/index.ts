77e05c21246e577d95509c622ac649fa92bd3eab
Zack Moore <zack.moore@hashicorp.com>
2024-12-06 15:42:11 -0500

    adding keyboard nav support

--- packages/components/src/components/hds/code-editor/index.ts
+++ packages/components/src/components/hds/code-editor/index.ts
@@ -5,12 +5,20 @@
 
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 import type { ComponentLike } from '@glint/template';
 import type { HdsCodeEditorSignature as HdsCodeEditorModifierSignature } from 'src/modifiers/hds-code-editor';
 import type { HdsButtonSignature } from 'src/components/hds/button';
 import type { HdsCodeEditorDescriptionSignature } from './description';
 import type { HdsCodeEditorTitleSignature } from './title';
+
+type HTMLInteractiveElement =
+  | HTMLButtonElement
+  | HTMLAnchorElement
+  | HTMLInputElement
+  | HTMLSelectElement
+  | HTMLTextAreaElement;
 
 export interface HdsCodeEditorSignature {
   Args: {
@@ -37,6 +45,8 @@ export interface HdsCodeEditorSignature {
 
 export default class HdsCodeEditor extends Component<HdsCodeEditorSignature> {
   @tracked isFullscreen = false;
+  @tracked fullscreenButton: HdsButtonSignature['Element'] | null = null;
+  @tracked codeEditorHeader: HTMLElement | null = null;
 
   get classNames(): string {
     const classes = ['hds-code-editor'];
@@ -54,5 +64,48 @@ export default class HdsCodeEditor extends Component<HdsCodeEditorSignature> {
 
   get hasToolbarAction() {
     return this.args.hasCopyButton || this.args.canExpand;
+  }
+
+  @action
+  registerCodeEditorHeader(element: HTMLElement) {
+    this.codeEditorHeader = element;
+  }
+
+  @action
+  registerFullscreenButton(element: HdsButtonSignature['Element']) {
+    this.fullscreenButton = element;
+  }
+
+  @action
+  toggleFullscreen() {
+    this.isFullscreen = !this.isFullscreen;
+  }
+
+  @action
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      if (this.isFullscreen) {
+        this.toggleFullscreen();
+      }
+
+      // if the fullscreen button is present, focus it
+      if (this.fullscreenButton != null) {
+        return this.fullscreenButton.focus();
+      }
+
+      // otherwise, focus the first interactive element in the header
+      const interactiveHeaderElements = this.codeEditorHeader?.querySelectorAll(
+        'button, a, input, select, textarea'
+      );
+
+      if (
+        interactiveHeaderElements != null &&
+        interactiveHeaderElements.length > 0
+      ) {
+        return (interactiveHeaderElements[0] as HTMLInteractiveElement).focus();
+      } else {
+        document.querySelector('body')?.focus();
+      }
+    }
   }
 }
