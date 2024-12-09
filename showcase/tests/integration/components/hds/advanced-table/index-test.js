74a99604ce1018f163b890d42ef0b1b53edd3c9b
shleewhite <lee.white@hahicorp.com>
2024-12-09 16:45:23 -0500

    fix: broken index test

--- showcase/tests/integration/components/hds/advanced-table/index-test.js
+++ showcase/tests/integration/components/hds/advanced-table/index-test.js
@@ -12,7 +12,19 @@ module('Integration | Component | hds/advanced-table/index', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it should render the component with a CSS class that matches the component name', async function (assert) {
-    await render(hbs`<Hds::AdvancedTable id="test-advanced-table" />`);
-    assert.dom('#test-advanced-table').hasClass('hds-advanced-table');
+    this.set('model', [
+      { key: 'artist', name: 'Test 1', description: 'Test 1 description' },
+      { key: 'album', name: 'Test 2', description: 'Test 2 description' },
+      { key: 'year', name: 'Test 3', description: 'Test 3 description' },
+    ]);
+
+    await render(
+      hbs`<Hds::AdvancedTable id="data-test-advanced-table" @model={{this.model}} @columns={{array
+        (hash key='artist' label='components.table.headers.artist')
+        (hash key='album' label='components.table.headers.album')
+        (hash key='year' label='components.table.headers.year')
+      }}/>`
+    );
+    assert.dom('#data-test-advanced-table').hasClass('hds-advanced-table');
   });
 });
