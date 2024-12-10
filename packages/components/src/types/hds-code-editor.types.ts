54228080bb3e61ce0c733b02427d0161e490f839
Zack Moore <zack.moore@hashicorp.com>
2024-12-09 19:46:46 -0500

    working on dynamic package importing

--- packages/components/src/types/hds-code-editor.types.ts
+++ packages/components/src/types/hds-code-editor.types.ts
@@ -2,12 +2,29 @@
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
  */
+import type { Extension } from '@codemirror/state';
 
 export enum HdsCodeEditorLanguageValues {
-  Javascript = 'javascript',
   Json = 'json',
   Sql = 'sql',
   Go = 'go',
 }
 
 export type HdsCodeEditorLanguages = `${HdsCodeEditorLanguageValues}`;
+
+export interface CodemirrorJsonModule {
+  json: () => Extension;
+}
+
+export interface CodemirrorGoModule {
+  go: () => Extension;
+}
+
+export interface CodemirrorSqlModule {
+  sql: () => Extension;
+}
+
+export type CodemirrorLanguageModule =
+  | CodemirrorJsonModule
+  | CodemirrorGoModule
+  | CodemirrorSqlModule;
