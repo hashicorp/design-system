4fb1df433b26d15041451279e020bb3e51a7de56
Zack Moore <zack.moore@hashicorp.com>
2024-12-10 19:08:27 -0500

    updating active line styles

--- packages/components/src/modifiers/hds-code-editor/themes/hds-dark-theme.ts
+++ packages/components/src/modifiers/hds-code-editor/themes/hds-dark-theme.ts
@@ -5,8 +5,9 @@
 
 import { EditorView } from '@codemirror/view';
 
-const BG_COLOR = '#0d0e12';
+const BG_COLOR = '#15181E';
 const ACTIVE_LINE_BG_COLOR = 'rgba(0, 74, 222, 0.2)';
+const ACTIVE_LINE_BORDER_COLOR = '#1555D4';
 
 const hdsDark = EditorView.theme(
   {
@@ -16,34 +17,38 @@ const hdsDark = EditorView.theme(
       height: '100%',
     },
     '.cm-content': {
+      backgroundColor: BG_COLOR,
       padding: '16px 0',
     },
-    '.cm-gutter': {
+    '.cm-gutters': {
       backgroundColor: BG_COLOR,
+    },
+    '.cm-gutter': {
       borderRight: '1px solid rgba(178, 182, 189, 40%)',
       color: '#b2b6bd',
     },
     '.cm-lineNumbers': {
-      color: '#b2b6bd',
+      color: '#878A8F',
     },
     '.cm-lineNumbers .cm-gutterElement': {
       borderLeft: '4px solid transparent',
       padding: '0 16px',
     },
     '.cm-gutterElement.cm-activeLineGutter': {
-      borderColor: '#1555D4',
+      borderColor: ACTIVE_LINE_BORDER_COLOR,
       backgroundColor: ACTIVE_LINE_BG_COLOR,
-      outline: '1px solid #1555D4',
+      color: '#EFEFF1',
+      outline: `1px solid ${ACTIVE_LINE_BORDER_COLOR}`,
     },
     '.cm-line': {
       padding: '0 16px',
     },
     '.cm-activeLine': {
       backgroundColor: ACTIVE_LINE_BG_COLOR,
-      outline: '1px solid #1555D4',
+      outline: `1px solid ${ACTIVE_LINE_BORDER_COLOR}`,
     },
     '.cm-matchingBracket': {
-      outline: '1px solid #BFBFC0',
+      outline: '1px solid #EFEFF1',
     },
   },
   { dark: true }
