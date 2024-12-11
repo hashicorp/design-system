a54dad60cd270a404ccf4a6b448d62c25e5189fd
Zack Moore <zack.moore@hashicorp.com>
2024-12-11 15:17:53 -0500

    using local colors

--- packages/components/src/modifiers/hds-code-editor/palettes/hds-dark-palette.ts
+++ packages/components/src/modifiers/hds-code-editor/palettes/hds-dark-palette.ts
@@ -1,6 +1,13 @@
+export const HDS_CODE_BLOCK_WHITE = '#efeff1';
+export const HDS_CODE_BLOCK_BLUE = '#2d8eff';
+export const HDS_CODE_BLOCK_GREEN = '#86ff13';
+export const HDS_CODE_BLOCK_ORANGE = '#ffa800';
+export const HDS_CODE_BLOCK_PURPLE = '#c76cff';
+export const HDS_CODE_BLOCK_RED = '#ff3b20';
+export const HDS_CODE_BLOCK_CYAN = '#32fff7';
+
 export const HDS_CODE_BLOCK_LINE_HIGHLIGHT = 'rgba(0, 74, 222, 0.2)';
 export const HDS_CODE_BLOCK_LINE_HIGHLIGHT_BORDER = '#1b5fe5';
-export const HDS_CODE_BLOCK_WHITE = '#efeff1';
 
 export const HDS_CODE_EDITOR_COLOR_FOREGROUND_PRIMARY = '#d5d7db';
 export const HDS_CODE_EDITOR_COLOR_FOREGROUND_FAINT = '#878a8f';
