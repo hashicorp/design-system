808ae191efa00964ead99282ae7965c56d61aa75
Zack Moore <zack.moore@hashicorp.com>
2024-12-11 15:17:53 -0500

    using local colors

--- packages/components/src/modifiers/hds-code-editor/highlight-styles/hds-dark-highlight-style.ts
+++ packages/components/src/modifiers/hds-code-editor/highlight-styles/hds-dark-highlight-style.ts
@@ -1,31 +1,33 @@
 import { tags } from '@lezer/highlight';
 import { HighlightStyle } from '@codemirror/language';
-
-const HDS_CODE_BLUE = '#2d8eff';
-const HDS_CODE_GREEN = '#86ff13';
-const HDS_CODE_ORANGE = '#ffa800';
-const HDS_CODE_PURPLE = '#c76cff';
-const HDS_CODE_RED = '#ff3b20';
-const HDS_CODE_CYAN = '#32fff7';
+import {
+  HDS_CODE_BLOCK_BLUE,
+  HDS_CODE_BLOCK_GREEN,
+  HDS_CODE_BLOCK_ORANGE,
+  HDS_CODE_BLOCK_PURPLE,
+  HDS_CODE_BLOCK_RED,
+  HDS_CODE_BLOCK_CYAN,
+  HDS_CODE_EDITOR_COLOR_FOREGROUND_PRIMARY,
+} from '../palettes/hds-dark-palette.ts';
 
 const hdsDarkHighlightStyle = HighlightStyle.define([
-  { tag: tags.comment, color: '#b2b6bd' },
-  { tag: tags.name, color: '#b2b6bd' },
-  { tag: tags.className, color: HDS_CODE_BLUE },
-  { tag: tags.string, color: HDS_CODE_ORANGE },
-  { tag: tags.attributeName, color: HDS_CODE_BLUE },
-  { tag: tags.attributeValue, color: HDS_CODE_ORANGE },
-  { tag: tags.number, color: HDS_CODE_PURPLE },
-  { tag: tags.bool, color: HDS_CODE_PURPLE },
-  { tag: tags.regexp, color: HDS_CODE_ORANGE },
-  { tag: tags.url, color: HDS_CODE_CYAN },
-  { tag: tags.keyword, color: HDS_CODE_GREEN },
-  { tag: tags.operator, color: HDS_CODE_CYAN },
-  { tag: tags.punctuation, color: '#d5d7db' },
-  { tag: tags.propertyName, color: HDS_CODE_BLUE },
-  { tag: tags.deleted, color: HDS_CODE_RED },
-  { tag: tags.function(tags.variableName), color: HDS_CODE_BLUE },
-  { tag: tags.function(tags.propertyName), color: HDS_CODE_BLUE },
+  { tag: tags.comment, color: HDS_CODE_EDITOR_COLOR_FOREGROUND_PRIMARY },
+  { tag: tags.name, color: HDS_CODE_EDITOR_COLOR_FOREGROUND_PRIMARY },
+  { tag: tags.className, color: HDS_CODE_BLOCK_BLUE },
+  { tag: tags.string, color: HDS_CODE_BLOCK_ORANGE },
+  { tag: tags.attributeName, color: HDS_CODE_BLOCK_BLUE },
+  { tag: tags.attributeValue, color: HDS_CODE_BLOCK_ORANGE },
+  { tag: tags.number, color: HDS_CODE_BLOCK_PURPLE },
+  { tag: tags.bool, color: HDS_CODE_BLOCK_PURPLE },
+  { tag: tags.regexp, color: HDS_CODE_BLOCK_ORANGE },
+  { tag: tags.url, color: HDS_CODE_BLOCK_CYAN },
+  { tag: tags.keyword, color: HDS_CODE_BLOCK_GREEN },
+  { tag: tags.operator, color: HDS_CODE_BLOCK_CYAN },
+  { tag: tags.punctuation, color: HDS_CODE_EDITOR_COLOR_FOREGROUND_PRIMARY },
+  { tag: tags.propertyName, color: HDS_CODE_BLOCK_BLUE },
+  { tag: tags.deleted, color: HDS_CODE_BLOCK_RED },
+  { tag: tags.function(tags.variableName), color: HDS_CODE_BLOCK_BLUE },
+  { tag: tags.function(tags.propertyName), color: HDS_CODE_BLOCK_BLUE },
 ]);
 
 export default hdsDarkHighlightStyle;
