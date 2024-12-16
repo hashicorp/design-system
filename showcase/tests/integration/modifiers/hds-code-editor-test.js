692e414fa533eba7e7d93e1f45d56d066536d212
Zack Moore <zack.moore@hashicorp.com>
2024-12-16 17:49:51 -0500

    fixing linting issues

--- showcase/tests/integration/modifiers/hds-code-editor-test.js
+++ showcase/tests/integration/modifiers/hds-code-editor-test.js
@@ -8,6 +8,8 @@ module('Integration | Modifier | hds-code-editor', function (hooks) {
 
   test('it converts the element it is applied to into a CodeMirror editor', async function (assert) {
     await render(hbs`<div id="code-editor-wrapper" {{hds-code-editor}}></div>`);
-    assert.dom('#code-editor-wrapper .cm-editor').exists('code editor is rendered');
+    assert
+      .dom('#code-editor-wrapper .cm-editor')
+      .exists('code editor is rendered');
   });
 });
