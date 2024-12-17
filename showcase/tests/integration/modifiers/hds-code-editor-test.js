31188731d31dd0507a7491092a9b2dcbe2a24c2a
Zack Moore <zack.moore@hashicorp.com>
2024-12-17 15:20:53 -0500

    cleaning up tests

--- showcase/tests/integration/modifiers/hds-code-editor-test.js
+++ showcase/tests/integration/modifiers/hds-code-editor-test.js
@@ -1,15 +1,54 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, waitFor } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import sinon from 'sinon';
+
+async function setupCodeEditor(hbsTemplate) {
+  await render(hbsTemplate);
+  return waitFor('.cm-editor');
+}
 
 module('Integration | Modifier | hds-code-editor', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it converts the element it is applied to into a CodeMirror editor', async function (assert) {
-    await render(hbs`<div id="code-editor-wrapper" {{hds-code-editor}}></div>`);
+    await setupCodeEditor(
+      hbs`<div id="code-editor-wrapper" {{hds-code-editor}}></div>`
+    );
     assert
       .dom('#code-editor-wrapper .cm-editor')
       .exists('code editor is rendered');
+  });
+
+  // value
+  test('it should render the component with the provided value', async function (assert) {
+    await setupCodeEditor(hbs`<Hds::CodeEditor @value="Test Code" />`);
+    assert.dom('.hds-code-editor__editor').includesText('Test Code');
+  });
+
+  // onInput
+  test('it should call the onInput action when the code editor value changes', async function (assert) {
+    const inputSpy = sinon.spy();
+
+    this.setProperties({
+      handleInput: inputSpy,
+      handleSetup: (editorView) => {
+        this.set('editorView', editorView);
+      },
+    });
+
+    await setupCodeEditor(
+      hbs`<div {{hds-code-editor onInput=this.handleInput onSetup=this.handleSetup}} />`
+    );
+
+    this.editorView.dispatch({
+      changes: {
+        from: this.editorView.state.selection.main.from,
+        insert: 'Test string',
+      },
+    });
+
+    assert.ok(inputSpy.calledOnceWith('Test string'));
   });
 });
