4ea8934521ef0109e3050c5a299e571f9958cc11
Zack Moore <zack.moore@hashicorp.com>
2024-12-17 16:39:06 -0500

    fixed typescript error ignores

--- packages/components/src/modifiers/hds-code-editor.ts
+++ packages/components/src/modifiers/hds-code-editor.ts
@@ -5,7 +5,7 @@
 
 import { assert } from '@ember/debug';
 import Modifier from 'ember-modifier';
-import { dropTask } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 import config from 'ember-get-config';
 
 import hdsDarkTheme from './hds-code-editor/themes/hds-dark-theme.ts';
@@ -52,15 +52,12 @@ export default class HdsCodeEditorModifier extends Modifier<HdsCodeEditorSignatu
     assert('HdsCodeEditor must have an element', element);
     // the intersection observer makes loading unreliable in tests
     if (config.environment === 'test') {
-      // @ts-ignore
       this.setupTask.perform(element, positional, named);
     } else {
       this.observer = new IntersectionObserver(
         (entries) => {
           entries.forEach((entry) => {
-            // @ts-ignore
             if (entry.isIntersecting && this.setupTask.performCount === 0) {
-              // @ts-ignore
               this.setupTask.perform(element, positional, named);
             }
           });
@@ -78,110 +75,117 @@ export default class HdsCodeEditorModifier extends Modifier<HdsCodeEditorSignatu
     this.observer.disconnect();
   }
 
-  @dropTask *loadLanguageTask(language?: HdsCodeEditorLanguages) {
-    if (language === undefined) {
-      return;
+  loadLanguageTask = task(
+    this,
+    { drop: true },
+    async (language?: HdsCodeEditorLanguages) => {
+      if (language === undefined) {
+        return;
+      }
+
+      let module: CodemirrorLanguageModule | null = null;
+      let languageFunction = null;
+
+      switch (language) {
+        case 'go':
+          module = await import('@codemirror/lang-go');
+          languageFunction = (module as CodemirrorGoModule).go;
+          break;
+        case 'json':
+          module = await import('@codemirror/lang-json');
+          languageFunction = (module as CodemirrorJsonModule).json;
+          break;
+        case 'sql':
+          module = await import('@codemirror/lang-sql');
+          languageFunction = (module as CodemirrorSqlModule).sql;
+          break;
+        case 'hcl':
+          module = await import('codemirror-lang-hcl');
+          languageFunction = (module as CodemirrorHclModule).hcl;
+          break;
+        default:
+          throw new Error(`Language ${language} is not supported`);
+      }
+
+      return languageFunction();
     }
+  );
 
-    let module: CodemirrorLanguageModule | null = null;
-    let languageFunction = null;
+  setupTask = task(
+    this,
+    { drop: true },
+    async (
+      element: HTMLElement,
+      _positional: PositionalArgs<HdsCodeEditorSignature>,
+      named: NamedArgs<HdsCodeEditorSignature>
+    ) => {
+      const { onInput, onSetup, language, value } = named;
 
-    switch (language) {
-      case 'go':
-        module = yield import('@codemirror/lang-go');
-        languageFunction = (module as CodemirrorGoModule).go;
-        break;
-      case 'json':
-        module = yield import('@codemirror/lang-json');
-        languageFunction = (module as CodemirrorJsonModule).json;
-        break;
-      case 'sql':
-        module = yield import('@codemirror/lang-sql');
-        languageFunction = (module as CodemirrorSqlModule).sql;
-        break;
-      case 'hcl':
-        module = yield import('codemirror-lang-hcl');
-        languageFunction = (module as CodemirrorHclModule).hcl;
-        break;
-      default:
-        throw new Error(`Language ${language} is not supported`);
+      const [
+        {
+          EditorView,
+          keymap,
+          lineNumbers,
+          highlightActiveLineGutter,
+          highlightSpecialChars,
+          highlightActiveLine,
+        },
+        { EditorState },
+        { defaultKeymap, history, historyKeymap },
+        { bracketMatching, syntaxHighlighting },
+      ] = await Promise.all([
+        import('@codemirror/view'),
+        import('@codemirror/state'),
+        import('@codemirror/commands'),
+        import('@codemirror/language'),
+      ]);
+
+      this.onInput = onInput;
+
+      const languageExtension = await this.loadLanguageTask.perform(language);
+
+      let extensions = [
+        lineNumbers(),
+        highlightActiveLineGutter(),
+        highlightSpecialChars(),
+        highlightActiveLine(),
+        EditorView.updateListener.of((update: ViewUpdate) => {
+          // toggle a class if the update has/does not have a selection
+          if (update.selectionSet) {
+            update.view.dom.classList.toggle(
+              'cm-hasSelection',
+              !update.state.selection.main.empty
+            );
+          }
+
+          // call the onInput callback if the document has changed
+          if (!update.docChanged || this.onInput === undefined) {
+            return;
+          }
+          this.onInput(update.state.doc.toString());
+        }),
+        hdsDarkTheme,
+        keymap.of([...defaultKeymap, ...historyKeymap]),
+        bracketMatching(),
+        syntaxHighlighting(hdsDarkHighlightStyle),
+        history(),
+      ];
+
+      if (languageExtension !== undefined) {
+        extensions = [languageExtension, ...extensions];
+      }
+
+      const state = EditorState.create({
+        doc: value,
+        extensions,
+      });
+
+      const editor = new EditorView({ state, parent: element });
+
+      this.editor = editor;
+      this.element = element;
+
+      onSetup?.(this.editor);
     }
-
-    return languageFunction();
-  }
-
-  @dropTask *setupTask(
-    element: HTMLElement,
-    _positional: PositionalArgs<HdsCodeEditorSignature>,
-    named: NamedArgs<HdsCodeEditorSignature>
-  ) {
-    const { onInput, onSetup, language, value } = named;
-
-    const [
-      {
-        EditorView,
-        keymap,
-        lineNumbers,
-        highlightActiveLineGutter,
-        highlightSpecialChars,
-        highlightActiveLine,
-      },
-      { EditorState },
-      { defaultKeymap, history, historyKeymap },
-      { bracketMatching, syntaxHighlighting },
-    ] = yield Promise.all([
-      import('@codemirror/view'),
-      import('@codemirror/state'),
-      import('@codemirror/commands'),
-      import('@codemirror/language'),
-    ]);
-
-    this.onInput = onInput;
-
-    // @ts-ignore
-    const languageExtension = yield this.loadLanguageTask.perform(language);
-
-    let extensions = [
-      lineNumbers(),
-      highlightActiveLineGutter(),
-      highlightSpecialChars(),
-      highlightActiveLine(),
-      EditorView.updateListener.of((update: ViewUpdate) => {
-        // toggle a class if the update has/does not have a selection
-        if (update.selectionSet) {
-          update.view.dom.classList.toggle(
-            'cm-hasSelection',
-            !update.state.selection.main.empty
-          );
-        }
-
-        // call the onInput callback if the document has changed
-        if (!update.docChanged || this.onInput === undefined) {
-          return;
-        }
-        this.onInput(update.state.doc.toString());
-      }),
-      hdsDarkTheme,
-      keymap.of([...defaultKeymap, ...historyKeymap]),
-      bracketMatching(),
-      syntaxHighlighting(hdsDarkHighlightStyle),
-      history(),
-    ];
-
-    if (languageExtension !== undefined) {
-      extensions = [languageExtension, ...extensions];
-    }
-
-    const state = EditorState.create({
-      doc: value,
-      extensions,
-    });
-
-    const editor = new EditorView({ state, parent: element });
-
-    this.editor = editor;
-    this.element = element;
-
-    onSetup?.(this.editor);
-  }
+  );
 }
