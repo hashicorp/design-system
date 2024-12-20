dfadc491ef04d40cefbc61bf39e423433688a8c7
Cristiano Rastelli <public@didoo.net>
2024-12-20 11:59:59 +0000

    SQUASHED COMMITS CRIS EXPLORATIONS Pt. 2
    added section with “themed” HDS components in theming showcase page
    
    HACK workardound to support nested theming
    
    refactored HDS theme switcher to be more similar to the one in use on developer website

--- packages/tokens/scripts/build-parts/generateExtraThemingFiles.ts
+++ packages/tokens/scripts/build-parts/generateExtraThemingFiles.ts
@@ -17,8 +17,8 @@ const prettierConfig = {
 
 export async function generateExtraThemingFiles(_dictionary: Dictionary, config: PlatformConfig ): Promise<void> {
 
-    const darkSource = await fs.readFile(`${config.buildPath}themed-tokens/with-root-selector/dark-tokens.css`, 'utf8');
     const lightSource = await fs.readFile(`${config.buildPath}themed-tokens/with-root-selector/light-tokens.css`, 'utf8');
+    const darkSource = await fs.readFile(`${config.buildPath}themed-tokens/with-root-selector/dark-tokens.css`, 'utf8');
     const commonSource = await fs.readFile(`${config.buildPath}themed-tokens/with-root-selector/common-tokens.css`, 'utf8');
 
     const header = await fileHeader({});
@@ -33,27 +33,33 @@ export async function generateExtraThemingFiles(_dictionary: Dictionary, config:
         let outputFilename;
         let outputContent = `${header}\n\n`;
 
-        outputContent += `${commonSource}\n\n`;
-
-        // this is the fallback to `light` mode (commented for now: consumers can always import the `themed-tokens/with-root-selector/light-tokens.css` as extra file if they want to)
-        outputContent += `${lightSource}\n\n`;
-
         if (method === 'prefers-color-scheme') {
             outputFilename = 'with-prefers-color-scheme';
-            outputContent += `@media (prefers-color-scheme: dark) { ${darkSource} }\n\n`;
+            outputContent += `${commonSource}\n\n`;
+            // this is the fallback to `light` mode (commented for now: consumers can always import the `themed-tokens/with-root-selector/light-tokens.css` as extra file if they want to)
+            outputContent += `${lightSource}\n\n`;
             outputContent += `@media (prefers-color-scheme: light) { ${lightSource} }\n\n`;
+            outputContent += `@media (prefers-color-scheme: dark) { ${darkSource} }\n\n`;
         }
         if (method === 'css-selectors') {
             outputFilename = 'with-css-selectors';
-            outputContent += `${darkSource.replace(/^:root/, '.hds-theme-dark, [data-hds-theme="dark"]')}\n\n`;
+            outputContent += `${commonSource}\n\n`;
+            // this is the fallback to `light` mode (commented for now: consumers can always import the `themed-tokens/with-root-selector/light-tokens.css` as extra file if they want to)
+            outputContent += `${lightSource}\n\n`;
             outputContent += `${lightSource.replace(/^:root/, '.hds-theme-light, [data-hds-theme="light"]')}\n\n`;
+            outputContent += `${darkSource.replace(/^:root/, '.hds-theme-dark, [data-hds-theme="dark"]')}\n\n`;
         }
         if (method === 'unified') {
             outputFilename = 'unified';
-            outputContent += `@media (prefers-color-scheme: dark) { ${darkSource.replace(/^:root/, ':root.hds-theme-auto, :root[data-hds-theme="auto"]')} }\n\n`;
+            // TODO this is a temporary workaround to have nested theming working
+            // in reality what needs to be done is to have only the variables that alias "themed" variables to be declared in this way (to do later)
+            outputContent += `${commonSource.replace(/^:root/, ':root, .hds-theme-light, [data-hds-theme="light"], .hds-theme-dark, [data-hds-theme="dark"]')}\n\n`;
+            // this is the fallback to `light` mode (commented for now: consumers can always import the `themed-tokens/with-root-selector/light-tokens.css` as extra file if they want to)
+            outputContent += `${lightSource}\n\n`;
             outputContent += `@media (prefers-color-scheme: light) { ${lightSource.replace(/^:root/, ':root.hds-theme-auto, :root[data-hds-theme="auto"]')} }\n\n`;
+            outputContent += `@media (prefers-color-scheme: dark) { ${darkSource.replace(/^:root/, ':root.hds-theme-auto, :root[data-hds-theme="auto"]')} }\n\n`;
+            outputContent += `${lightSource.replace(/^:root/, ':root, .hds-theme-light, [data-hds-theme="light"]')}\n\n`;
             outputContent += `${darkSource.replace(/^:root/, '.hds-theme-dark, [data-hds-theme="dark"]')}\n\n`;
-            outputContent += `${lightSource.replace(/^:root/, '.hds-theme-light, [data-hds-theme="light"]')}\n\n`;
         }
 
         const outputTokensCss = await prettier.format(outputContent, prettierConfig);;
