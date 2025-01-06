635fdcf935a1fdfad8e244d7c45af54da2c7b4dc
Zack Moore <zack.moore@hashicorp.com>
2025-01-06 17:54:56 -0500

    added support for aria-label and aria-labelledby
    added support for aria-label and aria-labelledby
    
    added tests and fixed the build

--- packages/components/src/components/hds/code-editor/title.ts
+++ packages/components/src/components/hds/code-editor/title.ts
@@ -7,17 +7,23 @@ import Component from '@glimmer/component';
 
 import type { HdsTextBodySignature } from '../text/body';
 
+type HdsCodeEditorTitleElement = HdsTextBodySignature['Element'];
+
 export interface HdsCodeEditorTitleSignature {
   Args: {
+    editorId: string;
     tag?: HdsTextBodySignature['Args']['tag'];
+    onInsert: (element: HdsCodeEditorTitleElement) => void;
   };
   Blocks: {
     default: [];
   };
-  Element: HdsTextBodySignature['Element'];
+  Element: HdsCodeEditorTitleElement;
 }
 
 export default class HdsCodeEditorTitle extends Component<HdsCodeEditorTitleSignature> {
+  private _id = `${this.args.editorId}-title`;
+
   get tag() {
     return this.args.tag ?? 'h2';
   }
