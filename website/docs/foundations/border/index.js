a0b5a3cc53199d7b9348371b017b7779bef1775f
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-01-08 11:14:03 -0800

    Design tokens - create border radius tokens (HDS-3948) (#2595)
    Co-authored-by: Jory Tindall <jory.tindall@hashicorp.com>

---
+++ website/docs/foundations/border/index.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+/*
+  --token-border-radius-x-small: 3px;
+  --token-border-radius-small: 5px;
+  --token-border-radius-medium: 6px;
+  --token-border-radius-large: 8px;
+*/
+
+const RADII = ['x-small', 'small', 'medium', 'large'];
+
+export default class Index extends Component {
+  get cssVariables() {
+    const cssVariables = { radii: [] };
+    RADII.forEach((radius) => {
+      cssVariables.radii.push(`--token-border-radius-${radius}`);
+    });
+    return cssVariables;
+  }
+}
