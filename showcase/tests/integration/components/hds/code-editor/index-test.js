d100dceec4dc4f03b14ca728f08d809eefd7644d
Zack Moore <zack.moore@hashicorp.com>
2025-01-09 12:43:34 -0500

    fixed ts errors
    linting error
    
    linting error

--- showcase/tests/integration/components/hds/code-editor/index-test.js
+++ showcase/tests/integration/components/hds/code-editor/index-test.js
@@ -5,7 +5,7 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { click, render, waitFor, setupOnerror } from '@ember/test-helpers';
+import { click, render, waitFor } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import sinon from 'sinon';
 
