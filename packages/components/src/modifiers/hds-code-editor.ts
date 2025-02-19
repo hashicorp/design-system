f372e7a70164822489447965031d2e850e813808
Zack Moore <zack.moore@hashicorp.com>
2025-02-19 10:07:24 -0500

    working on initial linting plugin

--- packages/components/src/modifiers/hds-code-editor.ts
+++ packages/components/src/modifiers/hds-code-editor.ts
@@ -21,6 +21,7 @@ import type {
   StreamLanguage as StreamLanguageType,
   StreamParser as StreamParserType,
 } from '@codemirror/language';
+import type { Diagnostic } from '@codemirror/lint';
 import type { Extension } from '@codemirror/state';
 import type {
   EditorView as EditorViewType,
@@ -40,6 +41,7 @@ export interface HdsCodeEditorSignature {
       ariaLabel?: string;
       ariaLabelledBy?: string;
       hasLineWrapping?: boolean;
+      isLintingEnabled?: boolean;
       language?: HdsCodeEditorLanguages;
       value?: string;
       onInput?: (newVal: string) => void;
@@ -273,7 +275,7 @@ export default class HdsCodeEditorModifier extends Modifier<HdsCodeEditorSignatu
 
   private _buildExtensionsTask = task(
     { drop: true },
-    async ({ language, hasLineWrapping }) => {
+    async ({ language, hasLineWrapping, isLintingEnabled }) => {
       const [
         {
           keymap,
@@ -314,6 +316,35 @@ export default class HdsCodeEditorModifier extends Modifier<HdsCodeEditorSignatu
         hasLineWrapping ? EditorView.lineWrapping : []
       );
 
+      let lintingExtensions: Extension[] = [];
+
+      if (isLintingEnabled && language === 'json') {
+        const [{ linter, lintGutter }, { syntaxTree }] = await Promise.all([
+          import('@codemirror/lint'),
+          import('@codemirror/language'),
+        ]);
+
+        lintingExtensions = [
+          linter((view): Diagnostic[] => {
+            const diagnostics: Diagnostic[] = [];
+            try {
+              JSON.parse(view.state.doc.toString());
+            } catch (error: any) {
+              const message = error.message;
+              const pos = error.position || 0;
+              diagnostics.push({
+                from: pos,
+                to: pos + 1,
+                severity: 'error',
+                message,
+              });
+            }
+            return diagnostics;
+          }),
+          lintGutter(),
+        ];
+      }
+
       let extensions = [
         lineWrappingExtension,
         bracketMatching(),
@@ -334,6 +365,10 @@ export default class HdsCodeEditorModifier extends Modifier<HdsCodeEditorSignatu
         extensions = [languageExtension, ...extensions];
       }
 
+      if (lintingExtensions.length !== 0) {
+        extensions = [...extensions, ...lintingExtensions];
+      }
+
       return extensions;
     }
   );
@@ -343,18 +378,20 @@ export default class HdsCodeEditorModifier extends Modifier<HdsCodeEditorSignatu
     async (
       element: HTMLElement,
       {
+        isLintingEnabled,
         language,
         value,
         hasLineWrapping,
       }: Pick<
         HdsCodeEditorSignature['Args']['Named'],
-        'language' | 'value' | 'hasLineWrapping'
+        'language' | 'value' | 'hasLineWrapping' | 'isLintingEnabled'
       >
     ) => {
       try {
         const { EditorState } = await import('@codemirror/state');
 
         const extensions = await this._buildExtensionsTask.perform({
+          isLintingEnabled,
           language,
           hasLineWrapping: hasLineWrapping ?? false,
         });
@@ -393,6 +430,7 @@ export default class HdsCodeEditorModifier extends Modifier<HdsCodeEditorSignatu
         ariaLabel,
         ariaLabelledBy,
         hasLineWrapping,
+        isLintingEnabled,
         language,
         value,
       } = named;
@@ -403,6 +441,7 @@ export default class HdsCodeEditorModifier extends Modifier<HdsCodeEditorSignatu
       this.element = element;
 
       const editor = await this._createEditorTask.perform(element, {
+        isLintingEnabled,
         language,
         value,
         hasLineWrapping,
@@ -422,6 +461,10 @@ export default class HdsCodeEditorModifier extends Modifier<HdsCodeEditorSignatu
         ariaDescribedBy,
         ariaLabel,
         ariaLabelledBy,
+      });
+
+      this.editor.dispatch({
+        changes: { from: 0, to: 0, insert: ' ' },
       });
 
       onSetup?.(this.editor);
