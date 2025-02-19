7c6999b9f8b463e709e60d14536f0734408b6c41
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-02-19 09:14:55 -0500

    HDS-4411 `Stepper::Navigation` and `Stepper::List` components added

---
+++ packages/components/src/components/hds/stepper/list/index.ts
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import type { ComponentLike } from '@glint/template';
+import type { HdsStepperListStepSignature } from './step';
+
+import { HdsStepperTitleTagValues } from '../types.ts';
+import type { HdsStepperTitleTags, HdsStepperListStepIds } from '../types.ts';
+
+export interface HdsStepperListSignature {
+  Args: {
+    currentStep?: number;
+    titleTag?: HdsStepperTitleTags;
+  };
+  Blocks: {
+    steps?: [
+      {
+        Step?: ComponentLike<HdsStepperListStepSignature>;
+      },
+    ];
+  };
+  Element: HTMLElement;
+}
+
+export default class HdsStepperList extends Component<HdsStepperListSignature> {
+  @tracked private _stepIds: HdsStepperListStepIds = [];
+  @tracked private _stepNodes: HTMLElement[] = [];
+
+  /**
+   * Get the DOM tag that should be used for the title
+   * @param titleTag
+   * @type {HdsStepperTitleTags}
+   * @default 'div'
+   */
+  get titleTag(): HdsStepperTitleTags {
+    return this.args.titleTag ?? HdsStepperTitleTagValues.Div;
+  }
+
+  /**
+   * @param currentStep
+   * @type {number}
+   * @default 0
+   */
+  get currentStep(): number {
+    const { currentStep } = this.args;
+
+    if (currentStep) {
+      if (currentStep < 0) {
+        return 0;
+      } else {
+        return currentStep;
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  @action
+  didInsertStep(element: HTMLElement): void {
+    this._stepIds = [...this._stepIds, element.id];
+    this._stepNodes = [...this._stepNodes, element];
+  }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    const classes = ['hds-stepper-list'];
+
+    return classes.join(' ');
+  }
+}
