c67f6dca0b9050d72e8de46430a740102f56b3ef
Alexey Kulakov <uni_que@me.com>
2025-02-26 21:27:27 -0800

    feat: wrap contextual components in `ensure-safe-component`

--- packages/components/unpublished-development-types/global.d.ts
+++ packages/components/unpublished-development-types/global.d.ts
@@ -9,6 +9,7 @@ import type EmberStargateRegistry from 'ember-stargate/template-registry';
 import type EmberStyleModifierRegistry from 'ember-style-modifier/template-registry';
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
 import type PowerSelectRegistry from 'ember-power-select/template-registry';
+import type { EmbroiderUtilRegistry } from '@embroider/util';
 // eslint-disable-next-line ember/no-at-ember-render-modifiers
 import type RenderModifiersRegistry from '@ember/render-modifiers/template-registry';
 
@@ -20,7 +21,8 @@ declare module '@glint/environment-ember-loose/registry' {
       EmberStyleModifierRegistry,
       EmberTruthRegistry,
       PowerSelectRegistry,
-      RenderModifiersRegistry /*, other addon registries */ {
+      RenderModifiersRegistry,
+      EmbroiderUtilRegistry /*, other addon registries */ {
     // local entries
     LinkToExternal: typeof LinkTo;
     // we have to use `ember-stargate` version `0.4.3` because version `0.5.0` causes a break in the tests for `ember-lts-3.28`
