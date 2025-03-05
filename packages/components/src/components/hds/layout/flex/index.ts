ce7a6db5d96b52a74670ee888d5851d788201b51
Cristiano Rastelli <public@didoo.net>
2025-03-05 15:01:39 +0000

    initial blueprint generation + cleanup after generation

---
+++ packages/components/src/components/hds/layout/flex/index.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+export interface HdsLayoutFlexSignature {
+  // The arguments accepted by the component
+  Args: {};
+  // Any blocks yielded by the component
+  Blocks: {
+    default: [];
+  };
+  // The element to which `...attributes` is applied in the component template
+  Element: HTMLDivElement;
+}
+
+export default class HdsLayoutFlex extends Component<HdsLayoutFlexSignature> {
+  // UNCOMMENT THIS IF YOU NEED A CONSTRUCTOR
+  // constructor() {
+  //   super(...arguments);
+  //   // ADD YOUR ASSERTIONS HERE
+  // }
+
+  /**
+   * Get the class names to apply to the component.
+   * @method classNames
+   * @return {string} The "class" attribute to apply to the component.
+   */
+  get classNames() {
+    let classes = ['hds-layout-flex'];
+
+    // add a class based on the @xxx argument
+    // classes.push(`hds-layout-flex--[variant]-${this.xxx}`);
+
+    return classes.join(' ');
+  }
+}
