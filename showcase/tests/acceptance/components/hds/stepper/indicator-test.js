111c5f0eecd9e439ddaba687a571c9a702749afb
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-03-06 11:55:25 -0500

    Feat: Separate stepper showcase to sub pages

---
+++ showcase/tests/acceptance/components/hds/stepper/indicator-test.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'showcase/tests/helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Acceptance | Component | hds/stepper/indicator', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('Components/hds/stepper/indicator page passes automated a11y checks', async function (assert) {
+    let axeOptions = {
+      rules: {
+        'landmark-unique': {
+          enabled: false,
+        },
+      },
+    };
+    await visit('/components/stepper/indicator');
+    await a11yAudit(axeOptions);
+
+    assert.ok(true, 'a11y automation audit passed');
+  });
+});
