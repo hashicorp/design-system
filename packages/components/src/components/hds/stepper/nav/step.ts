6e4928ca477da598bacee1bbf4e9c2159dc4dfce
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-03-06 12:49:18 -0500

    Fix: Address feedback

--- packages/components/src/components/hds/stepper/nav/step.ts
+++ packages/components/src/components/hds/stepper/nav/step.ts
@@ -46,11 +46,6 @@ export interface HdsStepperNavStepSignature {
 }
 
 export default class HdsStepperNavStep extends Component<HdsStepperNavStepSignature> {
-  /**
-   * Generate a unique ID for the Step
-   * @return {string}
-   * @param _stepId
-   */
   private _stepId = 'step-' + guidFor(this);
   private _elementId?: string;
 
@@ -71,62 +66,30 @@ export default class HdsStepperNavStep extends Component<HdsStepperNavStepSignat
     }
   );
 
-  /**
-   * Get the DOM tag that should be used for the title
-   * @param titleTag
-   * @type {HdsStepperTitleTags}
-   * @default 'div'
-   */
   get titleTag(): HdsStepperTitleTags {
     return this.args.titleTag ?? HdsStepperTitleTagValues.Div;
   }
 
-  /**
-   * Get the interactivity of the Nav parent
-   * @param isNavInteractive
-   * @type {boolean}
-   * @default false
-   */
   get isNavInteractive(): boolean {
     return this.args.isNavInteractive || false;
   }
 
-  /**
-   * Get the index of the step from the _stepIds list
-   * @param nodeIndex
-   * @type {number}
-   */
   get nodeIndex(): number | undefined {
     return this.args.stepIds?.indexOf(this._stepId);
   }
 
-  /**
-   * Get the step number that should be displayed
-   * @param stepNumber
-   * @type {number}
-   */
   get stepNumber(): number | undefined {
     return this.args.stepIds
       ? this.args.stepIds.indexOf(this._stepId) + 1
       : undefined;
   }
 
-  /**
-   * Get the ID of the panel coupled/associated with the step (it's used by the `aria-controls` attribute)
-   * @returns string}
-   */
   get coupledPanelId(): string | undefined {
     return this.nodeIndex !== undefined
       ? this.args.panelIds?.[this.nodeIndex]
       : undefined;
   }
 
-  /**
-   * Get the status that should be set on the Step::Indicator
-   * @param status
-   * @type {HdsStepperNavStatuses}
-   * @default 'incomplete'
-   */
   get status(): HdsStepperNavStatuses {
     if (this.nodeIndex != undefined && this.nodeIndex >= 0) {
       if (this.nodeIndex === this.args.currentStep) {
@@ -141,31 +104,14 @@ export default class HdsStepperNavStep extends Component<HdsStepperNavStepSignat
     }
   }
 
-  /**
-   * Get the status that should be set on the Step::Indicator
-   * @param stepIndicatorStatus
-   * @type {HdsStepperStatuses}
-   * @default 'incomplete'
-   */
   get stepIndicatorStatus(): HdsStepperStatuses {
     return MAPPING_STATUS_TO_INDICATOR_STATUS[this.status];
   }
 
-  /**
-   * Get the screen reader only text that should be added based on the step status
-   * @param statusSrOnlyText
-   * @type {string}
-   * @default ''
-   */
   get statusSrOnlyText(): string {
     return MAPPING_STATUS_TO_SR_ONLY_TEXT[this.status];
   }
 
-  /**
-   * Get if the step should be interactive
-   * @param isInteractive
-   * @type {boolean}
-   */
   get isInteractive(): boolean {
     return (
       this.isNavInteractive &&
@@ -220,11 +166,6 @@ export default class HdsStepperNavStep extends Component<HdsStepperNavStepSignat
     }
   }
 
-  /**
-   * Get the class names to apply to the component.
-   * @method classNames
-   * @return {string} The "class" attribute to apply to the component.
-   */
   get classNames(): string {
     const classes = ['hds-stepper-nav__step'];
 
