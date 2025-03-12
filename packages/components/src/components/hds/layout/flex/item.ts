5e2d2f5382992e6d4e5d96196cc6a2f16803a926
Cristiano Rastelli <public@didoo.net>
2025-03-12 18:20:11 +0000

    shared tag-related types across `Flex` and `Flex::Item`

--- packages/components/src/components/hds/layout/flex/item.ts
+++ packages/components/src/components/hds/layout/flex/item.ts
@@ -5,10 +5,7 @@
 
 import Component from '@glimmer/component';
 
-// A list of all existing tag names in the HTMLElementTagNameMap interface
-type AvailableTagNames = keyof HTMLElementTagNameMap;
-// A union of all types in the HTMLElementTagNameMap interface
-type AvailableElements = HTMLElementTagNameMap[keyof HTMLElementTagNameMap];
+import type { AvailableTagNames, AvailableElements } from './types.ts';
 
 export interface HdsLayoutFlexItemSignature {
   Args: {
--- packages/components/src/components/hds/layout/flex/types.ts
+++ packages/components/src/components/hds/layout/flex/types.ts
@@ -41,3 +41,9 @@ export enum HdsLayoutFlexGapValues {
 }
 
 export type HdsLayoutFlexGaps = `${HdsLayoutFlexGapValues}`;
+
+// A list of all existing tag names in the HTMLElementTagNameMap interface
+export type AvailableTagNames = keyof HTMLElementTagNameMap;
+// A union of all types in the HTMLElementTagNameMap interface
+export type AvailableElements = HTMLElementTagNameMap[keyof HTMLElementTagNameMap];
+
