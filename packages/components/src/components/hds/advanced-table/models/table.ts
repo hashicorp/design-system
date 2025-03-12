b745bbd1b7ab5c431cdf185a2a8a8994a0f7d6ae
Zack Moore <zack.moore@hashicorp.com>
2025-03-12 15:11:58 -0400

    adding new models

---
+++ packages/components/src/components/hds/advanced-table/models/table.ts
@@ -0,0 +1,63 @@
+import HdsAdvancedTableRow from './row.ts';
+import { action } from '@ember/object';
+
+import type { HdsAdvancedTableModel } from '../types';
+
+interface HdsAdvancedTableTableArgs {
+  model: HdsAdvancedTableModel;
+  childrenKey?: string;
+}
+
+function getVisibleRows(rows: HdsAdvancedTableRow[]): HdsAdvancedTableRow[] {
+  return rows.reduce((acc, row) => {
+    acc.push(row);
+
+    if (row.isOpen && row.children) {
+      acc.push(...getVisibleRows(row['children'] as HdsAdvancedTableRow[]));
+    }
+
+    return acc;
+  }, [] as HdsAdvancedTableRow[]);
+}
+
+export default class HdsAdvancedTableTable {
+  rows: HdsAdvancedTableRow[] = [];
+
+  get flattenedVisibleRows(): HdsAdvancedTableRow[] {
+    return getVisibleRows(this.rows);
+  }
+
+  get lastVisibleRow(): HdsAdvancedTableRow | undefined {
+    return this.flattenedVisibleRows[this.flattenedVisibleRows.length - 1];
+  }
+
+  get openState(): boolean | null {
+    if (this.rows.length === 0) {
+      return false;
+    }
+
+    return this.rows.every((row) => row.openState) ? true : null;
+  }
+
+  get hasRowsWithChildren(): boolean {
+    return this.rows.some((row) => row.hasChildren);
+  }
+
+  constructor(args: HdsAdvancedTableTableArgs) {
+    const { model, childrenKey } = args;
+
+    this.rows = model.map((row) => {
+      return new HdsAdvancedTableRow({ ...row, childrenKey });
+    });
+  }
+
+  @action
+  openAll() {
+    this.rows.forEach((row) => row.openAll());
+  }
+
+  @action
+  collapseAll() {
+    this.rows.forEach((row) => row.collapseAll());
+  }
+}
