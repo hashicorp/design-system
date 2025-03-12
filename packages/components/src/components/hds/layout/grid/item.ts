5b475d85fe512436bea1d8f7e0d3fc6ee27434b3
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-03-11 23:10:34 -0700

    HDS-4532 Add colSpan and rowSpan features to GridItem, add GridItem Showcase examples

--- packages/components/src/components/hds/layout/grid/item.ts
+++ packages/components/src/components/hds/layout/grid/item.ts
@@ -13,6 +13,8 @@ type AvailableElements = HTMLElementTagNameMap[keyof HTMLElementTagNameMap];
 export interface HdsLayoutGridItemSignature {
   Args: {
     tag?: AvailableTagNames;
+    colSpan?: string;
+    rowSpan?: string;
   };
   Blocks: {
     default: [];
@@ -23,6 +25,20 @@ export interface HdsLayoutGridItemSignature {
 export default class HdsLayoutFlexItem extends Component<HdsLayoutGridItemSignature> {
   get componentTag(): AvailableTagNames {
     return this.args.tag ?? 'div';
+  }
+
+  get colSpanStyle(): Record<string, string> {
+    const colSpanStyle: { [key: string]: string } = {};
+
+    colSpanStyle['--hds-layout-grid-column-span'] = this.args.colSpan ?? '1';
+    return colSpanStyle;
+  }
+
+  get rowSpanStyle(): Record<string, string> {
+    const rowSpanStyle: { [key: string]: string } = {};
+
+    rowSpanStyle['--hds-layout-grid-row-span'] = this.args.rowSpan ?? '1';
+    return rowSpanStyle;
   }
 
   get classNames(): string {
