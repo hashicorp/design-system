2931446faa4f6a6531673e288be735a9dcfe63dd
Zack Moore <zack.moore@hashicorp.com>
2025-03-12 22:24:13 -0400

    working on using model

---
+++ packages/components/src/components/hds/advanced-table/models/table.ts
@@ -0,0 +1,57 @@
+import HdsAdvancedTableRow from './row.ts';
+import { action } from '@ember/object';
+
+import type {
+  HdsAdvancedTableModel,
+} from '../types';
+
+interface HdsAdvancedTableTableArgs {
+  model: HdsAdvancedTableModel;
+  childrenKey?: string;
+}
+
+function getVisibleRows(rows: HdsAdvancedTableRow[]): HdsAdvancedTableRow[] {
+  return rows.reduce((acc, row) => {
+    acc.push(row);
+
+    if (row.isOpen && row.children) {
+      acc.push(...getVisibleRows(row.children));
+    }
+
+    return acc;
+  }, [] as HdsAdvancedTableRow[]);
+}
+
+export default class HdsAdvancedTableTableModel {
+  rows: HdsAdvancedTableRow[] = [];
+
+  get flattenedVisibleRows(): HdsAdvancedTableRow[] {
+    return getVisibleRows(this.rows);
+  }
+
+  get lastVisibleRow(): HdsAdvancedTableRow | undefined {
+    return this.flattenedVisibleRows[this.flattenedVisibleRows.length - 1];
+  }
+
+  get hasRowsWithChildren(): boolean {
+    return this.rows.some((row) => row.hasChildren);
+  }
+
+  constructor(args: HdsAdvancedTableTableArgs) {
+    const { model, childrenKey } = args;
+
+    this.rows = model.map((row) => {
+      return new HdsAdvancedTableRow({ ...row, childrenKey });
+    });
+  }
+
+  @action
+  openAll() {
+    this.rows.forEach((row) => row.openAll());
+  }
+
+  @action
+  collapseAll() {
+    this.rows.forEach((row) => row.collapseAll());
+  }
+}
