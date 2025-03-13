35f105e6cd62ffe8218cc1df691cf88f6feb16ba
Zack Moore <zack.moore@hashicorp.com>
2025-03-13 11:01:34 -0400

    wip

--- packages/components/src/components/hds/advanced-table/models/row.ts
+++ packages/components/src/components/hds/advanced-table/models/row.ts
@@ -27,6 +27,12 @@ export default class HdsAdvancedTableRow {
     return this.isOpen && this.hasChildren;
   }
 
+  get allDescendantsAreOpen(): boolean {
+    return this.children.every(
+      (child) => child.isOpen && child.allDescendantsAreOpen
+    );
+  }
+
   constructor(args: HdsAdvancedTableRowArgs) {
     // set row data
     Object.assign(this, args);
