c9da13ed52d72b29b0e63ad956568368ca9610d7
Alexey Kulakov <uni_que@me.com>
2025-03-14 02:24:16 -0700

    fix(showcase): comment out AppHeader parts
    refer to this PR to turn things back on

--- showcase/types/template-registry.ts
+++ showcase/types/template-registry.ts
@@ -21,8 +21,8 @@ import ShwTextH2 from '../app/components/shw/text/h2';
 import ShwTextH3 from '../app/components/shw/text/h3';
 import ShwTextH4 from '../app/components/shw/text/h4';
 
-import MockApp from '../app/components/mock/app/index';
-import MockAppHeaderAppHeader from '../app/components/mock/app/header/app-header';
+// import MockApp from '../app/components/mock/app/index';
+// import MockAppHeaderAppHeader from '../app/components/mock/app/header/app-header';
 import MockAppSidebarSideNav from '../app/components/mock/app/sidebar/side-nav';
 import MockAppMainPageHeader from '../app/components/mock/app/main/page-header';
 import MockAppMainGenericTextContent from '../app/components/mock/app/main/generic-text-content';
@@ -65,10 +65,10 @@ export default interface ShowcaseTemplateRegistry {
   'Shw::Text::Body': typeof ShwTextBody;
   'shw/text/body': typeof ShwTextBody;
   // MOCK APP
-  'Mock::App': typeof MockApp;
-  'mock/app': typeof MockApp;
-  'Mock::App::Header::AppHeader': typeof MockAppHeaderAppHeader;
-  'mock/app/header/app-header': typeof MockAppHeaderAppHeader;
+  // 'Mock::App': typeof MockApp;
+  // 'mock/app': typeof MockApp;
+  // 'Mock::App::Header::AppHeader': typeof MockAppHeaderAppHeader;
+  // 'mock/app/header/app-header': typeof MockAppHeaderAppHeader;
   'Mock::App::Sidebar::AppSideNav': typeof MockAppSidebarSideNav;
   'mock/app/sidebar/app-side-nav': typeof MockAppSidebarSideNav;
   'Mock::App::Main::PageHeader': typeof MockAppMainPageHeader;
