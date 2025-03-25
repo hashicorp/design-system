6e5985c3ca04860b36386cf24ad8fa9c429aa19a
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-03-25 16:33:55 -0400

    Feat: Add `RichTooltip` acceptance test

---
+++ showcase/tests/acceptance/components/hds/rich-tooltip-test.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'showcase/tests/helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Acceptance | Component | hds/rich-tooltip', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('Components/hds/rich-tooltip page passes automated a11y checks', async function (assert) {
+    await visit('/components/rich-tooltip');
+    await a11yAudit();
+
+    assert.ok(true, 'a11y automation audit passed');
+  });
+});
