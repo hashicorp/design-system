9ddee5ef19739c475e3f29c079644d3871719507
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-03-26 09:58:48 -0400

    Fix: Rename acceptance test files

--- showcase/tests/acceptance/components/hds/form/base-elements-test.js
+++ showcase/tests/acceptance/components/hds/form/base-elements-test.js
@@ -8,10 +8,10 @@ import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'showcase/tests/helpers';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 
-module('Acceptance | Component | hds/form/primitives', function (hooks) {
+module('Acceptance | Component | hds/form/base-elements', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('components/form/primitives page passes automated a11y checks', async function (assert) {
+  test('components/form/base-elements page passes automated a11y checks', async function (assert) {
     await visit('/components/form/base-elements');
     await a11yAudit();
     assert.ok(true, 'a11y automation audit passed');
