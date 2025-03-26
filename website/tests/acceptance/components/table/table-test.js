e6e5ca904db00d5fac04cf1658e91853cc58bbea
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-03-26 11:06:34 -0400

    Fix: Rename website acceptance test files

--- website/tests/acceptance/components/table/table-test.js
+++ website/tests/acceptance/components/table/table-test.js
@@ -8,7 +8,7 @@ import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'website/tests/helpers';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 
-module('Acceptance | components/table', function (hooks) {
+module('Acceptance | components/table/table', function (hooks) {
   setupApplicationTest(hooks);
 
   test('visiting /components/table/table', async function (assert) {
