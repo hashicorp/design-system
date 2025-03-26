9754fd8e16832851dc11cee3a66a88a4d14614db
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-03-26 11:10:05 -0400

    Feat: Add `CodeEditor` website acceptance tests

---
+++ website/tests/acceptance/components/code-editor-test.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'website/tests/helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Acceptance | components/code-editor', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /components/code-editor', async function (assert) {
+    await visit('/components/code-editor');
+
+    assert.strictEqual(currentURL(), '/components/code-editor');
+  });
+
+  test('components/code-editor page passes automated a11y checks', async function (assert) {
+    await visit('/components/code-editor');
+
+    await a11yAudit();
+
+    assert.ok(true, 'a11y automation audit passed');
+  });
+});
