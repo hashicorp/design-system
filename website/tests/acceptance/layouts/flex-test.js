5ee238b871414045421efe1cbd5474127fa6cd49
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-03-26 11:12:25 -0700

    Add basic acceptance tests

---
+++ website/tests/acceptance/layouts/flex-test.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'website/tests/helpers';
+
+module('Acceptance | layouts/flex', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /layouts/flex', async function (assert) {
+    await visit('/layouts/flex');
+
+    assert.strictEqual(currentURL(), '/layouts/flex');
+  });
+});
