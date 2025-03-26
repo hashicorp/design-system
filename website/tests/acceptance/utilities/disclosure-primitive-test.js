083ea1d9cb870ec2136a9a175b500e2a0a8171f9
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-03-26 15:42:24 -0400

    Feat: Add `DisclosurePrimitive` website acceptance test

---
+++ website/tests/acceptance/utilities/disclosure-primitive-test.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'website/tests/helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Acceptance | utilities/disclosure-primitive', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /utilities/disclosure-primitive', async function (assert) {
+    await visit('/utilities/disclosure-primitive');
+
+    assert.strictEqual(currentURL(), '/utilities/disclosure-primitive');
+  });
+
+  test('utilities/disclosure-primitive page passes automated a11y checks', async function (assert) {
+    await visit('/utilities/disclosure-primitive');
+    await a11yAudit();
+    assert.ok(true, 'a11y automation audit passed');
+  });
+});
