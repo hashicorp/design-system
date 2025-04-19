6c8f37d0072dc96054f06a31e6a3b7f2f2dcd612
Alexey Kulakov <uni_que@me.com>
2025-04-19 01:46:56 -0700

    fix(components): make `ember-engines` optional

--- packages/components/unpublished-development-types/global.d.ts
+++ packages/components/unpublished-development-types/global.d.ts
@@ -3,14 +3,14 @@ import '@glint/environment-ember-template-imports';
 
 import { Portal, PortalTarget } from 'ember-stargate';
 
-import type HdsComponentsRegistry from '../src/template-registry';
+import type { EmbroiderUtilRegistry } from '@embroider/util';
 import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
 import type EmberMathHelpersRegistry from 'ember-math-helpers/template-registry';
+import type PowerSelectRegistry from 'ember-power-select/template-registry';
 import type EmberStargateRegistry from 'ember-stargate/template-registry';
 import type EmberStyleModifierRegistry from 'ember-style-modifier/template-registry';
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
-import type PowerSelectRegistry from 'ember-power-select/template-registry';
-import type { EmbroiderUtilRegistry } from '@embroider/util';
+import type HdsComponentsRegistry from '../src/template-registry';
 // eslint-disable-next-line ember/no-at-ember-render-modifiers
 import type RenderModifiersRegistry from '@ember/render-modifiers/template-registry';
 import type EmberComposableHelpersRegistry from '@nullvoxpopuli/ember-composable-helpers/template-registry';
