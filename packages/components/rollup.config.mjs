562e87e6dde2525f58362200d407397598b35f7b
shleewhite <lee.white@hahicorp.com>
2025-04-22 12:05:45 -0400

    chore: lint

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -17,27 +17,23 @@ const addon = new Addon({
 const plugins = [
   // These are the modules that users should be able to import from your
   // addon. Anything not listed here may get optimized away.
-  addon.publicEntrypoints(
-    [
-      '**/*.{js,ts}',
-      'index.js',
-      'template-registry.js',
-      'styles/@hashicorp/design-system-components.scss',
-    ],
-  ),
+  addon.publicEntrypoints([
+    '**/*.{js,ts}',
+    'index.js',
+    'template-registry.js',
+    'styles/@hashicorp/design-system-components.scss',
+  ]),
 
   // These are the modules that should get reexported into the traditional
   // "app" tree. Things in here should also be in publicEntrypoints above, but
   // not everything in publicEntrypoints necessarily needs to go here.
-  addon.appReexports(
-    [
-      'components/**/!(*types).js',
-      'helpers/**/*.js',
-      'modifiers/**/*.js',
-      'services/**/!(*types).js',
-      'instance-initializers/**/*.js',
-    ],
-  ),
+  addon.appReexports([
+    'components/**/!(*types).js',
+    'helpers/**/*.js',
+    'modifiers/**/*.js',
+    'services/**/!(*types).js',
+    'instance-initializers/**/*.js',
+  ]),
 
   // Follow the V2 Addon rules about dependencies. Your code can import from
   // `dependencies` and `peerDependencies` as well as standard Ember-provided
