dafb725ac81cad4c500fc435f011d90da7c08f32
Zack Moore <zack.moore@hashicorp.com>
2025-05-01 21:33:10 -0400

    resize handle extends to full height of table

--- packages/components/src/components/hds/advanced-table/models/table.ts
+++ packages/components/src/components/hds/advanced-table/models/table.ts
@@ -5,6 +5,7 @@
 
 import HdsAdvancedTableRow from './row.ts';
 import { action } from '@ember/object';
+import { modifier } from 'ember-modifier';
 
 import HdsAdvancedTableColumn from './column.ts';
 
