691f914eee60b75e38946bc6073b81a8cff73ef1
Zack Moore <zack.moore@hashicorp.com>
2025-05-09 13:07:12 -0400

    column resize in progress

--- packages/components/src/components/hds/advanced-table/models/column.ts
+++ packages/components/src/components/hds/advanced-table/models/column.ts
@@ -1,13 +1,28 @@
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { assert } from '@ember/debug';
+import { CssSizeUnitValues } from '../types.ts';
 
 import type { HdsAdvancedTableColumn as HdsAdvancedTableColumnType } from '../types';
 import type { HdsAdvancedTableHorizontalAlignment } from '../types';
-import { assert } from '@ember/debug';
+import type { CssSize, CssSizeUnit } from '../types';
+import type HdsAdvancedTableTableModel from './table.ts';
 
-function getNumericalWidth(width: string | undefined): number {
+function getCssUnit(cssString?: string): CssSizeUnit | undefined {
+  if (cssString === undefined) {
+    return undefined;
+  }
+
+  const match = cssString.match(/([a-zA-Z%]+)/);
+
+  if (match) {
+    return match[0] as CssSizeUnit;
+  }
+}
+
+function getNumericalWidth(width?: CssSize, defaultWidth?: number): number {
   if (width === undefined) {
-    return 0;
+    return defaultWidth ?? 0;
   }
 
   // width is a css string
@@ -24,13 +39,14 @@ export default class HdsAdvancedTableColumn {
   @tracked isSortable?: boolean = false;
   @tracked isVisuallyHidden?: boolean = false;
   @tracked tooltip?: string = undefined;
-  @tracked width?: string = undefined; // css width like `100px`
-  @tracked minWidth?: string = undefined; // css width like `100px`
-  @tracked maxWidth?: string = undefined; // css width like `100px`
+  @tracked width?: CssSize = undefined;
+  @tracked minWidth?: CssSize = undefined;
+  @tracked maxWidth?: CssSize = undefined;
 
   @tracked sortingFunction?: (a: unknown, b: unknown) => number = undefined;
 
-  private _originalWidth?: string = undefined;
+  private _originalWidth?: CssSize = undefined;
+  private _cssWidthUnit?: CssSizeUnit = CssSizeUnitValues.Px;
 
   get isResizable(): boolean | undefined {
     if (!this._isResizable) {
@@ -42,11 +58,32 @@ export default class HdsAdvancedTableColumn {
     return this._isResizable;
   }
 
+  get pixelWidth(): number | undefined {
+    const numericalWidth = getNumericalWidth(this.width);
+
+    if (this._cssWidthUnit === CssSizeUnitValues.Px) {
+      return numericalWidth;
+    }
+
+    if (this._cssWidthUnit === CssSizeUnitValues.Percent) {
+      return 0;
+    }
+  }
+
   get numericalWidth(): number {
     return getNumericalWidth(this.width);
   }
   set numericalWidth(value: number) {
-    this.width = `${value}px`;
+    if (this._cssWidthUnit === CssSizeUnitValues.Px) {
+      this.width = `${value}${CssSizeUnitValues.Px}`;
+      console.log({ width: this.width });
+      return;
+    }
+    if (this._cssWidthUnit === CssSizeUnitValues.Percent) {
+      // TODO: actually calculate the width based on the table width
+      this.width = `${value}${CssSizeUnitValues.Percent}`;
+      return;
+    }
   }
 
   get numericalMinWidth(): number {
@@ -57,7 +94,9 @@ export default class HdsAdvancedTableColumn {
     return getNumericalWidth(this.maxWidth);
   }
 
-  constructor(args: HdsAdvancedTableColumnType) {
+  constructor(
+    args: HdsAdvancedTableColumnType & { table: HdsAdvancedTableTableModel }
+  ) {
     this.label = args.label;
     this.align = args.align;
     this.key = args.key;
@@ -66,18 +105,42 @@ export default class HdsAdvancedTableColumn {
     this.isVisuallyHidden = args.isVisuallyHidden;
     this.tooltip = args.tooltip;
 
-    this.width = args.width;
-    this._originalWidth = args.width;
-    this.minWidth = args.minWidth ?? `100px`; // default min width
-    this.maxWidth = args.maxWidth ?? `500px`; // default max width
-
     this.sortingFunction = args.sortingFunction;
+
+    this.setWidthValues(args);
+  }
+
+  setWidthValues({
+    width,
+    minWidth,
+    maxWidth,
+  }: HdsAdvancedTableColumnType): void {
+    this.width = width;
+    // set default minWidth and maxWidth if width is set
+    this.minWidth = (minWidth ?? this.width !== undefined) ? '50px' : undefined;
+    this.maxWidth =
+      (maxWidth ?? this.width !== undefined) ? '800px' : undefined;
+
+    // capture the width at the time of instantiation so it can be restored
+    this._originalWidth = width;
+
+    this._cssWidthUnit = getCssUnit(this.width);
+  }
+
+  @action
+  setNumericalWidth(newNumericalWidth: number): void {
+    this.numericalWidth = Math.min(
+      Math.max(newNumericalWidth, this.numericalMinWidth),
+      this.numericalMaxWidth
+    );
   }
 
   @action
   restoreWidth(): void {
-    if (this._originalWidth !== undefined) {
-      this.width = this._originalWidth;
+    if (this._originalWidth === undefined) {
+      return;
     }
+
+    this.width = this._originalWidth;
   }
 }
