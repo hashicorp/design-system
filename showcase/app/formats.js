97a36c8b4716e4927a1a9e2c452fc5195cd0eb23
Zack Moore <zack.moore@hashicorp.com>
2025-05-14 17:34:01 -0400

    updated lockfile

---
+++ showcase/app/formats.js
@@ -0,0 +1,29 @@
+export default {
+  time: {
+    hhmmss: {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    },
+  },
+  date: {
+    hhmmss: {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    },
+  },
+  number: {
+    compact: {
+      notation: 'compact',
+    },
+    EUR: {
+      style: 'currency',
+      currency: 'EUR',
+    },
+    USD: {
+      style: 'currency',
+      currency: 'USD',
+    },
+  },
+};
