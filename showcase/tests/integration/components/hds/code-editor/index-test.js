5492f4f9ce883071c6b51b560af47126fd357fcb
Zack Moore <zack.moore@hashicorp.com>
2025-05-16 15:44:56 -0400

    setting up rendering tests correctly

--- showcase/tests/integration/components/hds/code-editor/index-test.js
+++ showcase/tests/integration/components/hds/code-editor/index-test.js
@@ -4,7 +4,7 @@
  */
 
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
+import { setupRenderingTest } from 'showcase/tests/helpers';
 import { click, render, waitFor } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import sinon from 'sinon';
--- showcase/tests/integration/components/hds/tag/index-test.js
+++ showcase/tests/integration/components/hds/tag/index-test.js
@@ -4,7 +4,7 @@
  */
 
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
+import { setupRenderingTest } from 'showcase/tests/helpers';
 import {
   render,
   resetOnerror,
