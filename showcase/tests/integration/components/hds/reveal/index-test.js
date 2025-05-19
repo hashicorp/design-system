8f0978e09ecc2553598a6431a900022778cb2231
Zack Moore <zack.moore@hashicorp.com>
2025-05-19 12:06:33 -0400

    using test helpers import path

--- showcase/tests/integration/components/hds/reveal/index-test.js
+++ showcase/tests/integration/components/hds/reveal/index-test.js
@@ -4,7 +4,7 @@
  */
 
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
+import { setupRenderingTest } from 'showcase/tests/helpers';
 import { click, render, setupOnerror } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
