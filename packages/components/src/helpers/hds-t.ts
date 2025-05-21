210b73adf8c922d6b926a3d5e55ffb819ca79861
Zack Moore <zack.moore@hashicorp.com>
2025-05-21 12:10:39 -0400

    working on making things work in projects without ember-intl

--- packages/components/src/helpers/hds-t.ts
+++ packages/components/src/helpers/hds-t.ts
@@ -4,27 +4,38 @@
  */
 
 import Helper from '@ember/component/helper';
-import { inject as service } from '@ember/service';
+import { getOwner } from '@ember/owner';
 import { assert } from '@ember/debug';
 import { isPresent } from '@ember/utils';
 
 import type { IntlService } from 'ember-intl';
+import type { FormatMessageParameters } from 'ember-intl/-private/formatjs';
 
 interface HdsSafeTHelperNamedArgs {
   default: string;
-  [key: string]: unknown;
 }
 
 interface HdsTHelperSignature {
   Args: {
     Positional: string[];
-    Named: HdsSafeTHelperNamedArgs;
+    Named: HdsSafeTHelperNamedArgs & FormatMessageParameters[1];
   };
   Return: string;
 }
 
 export default class HdsTHelper extends Helper<HdsTHelperSignature> {
-  @service intl?: IntlService;
+  get intl(): IntlService | undefined {
+    const owner = getOwner(this);
+
+    if (
+      typeof owner?.factoryFor === 'function' &&
+      owner.factoryFor('service:intl')
+    ) {
+      return owner.lookup('service:intl');
+    }
+
+    return undefined;
+  }
 
   compute(
     positional: HdsTHelperSignature['Args']['Positional'],
@@ -40,14 +51,12 @@ export default class HdsTHelper extends Helper<HdsTHelperSignature> {
 
     // try to use ember-intl if available and a translation key exists
     if (this.intl !== undefined) {
-      const localeIsSet = isPresent(this.intl.locale);
+      const localeIsSet = isPresent(this.intl.locales);
 
       if (localeIsSet && this.intl.exists(key)) {
         return this.intl.t(key, options);
       }
     }
-
-    console.log('oh no');
 
     return defaultString;
   }
