e548adfb89e5eef957e9a3089d7f7846a8474d4a
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-05-22 14:57:26 -0700

    HDS-4885 Add Form component, Showcase, and tests

--- showcase/tests/acceptance/percy-test.js
+++ showcase/tests/acceptance/percy-test.js
@@ -91,6 +91,9 @@ module('Acceptance | Percy test', function (hooks) {
     await visit('/components/flyout');
     await percySnapshot('Flyout');
 
+    await visit('/components/form');
+    await percySnapshot('Form');
+
     await visit('/components/form/base-elements');
     await click('button#dummy-toggle-highlight');
     await percySnapshot('Form - Base elements');
