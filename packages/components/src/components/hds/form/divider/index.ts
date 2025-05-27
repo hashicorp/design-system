c8102224b42a4e4e8c9f2d991db881eea2d17495
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-05-27 16:54:18 -0700

    HDS-4885  Add FormDivider component, Showcase examples, and tests

--- packages/components/src/components/hds/form/divider/index.ts
+++ packages/components/src/components/hds/form/divider/index.ts
@@ -5,27 +5,25 @@
 
 import Component from '@glimmer/component';
 
-export interface HdsFormSectionSignature {
+export interface HdsFormDividerSignature {
   Args: { hasMaxWidth?: boolean };
-  // Any blocks yielded by the component
   Blocks: {
     default: [];
   };
   Element: HTMLDivElement;
 }
-// More info on types and signatures: https://hashicorp.atlassian.net/wiki/spaces/HDS/pages/3245932580/Using+Typescript
 
-export default class HdsFormSection extends Component<HdsFormSectionSignature> {
+export default class HdsFormDivider extends Component<HdsFormDividerSignature> {
   get hasMaxWidth(): boolean {
     const { hasMaxWidth = true } = this.args;
     return hasMaxWidth;
   }
 
   get classNames(): string {
-    const classes = ['hds-form__section'];
+    const classes = ['hds-form__divider'];
 
     if (!this.hasMaxWidth) {
-      classes.push('hds-form__section--has-max-width-false');
+      classes.push('hds-form__divider--has-max-width-false');
     }
 
     return classes.join(' ');
