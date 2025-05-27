a3a7a4a13650c1d6c0f75ccfd00d307805fd47f1
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-05-27 15:54:23 -0700

    HDS-4885 Add FormSection component, Showcase examples, and integration tests

---
+++ showcase/tests/integration/components/hds/form/section/index-test.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'showcase/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | hds/form/section/index', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+    await render(hbs`<Hds::Form::Section id="test-form-section" />`);
+    assert.dom('#test-form-section').hasClass('hds-form__section');
+  });
+
+  // Options
+
+  // hasMaxWidth
+  test(`it should have the default max-width if no @hasMaxWidth prop is declared`, async function (assert) {
+    await render(hbs`<Hds::Form::Section id="test-form-section" />`);
+    assert
+      .dom('#test-form-section')
+      .doesNotHaveClass('hds-form-section--has-max-width-false');
+  });
+
+  test(`if @hasMaWidth is false, it should have the class "hds-form-section--has-max-width-false"`, async function (assert) {
+    await render(
+      hbs`<Hds::Form::Section id="test-form-section" @hasMaxWidth={{false}} />`,
+    );
+    assert
+      .dom('#test-form-section')
+      .hasClass('hds-form-section--has-max-width-false');
+  });
+});
