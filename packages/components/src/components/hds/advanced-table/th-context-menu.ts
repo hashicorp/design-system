3bb58fc2b98f4349152534ccbed61d85074a3b4c
Zack Moore <zack.moore@hashicorp.com>
2025-05-27 10:48:52 -0400

    context menu is added

---
+++ packages/components/src/components/hds/advanced-table/th-context-menu.ts
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+
+import type HdsAdvancedTableColumn from './models/column.ts';
+import type { HdsDropdownToggleIconSignature } from '../dropdown/toggle/icon.ts';
+
+interface HdsAdvancedTableThContextMenuOption {
+  label: string;
+  icon: HdsDropdownToggleIconSignature['Args']['icon'];
+  action: (dropdownCloseCallback: () => void) => void;
+}
+
+export interface HdsAdvancedTableThContextMenuSignature {
+  Args: {
+    column: HdsAdvancedTableColumn;
+  };
+  Element: HTMLDivElement;
+}
+
+export default class HdsAdvancedTableThContextMenu extends Component<HdsAdvancedTableThContextMenuSignature> {
+  get classNames() {
+    const classes = ['hds-advanced-table__th-context-menu'];
+
+    return classes.join(' ');
+  }
+
+  get _options(): HdsAdvancedTableThContextMenuOption[] {
+    const { column } = this.args;
+
+    const options: HdsAdvancedTableThContextMenuOption[] = [];
+
+    if (column.isResizable) {
+      options.push({
+        label: 'Reset column width',
+        icon: 'reload',
+        action: (dropdownCloseCallback: () => void) => {
+          dropdownCloseCallback();
+        },
+      });
+    }
+
+    return options;
+  }
+}
