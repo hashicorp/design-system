55127355d8b32741cbcef5f2e233ed1c8decb061
Lee White <lee.white@hashicorp.com>
2025-05-28 14:27:19 -0400

    `AppSideNav`, `SideNav`: fix typing issue (#2908)
    Co-authored-by: shleewhite <lee.white@hahicorp.com>

--- packages/components/src/components/hds/app-side-nav/portal/index.ts
+++ packages/components/src/components/hds/app-side-nav/portal/index.ts
@@ -7,21 +7,8 @@ import TemplateOnlyComponent from '@ember/component/template-only';
 
 import type { HdsAppSideNavListSignature } from '../list/index';
 
-// TODO! understand how this should be done "correctly"
-// import type { PortalSignature } from 'ember-stargate/components/portal';
-interface PortalSignature {
-  Args: {
-    target: string;
-    renderInPlace?: boolean;
-    fallback?: 'inplace';
-  };
-  Blocks: {
-    default: [];
-  };
-}
-
 export interface HdsAppSideNavPortalSignature {
-  Args: PortalSignature['Args'] & {
+  Args: {
     ariaLabel?: string;
     targetName?: string;
   };
--- packages/components/src/components/hds/side-nav/portal/index.ts
+++ packages/components/src/components/hds/side-nav/portal/index.ts
@@ -7,21 +7,8 @@ import TemplateOnlyComponent from '@ember/component/template-only';
 
 import type { HdsSideNavListSignature } from '../list/index';
 
-// TODO! understand how this should be done "correctly"
-// import type { PortalSignature } from 'ember-stargate/components/portal';
-export interface PortalSignature {
-  Args: {
-    target: string;
-    renderInPlace?: boolean;
-    fallback?: 'inplace';
-  };
-  Blocks: {
-    default: [];
-  };
-}
-
 export interface HdsSideNavPortalSignature {
-  Args: PortalSignature['Args'] & {
+  Args: {
     ariaLabel?: string;
     targetName?: string;
   };
