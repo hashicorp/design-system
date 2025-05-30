d9f331c9ca53333faa3f4be693bfdee1a4704a7f
shleewhite <lee.white@hahicorp.com>
2025-05-30 09:07:49 -0400

    chore: lint

--- showcase/app/controllers/components/key-value-pair.js
+++ showcase/app/controllers/components/key-value-pair.js
@@ -3,27 +3,26 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 const DEFAULT_DATA = [
-    {
-      key: 'prod',
-      value: 'This is a production tag',
-      error: 'This is an error message',
-    },
-    {
-      key: 'enterprise',
-      value: '',
-    },
-    {
-      key: 'beta',
-      value: 'Feature includes beta',
-      error: 'This is an error message',
-    }
-  ];
+  {
+    key: 'prod',
+    value: 'This is a production tag',
+    error: 'This is an error message',
+  },
+  {
+    key: 'enterprise',
+    value: '',
+  },
+  {
+    key: 'beta',
+    value: 'Feature includes beta',
+    error: 'This is an error message',
+  },
+];
 
 export default class KeyValuePairController extends Controller {
   @tracked sampleData = DEFAULT_DATA;
