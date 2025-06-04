3488b26486a17f753770af1c97ceda3d916417ca
shleewhite <lee.white@hahicorp.com>
2025-06-04 14:25:59 -0400

    finish columns work

--- packages/components/src/components/hds/form/key-value-pair/add-row-button.ts
+++ packages/components/src/components/hds/form/key-value-pair/add-row-button.ts
@@ -3,12 +3,14 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 import type { HdsButtonSignature } from '../../button/index.ts';
 
 export interface HdsFormKeyValuePairAddRowButtonSignature {
   Args: {
     text?: HdsButtonSignature['Args']['text'];
+    onClick: () => void;
   };
   Element: HdsButtonSignature['Element'];
 }
@@ -16,5 +18,14 @@ export interface HdsFormKeyValuePairAddRowButtonSignature {
 export default class HdsFormKeyValuePairAddRowButton extends Component<HdsFormKeyValuePairAddRowButtonSignature> {
   get text(): string {
     return this.args.text ?? 'Add row';
+  }
+
+  @action
+  onClick(): void {
+    const { onClick } = this.args;
+
+    if (typeof onClick === 'function') {
+      onClick();
+    }
   }
 }
---
+++ showcase/app/routes/components/form/key-value-pair.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Route from '@ember/routing/route';
+
+export default class ComponentsSuperSelectRoute extends Route {
+  model() {
+    // these are used only for presentation purpose in the showcase
+    const STATES = ['default', 'hover', 'focus'];
+
+    // Options data
+    const OPTIONS = ['Option 1', 'Option 2', 'Option 3'];
+    const SELECTED_OPTION = OPTIONS[0];
+    const SELECTED_OPTIONS = [OPTIONS[0], OPTIONS[1]];
+
+    return {
+      STATES,
+      OPTIONS,
+      SELECTED_OPTION,
+      SELECTED_OPTIONS,
+    };
+  }
+}
