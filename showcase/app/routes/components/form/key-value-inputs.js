d94a20e573ca55f99d356cb380b9dbb53ef27864
shleewhite <lee.white@hahicorp.com>
2025-06-09 10:43:03 -0400

    fix: columns when add/remove, add superselect label correctly

--- showcase/app/routes/components/form/key-value-inputs.js
+++ showcase/app/routes/components/form/key-value-inputs.js
@@ -7,12 +7,16 @@ import Route from '@ember/routing/route';
 
 export default class ComponentsKeyValueInputsRoute extends Route {
   model() {
+    // these are used only for presentation purpose in the showcase
+    const STATES = ['default', 'hover', 'focus'];
+
     // Options data
     const OPTIONS = ['Option 1', 'Option 2', 'Option 3'];
     const SELECTED_OPTION = OPTIONS[0];
     const SELECTED_OPTIONS = [OPTIONS[0], OPTIONS[1]];
 
     return {
+      STATES,
       OPTIONS,
       SELECTED_OPTION,
       SELECTED_OPTIONS,
