577656ab488b4c8e86f922c1734ef7dd4081fb1e
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-06-10 10:48:00 -0700

    HDS-4885 Add type for Header tags and update related JS

--- packages/components/src/components/hds/form/types.ts
+++ packages/components/src/components/hds/form/types.ts
@@ -7,5 +7,15 @@ export enum HdsFormTagValues {
   Form = 'form',
   Div = 'div',
 }
-
 export type HdsFormTag = `${HdsFormTagValues}`;
+
+export enum HdsFormHeaderTagValues {
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  H4 = 'h4',
+  H5 = 'h5',
+  H6 = 'h6',
+  Div = 'div',
+}
+export type HdsFormHeaderTag = `${HdsFormHeaderTagValues}`;
