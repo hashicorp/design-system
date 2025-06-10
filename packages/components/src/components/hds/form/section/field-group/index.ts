3785e8401a6cd966c9b1a6341a33d9fb349d073d
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-06-10 13:46:22 -0700

    HDS-4885 Replace wrap prop with direction prop on FieldGroup component

--- packages/components/src/components/hds/form/section/field-group/index.ts
+++ packages/components/src/components/hds/form/section/field-group/index.ts
@@ -3,19 +3,16 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import Component from '@glimmer/component';
+import TemplateOnlyComponent from '@ember/component/template-only';
 
 import type { ComponentLike } from '@glint/template';
-
+import type { HdsLayoutFlexSignature } from '../../../layout/flex/index.ts';
 import type { HdsLayoutFlexItemSignature } from '../../../layout/flex/item.ts';
-
-// Q: I want to hardcode the @tag for this component to 'div'. So should I need to import `AvailableTagNames`?
-// (The linter says I should, but it doesn't make sense to me if I just want to use a div.)
 import type { AvailableElements } from '../../../layout/flex/types.ts';
 
 export interface HdsFormSectionFieldGroupSignature {
   Args: {
-    wrap?: boolean; // Whether the field group should wrap its contents
+    direction?: HdsLayoutFlexSignature['Args']['direction'];
   };
   Blocks: {
     default: [
@@ -24,16 +21,10 @@ export interface HdsFormSectionFieldGroupSignature {
       },
     ];
   };
-  Element: AvailableElements; // Was: HTMLDivElement
+  Element: AvailableElements;
 }
 
-export default class HdsFormSectionFieldGroup extends Component<HdsFormSectionFieldGroupSignature> {
-  get classNames(): string {
-    const classes = ['hds-form__section-field-group'];
+const HdsFormSectionFieldGroup =
+  TemplateOnlyComponent<HdsFormSectionFieldGroupSignature>();
 
-    // add a class based on the @xxx argument
-    // classes.push(`hds-form-field-group--[variant]-${this.xxx}`);
-
-    return classes.join(' ');
-  }
-}
+export default HdsFormSectionFieldGroup;
--- showcase/tests/integration/components/hds/form/section/field-group/index-test.js
+++ showcase/tests/integration/components/hds/form/section/field-group/index-test.js
@@ -24,23 +24,23 @@ module(
 
     // OPTIONS
 
-    // wrap
-    test('it should have the default wrap if no @wrap prop is declared', async function (assert) {
+    // direction
+    test('it should render the component with the default direction', async function (assert) {
       await render(
         hbs`<Hds::Form::Section::FieldGroup id="test-form-section-field-group" />`,
       );
       assert
         .dom('#test-form-section-field-group')
-        .doesNotHaveClass('hds-layout-flex--has-wrapping');
+        .hasClass('hds-layout-flex--direction-row');
     });
 
-    test('it should have the class "hds-layout-flex--has-wrapping" if @wrap is true', async function (assert) {
+    test('it should render the component with a specified direction', async function (assert) {
       await render(
-        hbs`<Hds::Form::Section::FieldGroup id="test-form-section-field-group" @wrap={{true}} />`,
+        hbs`<Hds::Form::Section::FieldGroup id="test-form-section-field-group" @direction="column" />`,
       );
       assert
         .dom('#test-form-section-field-group')
-        .hasClass('hds-layout-flex--has-wrapping');
+        .hasClass('hds-layout-flex--direction-column');
     });
   },
 );
