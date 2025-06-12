c33a70148a9ebfd0dc8bf60a79b62f5aba26eeac
Alex <alex-ju@users.noreply.github.com>
2025-06-12 17:12:19 +0100

    Remove re-exports from modifiers barrel

--- packages/components/src/modifiers.ts
+++ packages/components/src/modifiers.ts
@@ -4,13 +4,3 @@
  */
 
 // This file is use to expose public modifiers
-
-// hds-advanced-table-cell
-export { default as HdsAdvancedTableDomManagement } from './modifiers/hds-advanced-table-cell/dom-management.ts';
-export { default as HdsAdvancedTableKeyboardNavigation } from './modifiers/hds-advanced-table-cell/keyboard-navigation.ts';
-
-// hds-code-editor
-export { default as HdsCodeEditorRegoLanguageParser } from './modifiers/hds-code-editor/languages/rego.ts';
-export { default as HdsCodeEditorSentinelLanguageParser } from './modifiers/hds-code-editor/languages/sentinel.ts';
-export { default as HdsCodeEditorDarkPalette } from './modifiers/hds-code-editor/palettes/hds-dark-palette.ts';
-export * from './modifiers/hds-code-editor/types.ts';
