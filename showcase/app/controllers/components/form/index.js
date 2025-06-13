dc6905d756046d0c2c7d894bc4fc3afa65fe20f8
Cristiano Rastelli <public@didoo.net>
2025-06-13 23:00:50 +0100

    added use cases where the form is within containers

--- showcase/app/controllers/components/form/index.js
+++ showcase/app/controllers/components/form/index.js
@@ -10,6 +10,8 @@ import { tracked } from '@glimmer/tracking';
 export default class FormController extends Controller {
   @tracked showHighlight = false;
   @tracked showDemoCustomWidthsToggleContent = {};
+  @tracked isModalActive = false;
+  @tracked isFlyoutActive = false;
 
   @action
   toggleHighlight() {
@@ -24,5 +26,25 @@ export default class FormController extends Controller {
       ...this.showDemoCustomWidthsToggleContent,
     };
     this.showDemoCustomWidthsToggleContent[usecase] = !current;
+  }
+
+  @action
+  activateModal() {
+    this.isModalActive = true;
+  }
+
+  @action
+  deactivateModal() {
+    this.isModalActive = false;
+  }
+
+  @action
+  activateFlyout() {
+    this.isFlyoutActive = true;
+  }
+
+  @action
+  deactivateFlyout() {
+    this.isFlyoutActive = false;
   }
 }
