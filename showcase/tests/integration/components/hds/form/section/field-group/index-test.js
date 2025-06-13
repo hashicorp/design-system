e2da828f74b186154b2b847e6416977f44c5dae7
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-06-13 16:57:15 -0700

    HDS-4885 Add tests for SectionFieldGroup yielded content

--- showcase/tests/integration/components/hds/form/section/field-group/index-test.js
+++ showcase/tests/integration/components/hds/form/section/field-group/index-test.js
@@ -21,5 +21,28 @@ module(
         .dom('#test-form-section-field-group')
         .hasClass('hds-form__section-field-group');
     });
+
+    // CONTENT
+
+    test('it should render the yielded content', async function (assert) {
+      await render(
+        hbs`<Hds::Form::Section::FieldGroup id="test-form-section-field-group"><pre>test</pre></Hds::Form::Section::FieldGroup>`,
+      );
+      assert.dom('#test-form-section-field-group > pre').exists().hasText('test');
+    });
+
+    test('it should render the `Item` yielded contextual component', async function (assert) {
+      await render(
+        hbs`
+          <Hds::Form::Section::FieldGroup id="test-form-section-field-group" as |FG|>
+            <FG.Item><pre>test</pre></FG.Item>
+          </Hds::Form::Section::FieldGroup>
+        `,
+      );
+      assert
+        .dom('#test-form-section-field-group > .hds-layout-flex-item > pre')
+        .exists()
+        .hasText('test');
+    });
   },
 );
