41e6fcfef211993dd9062a91f76e2abdf5a0c528
Zack Moore <zack.moore@hashicorp.com>
2025-06-13 16:14:39 -0400

    added additional onResize test

--- showcase/tests/integration/components/hds/advanced-table/index-test.js
+++ showcase/tests/integration/components/hds/advanced-table/index-test.js
@@ -1583,7 +1583,38 @@ module('Integration | Component | hds/advanced-table/index', function (hooks) {
     );
   });
 
-  test('it should call `onColumnResize` when a column is resized', async function (assert) {
+  test('it should call `onColumnResize` when a column is resized by dragging', async function (assert) {
+    setResizableColumnsTableData(this);
+    const onColumnResizeSpy = sinon.spy();
+    this.set('onColumnResize', onColumnResizeSpy);
+
+    await render(hbs`
+      <Hds::AdvancedTable
+        @model={{this.model}}
+        @columns={{this.columns}}
+        @hasResizableColumns={{true}}
+        @onColumnResize={{this.onColumnResize}}
+        id="resize-test-table"
+      >
+        <:body as |B|>
+          <B.Tr>
+            <B.Td>{{B.data.col1}}</B.Td>
+            <B.Td>{{B.data.col2}}</B.Td>
+          </B.Tr>
+        </:body>
+      </Hds::AdvancedTable>
+    `);
+
+    const handle = find('.hds-advanced-table__th-resize-handle');
+
+    await focus(handle);
+
+    await triggerKeyEvent(handle, 'keydown', 'ArrowRight');
+
+    assert.ok(onColumnResizeSpy.calledOnce, 'onColumnResize was called');
+  });
+
+  test('it should call `onColumnResize` when a column is resized by keyboard', async function (assert) {
     setResizableColumnsTableData(this);
     const onColumnResizeSpy = sinon.spy();
     this.set('onColumnResize', onColumnResizeSpy);
