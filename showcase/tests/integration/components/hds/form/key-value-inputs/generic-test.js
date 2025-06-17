741c3cab687de6846ed8631f46420a4a03d4b6be
shleewhite <lee.white@hahicorp.com>
2025-06-17 14:26:23 -0400

    fix: resolve remaining test failures

--- showcase/tests/integration/components/hds/form/key-value-inputs/generic-test.js
+++ showcase/tests/integration/components/hds/form/key-value-inputs/generic-test.js
@@ -9,24 +9,26 @@ import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module(
-  'Integration | Component | hds/form/key-value-inputs/yield',
+  'Integration | Component | hds/form/key-value-inputs/generic',
   function (hooks) {
     setupRenderingTest(hooks);
 
     test('it should render the component with a CSS class that matches the component name', async function (assert) {
-      await render(hbs`<Hds::Form::KeyValueInputs::Yield id="test-form-key-value-yield">
+      await render(hbs`<Hds::Form::KeyValueInputs::Generic id="test-form-key-value-generic">
       <span id='foo'>Generic content</span>
-    </Hds::Form::KeyValueInputs::Yield>`);
+    </Hds::Form::KeyValueInputs::Generic>`);
       assert
-        .dom('#test-form-key-value-yield')
+        .dom('#test-form-key-value-generic')
         .hasClass('hds-form-key-value-inputs__generic-container');
     });
 
     test('it should render the content', async function (assert) {
-      await render(hbs`<Hds::Form::KeyValueInputs::Yield id="test-form-key-value-yield">
+      await render(hbs`<Hds::Form::KeyValueInputs::Generic id="test-form-key-value-generic">
       <span id='foo'>Generic content</span>
-    </Hds::Form::KeyValueInputs::Yield>`);
-      assert.dom('#test-form-key-value-yield #foo').hasText('Generic content');
+    </Hds::Form::KeyValueInputs::Generic>`);
+      assert
+        .dom('#test-form-key-value-generic #foo')
+        .hasText('Generic content');
     });
   },
 );
