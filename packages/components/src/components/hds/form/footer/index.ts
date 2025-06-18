d28c73e7c2f80b796e9955911aca90cbdf474a5e
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-06-18 11:54:06 -0700

    HDS-4885 Change Form Footer to use a div instead of ButtonSet, Put ButtonSet inside FormFooter in examples

--- packages/components/src/components/hds/form/footer/index.ts
+++ packages/components/src/components/hds/form/footer/index.ts
@@ -5,18 +5,25 @@
 
 import Component from '@glimmer/component';
 
+import type { ComponentLike } from '@glint/template';
+import type { HdsButtonSetSignature } from '../../button-set/index.ts';
+
 export interface HdsFormFooterSignature {
   Args: {
     isFullWidth?: boolean;
   };
   Blocks: {
-    default: [];
-  };
+      default: [
+        {
+          ButtonSet?: ComponentLike<HdsButtonSetSignature>;
+        },
+      ];
+    };
   Element: HTMLDivElement;
 }
 
 export default class HdsFormFooter extends Component<HdsFormFooterSignature> {
-  get classNames() {
+  get classNames():string {
     const classes = ['hds-form__footer'];
 
     // add a class based on the @isFullWidth argument
