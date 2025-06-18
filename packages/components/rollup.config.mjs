202e978c2463c4f4749ae75b1f09a353ecc11b88
Zack Moore <zack.moore@hashicorp.com>
2025-06-18 11:50:21 -0400

    changing the rollup config

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -18,9 +18,7 @@ const plugins = [
   // These are the modules that users should be able to import from your
   // addon. Anything not listed here may get optimized away.
   addon.publicEntrypoints([
-    '**/*.{js,ts}',
-    '!helpers/hds-t.js',
-    '!services/hds-intl.js',
+    '**/!(hds-t|hds-intl).{js,ts}',
     'index.js',
     'template-registry.js',
     'styles/@hashicorp/design-system-components.scss',
