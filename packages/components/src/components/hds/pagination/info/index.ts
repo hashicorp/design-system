0552e9495bfc5a40158d66a67b7551706ac34b38
Zack Moore <zack.moore@hashicorp.com>
2025-06-20 14:48:18 -0400

    responding to pr feedback

--- packages/components/src/components/hds/pagination/info/index.ts
+++ packages/components/src/components/hds/pagination/info/index.ts
@@ -4,8 +4,12 @@
  */
 
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
+
 import type { HdsPaginationNumberedSignature } from '../numbered/index';
 import type { HdsTextBodySignature } from '../../text/body';
+import type HdsIntlService from '../../../../services/hds-intl';
+
 export interface HdsPaginationInfoSignature {
   Args: {
     itemsRangeStart: number;
@@ -17,7 +21,31 @@ export interface HdsPaginationInfoSignature {
 }
 
 export default class HdsPaginationInfo extends Component<HdsPaginationInfoSignature> {
+  @service hdsIntl!: HdsIntlService;
+
   get showTotalItems(): boolean {
     return this.args.showTotalItems ?? true;
+  }
+
+  get translatedItemsRange(): string {
+    const { itemsRangeStart, itemsRangeEnd, totalItems } = this.args;
+
+    let translationKey = 'hds.components.pagination.info.page_range';
+    let interpolationProps: { itemsRangeStart: number; itemsRangeEnd: number; totalItems?: number } = {
+      itemsRangeStart,
+      itemsRangeEnd,
+    };
+    let defaultValue = `${itemsRangeStart}–${itemsRangeEnd}`;
+    
+    if (this.showTotalItems) {
+      translationKey = 'hds.components.pagination.info.page_range_with_total';
+      interpolationProps = {
+        ...interpolationProps,
+        totalItems,
+      };
+      defaultValue = `${defaultValue} of ${totalItems}`;
+    }
+
+    return this.hdsIntl.t(translationKey, { ...interpolationProps, default: defaultValue });
   }
 }
