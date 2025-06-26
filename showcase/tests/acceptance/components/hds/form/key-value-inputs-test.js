82808c9e9e8e318664772a3b7802198e34dc9ecf
Cristiano Rastelli <public@didoo.net>
2025-06-26 21:58:18 +0100

    added a11y acceptance and percy tests

---
+++ showcase/tests/acceptance/components/hds/form/key-value-inputs-test.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'showcase/tests/helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Acceptance | Component | hds/form/key-value-inputs', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('components/form/key-value-inputs passes a11y automated checks', async function (assert) {
+    await visit('/components/form/key-value-inputs');
+    await a11yAudit();
+    assert.ok(true, 'a11y automation audit passed');
+  });
+});
