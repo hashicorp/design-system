07a0c5b4cf925e103cdea4703aae62e7422e01e5
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-06-26 13:34:20 -0700

    Form layout—Documentation (HDS-4886) (#2947)
    Co-authored-by: Cristiano Rastelli <cristiano.rastelli@hashicorp.com>
    Co-authored-by: Cristiano Rastelli <public@didoo.net>
    Co-authored-by: LilithJames-HDS <lilith.james@hashicorp.com>
    Co-authored-by: Heather Larsen <hlarsen@hashicorp.com>
    Co-authored-by: Dylan Hyun <dylan.hyun@hashicorp.com>
    Co-authored-by: Lee White <lee.white@hashicorp.com>
    Co-authored-by: Majed <156002572+majedelass@users.noreply.github.com>

---
+++ website/tests/acceptance/components/form/layout.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'website/tests/helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Acceptance | components/form/layout', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /components/form/layout', async function (assert) {
+    await visit('/components/form/layout');
+
+    assert.strictEqual(currentURL(), '/components/form/layout');
+  });
+
+  test('Components/form/layout page passes automated a11y checks', async function (assert) {
+    await visit('/components/form/layout');
+
+    await a11yAudit();
+
+    assert.ok(true, 'a11y automation audit passed');
+  });
+});
