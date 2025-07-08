78b5116eca6893eb372a0b1150cf2e70a855b985
shleewhite <lee.white@hahicorp.com>
2025-07-08 10:04:06 -0400

    chore: lint

--- showcase/app/routes/components/dropdown.ts
+++ showcase/app/routes/components/dropdown.ts
@@ -9,9 +9,9 @@ import { COLORS as TOGGLE_BUTTON_COLORS } from '@hashicorp/design-system-compone
 import { COLORS as ITEM_INTERACTIVE_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/list-item/interactive';
 import { POSITIONS } from '@hashicorp/design-system-components/components/hds/dropdown/index';
 
-import type {HdsDropdownToggleButtonColors} from '@hashicorp/design-system-components/components/hds/dropdown/toggle/types';
-import type {HdsDropdownListItemInteractiveColors} from '@hashicorp/design-system-components/components/hds/dropdown/list-item/types';
-import type {HdsDropdownPositions} from '@hashicorp/design-system-components/components/hds/dropdown/types';
+import type { HdsDropdownToggleButtonColors } from '@hashicorp/design-system-components/components/hds/dropdown/toggle/types';
+import type { HdsDropdownListItemInteractiveColors } from '@hashicorp/design-system-components/components/hds/dropdown/list-item/types';
+import type { HdsDropdownPositions } from '@hashicorp/design-system-components/components/hds/dropdown/types';
 
 export default class ComponentsDropdownRoute extends Route {
   model() {
@@ -19,9 +19,11 @@ export default class ComponentsDropdownRoute extends Route {
     const TOGGLE_STATES = ['default', 'hover', 'active', 'focus', 'disabled'];
     const ITEM_STATES = ['default', 'hover', 'active', 'focus'];
     return {
-      TOGGLE_BUTTON_COLORS: TOGGLE_BUTTON_COLORS as HdsDropdownToggleButtonColors[],
+      TOGGLE_BUTTON_COLORS:
+        TOGGLE_BUTTON_COLORS as HdsDropdownToggleButtonColors[],
       TOGGLE_STATES,
-      ITEM_INTERACTIVE_COLORS: ITEM_INTERACTIVE_COLORS as HdsDropdownListItemInteractiveColors[],
+      ITEM_INTERACTIVE_COLORS:
+        ITEM_INTERACTIVE_COLORS as HdsDropdownListItemInteractiveColors[],
       ITEM_STATES,
       POSITIONS: POSITIONS as HdsDropdownPositions[],
     };
