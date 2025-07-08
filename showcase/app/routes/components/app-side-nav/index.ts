a8a7dab2eef12076c2ad03251b55190666e4cfa6
shleewhite <lee.white@hahicorp.com>
2025-07-08 13:28:52 -0400

    feat: convert AppSideNav showcase page to TS

--- showcase/app/routes/components/app-side-nav/index.ts
+++ showcase/app/routes/components/app-side-nav/index.ts
@@ -8,6 +8,9 @@ import Route from '@ember/routing/route';
 import { COLORS as DROPDOWN_TOGGLE_BUTTON_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/toggle/button';
 import { COLORS as DROPDOWN_ITEM_INTERACTIVE_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/list-item/interactive';
 
+import type { HdsDropdownToggleButtonColors } from '@hashicorp/design-system-components/components/hds/dropdown/toggle/types';
+import type { HdsDropdownListItemInteractiveColors } from '@hashicorp/design-system-components/components/hds/dropdown/list-item/types';
+
 export default class ComponentsAppSideNavRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
@@ -21,9 +24,11 @@ export default class ComponentsAppSideNavRoute extends Route {
     const DROPDOWN_ITEM_STATES = ['default', 'hover', 'active', 'focus'];
 
     return {
-      DROPDOWN_TOGGLE_BUTTON_COLORS,
+      DROPDOWN_TOGGLE_BUTTON_COLORS:
+        DROPDOWN_TOGGLE_BUTTON_COLORS as HdsDropdownToggleButtonColors[],
       DROPDOWN_TOGGLE_STATES,
-      DROPDOWN_ITEM_INTERACTIVE_COLORS,
+      DROPDOWN_ITEM_INTERACTIVE_COLORS:
+        DROPDOWN_ITEM_INTERACTIVE_COLORS as HdsDropdownListItemInteractiveColors[],
       DROPDOWN_ITEM_STATES,
     };
   }
