5644a2e74f372f4cc0da9eb773d325361ff77228
shleewhite <lee.white@hahicorp.com>
2025-07-08 15:04:12 -0400

    feat: convert Copy/Snippet showcase page to TS

---
+++ showcase/app/controllers/components/copy/snippet.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { scheduleOnce } from '@ember/runloop';
+
+import type Owner from '@ember/owner';
+import type { Registry as Services } from '@ember/service';
+
+import type { ComponentsCopySnippetModelSignature } from 'showcase/routes/components/copy/snippet';
+
+export default class CopySnippetController extends Controller {
+  declare model: ComponentsCopySnippetModelSignature;
+  @service router!: Services['router'];
+
+  constructor(owner: Owner) {
+    super(owner);
+    this.router.on('routeDidChange', this.routeDidChange.bind(this));
+  }
+
+  routeDidChange() {
+    if (this.router.currentRoute?.name === 'components.copy.snippet') {
+      // eslint-disable-next-line ember/no-runloop
+      scheduleOnce('afterRender', this, this.replaceMockCopyStatus.bind(this));
+    }
+  }
+
+  replaceMockCopyStatus() {
+    document.querySelectorAll('[mock-copy-status]').forEach((element) => {
+      const status = element.getAttribute('mock-copy-status');
+      element.classList.remove('hds-copy-button--status-idle');
+      element.classList.add(`hds-copy-button--status-${status}`);
+
+      const icon = element.querySelector('svg use');
+
+      if (icon) {
+        if (status === 'success') {
+          // eg. href="#flight-clipboard-checked-16"
+          icon.setAttribute('href', `#flight-${this.model.SUCCESS_ICON}-16`);
+        } else if (status === 'error') {
+          icon.setAttribute('href', `#flight-${this.model.ERROR_ICON}-16`);
+        }
+      }
+    });
+  }
+}
--- showcase/app/routes/components/copy/snippet.ts
+++ showcase/app/routes/components/copy/snippet.ts
@@ -10,10 +10,24 @@ import {
   ERROR_ICON,
 } from '@hashicorp/design-system-components/components/hds/copy/snippet/index';
 
+import type { HdsCopyButtonSizes } from '@hashicorp/design-system-components/components/hds/copy/button/types';
+
+export interface ComponentsCopySnippetModelSignature {
+  SIZES: HdsCopyButtonSizes[];
+  STATES: string[];
+  SUCCESS_ICON: string;
+  ERROR_ICON: string;
+}
+
 export default class ComponentsCopySnippetRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
     const STATES = ['default', 'hover', 'active', 'focus'];
-    return { COLORS, STATES, SUCCESS_ICON, ERROR_ICON };
+    return {
+      COLORS: COLORS as HdsCopyButtonSizes[],
+      STATES,
+      SUCCESS_ICON,
+      ERROR_ICON,
+    };
   }
 }
