486a0014e0e03dd81f2181e2c62e11b1382782c1
shleewhite <lee.white@hahicorp.com>
2025-07-08 10:03:31 -0400

    fix: add types for arrays of Dropdown variants

--- showcase/app/routes/components/dropdown.ts
+++ showcase/app/routes/components/dropdown.ts
@@ -9,17 +9,21 @@ import { COLORS as TOGGLE_BUTTON_COLORS } from '@hashicorp/design-system-compone
 import { COLORS as ITEM_INTERACTIVE_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/list-item/interactive';
 import { POSITIONS } from '@hashicorp/design-system-components/components/hds/dropdown/index';
 
+import type {HdsDropdownToggleButtonColors} from '@hashicorp/design-system-components/components/hds/dropdown/toggle/types';
+import type {HdsDropdownListItemInteractiveColors} from '@hashicorp/design-system-components/components/hds/dropdown/list-item/types';
+import type {HdsDropdownPositions} from '@hashicorp/design-system-components/components/hds/dropdown/types';
+
 export default class ComponentsDropdownRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
     const TOGGLE_STATES = ['default', 'hover', 'active', 'focus', 'disabled'];
     const ITEM_STATES = ['default', 'hover', 'active', 'focus'];
     return {
-      TOGGLE_BUTTON_COLORS,
+      TOGGLE_BUTTON_COLORS: TOGGLE_BUTTON_COLORS as HdsDropdownToggleButtonColors[],
       TOGGLE_STATES,
-      ITEM_INTERACTIVE_COLORS,
+      ITEM_INTERACTIVE_COLORS: ITEM_INTERACTIVE_COLORS as HdsDropdownListItemInteractiveColors[],
       ITEM_STATES,
-      POSITIONS,
+      POSITIONS: POSITIONS as HdsDropdownPositions[],
     };
   }
 }
