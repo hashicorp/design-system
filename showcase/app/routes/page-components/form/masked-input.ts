9ee07019a7682448c21e4da53bba52ba3ff779b4
shleewhite <lee.white@hahicorp.com>
2025-07-14 13:04:19 -0400

    feat: convert Form > Masked Input page to TS

--- showcase/app/routes/page-components/form/masked-input.ts
+++ showcase/app/routes/page-components/form/masked-input.ts
@@ -5,6 +5,11 @@
 
 import Route from '@ember/routing/route';
 
+import type { ModelFrom } from 'showcase/utils/ModelFromRoute';
+
+export type PageComponentsFormMaskedInputModel =
+  ModelFrom<PageComponentsFormMaskedInputRoute>;
+
 export default class PageComponentsFormMaskedInputRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
