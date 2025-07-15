54083ecb163a0ce47c03cd1a3438274a3d106d22
shleewhite <lee.white@hahicorp.com>
2025-07-15 09:37:44 -0400

    feat: convert Form > Toggle page to TS

--- showcase/app/routes/page-components/form/toggle.ts
+++ showcase/app/routes/page-components/form/toggle.ts
@@ -5,6 +5,11 @@
 
 import Route from '@ember/routing/route';
 
+import type { ModelFrom } from 'showcase/utils/ModelFromRoute';
+
+export type PageComponentsFormToggleModel =
+  ModelFrom<PageComponentsFormToggleRoute>;
+
 export default class PageComponentsFormToggleRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
