7e9dc240d7b4a8d7bec4287d39e003077f3c14d4
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-07-16 14:34:13 -0400

    Feat: Convert Time showcase pages to TS

--- showcase/app/routes/page-components/time.ts
+++ showcase/app/routes/page-components/time.ts
@@ -7,6 +7,10 @@ import Route from '@ember/routing/route';
 
 import { DISPLAYS } from '@hashicorp/design-system-components/services/hds-time';
 
+import type { ModelFrom } from 'showcase/utils/ModelFromRoute';
+
+export type PageComponentsTimeModel = ModelFrom<PageComponentsTimeRoute>;
+
 export default class PageComponentsTimeRoute extends Route {
   model() {
     // these are used only for presentation purpose in the showcase
