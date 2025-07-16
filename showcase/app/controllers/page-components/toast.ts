032c32a5610dd2952dc90eab4849051c8e336e8a
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-07-16 14:26:02 -0400

    Feat: Convert Toast showcase pages to TS

--- showcase/app/controllers/page-components/toast.ts
+++ showcase/app/controllers/page-components/toast.ts
@@ -6,7 +6,11 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
+import type { PageComponentsToastModel } from '../../routes/page-components/toast';
+
 export default class PageComponentsToastController extends Controller {
+  declare model: PageComponentsToastModel;
+
   @action
   noop() {}
 }
