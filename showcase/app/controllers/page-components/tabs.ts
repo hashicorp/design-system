2751ed9081f209c35df617783467cb69fbb6e596
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-07-17 14:26:43 -0400

    Feat: Convert Tabs showcase pages to TS

--- showcase/app/controllers/page-components/tabs.ts
+++ showcase/app/controllers/page-components/tabs.ts
@@ -8,8 +8,14 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
+import type RouterService from '@ember/routing/router-service';
+import type { PageComponentsTabsModel } from '../../routes/page-components/tabs';
+
 const getRandomInteger = (max = 125) => Math.floor(Math.random() * max);
+
 export default class PageComponentsTabsController extends Controller {
+  declare model: PageComponentsTabsModel;
+
   queryParams = [
     'currentTab_demo1',
     'currentTab_demo2',
@@ -17,22 +23,22 @@ export default class PageComponentsTabsController extends Controller {
     'currentTab_demo2_subtab2',
   ];
 
-  @service router;
+  @service declare router: RouterService;
 
   @tracked showHighlight = false;
 
-  @tracked currentTab_demo1;
-  @tracked currentTab_demo2;
-  @tracked currentTab_demo2_subtab1;
-  @tracked currentTab_demo2_subtab2;
+  @tracked currentTab_demo1: number = 0;
+  @tracked currentTab_demo2: number = 0;
+  @tracked currentTab_demo2_subtab1: number = 0;
+  @tracked currentTab_demo2_subtab2: number = 0;
   // --- we initialize to non-random values to avoid visual regression tests to fail
-  @tracked badge1_demo3 = undefined;
-  @tracked badge2_demo3 = 2;
-  @tracked badge3_demo3 = 3;
+  @tracked badge1_demo3: number | undefined = undefined;
+  @tracked badge2_demo3: number = 2;
+  @tracked badge3_demo3: number = 3;
   // ---
-  @tracked atSelected_demo4 = 'two';
+  @tracked atSelected_demo4: string = 'two';
   // ---
-  @tracked selectedTab_demo5 = 'two';
+  @tracked selectedTab_demo5: string = 'two';
 
   // =============================
   // GENERIC HANDLERS
@@ -44,8 +50,9 @@ export default class PageComponentsTabsController extends Controller {
   }
 
   @action
-  logClickedTab(event, index) {
-    const tabId = event.target.id;
+  logClickedTab(event: MouseEvent, index: number) {
+    const eventTarget = event.target as HTMLElement;
+    const tabId = eventTarget?.id;
     console.log(`Tab with ID "${tabId}" and index "${index}" clicked!`);
   }
 
@@ -56,48 +63,44 @@ export default class PageComponentsTabsController extends Controller {
   // DEMO #1
 
   get selectedTabDemo1() {
-    return this.currentTab_demo1 !== undefined
-      ? parseInt(this.currentTab_demo1, 10)
-      : 0;
+    return this.currentTab_demo1 !== undefined ? this.currentTab_demo1 : 0;
   }
 
   @action
-  updateQueryParamDemo1(_element, index) {
+  updateQueryParamDemo1(_event: MouseEvent, index: number) {
     this.currentTab_demo1 = index;
   }
 
   // DEMO #2
 
   get selectedTabDemo2() {
-    return this.currentTab_demo2 !== undefined
-      ? parseInt(this.currentTab_demo2, 10)
-      : 0;
+    return this.currentTab_demo2 !== undefined ? this.currentTab_demo2 : 0;
   }
 
   get selectedTabDemo2_SubTab1() {
     return this.currentTab_demo2_subtab1 !== undefined
-      ? parseInt(this.currentTab_demo2_subtab1, 10)
+      ? this.currentTab_demo2_subtab1
       : 0;
   }
 
   get selectedTabDemo2_SubTab2() {
     return this.currentTab_demo2_subtab2 !== undefined
-      ? parseInt(this.currentTab_demo2_subtab2, 10)
+      ? this.currentTab_demo2_subtab2
       : 0;
   }
 
   @action
-  updateQueryParamDemo2(_element, index) {
+  updateQueryParamDemo2(_event: MouseEvent, index: number) {
     this.currentTab_demo2 = index;
   }
 
   @action
-  updateQueryParamDemo2_SubTab1(_element, index) {
+  updateQueryParamDemo2_SubTab1(_event: MouseEvent, index: number) {
     this.currentTab_demo2_subtab1 = index;
   }
 
   @action
-  updateQueryParamDemo2_SubTab2(_element, index) {
+  updateQueryParamDemo2_SubTab2(_event: MouseEvent, index: number) {
     this.currentTab_demo2_subtab2 = index;
   }
 
@@ -113,7 +116,7 @@ export default class PageComponentsTabsController extends Controller {
   // DEMO #4
 
   @action
-  setAtSelectedDemo4(tab) {
+  setAtSelectedDemo4(tab: string) {
     this.atSelected_demo4 = tab;
   }
 
@@ -140,7 +143,7 @@ export default class PageComponentsTabsController extends Controller {
   }
 
   @action
-  setSelectedTabDemo5(tab) {
+  setSelectedTabDemo5(tab: string) {
     this.selectedTab_demo5 = tab;
   }
 }
