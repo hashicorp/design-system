1023b921dd6f92decab0ae6da53d01530bf98d44
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-07-17 13:51:46 -0400

    Feat: Convert Tag showcase pages to TS

--- showcase/app/controllers/page-components/tag.ts
+++ showcase/app/controllers/page-components/tag.ts
@@ -6,7 +6,11 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
+import type { PageComponentsTagModel } from '../../routes/page-components/tag';
+
 export default class PageComponentsTagController extends Controller {
+  declare model: PageComponentsTagModel;
+
   @action
   noop() {}
 }
