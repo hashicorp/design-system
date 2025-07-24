7178481ecea3e4b587bc8b056dae74eecd84b234
shleewhite <lee.white@hahicorp.com>
2025-07-24 14:20:28 -0400

    feat: convert Alert to TS

--- showcase/app/controllers/page-components/alert.ts
+++ showcase/app/controllers/page-components/alert.ts
@@ -6,7 +6,11 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
+import type { PageComponentsAlertModel } from 'showcase/routes/page-components/alert';
+
 export default class PageComponentsAlertController extends Controller {
+  declare model: PageComponentsAlertModel;
+
   @action
   noop() {}
 }
