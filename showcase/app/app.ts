17803a5898f5ded182e261d0006d21fcabb35301
Alexey Kulakov <uni_que@me.com>
2025-07-28 21:39:44 -0700

    feat(showcase): vite

--- showcase/app/app.ts
+++ showcase/app/app.ts
@@ -6,6 +6,7 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
+import compatModules from '@embroider/virtual/compat-modules';
 import config from 'showcase/config/environment';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 
@@ -16,7 +17,7 @@ if (macroCondition(isDevelopingApp())) {
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(compatModules);
 }
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix, compatModules);
