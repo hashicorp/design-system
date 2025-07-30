c39529137bf0ff542685ee4721901887bb09d287
Zack Moore <zack.moore@hashicorp.com>
2025-07-29 21:54:36 -0700

    removed unneeded prop

--- packages/components/src/components/hds/advanced-table/index.ts
+++ packages/components/src/components/hds/advanced-table/index.ts
@@ -182,7 +182,6 @@ export default class HdsAdvancedTable extends Component<HdsAdvancedTableSignatur
     undefined;
   @tracked private _isSelectAllCheckboxSelected?: boolean = undefined;
   @tracked private _tableHeight = 0;
-  @tracked private _tableWidth = 0;
   private _selectableRows: HdsAdvancedTableSelectableRow[] = [];
   private _captionId = 'caption-' + guidFor(this);
   private _tableModel!: HdsAdvancedTableTableModel;
@@ -488,7 +487,6 @@ export default class HdsAdvancedTable extends Component<HdsAdvancedTableSignatur
 
     const updateMeasurements = () => {
       this._tableHeight = element.clientHeight;
-      this._tableWidth = element.clientWidth;
 
       this.scrollIndicatorDimensions = getScrollIndicatorDimensions(
         element,
