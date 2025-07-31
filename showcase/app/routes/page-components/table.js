440a8decbf2883dfa17bf480f366a2e9d2124c82
shleewhite <lee.white@hahicorp.com>
2025-07-31 17:14:15 -0400

    fix: update table too

--- showcase/app/routes/page-components/table.js
+++ showcase/app/routes/page-components/table.js
@@ -6,6 +6,8 @@
 import Route from '@ember/routing/route';
 import { DENSITIES } from '@hashicorp/design-system-components/components/hds/table/index';
 
+import { fetchJson } from 'showcase/utils/fetchJson';
+
 // basic function that clones an array of objects (not deep)
 const clone = (arr) => {
   return arr.map((item) => ({ ...item }));
@@ -15,18 +17,12 @@ const STATES = ['default', 'hover', 'active', 'focus'];
 
 export default class PageComponentsTableRoute extends Route {
   async model() {
-    let responseMusic = await fetch('/api/folk.json');
-    let responseClusters = await fetch('/api/mock-clusters-with-status.json');
-    let responseManyColumns = await fetch('/api/mock-many-columns.json');
-    let { data: music } = await responseMusic.json();
-    let clusters = await responseClusters.json();
-    let manycolumns = await responseManyColumns.json();
 
-    let responseSelectableData = await fetch('/api/mock-selectable-data.json');
-    let selectableData = await responseSelectableData.json();
-
-    let responseUserData = await fetch('/api/mock-users.json');
-    let userData = await responseUserData.json();
+    const {data: music} = await fetchJson('/api/folk.json');
+    const clusters = await fetchJson('/api/mock-clusters-with-status.json');
+    const manycolumns = await fetchJson('/api/mock-many-columns.json');
+    const selectableData = await fetchJson('/api/mock-selectable-data.json');
+    const userData = await fetchJson('/api/mock-users.json');
 
     return {
       music: music.map((record) => ({ id: record.id, ...record.attributes })),
