95d29fa564d7de4251f476db22e4f728e15bd367
Lee White <lee.white@hashicorp.com>
2025-08-04 15:00:04 -0400

    Showcase: convert AdvancedTable route to TS (#3056)

--- showcase/app/controllers/page-components/advanced-table.ts
+++ showcase/app/controllers/page-components/advanced-table.ts
@@ -9,6 +9,14 @@ import { tracked } from '@glimmer/tracking';
 import { deepTracked } from 'ember-deep-tracked';
 import { later } from '@ember/runloop';
 
+import type { PageComponentsAdvancedTableModel } from 'showcase/routes/page-components/advanced-table';
+
+import type {
+  HdsAdvancedTableOnSelectionChangeSignature,
+  HdsAdvancedTableThSortOrder,
+} from '@hashicorp/design-system-components/components/hds/advanced-table/types';
+import HdsAdvancedTableModel from '@hashicorp/design-system-components/components/hds/advanced-table/models/table';
+
 // we use an array to declare the custom sorting order for the clusters' status
 const customSortingCriteriaArray = [
   'failing',
@@ -17,35 +25,51 @@ const customSortingCriteriaArray = [
   'pending',
 ];
 
-const updateModelWithSelectAllState = (modelData, selectAllState) => {
+const updateModelWithSelectAllState = <T>(
+  modelData: T[],
+  selectAllState: boolean,
+) => {
   modelData.forEach((modelRow) => {
-    modelRow.isSelected = selectAllState;
+    if (modelRow instanceof Object && 'isSelected' in modelRow) {
+      modelRow.isSelected = selectAllState;
+    }
   });
 };
 
-const updateModelWithSelectableRowsStates = (
-  modelData,
-  selectableRowsStates,
-) => {
-  const modelDataMap = new Map(
-    modelData.map((modelRow) => [modelRow.id, modelRow]),
+function updateModelWithSelectableRowsStates<
+  T extends { id: number; isSelected?: boolean },
+>(
+  modelData: T[],
+  selectableRowsStates: HdsAdvancedTableOnSelectionChangeSignature['selectableRowsStates'],
+): void {
+  // Create a map from id to row for quick lookup
+  const modelDataMap: Map<string, T> = new Map(
+    modelData.map((modelRow) => [String(modelRow.id), modelRow]),
   );
+
   selectableRowsStates.forEach((row) => {
-    // safe to assume that there is always a record for the "selectionKey" since it's coming from the model (the selectable "rows" are a subset of the model dataset)
-    modelDataMap.get(row.selectionKey).isSelected = row.isSelected;
+    const record = modelDataMap.get(row.selectionKey);
+    if (record) {
+      record.isSelected = row.isSelected;
+    }
   });
-};
+}
 
 export default class PageComponentsAdvancedTableController extends Controller {
+  declare model: PageComponentsAdvancedTableModel;
+
   @tracked customSortOrder = 'asc';
+  // @ts-expect-error - we need to reevaluate how we get the data for the table demos when we break up the template files into sub components
   @deepTracked multiSelectSelectableData = [...this.model.selectableData];
   @tracked multiSelectFilterRows__demo1 = 'all';
   @tracked multiSelectToggleScope__demo1 = false;
   @tracked multiSelectToggleDebug__demo1 = false;
   @deepTracked multiSelectModelData__demo1 = [
+    // @ts-expect-error - we need to reevaluate how we get the data for the table demos when we break up the template files into sub components
     ...this.model.selectableDataDemo1,
   ];
   @deepTracked multiSelectModelData__demo2 = [
+    // @ts-expect-error - we need to reevaluate how we get the data for the table demos when we break up the template files into sub components
     ...this.model.selectableDataDemo2,
   ];
   @tracked multiSelectToggleScope__demo2 = false;
@@ -54,11 +78,22 @@ export default class PageComponentsAdvancedTableController extends Controller {
   @tracked multiSelectPaginatedCurrentPageSize_demo2 = 2;
   @tracked multiSelectToggleScope__demo3 = false;
   @tracked multiSelectToggleDebug__demo3 = false;
+  // @ts-expect-error - we need to reevaluate how we get the data for the table demos when we break up the template files into sub components
   @deepTracked multiSelectModelData__demo3 = [...this.model.userDataDemo3];
   @tracked multiSelectUsersCurrentPage_demo3 = 1;
   @tracked multiSelectUsersCurrentPageSize_demo3 = 4;
+  // @ts-expect-error - we need to reevaluate how we get the data for the table demos when we break up the template files into sub components
   @deepTracked multiSelectUserData__demo4 = [...this.model.userDataDemo4];
   @tracked focusableElementsVisible = false;
+  @deepTracked thResizeHandleTable = new HdsAdvancedTableModel({
+    // @ts-expect-error - this will be fixed in HDS-5090
+    model: this.model.userDataShort,
+    columns: [
+      {
+        label: 'Label',
+      },
+    ],
+  });
 
   get clustersWithExtraData() {
     return this.model.clusters.map((record) => {
@@ -74,43 +109,64 @@ export default class PageComponentsAdvancedTableController extends Controller {
   // CUSTOM SORTING DEMO
   // Sortable table with custom `sortingFunction` declared in the column hash
 
-  get customSortingFunction() {
+  get customSortingFunction(): <T>(s1: T, s2: T) => number {
     return (s1, s2) => {
-      const index1 = customSortingCriteriaArray.indexOf(s1['status']);
-      const index2 = customSortingCriteriaArray.indexOf(s2['status']);
-      if (index1 < index2) {
-        return this.customSortOrder === 'asc' ? -1 : 1;
-      } else if (index1 > index2) {
-        return this.customSortOrder === 'asc' ? 1 : -1;
-      } else {
-        return 0;
+      // check that s1 and s2 are objects and have the 'status' property
+      if (
+        s1 instanceof Object &&
+        s2 instanceof Object &&
+        'status' in s1 &&
+        'status' in s2 &&
+        typeof s1['status'] === 'string' &&
+        typeof s2['status'] === 'string'
+      ) {
+        const index1 = customSortingCriteriaArray.indexOf(s1['status']);
+        const index2 = customSortingCriteriaArray.indexOf(s2['status']);
+        if (index1 < index2) {
+          return this.customSortOrder === 'asc' ? -1 : 1;
+        } else if (index1 > index2) {
+          return this.customSortOrder === 'asc' ? 1 : -1;
+        } else {
+          return 0;
+        }
       }
+      return 0;
     };
   }
 
   @action
-  customOnSort(_sortBy, sortOrder) {
+  customOnSort(_sortBy: string, sortOrder: HdsAdvancedTableThSortOrder) {
     this.customSortOrder = sortOrder;
   }
 
   // GENERIC MULTI-SELECT FUNCTIONALITIES
 
   @action
-  onSelectionChangeLogArguments() {
-    console.log(...arguments);
+  onSelectionChangeLogArguments(
+    args: HdsAdvancedTableOnSelectionChangeSignature,
+  ) {
+    console.group('onSelectionChangeLogArguments');
+    console.log('Selection Key:', args.selectionKey);
+    console.log('Checkbox Element:', args.selectionCheckboxElement);
+    console.log('Selectable Rows Keys:', args.selectedRowsKeys);
+    console.log('Selectable Rows States:', args.selectableRowsStates);
+    console.groupEnd();
   }
 
   // Sortable table with model and sorting by selected row
 
   @action
-  onMultiSelectSelectionChange({ selectionKey, selectionCheckboxElement }) {
-    if (selectionKey === 'all') {
+  onMultiSelectSelectionChange({
+    selectionKey,
+    selectionCheckboxElement,
+  }: HdsAdvancedTableOnSelectionChangeSignature) {
+    if (selectionKey === 'all' && selectionCheckboxElement) {
       this.multiSelectSelectableData.forEach((modelRow) => {
         modelRow.isSelected = selectionCheckboxElement.checked;
       });
     } else {
       const recordToUpdate = this.multiSelectSelectableData.find(
-        (modelRow) => modelRow.id === selectionKey,
+        (modelRow) => modelRow.id === Number(selectionKey),
       );
 
       if (recordToUpdate) {
@@ -120,7 +176,7 @@ export default class PageComponentsAdvancedTableController extends Controller {
   }
 
   @action
-  mockIndeterminateState(checkbox) {
+  mockIndeterminateState(checkbox: HTMLInputElement) {
     checkbox.indeterminate = true;
   }
 
@@ -139,18 +195,24 @@ export default class PageComponentsAdvancedTableController extends Controller {
   }
 
   @action
-  toggleMultiSelectToggleScope__demo1(event) {
-    this.multiSelectToggleScope__demo1 = event.target.checked;
+  toggleMultiSelectToggleScope__demo1(event: Event) {
+    this.multiSelectToggleScope__demo1 = (
+      event.target as HTMLInputElement
+    ).checked;
   }
 
   @action
-  toggleMultiSelectToggleDebug__demo1(event) {
-    this.multiSelectToggleDebug__demo1 = event.target.checked;
+  toggleMultiSelectToggleDebug__demo1(event: Event) {
+    this.multiSelectToggleDebug__demo1 = (
+      event.target as HTMLInputElement
+    ).checked;
   }
 
   @action
-  onChangeMultiSelectFilter__demo1(event) {
-    this.multiSelectFilterRows__demo1 = event.target.value;
+  onChangeMultiSelectFilter__demo1(event: Event) {
+    this.multiSelectFilterRows__demo1 = (
+      event.target as HTMLInputElement
+    ).value;
   }
 
   @action
@@ -158,9 +220,17 @@ export default class PageComponentsAdvancedTableController extends Controller {
     selectionKey,
     selectionCheckboxElement,
     selectableRowsStates,
-  }) {
-    console.log(...arguments);
-    if (selectionKey === 'all' && this.multiSelectToggleScope__demo1) {
+  }: HdsAdvancedTableOnSelectionChangeSignature) {
+    console.group('onSelectionChangeWithModel__demo1');
+    console.log('Selection Key:', selectionKey);
+    console.log('Checkbox Element:', selectionCheckboxElement);
+    console.log('Selectable Rows States:', selectableRowsStates);
+    console.groupEnd();
+    if (
+      selectionKey === 'all' &&
+      this.multiSelectToggleScope__demo1 &&
+      selectionCheckboxElement
+    ) {
       updateModelWithSelectAllState(
         this.multiSelectModelData__demo1,
         selectionCheckboxElement.checked,
@@ -173,38 +243,21 @@ export default class PageComponentsAdvancedTableController extends Controller {
     }
   }
 
-  @action
-  onSelectionChangeWithoutModel__demo1({
-    selectionKey,
-    selectionCheckboxElement,
-    selectableRowsStates,
-  }) {
-    console.log(...arguments);
-    // notice: the shape of the "model" is slightly different, it's not an array of objects but an object with keys so
-    // we can't use the `updateModelWithSelectAllsState` and `updateModelWithSelectableRowsStates` functions
-    if (selectionKey === 'all' && this.multiSelectToggleScope__demo1) {
-      const selectAllState = selectionCheckboxElement.checked;
-      Object.keys(this.multiSelectNoModelState__demo1).forEach((rowKey) => {
-        this.multiSelectNoModelState__demo1[rowKey] = selectAllState;
-      });
-    } else {
-      selectableRowsStates.forEach((row) => {
-        this.multiSelectNoModelState__demo1[row.selectionKey] = row.isSelected;
-      });
-    }
-  }
-
   // MULTI-SELECT DEMO #2
   // Multi-select table with pagination
 
   @action
-  toggleMultiSelectPaginatedToggleScope__demo2(event) {
-    this.multiSelectToggleScope__demo2 = event.target.checked;
+  toggleMultiSelectPaginatedToggleScope__demo2(event: Event) {
+    this.multiSelectToggleScope__demo2 = (
+      event.target as HTMLInputElement
+    ).checked;
   }
 
   @action
-  toggleMultiSelectPaginatedToggleDebug__demo2(event) {
-    this.multiSelectToggleDebug__demo2 = event.target.checked;
+  toggleMultiSelectPaginatedToggleDebug__demo2(event: Event) {
+    this.multiSelectToggleDebug__demo2 = (
+      event.target as HTMLInputElement
+    ).checked;
   }
 
   get multiSelectPaginatedTotalItems_demo2() {
@@ -222,12 +275,12 @@ export default class PageComponentsAdvancedTableController extends Controller {
   }
 
   @action
-  onMultiSelectPaginatedPageChange_demo2(page) {
+  onMultiSelectPaginatedPageChange_demo2(page: number) {
     this.multiSelectPaginatedCurrentPage_demo2 = page;
   }
 
   @action
-  onMultiSelectPaginatedPageSizeChange_demo2(pageSize) {
+  onMultiSelectPaginatedPageSizeChange_demo2(pageSize: number) {
     // we agreed to reset the pagination to the first element (any alternative would result in an unpredictable UX)
     this.multiSelectPaginatedCurrentPage_demo2 = 1;
     this.multiSelectPaginatedCurrentPageSize_demo2 = pageSize;
@@ -238,12 +291,16 @@ export default class PageComponentsAdvancedTableController extends Controller {
     selectionKey,
     selectionCheckboxElement,
     selectableRowsStates,
-  }) {
-    console.log(...arguments);
+  }: HdsAdvancedTableOnSelectionChangeSignature) {
+    console.group('onMultiSelectPaginatedSelectionChange__demo2');
+    console.log('Selection Key:', selectionKey);
+    console.log('Checkbox Element:', selectionCheckboxElement);
+    console.log('Selectable Rows States:', selectableRowsStates);
+    console.groupEnd();
     if (selectionKey === 'all' && this.multiSelectToggleScope__demo2) {
       updateModelWithSelectAllState(
         this.multiSelectModelData__demo2,
-        selectionCheckboxElement.checked,
+        selectionCheckboxElement ? selectionCheckboxElement.checked : false,
       );
     } else {
       updateModelWithSelectableRowsStates(
@@ -257,13 +314,17 @@ export default class PageComponentsAdvancedTableController extends Controller {
   // Delete selected rows
 
   @action
-  toggleMultiSelectPaginatedToggleScope__demo3(event) {
-    this.multiSelectToggleScope__demo3 = event.target.checked;
+  toggleMultiSelectPaginatedToggleScope__demo3(event: Event) {
+    this.multiSelectToggleScope__demo3 = (
+      event.target as HTMLInputElement
+    ).checked;
   }
 
   @action
-  toggleMultiSelectPaginatedToggleDebug__demo3(event) {
-    this.multiSelectToggleDebug__demo3 = event.target.checked;
+  toggleMultiSelectPaginatedToggleDebug__demo3(event: Event) {
+    this.multiSelectToggleDebug__demo3 = (
+      event.target as HTMLInputElement
+    ).checked;
   }
 
   get multiSelectUsersTotalItems_demo3() {
@@ -281,12 +342,12 @@ export default class PageComponentsAdvancedTableController extends Controller {
   }
 
   @action
-  onMultiSelectUsersPageChange_demo3(page) {
+  onMultiSelectUsersPageChange_demo3(page: number) {
     this.multiSelectUsersCurrentPage_demo3 = page;
   }
 
   @action
-  onMultiSelectUsersPageSizeChange_demo3(pageSize) {
+  onMultiSelectUsersPageSizeChange_demo3(pageSize: number) {
     // we agreed to reset the pagination to the first element (any alternative would result in an unpredictable UX)
     this.multiSelectUsersCurrentPage_demo3 = 1;
     this.multiSelectUsersCurrentPageSize_demo3 = pageSize;
@@ -297,17 +358,21 @@ export default class PageComponentsAdvancedTableController extends Controller {
     selectionKey,
     selectionCheckboxElement,
     selectableRowsStates,
-  }) {
-    console.log(...arguments);
+  }: HdsAdvancedTableOnSelectionChangeSignature) {
+    console.group('onMultiSelectUsersSelectionChange__demo3');
+    console.log('Selection Key:', selectionKey);
+    console.log('Checkbox Element:', selectionCheckboxElement);
+    console.log('Selectable Rows States:', selectableRowsStates);
+    console.groupEnd();
     if (selectionKey === 'all' && this.multiSelectToggleScope__demo3) {
       updateModelWithSelectAllState(
         this.multiSelectModelData__demo3,
-        selectionCheckboxElement.checked,
+        selectionCheckboxElement ? selectionCheckboxElement.checked : false,
       );
     } else {
       selectableRowsStates.forEach((row) => {
         const recordToUpdate = this.multiSelectModelData__demo3.find(
-          (modelRow) => modelRow.id === row.selectionKey,
+          (modelRow) => String(modelRow.id) === row.selectionKey,
         );
         if (recordToUpdate) {
           recordToUpdate.isSelected = row.isSelected;
@@ -328,18 +393,23 @@ export default class PageComponentsAdvancedTableController extends Controller {
   // Execute action on selected rows
 
   @action
-  onMultiSelectSelectionChange__demo4({ selectedRowsKeys }) {
-    console.log(...arguments);
+  onMultiSelectSelectionChange__demo4({
+    selectedRowsKeys,
+  }: HdsAdvancedTableOnSelectionChangeSignature) {
+    console.group('onMultiSelectSelectionChange__demo4');
+    console.log('Selected Rows Keys:', selectedRowsKeys);
+    console.groupEnd();
     this.multiSelectUserData__demo4.forEach((user) => {
-      user.isSelected = selectedRowsKeys.includes(user.id);
+      user.isSelected = selectedRowsKeys.includes(String(user.id));
     });
   }
 
   @action
   multiSelectAnimateSelectedUsers_demo4() {
     this.multiSelectUserData__demo4.forEach((user) => {
-      user.isAnimated = user.isSelected;
+      user.isAnimated = user.isSelected ? user.isSelected : false;
     });
+
     // eslint-disable-next-line ember/no-runloop
     later(() => {
       this.multiSelectResetUserAnimation_demo4();
@@ -390,11 +460,6 @@ export default class PageComponentsAdvancedTableController extends Controller {
       };
     },
   );
-
-  thResizeHandleColumn = {
-    pxWidth: 200,
-    setPxWidth: this.noop,
-  };
 
   @action
   noop() {
