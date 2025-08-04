200f5752ddf06a686fe3935d3042da526018f765
Lee White <lee.white@hashicorp.com>
2025-08-04 09:30:31 -0400

    Showcase: convert RichTooltip route to TS (#3070)

--- showcase/app/controllers/page-components/rich-tooltip.ts
+++ showcase/app/controllers/page-components/rich-tooltip.ts
@@ -7,7 +7,11 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
+import type { PageComponentsRichTooltipModel } from 'showcase/routes/page-components/rich-tooltip';
+
 export default class PageComponentsRichTooltipController extends Controller {
+  declare model: PageComponentsRichTooltipModel;
+
   @tracked isModalActive = false;
 
   @action
