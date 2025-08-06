88dda279f027a3ad3ab2bf6df9530d06ec106bce
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-08-06 16:13:11 -0700

    HDS-5289 Add basic example donut chart using Carbon Charts DonutChart

---
+++ packages/components/src/components/hds/charts/donut/options.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Donut (centered)',
+  resizable: true,
+  legend: {
+    position: 'right',
+    truncation: {
+      type: 'none',
+    },
+  },
+  donut: {
+    center: {
+      label: 'Browsers',
+    },
+    alignment: 'center',
+  },
+  height: '400px',
+};
