c9f52705468593726281965c8d4a0963b084c9c5
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-08-08 14:27:47 -0700

    HDS-5289 Add colorMap option to Donut, pass in custom colors for services instances chart example

--- packages/components/src/components/hds/charts/donut/index.ts
+++ packages/components/src/components/hds/charts/donut/index.ts
@@ -8,32 +8,33 @@ import { action } from '@ember/object';
 
 import { DonutChart } from '@carbon/charts';
 import options from './options.js';
-import data from './data.js';
 import '@carbon/charts/styles.css';
-
 export interface HdsChartsDonutSignature {
   Args: {
     title?: string;
-    data?: Array<{ group: string; value: number }>;
+    data: Array<{ group: string; value: number }>;
+    colorMap?: { [key: string]: string }; // Optional custom colors for the chart segments/slices
   };
   Blocks: {
     default: [];
   };
   Element: HTMLDivElement;
 }
-// More info on types and signatures: https://hashicorp.atlassian.net/wiki/spaces/HDS/pages/3245932580/Using+Typescript
 
 export default class HdsChartsDonut extends Component<HdsChartsDonutSignature> {
   chart: DonutChart | null = null;
 
   @action
   setupChart(element: HTMLDivElement): void {
-    const chartData = this.args.data || data;
+    const chartData = this.args.data;
 
     // Merge the dynamic title into the options
     const chartOptions = {
       ...options,
-      title: this.args.title || options.title, // Use the passed title or fallback to the default
+      title: this.args.title || options.title,
+      color: {
+        scale: this.args.colorMap,
+      },
     };
 
     this.chart = new DonutChart(element, {
