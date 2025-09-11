2d7ba538dd2b582dbbf6580e5cdaf02f02bc96a6
Cristiano Rastelli <public@didoo.net>
2025-09-11 22:34:35 +0100

    converted `page-components/icon` to gts

--- showcase/app/routes/page-components/icon.ts
+++ showcase/app/routes/page-components/icon.ts
@@ -5,16 +5,8 @@
 
 import Route from '@ember/routing/route';
 
-import { COLORS } from '@hashicorp/design-system-components/components/hds/icon/index';
-
 import type { ModelFrom } from 'showcase/utils/ModelFromRoute';
 
 export type PageComponentsIconModel = ModelFrom<PageComponentsIconRoute>;
 
-export default class PageComponentsIconRoute extends Route {
-  model() {
-    return {
-      COLORS,
-    };
-  }
-}
+export default class PageComponentsIconRoute extends Route {}
