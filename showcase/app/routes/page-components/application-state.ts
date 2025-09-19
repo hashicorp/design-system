263bb08a49c562fddfbdb67501d8d8457d40d603
Zack Moore <zack.moore@hashicorp.com>
2025-09-19 12:36:18 -0400

    removed route/controller code

--- showcase/app/routes/page-components/application-state.ts
+++ showcase/app/routes/page-components/application-state.ts
@@ -5,21 +5,4 @@
 
 import Route from '@ember/routing/route';
 
-import { ALIGNS } from '@hashicorp/design-system-components/components/hds/application-state/index';
-
-import { HdsCardLevelValues as CARD_LEVELS } from '@hashicorp/design-system-components/components/hds/card/types';
-
-import type { ModelFrom } from 'showcase/utils/ModelFromRoute';
-
-export type PageComponentsApplicationStateModel =
-  ModelFrom<PageComponentsApplicationStateRoute>;
-
-export default class PageComponentsApplicationStateRoute extends Route {
-  model() {
-    return {
-      ALIGNS,
-      // TODO - will be fixed by https://hashicorp.atlassian.net/browse/HDS-5154
-      CARD_LEVELS,
-    };
-  }
-}
+export default class PageComponentsApplicationStateRoute extends Route {}
