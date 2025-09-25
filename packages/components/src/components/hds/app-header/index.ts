5f404479d887066b7aa294af743aa33940a9ce55
shleewhite <lee.white@hahicorp.com>
2025-09-25 17:52:34 -0400

    fix: mostly right now

--- packages/components/src/components/hds/app-header/index.ts
+++ packages/components/src/components/hds/app-header/index.ts
@@ -9,7 +9,7 @@ import { guidFor } from '@ember/object/internals';
 import { tracked } from '@glimmer/tracking';
 import { registerDestructor } from '@ember/destroyable';
 import type Owner from '@ember/owner';
-import type {NavigationNarratorSignature} from 'ember-a11y-refocus/components/navigation-narrator';
+import type { NavigationNarratorSignature } from 'ember-a11y-refocus/components/navigation-narrator';
 
 import { hdsBreakpoints } from '../../../utils/hds-breakpoints.ts';
 
