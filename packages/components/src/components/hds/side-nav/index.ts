7866af94ce79ca3db3624d7817ca2bd0d94d3d79
shleewhite <lee.white@hahicorp.com>
2025-09-25 12:27:45 -0400

    chore: upgrade glimmer/component for types to work properly

--- packages/components/src/components/hds/side-nav/index.ts
+++ packages/components/src/components/hds/side-nav/index.ts
@@ -11,6 +11,7 @@ import { registerDestructor } from '@ember/destroyable';
 
 import type { HdsSideNavBaseSignature } from './base';
 import type Owner from '@ember/owner';
+import type {NavigationNarratorSignature} from 'ember-a11y-refocus/components/navigation-narrator';
 
 export interface HdsSideNavSignature {
   Args: {
@@ -21,7 +22,7 @@ export interface HdsSideNavSignature {
     a11yRefocusSkipTo?: string;
     a11yRefocusSkipText?: string;
     a11yRefocusNavigationText?: string;
-    a11yRefocusRouteChangeValidator?: string;
+    a11yRefocusRouteChangeValidator?: NavigationNarratorSignature['Args']['routeChangeValidator'];
     a11yRefocusExcludeAllQueryParams?: boolean;
     /**
      * @deprecated The `@ariaLabel` argument for "Hds::SideNav" has been deprecated. It is replaced by aria-labelledby and aria-expanded on the toggle button
