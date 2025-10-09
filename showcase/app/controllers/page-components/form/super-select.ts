4dc4a73d4fae955b940bfde0a49a7a1eef3affef
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-10-09 11:59:37 -0700

    HDS-5037 Add test code to attempt to scope SuperSelect escape event

--- showcase/app/controllers/page-components/form/super-select.ts
+++ showcase/app/controllers/page-components/form/super-select.ts
@@ -5,11 +5,15 @@
 
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
+import type { Select } from 'ember-power-select/components/power-select';
 import type { PageComponentsFormSuperSelectModel } from 'showcase/routes/page-components/form/super-select';
 
 export default class PageComponentsFormSuperSelectController extends Controller {
   declare model: PageComponentsFormSuperSelectModel;
+
+  @tracked basicModalActive = false;
 
   @action
   resultCountMessage(resultCount: number) {
@@ -18,4 +22,23 @@ export default class PageComponentsFormSuperSelectController extends Controller 
 
   @action
   noop() {}
+
+  @action
+  handleKeydown(select: Select, e: KeyboardEvent): undefined {
+    // console.log('handleKeyDown activated - ', e.key);
+    if (e.key === 'Escape') {
+      // console.log('ESCAPE KEY pressed');
+      e.stopImmediatePropagation();
+    }
+  }
+
+  @action
+  activateModal(): void {
+    this.basicModalActive = true;
+  }
+
+  @action
+  deactivateModal(): void {
+    this.basicModalActive = false;
+  }
 }
