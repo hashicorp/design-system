0fa6253656a70a32f8d4d014945846c4cdaeb543
Lee White <lee.white@hashicorp.com>
2025-10-14 13:27:06 -0400

    Showcase: convert SideNav page to gts (#3301)

--- showcase/app/routes/page-components/side-nav.ts
+++ showcase/app/routes/page-components/side-nav.ts
@@ -5,30 +5,8 @@
 
 import Route from '@ember/routing/route';
 
-import { COLORS as DROPDOWN_TOGGLE_BUTTON_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/toggle/button';
-import { COLORS as DROPDOWN_ITEM_INTERACTIVE_COLORS } from '@hashicorp/design-system-components/components/hds/dropdown/list-item/interactive';
-
 import type { ModelFrom } from 'showcase/utils/model-from-route';
 
 export type PageComponentsSideNavModel = ModelFrom<PageComponentsSideNavRoute>;
 
-export default class PageComponentsSideNavRoute extends Route {
-  model() {
-    // these are used only for presentation purpose in the showcase
-    const DROPDOWN_TOGGLE_STATES = [
-      'default',
-      'hover',
-      'active',
-      'focus',
-      'disabled',
-    ];
-    const DROPDOWN_ITEM_STATES = ['default', 'hover', 'active', 'focus'];
-
-    return {
-      DROPDOWN_TOGGLE_BUTTON_COLORS,
-      DROPDOWN_TOGGLE_STATES,
-      DROPDOWN_ITEM_INTERACTIVE_COLORS,
-      DROPDOWN_ITEM_STATES,
-    };
-  }
-}
+export default class PageComponentsSideNavRoute extends Route {}
