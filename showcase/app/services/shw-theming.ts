ba4157a2bc34997d5dd4d433caf88547ad18283e
Cristiano Rastelli <public@didoo.net>
2025-10-15 15:32:24 +0100

    moved theming logic from `ShwThemeSwitcher` component/subcomponents to `hdsTheming` service

--- showcase/app/services/shw-theming.ts
+++ showcase/app/services/shw-theming.ts
@@ -1,3 +1,5 @@
+import { tracked } from '@glimmer/tracking';
+
 import HdsThemingService from '@hashicorp/design-system-components/services/hds-theming';
 import type {
   OnSetThemeCallback,
@@ -5,7 +7,47 @@ import type {
   HdsModes,
 } from '@hashicorp/design-system-components/services/hds-theming';
 
+import config from 'showcase/config/environment';
+
+const updatePageStylesheet = (currentStylesheet: string) => {
+  let newStylesheet;
+  switch (currentStylesheet) {
+    case 'prefers-color-scheme':
+      // themed CSS where theming is applied via `@media(prefers-color-scheme)`
+      newStylesheet =
+        'assets/styles/@hashicorp/design-system-components-theming-with-prefers-color-scheme.css';
+      break;
+    case 'css-selectors':
+      // themed CSS where theming is applied via CSS selectors
+      newStylesheet =
+        'assets/styles/@hashicorp/design-system-components-theming-with-css-selectors.css';
+      break;
+    case 'combined-strategies':
+      // this is used for local testing purposes
+      newStylesheet =
+        'assets/styles/@hashicorp/design-system-components-theming-with-combined-strategies.css';
+      break;
+    default:
+      // this is the standard CSS for HDS components, without any theming
+      newStylesheet = 'assets/styles/@hashicorp/design-system-components.css';
+      break;
+  }
+
+  // re-assign the stylesheet `href` attribute
+  const hdsComponentsStylesheet = document.getElementById(
+    'hds-components-stylesheet',
+  );
+  if (hdsComponentsStylesheet) {
+    hdsComponentsStylesheet.setAttribute(
+      'href',
+      `${config.rootURL}${newStylesheet}`,
+    );
+  }
+};
+
 export default class ShwThemingService extends HdsThemingService {
+  @tracked currentStylesheet: string = 'standard';
+
   globalOnSetTheme: OnSetThemeCallback = ({
     currentTheme,
     currentMode,
@@ -14,5 +56,17 @@ export default class ShwThemingService extends HdsThemingService {
     currentMode: HdsModes;
   }) => {
     console.log('EXTENDED SERVICE', currentTheme, currentMode);
+    // update the page's stylesheet
+    updatePageStylesheet(this.currentStylesheet);
   };
+
+  setCurrentStylesheet(
+    currentStylesheet:
+      | 'standard'
+      | 'prefers-color-scheme'
+      | 'css-selectors'
+      | 'combined-strategies',
+  ): void {
+    this.currentStylesheet = currentStylesheet;
+  }
 }
