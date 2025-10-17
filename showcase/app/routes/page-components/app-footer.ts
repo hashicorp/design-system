107b1d454a0c997eb8b3a8c015b9ed2e3d9ca59a
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-10-17 13:50:49 -0700

    HDS-5326 Fix imports, move data and functions into GTS file

--- showcase/app/routes/page-components/app-footer.ts
+++ showcase/app/routes/page-components/app-footer.ts
@@ -5,18 +5,9 @@
 
 import Route from '@ember/routing/route';
 
-import { STATUSES as STATUS_LINK_STATUSES } from '@hashicorp/design-system-components/components/hds/app-footer/status-link';
-
 import type { ModelFrom } from 'showcase/utils/model-from-route';
 
 export type PageComponentsAppFooterModel =
   ModelFrom<PageComponentsAppFooterRoute>;
 
-export default class PageComponentsAppFooterRoute extends Route {
-  model() {
-    // these are used only for presentation purpose in the showcase
-    const LINK_STATES = ['default', 'hover', 'active', 'focus'];
-
-    return { LINK_STATES, STATUS_LINK_STATUSES };
-  }
-}
+export default class PageComponentsAppFooterRoute extends Route {}
