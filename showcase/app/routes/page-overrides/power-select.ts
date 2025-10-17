442f626141c7af468697393ba0f09812590ce83b
Lee White <lee.white@hashicorp.com>
2025-10-17 13:20:16 -0400

    Showcase: convert PowerSelect page to gts (#3307)

--- showcase/app/routes/page-overrides/power-select.ts
+++ showcase/app/routes/page-overrides/power-select.ts
@@ -10,22 +10,4 @@ import type { ModelFrom } from 'showcase/utils/model-from-route';
 export type PageOverridesPowerSelectModel =
   ModelFrom<PageOverridesPowerSelectRoute>;
 
-export default class PageOverridesPowerSelectRoute extends Route {
-  model() {
-    // these are used only for presentation purpose in the showcase
-    const OPTIONS: string[] = [
-      'Oregon (us-west-2)',
-      'N. Virginia (us-east-1)',
-      'Ireland (eu-west-1)',
-      'London (eu-west-2)',
-      'Frankfurt (eu-central-1)',
-    ];
-    const SELECTED: string[] = ['Oregon (us-west-2)'];
-    const SELECTEDMULTIPLE: string[] = [
-      'Oregon (us-west-2)',
-      'N. Virginia (us-east-1)',
-      'Ireland (eu-west-1)',
-    ];
-    return { OPTIONS, SELECTED, SELECTEDMULTIPLE };
-  }
-}
+export default class PageOverridesPowerSelectRoute extends Route {}
---
+++ showcase/app/templates/page-overrides/power-select.gts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+import type { TemplateOnlyComponent } from '@ember/component/template-only';
+
+import PowerSelectIndex from 'showcase/components/page-overrides/power-select/index';
+
+const PageOverridesPowerSelect: TemplateOnlyComponent = <template>
+  <PowerSelectIndex />
+</template>;
+
+export default PageOverridesPowerSelect;
