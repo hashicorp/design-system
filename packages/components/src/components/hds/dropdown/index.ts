5dd922d9dba74c637d3a72371af8af155bd744b7
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-10-30 13:14:46 -0400

    Fix: Revert search and dismiss features in dropdown

--- packages/components/src/components/hds/dropdown/index.ts
+++ packages/components/src/components/hds/dropdown/index.ts
@@ -6,7 +6,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { assert } from '@ember/debug';
-import { modifier } from 'ember-modifier';
 
 import {
   // map Dropdown's `listPosition` values to PopoverPrimitive's `placement` values
@@ -50,10 +49,6 @@ export interface HdsDropdownSignature {
     preserveContentInDom?: boolean;
     matchToggleWidth?: boolean;
     onClose?: HdsPopoverPrimitiveSignature['Args']['onClose'];
-    searchEnabled?: boolean;
-    searchPlaceholder?: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    onDismiss?: (event: MouseEvent, ...args: any[]) => void;
   };
   Blocks: {
     default: [
@@ -79,14 +74,6 @@ export interface HdsDropdownSignature {
 }
 
 export default class HdsDropdown extends Component<HdsDropdownSignature> {
-  private _element!: HTMLDivElement;
-
-  private _setUpDropdown = modifier((element: HTMLDivElement) => {
-    this._element = element;
-
-    return () => {};
-  });
-
   /**
    * @param listPosition
    * @type {string}
@@ -130,17 +117,6 @@ export default class HdsDropdown extends Component<HdsDropdownSignature> {
     };
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  get onDismiss(): ((event: MouseEvent, ...args: any[]) => void) | false {
-    const { onDismiss } = this.args;
-
-    if (typeof onDismiss === 'function') {
-      return onDismiss;
-    } else {
-      return false;
-    }
-  }
-
   /**
    * Get the class names to apply to the element
    * @method classNames
@@ -152,10 +128,6 @@ export default class HdsDropdown extends Component<HdsDropdownSignature> {
     // add a class based on the @isInline argument
     if (this.args.isInline) {
       classes.push('hds-dropdown--is-inline');
-    }
-
-    if (this.args.onDismiss) {
-      classes.push('hds-dropdown--has-dismiss');
     }
 
     return classes.join(' ');
@@ -198,20 +170,4 @@ export default class HdsDropdown extends Component<HdsDropdownSignature> {
       }
     }
   }
-
-  onSearch = (event: Event) => {
-    const listItems = this._element.querySelectorAll('.hds-dropdown-list-item') as NodeListOf<HTMLLIElement>;
-    const input = event.target as HTMLInputElement;
-    listItems.forEach((item) => {
-      if (item.textContent) {
-        const text = item.textContent.toLowerCase();
-        const searchText = input.value.toLowerCase();
-        if (text.includes(searchText)) {
-          item.style.display = '';
-        } else {
-          item.style.display = 'none';
-        }
-      }
-    });
-  };
 }
