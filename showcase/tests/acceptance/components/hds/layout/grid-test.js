72d4109f2b93b7048ef362a26a6b7bbddb5f2e55
Alexey Kulakov <uni_que@me.com>
2025-11-03 19:58:35 -0800

    fix(showcase): layout grid acceptance test

---
+++ showcase/tests/acceptance/components/hds/layout/grid-test.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'showcase/tests/helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Acceptance | layouts/grid', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('Layout/grid page passes automated a11y checks', async function (assert) {
+    let axeOptions = {
+      rules: {
+        'color-contrast': {
+          enabled: false,
+          selectors: [['.shw-placeholder']],
+        },
+      },
+    };
+
+    await visit('/layouts/grid');
+
+    await a11yAudit(axeOptions);
+
+    assert.ok(true, 'a11y automation audit passed');
+  });
+});
