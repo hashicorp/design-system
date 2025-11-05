99445eb5ee3a3c903ad27accb9c8faf8fe516f02
Alexey Kulakov <uni_que@me.com>
2025-11-04 16:17:17 -0800

    feat(showcase): vite

--- showcase/tests/test-helper.ts
+++ showcase/tests/test-helper.ts
@@ -8,8 +8,7 @@ import config from 'showcase/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { loadTests } from 'ember-qunit/test-loader';
-import { start, setupEmberOnerrorValidation } from 'ember-qunit';
+import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
 import {
   DEFAULT_A11Y_TEST_HELPER_NAMES,
   setRunOptions,
@@ -20,50 +19,52 @@ import {
   setupMiddlewareReporter,
 } from 'ember-a11y-testing/test-support';
 
-setApplication(Application.create(config.APP));
+export function start() {
+  setApplication(Application.create(config.APP));
 
-setupGlobalA11yHooks(() => true, {
-  helpers: [
-    ...DEFAULT_A11Y_TEST_HELPER_NAMES,
-    'render',
-    'tab',
-    'focus',
-    'select',
-  ],
-});
-
-setRunOptions({
-  runOnly: {
-    type: 'tag',
-    values: [
-      'wcag2a',
-      'wcag2aa',
-      'wcag21a',
-      'wcag21aa',
-      'wcag22aa',
-      'best-practice',
+  setupGlobalA11yHooks(() => true, {
+    helpers: [
+      ...DEFAULT_A11Y_TEST_HELPER_NAMES,
+      'render',
+      'tab',
+      'focus',
+      'select',
     ],
-  },
-  // @ts-expect-error: This is a valid option
-  include: [['#ember-testing-container']],
-  exclude: [['.flight-sprite-container'], ['.shw-page-main']],
-});
+  });
 
-// This will be used by developers to run the tests locally
-// Either with the `enableA11yAudit` as a query param in the URL
-// or `pnpm test:a11y` in the CLI
-// Note: if you want to filter what test is run from the start, use the --filter flag: `pnpm test:a11y --filter="alert"`
-// Docs: https://guides.emberjs.com/release/testing/#toc_how-to-filter-tests
-if (shouldForceAudit()) {
-  setEnableA11yAudit(true);
-}
-// Note, as a convenience, useMiddlewareReporter automatically forces audits, thus explicitly specifying the enableA11yAudit query param or the ENABLE_A11Y_AUDIT environment variable is unnecessary.
-if (useMiddlewareReporter()) {
-  // Only runs if `enableA11yMiddlewareReporter` is set in URL
-  setupMiddlewareReporter();
-}
+  setRunOptions({
+    runOnly: {
+      type: 'tag',
+      values: [
+        'wcag2a',
+        'wcag2aa',
+        'wcag21a',
+        'wcag21aa',
+        'wcag22aa',
+        'best-practice',
+      ],
+    },
+    // @ts-expect-error: This is a valid option
+    include: [['#ember-testing-container']],
+    exclude: [['.flight-sprite-container'], ['.shw-page-main']],
+  });
 
-setup(QUnit.assert);
-setupEmberOnerrorValidation();
-loadTests();
-start();
+  // This will be used by developers to run the tests locally
+  // Either with the `enableA11yAudit` as a query param in the URL
+  // or `pnpm test:a11y` in the CLI
+  // Note: if you want to filter what test is run from the start, use the --filter flag: `pnpm test:a11y --filter="alert"`
+  // Docs: https://guides.emberjs.com/release/testing/#toc_how-to-filter-tests
+  if (shouldForceAudit()) {
+    setEnableA11yAudit(true);
+  }
+  // Note, as a convenience, useMiddlewareReporter automatically forces audits, thus explicitly specifying the enableA11yAudit query param or the ENABLE_A11Y_AUDIT environment variable is unnecessary.
+  if (useMiddlewareReporter()) {
+    // Only runs if `enableA11yMiddlewareReporter` is set in URL
+    setupMiddlewareReporter();
+  }
+
+  setup(QUnit.assert);
+
+  setupEmberOnerrorValidation();
+  qunitStart();
+}
--- website/app/services/head-data.js
+++ website/app/services/head-data.js
@@ -7,8 +7,6 @@ import HeadDataService from 'ember-meta/services/head-data';
 import { service } from '@ember/service';
 import config from 'website/config/environment';
 
-import config from 'website/config/environment';
-
 export default class CustomHeadDataService extends HeadDataService {
   @service router;
 
