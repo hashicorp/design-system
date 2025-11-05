a8229dc6adc2b88a2f92c1495caaa58fcadd6d8b
Alexey Kulakov <uni_que@me.com>
2025-11-04 16:17:17 -0800

    feat(showcase): vite

--- showcase/types/global.d.ts
+++ showcase/types/global.d.ts
@@ -2,6 +2,7 @@
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
  */
-
+/// <reference types="@embroider/core/virtual" />
+/// <reference types="vite/client" />
 import '@glint/environment-ember-loose';
 import '@glint/environment-ember-template-imports';
