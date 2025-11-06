82bb5b6ee0244ecfdc5c3e799939b742caff9667
Kristin Bradley <kristin.bradley@hashicorp.com>
2025-11-06 08:33:09 -0800

    `hds-link-to-query` helper - delete comment (HDS-5534) (#3354)

--- packages/components/src/helpers/hds-link-to-query.ts
+++ packages/components/src/helpers/hds-link-to-query.ts
@@ -17,7 +17,6 @@ import { helper } from '@ember/component/helper';
  */
 
 // this is a workaround for https://github.com/emberjs/ember.js/issues/19693
-// don't remove until we drop support for ember 3.27 and 3.28
 
 export function hdsLinkToQuery([query]: [
   Record<string, unknown> | undefined,
