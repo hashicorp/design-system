908f4636813ebeeda1f64f4a79f6eb68b8b3e299
Cristiano Rastelli <public@didoo.net>
2025-11-13 11:43:22 +0000

    implemented `ThemeContex` component

---
+++ packages/components/src/components/hds/theme-context/index.ts
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
+import type Owner from '@ember/owner';
+
+import type { HdsThemes } from './types.ts';
+import { HdsThemeValues } from './types.ts';
+
+export interface HdsThemeContextSignature {
+  Args: {
+    theme: HdsThemes;
+  };
+  Blocks: {
+    default: [];
+  };
+  Element: HTMLElement;
+}
+
+export const THEMES: HdsThemes[] = Object.values(HdsThemeValues);
+
+export default class HdsThemeContext extends Component<HdsThemeContextSignature> {
+  constructor(owner: Owner, args: HdsThemeContextSignature['Args']) {
+    super(owner, args);
+
+    const { theme } = args;
+
+    assert(
+      `@theme for "Hds::ThemeContext" must be one of the following: ${THEMES.join(
+        ', '
+      )}; received: ${theme}`,
+      THEMES.includes(theme)
+    );
+  }
+
+  // Get the class names to apply to the component.
+  get classNames(): string {
+    const classes = ['hds-theme-context'];
+
+    // add "theme" classes based on the @theme arguments
+    classes.push(`hds-theme-${this.args.theme}`);
+
+    return classes.join(' ');
+  }
+}
---
+++ packages/components/src/components/hds/theme-context/types.ts
@@ -0,0 +1,7 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+export type { HdsThemes } from '../../../services/hds-theming';
+export { HdsThemeValues } from '../../../services/hds-theming.ts';
