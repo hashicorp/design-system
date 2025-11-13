fa6e5ce502e76007dc01900a9ddb9098de6aeaaa
Melanie Sumner <melaniersumner@gmail.com>
2025-11-12 20:23:42 -0600

    Adding a11y audit tests back in to find out why skipped

--- website/tests/acceptance/components/tabs-test.js
+++ website/tests/acceptance/components/tabs-test.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { module, test, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'website/tests/helpers';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
@@ -17,7 +17,7 @@ module('Acceptance | components/tabs', function (hooks) {
     assert.strictEqual(currentURL(), '/components/tabs');
   });
 
-  skip('components/tabs passes a11y automated checks', async function (assert) {
+  test('components/tabs passes a11y automated checks', async function (assert) {
     await visit('/components/tabs');
     await a11yAudit();
     assert.ok(true, 'a11y automation audit passed');
