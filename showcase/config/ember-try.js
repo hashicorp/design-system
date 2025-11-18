b7b3490efd13b458939d5816e477a2cfce3b4f25
Melanie Sumner <melaniersumner@gmail.com>
2025-11-18 11:34:49 -0600

    removed unnecessary try scenario

--- showcase/config/ember-try.js
+++ showcase/config/ember-try.js
@@ -22,14 +22,6 @@ module.exports = async function () {
         },
       },
       {
-        name: 'hds-ember',
-        npm: {
-          devDependencies: {
-            'ember-source': '~6.4.0',
-          },
-        },
-      },
-      {
         name: 'ember-release',
         npm: {
           devDependencies: {
