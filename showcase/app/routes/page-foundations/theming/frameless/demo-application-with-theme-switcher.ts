6897ea456e6d07f2e2f4f4040f424f5a4fbfd84a
Cristiano Rastelli <public@didoo.net>
2025-11-21 20:18:44 +0000

    some cleanup before code review

--- showcase/app/routes/page-foundations/theming/frameless/demo-application-with-theme-switcher.ts
+++ showcase/app/routes/page-foundations/theming/frameless/demo-application-with-theme-switcher.ts
@@ -15,6 +15,9 @@ export type PageComponentsFormFramelessDemoFormComplexModel =
 export default class PageComponentsFormFramelessDemoFormComplexRoute extends Route {
   @service declare readonly shwTheming: ShwThemingService;
 
+  // this is not a perfect solution, but for the moment it will do
+  // later we can evaluate if use a different approach
+  // see: https://hashicorp.atlassian.net/browse/HDS-5680
   activate() {
     this.shwTheming.setStylesheet('css-selectors--migration');
   }
