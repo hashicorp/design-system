140c3bc84aedd1cb7ea252adabeb613a42b95d36
Lee White <lee.white@hashicorp.com>
2025-12-08 10:19:01 -0500

    Website: enable breadcrumb, tabs a11y tests (#3421)

--- website/tests/acceptance/components/breadcrumb-test.js
+++ website/tests/acceptance/components/breadcrumb-test.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { module, test, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'website/tests/helpers';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
@@ -17,7 +17,7 @@ module('Acceptance | components/breadcrumb', function (hooks) {
     assert.strictEqual(currentURL(), '/components/breadcrumb');
   });
 
-  skip('components/breadcrumb page passes automated a11y checks', async function (assert) {
+  test('components/breadcrumb page passes automated a11y checks', async function (assert) {
     await visit('/components/breadcrumb');
     await a11yAudit();
     assert.ok(true, 'a11y automation audit passed');
