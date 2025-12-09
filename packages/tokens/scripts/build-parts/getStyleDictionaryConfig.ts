27c664eb795ce500b35df760311404e72059b1a2
Cristiano Rastelli <public@didoo.net>
2025-12-09 19:33:32 +0000

    STARTED EXPLORATION

--- packages/tokens/scripts/build-parts/getStyleDictionaryConfig.ts
+++ packages/tokens/scripts/build-parts/getStyleDictionaryConfig.ts
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import type { Config, DesignToken } from 'style-dictionary/types';
+import type { Config, DesignToken, Dictionary } from 'style-dictionary/types';
+import { outputReferencesFilter, outputReferencesTransformed } from 'style-dictionary/utils';
 
 export const targets = ['products', 'devdot', 'marketing', 'cloud-email'];
 export const modes = ['default', 'cds-g0', 'cds-g10', 'cds-g90', 'cds-g100'];
@@ -26,6 +27,50 @@ const excludePrivateTokens = (token: DesignToken) => {
   return !token.private;
 }
 
+const outputReferencesCustomFunction = (token: DesignToken, options: { dictionary: Dictionary, usesDtcg?: boolean }) => {
+  const isFiltered = outputReferencesFilter(token, options);
+  const isTransformed = outputReferencesTransformed(token, options);
+  // {
+  //   "key": "{base.themed-mixed}",
+  //   "$type": "color",
+  //   "$value": "#000000",
+  //   "group": "palette",
+  //   "$modes": {
+  //     "default": "#123456",
+  //     "cds-g0": "#000000",
+  //     "cds-g10": "#000000",
+  //     "cds-g90": "#000000",
+  //     "cds-g100": "#000000"
+  //   },
+  //   "filePath": "src/test/test.json",
+  //   "isSource": true,
+  //   "original": {
+  //     "$type": "color",
+  //     "$value": "{base.themed}",
+  //     "group": "palette",
+  //     "$modes": {
+  //       "default": "{base.simple}",
+  //       "cds-g0": "{base.themed}",
+  //       "cds-g10": "{base.themed}",
+  //       "cds-g90": "{base.themed}",
+  //       "cds-g100": "{base.themed}"
+  //     },
+  //     "key": "{base.themed-mixed}"
+  //   },
+  //   "name": "token-base-themed-mixed",
+  //   "attributes": {
+  //     "themeable": true,
+  //     "category": "base"
+  //   },
+  //   "path": [
+  //     "base",
+  //     "themed-mixed"
+  //   ]
+  // }
+  console.log(token.name, `isFiltered=${isFiltered}`, `isTransformed=${isTransformed}`);
+  return isFiltered && isTransformed;
+}
+
 export function getStyleDictionaryConfig({ target, mode }: { target: Target, mode?: Mode }): Config {
 
   // -----------------------
@@ -43,9 +88,10 @@ export function getStyleDictionaryConfig({ target, mode }: { target: Target, mod
       return {
         ...baseConfig,
         source: [
-          `src/carbon-extracted/**/*.json`,
-          `src/global/**/*.json`,
-          `src/products/shared/**/*.json`
+          `src/test/**/*.json`,
+          // `src/carbon-extracted/**/*.json`,
+          // `src/global/**/*.json`,
+          // `src/products/shared/**/*.json`
         ],
         platforms: {
           [`web/themed-css-variables--mode-${mode}`]: {
@@ -59,16 +105,16 @@ export function getStyleDictionaryConfig({ target, mode }: { target: Target, mod
                 format: 'css/variables',
                 options: {
                   // TODO understand if is better to output references or not for the "common" definitions (almost certainly no) - see: https://hashicorp.atlassian.net/browse/HDS-5669
-                  outputReferences: false,
+                  // outputReferences: false,
                   // outputReferences: (token, { dictionary, usesDtcg }) => {
                   //   // `dictionary` contains `allTokens`, `tokens`, `tokenMap`, `unfilteredTokens`, `unfilteredAllTokens` and `unfilteredTokenMap` props
                   //   // `usesDtcg` tells you whether the Design Token Community Group spec is used with $ prefixes ($value, $type etc.)
                   //   // return true or false
                   // },
                   // see: https://styledictionary.com/reference/utils/references/#combining-multiple-outputreference-utility-functions
-                  // outputReferences: (token, options) => outputReferencesFilter(token, options) && outputReferencesTransformed(token, options),
-                },
-                filter: (token: DesignToken) => {
+                    outputReferences: outputReferencesCustomFunction,
+                  },
+                  filter: (token: DesignToken) => {
                   return !token.private && !(token.attributes && token.attributes.themeable);
                 },
               },
@@ -77,33 +123,18 @@ export function getStyleDictionaryConfig({ target, mode }: { target: Target, mod
                 format: 'css/variables',
                 options: {
                   // TODO understand if is better to output references or not for the "themed" definitions (almost certainly no) - see: https://hashicorp.atlassian.net/browse/HDS-5669
-                  outputReferences: false,
+                  // outputReferences: false,
                   // outputReferences: (token, { dictionary, usesDtcg }) => {
                   //   // `dictionary` contains `allTokens`, `tokens`, `tokenMap`, `unfilteredTokens`, `unfilteredAllTokens` and `unfilteredTokenMap` props
                   //   // `usesDtcg` tells you whether the Design Token Community Group spec is used with $ prefixes ($value, $type etc.)
                   //   // return true or false
                   // },
                   // see: https://styledictionary.com/reference/utils/references/#combining-multiple-outputreference-utility-functions
-                  // outputReferences: (token, options) => outputReferencesFilter(token, options) && outputReferencesTransformed(token, options),
+                  outputReferences: outputReferencesCustomFunction,
                 },
                 filter: (token: DesignToken) => {
                   return !token.private && (token.attributes && token.attributes.themeable);
                 },
-              }
-            ],
-            // this has been registered in the `build` file
-            preprocessors: [`replace-value-for-mode-${mode}`],
-          },
-          [`docs/themed-json--mode-${mode}`]: {
-            buildPath: 'dist/docs/products/',
-            transformGroup: 'products/web',
-            prefix: 'token',
-            basePxFontSize: 16,
-            files: [
-              {
-                destination: `themed-tokens/${mode}.json`,
-                format: 'docs/json',
-                filter: excludePrivateTokens,
               }
             ],
             // this has been registered in the `build` file
@@ -121,9 +152,10 @@ export function getStyleDictionaryConfig({ target, mode }: { target: Target, mod
       return {
         ...baseConfig,
         source: [
-          `src/carbon-extracted/**/*.json`,
-          `src/global/**/*.json`,
-          `src/products/shared/**/*.json`
+          `src/test/**/*.json`,
+          // `src/carbon-extracted/**/*.json`,
+          // `src/global/**/*.json`,
+          // `src/products/shared/**/*.json`
         ],
         platforms: {
           'web/css-variables': {
@@ -139,133 +171,9 @@ export function getStyleDictionaryConfig({ target, mode }: { target: Target, mod
               }
             ],
             actions: ['generate-css-helpers', 'generate-theming-css-files'],
-          },
-          'docs/json': {
-            buildPath: 'dist/docs/products/',
-            transformGroup: 'products/web',
-            prefix: 'token',
-            basePxFontSize: 16,
-            files: [
-              {
-                destination: 'tokens.json',
-                format: 'docs/json',
-                filter: excludePrivateTokens,
-              }
-            ],
-            actions: ['generate-theming-docs-files'],
           }
         }
       };
     }
   }
-
-  // -----------------------
-  // DEVDOT
-  // -----------------------
-
-  if (target === 'devdot') {
-    return {
-      ...baseConfig,
-      'source': [
-        `src/carbon-extracted/**/*.json`,
-        `src/global/**/*.json`,
-        `src/products/shared/**/*.json`,
-        // custom overrides for 'devdot' tokens
-        `src/devdot/**/*.json`
-      ],
-      'platforms': {
-        'web/css-variables': {
-          buildPath: 'dist/devdot/css/',
-          'transformGroup': 'products/web',
-          prefix: 'token',
-          basePxFontSize: 16,
-          files: [
-            {
-              destination: 'tokens.css',
-              format: 'css/variables',
-              filter: excludePrivateTokens,
-            }
-          ],
-          actions: ['generate-css-helpers'],
-        }
-      }
-    };
-  }
-
-  // -----------------------
-  // MARKETING
-  // -----------------------
-
-  if (target === 'marketing') {
-    return {
-      ...baseConfig,
-      'source': [
-        `src/carbon-extracted/**/*.json`,
-        `src/global/**/*.json`,
-        `src/products/shared/**/*.json`,
-      ],
-      'platforms': {
-        'web/css-variables': {
-          buildPath: 'dist/marketing/css/',
-          'transformGroup': 'marketing/web',
-          prefix: 'token',
-          basePxFontSize: 16,
-          files: [
-            {
-              destination: 'tokens.css',
-              format: 'css/variables',
-              filter: excludePrivateTokens,
-            }
-          ],
-          actions: ['generate-css-helpers'],
-        },
-        'json': {
-          buildPath: 'dist/marketing/',
-          'transformGroup': 'marketing/web',
-          prefix: 'token',
-          basePxFontSize: 16,
-          files: [
-            {
-              destination: 'tokens.json',
-              format: 'json',
-              filter: excludePrivateTokens,
-            }
-          ]
-        }
-      }
-    };
-  }
-
-  // -----------------------
-  // CLOUD-EMAIL
-  // -----------------------
-
-  if (target === 'cloud-email') {
-    // these tokens will be consumed by the email templating system in https://github.com/hashicorp/cloud-email
-    return {
-      ...baseConfig,
-      // we need only foundational tokens (colors, typography, etc)
-      'source': [
-        `src/carbon-extracted/**/*.json`,
-        `src/global/**/*.json`,
-        `src/products/shared/color/**/*.json`,
-        `src/products/shared/typography.json`,
-      ],
-      'platforms': {
-        'email/sass-variables': {
-          buildPath: `dist/cloud-email/`,
-          'transformGroup': 'products/email',
-          prefix: 'token',
-          files: [
-            {
-              destination: 'tokens.scss',
-              format: 'scss/variables',
-              filter: excludePrivateTokens,
-            }
-          ],
-          actions: ['generate-css-helpers'],
-        }
-      }
-    }
-  };
 };
