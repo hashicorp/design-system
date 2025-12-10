1a887f08008d14617dc43020094404432b825c25
shleewhite <lee.white@hahicorp.com>
2025-12-10 18:44:17 -0500

    fix: prettier config

--- packages/components/.template-lintrc.cjs
+++ packages/components/.template-lintrc.cjs
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = {
-  plugins: ['ember-template-lint-plugin-prettier'],
-  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  extends: 'recommended',
   rules: {
     'no-html-comments': false,
     'no-trailing-spaces': true,
