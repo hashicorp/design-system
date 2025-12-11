df3a8d95ed29c96904810314e4e82f7e036ec013
Cristiano Rastelli <cristiano.rastelli@hashicorp.com>
2025-12-11 18:20:02 +0000

    [Project Solar / Phase 1 / Engineering Follow-ups]  Add tests for HDS theming service and components (#3428)
    Co-authored-by: Dylan Hyun <dylan.hyun@hashicorp.com>
    Co-authored-by: Kristin Bradley <kristin.bradley@hashicorp.com>

---
+++ showcase/tests/integration/components/hds/theme-context/index-test.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'showcase/tests/helpers';
+import { render, resetOnerror, setupOnerror } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+import {
+  CONTEXTUAL_THEMES,
+  CONTEXTUAL_MODES,
+} from '@hashicorp/design-system-components/components/hds/theme-context/index';
+
+module('Integration | Component | hds/theme-context/index', function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.afterEach(() => {
+    resetOnerror();
+  });
+
+  test('it should render the component with a CSS class that matches the component name', async function (assert) {
+    await render(
+      hbs`<Hds::ThemeContext @context="light" id="test-theme-context" />`,
+    );
+    assert.dom('#test-theme-context').hasClass('hds-theme-context');
+  });
+
+  // CONTEXT - THEMES
+
+  CONTEXTUAL_THEMES.forEach((context) => {
+    test(`it should render the correct CSS "theme" class for "${context}" context`, async function (assert) {
+      this.set('context', context);
+      await render(
+        hbs`<Hds::ThemeContext @context={{this.context}} id="test-theme-context" />`,
+      );
+      assert.dom('#test-theme-context').hasClass(`hds-theme-${context}`);
+    });
+  });
+
+  // CONTEXT - MODES
+
+  CONTEXTUAL_MODES.forEach((context) => {
+    test(`it should render the correct CSS "mode" class for "${context}" context`, async function (assert) {
+      this.set('context', context);
+      await render(
+        hbs`<Hds::ThemeContext @context={{this.context}} id="test-theme-context" />`,
+      );
+      assert.dom('#test-theme-context').hasClass(`hds-mode-${context}`);
+    });
+  });
+
+  // YIELDED CONTENT
+
+  test('it correctly yields content passed in the default block', async function (assert) {
+    await render(hbs`
+      <Hds::ThemeContext @context="light" id="test-theme-context">
+        <p id="test-content">This is yielded content</p>
+      </Hds::ThemeContext>
+    `);
+    assert.dom('#test-content').exists();
+    assert.dom('#test-content').hasText('This is yielded content');
+    assert.dom('#test-theme-context #test-content').exists();
+  });
+
+  // ASSERTIONS
+
+  test('it should throw an assertion if an incorrect value for @context is provided', async function (assert) {
+    const errorMessage =
+      '@context for "Hds::ThemeContext" must be one of the following: default, system, light, dark, cds-g0, cds-g10, cds-g90, cds-g100; received: foo';
+    assert.expect(2);
+    setupOnerror(function (error) {
+      assert.strictEqual(error.message, `Assertion Failed: ${errorMessage}`);
+    });
+    await render(hbs`<Hds::ThemeContext @context="foo" />`);
+    assert.throws(function () {
+      throw new Error(errorMessage);
+    });
+  });
+});
