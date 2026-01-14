960bbcac8b8f1a52238c9175a8c1588c0e150709
Zack Moore <zack.moore@hashicorp.com>
2026-01-14 13:37:56 -0500

    Upgrade `ember-intl` to `^8.0.0` (#3460)

--- packages/components/src/services/hds-intl.ts
+++ packages/components/src/services/hds-intl.ts
@@ -9,12 +9,11 @@ import { isPresent } from '@ember/utils';
 import { assert } from '@ember/debug';
 
 import type { IntlService } from 'ember-intl';
-import type { FormatMessageParameters } from 'ember-intl/-private/formatjs';
 
-export type HdsIntlTOptions = FormatMessageParameters[1] & {
+type IntlTOptions = Parameters<IntlService['t']>[1];
+
+export type HdsIntlTOptions = IntlTOptions & {
   default: string;
-  htmlSafe?: boolean | undefined;
-  locale?: string | undefined;
 };
 
 export default class HdsIntlService extends Service {
