f726d19b28c039239fe222a06c8cb7af3354ce41
Lee White <lee.white@hashicorp.com>
2026-01-26 16:54:59 -0500

    Website: convert ButtonSet demos to new format (#3503)

--- website/docs/components/button-set/partials/code/code-snippets/button-set-loading.classic.js
+++ website/docs/components/button-set/partials/code/code-snippets/button-set-loading.classic.js
@@ -1,13 +1,8 @@
-/**
- * Copyright IBM Corp. 2021, 2025
- * SPDX-License-Identifier: MPL-2.0
- */
-
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
-export default class Index extends Component {
+export default class LocalComponent extends Component {
   @tracked isLoading = false;
   @tracked timer;
 
