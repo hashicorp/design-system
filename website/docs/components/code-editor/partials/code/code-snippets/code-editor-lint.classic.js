cebaa5d7d7b85d753cda8ee0dbcb6374365a6da1
Lee White <lee.white@hashicorp.com>
2026-01-27 16:27:11 -0500

    Website: convert CodeEditor demos to new format (#3511)

---
+++ website/docs/components/code-editor/partials/code/code-snippets/code-editor-lint.classic.js
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+
+export default class LocalComponent extends Component {
+  badJsonCode = `{
+  message: "Hello, world!",
+  : "success"
+  "data": null,
+}`;
+}
