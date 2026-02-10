4d8af169187096cbc924754269ba5ba2703ed8c1
Kristin Bradley <kristin.bradley@hashicorp.com>
2026-02-10 14:46:18 -0800

    HDS-6061 Add IconTile carbonization page with initial examples

--- showcase/app/router.ts
+++ showcase/app/router.ts
@@ -177,6 +177,7 @@ Router.map(function () {
         this.route('text-input');
         this.route('radio-card');
       });
+      this.route('icon-tile');
       this.route('modal');
       this.route('segmented-group');
     });
