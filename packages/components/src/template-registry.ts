85c1e164798bc76fa35f51ca22e8c69a9fbf181d
Zack Moore <zack.moore@hashicorp.com>
2026-03-03 17:34:18 -0500

    using modifier on th/td components

--- packages/components/src/template-registry.ts
+++ packages/components/src/template-registry.ts
@@ -246,6 +246,7 @@ import type HdsClipboardModifier from './modifiers/hds-clipboard.ts';
 import type HdsRegisterEventModifier from './modifiers/hds-register-event.ts';
 import type HdsTooltipModifier from './modifiers/hds-tooltip.ts';
 import type HdsAdvancedTableCellModifier from './modifiers/hds-advanced-table-cell.ts';
+import type HdsScrollIntoViewOnFocusModifier from './modifiers/hds-scroll-into-view-on-focus.ts';
 
 export default interface HdsComponentsRegistry {
   // ----- COMPONENTS ---------------------------------------------------
@@ -1019,4 +1020,7 @@ export default interface HdsComponentsRegistry {
 
   // hds-advanced-table-cell
   'hds-advanced-table-cell': typeof HdsAdvancedTableCellModifier;
+
+  // hds-scroll-into-view-on-focus
+  'hds-scroll-into-view-on-focus': typeof HdsScrollIntoViewOnFocusModifier;
 }
