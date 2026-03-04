2c6bff63685a0a0da3a3dd118053aa253df67384
Cristiano Rastelli <public@didoo.net>
2026-03-04 18:26:30 +0000

    removed generation of ZIP file from Flight icons pipeline

--- packages/flight-icons/scripts/build.ts
+++ packages/flight-icons/scripts/build.ts
@@ -12,7 +12,6 @@ import { optimizeAssetsSVG } from './build-parts/optimizeAssetsSVG';
 import { generateBundleSVG } from './build-parts/generateBundleSVG';
 import { generateBundleSVGSprite } from './build-parts/generateBundleSVGSprite';
 import { generateBundleSVGReact } from './build-parts/generateBundleSVGReact';
-import { generatePublicZIPFile } from './build-parts/generatePublicZIPFile';
 
 // read the environment variables from the ".env" file
 dotenv.config();
@@ -67,10 +66,6 @@ async function build() {
     // generate the bundle for the SVGs in React
     console.log('Generating bundle for SVG React');
     await generateBundleSVGReact({ config, catalog });
-
-    // zip the standalone SVG bundle and save it in the dummy app public folder
-    console.log('Generating ZIP file with standalone SVG files');
-    await generatePublicZIPFile({ config });
 
     // remove temporary folder
     // notice: comment this if you need to debug the assets initial SVG processing
