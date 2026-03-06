40636e5a2b051da1b31dab4152de47228f526993
shleewhite <lee.white@hahicorp.com>
2026-03-06 10:42:01 -0500

    fix: change all Index => LocalComponent

--- website/docs/components/form/checkbox/partials/code/code-snippets/checkbox-group-attrs.classic.js
+++ website/docs/components/form/checkbox/partials/code/code-snippets/checkbox-group-attrs.classic.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
-export default class Index extends Component {
+export default class LocalComponent extends Component {
   @action
   yourOnChangeFunction() {
     console.log('Invoked "yourOnChangeFunction"');
--- website/docs/utilities/dismiss-button/partials/code/code-snippets/dismiss-button-basic.classic.js
+++ website/docs/utilities/dismiss-button/partials/code/code-snippets/dismiss-button-basic.classic.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
-export default class Index extends Component {
+export default class LocalComponent extends Component {
   @action
   onClickDismissButton() {
     console.log('`Hds::DismissButton` clicked');
