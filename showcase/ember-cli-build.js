c8da9a9bb919086220163bf111af5d84448a8865
Alex <alex-ju@users.noreply.github.com>
2026-03-12 10:25:40 +0000

    Enable hot reload for component Sass files in showcase

--- showcase/ember-cli-build.js
+++ showcase/ember-cli-build.js
@@ -18,7 +18,8 @@ module.exports = function (defaults) {
       precision: 4,
       includePaths: [
         'node_modules/@hashicorp/design-system-tokens/dist/products/css',
-        'node_modules/@hashicorp/design-system-components/dist/styles',
+        // we use src/styles (instead of dist/styles) to enable hot reload during development
+        'node_modules/@hashicorp/design-system-components/src/styles',
         'node_modules/ember-power-select/vendor',
       ],
     },
