560398a4ad3ace4ab97ed0eab08611586649cd4b
shleewhite <lee.white@hahicorp.com>
2026-03-19 15:59:55 -0400

    fix: make mock data file name match convention

--- website/docs/components/table/table/partials/code/code-snippets/table-scroll-container.classic.js
+++ website/docs/components/table/table/partials/code/code-snippets/table-scroll-container.classic.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 
-import USER_WITH_MORE_COLUMNS from 'website/mocks/user-with-more-columns';
+import USER_WITH_MORE_COLUMNS from 'website/mocks/user-with-more-columns-data';
 
 export default class LocalComponent extends Component {
   get model() {
