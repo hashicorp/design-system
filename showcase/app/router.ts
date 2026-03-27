8db6cc6b21c41cbce6b7c7be44798f27ca246eb8
Dylan Hyun <dylan.hyun@hashicorp.com>
2026-03-27 14:45:40 -0400

    [Project Solar / Phase 1 / Components] `CopySnippet` carbonization (#3733)

--- showcase/app/router.ts
+++ showcase/app/router.ts
@@ -175,6 +175,9 @@ Router.map(function () {
       this.route('badge');
       this.route('badge-count');
       this.route('button');
+      this.route('copy', function () {
+        this.route('snippet');
+      });
       this.route('form', function (): void {
         this.route('text-input');
         this.route('radio-card');
