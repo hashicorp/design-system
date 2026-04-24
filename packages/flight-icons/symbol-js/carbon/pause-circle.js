942a3994ceb288e033eb2e43f4e6f1b6754e5330
Zack Moore <zack.moore@hashicorp.com>
2026-04-24 10:41:33 -0400

    Carbon icon mapping improvements (#3767)

---
+++ packages/flight-icons/symbol-js/carbon/pause-circle.js
@@ -0,0 +1,1 @@
+export default `<symbol id="hds-icon-carbon-pause-circle" viewBox="0 0 32 32"><path d="M14 10 12 10 12 22 14 22 14 10 14 10z"/><path d="M20 10 18 10 18 22 20 22 20 10 20 10z"/><path d="M16,4A12,12,0,1,1,4,16,12,12,0,0,1,16,4m0-2A14,14,0,1,0,30,16,14,14,0,0,0,16,2Z"/></symbol>`;
