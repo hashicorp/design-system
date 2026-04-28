4862f94590bd0821a32156e898bbff0f3d061b5a
Alex <alex-ju@users.noreply.github.com>
2026-04-28 16:19:17 +0100

    Suppress deprecation warnings for fastboot in `website` (#3837)

--- website/config/environment.js
+++ website/config/environment.js
@@ -23,6 +23,7 @@ module.exports = function (environment) {
 
     EmberENV: {
       EXTEND_PROTOTYPES: false,
+      LOG_STACKTRACE_ON_DEPRECATION: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
