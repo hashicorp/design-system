5a0e0a941755a831a998ce177bca256c2eb61b73
Zack Moore <zack.moore@hashicorp.com>
2026-06-01 09:38:14 -0400

    Updates `eslint` in `codemods` package (#3849)

---
+++ packages/codemods/eslint.config.mjs
@@ -0,0 +1,45 @@
+/**
+ * Copyright IBM Corp. 2021, 2025
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import js from '@eslint/js';
+import n from 'eslint-plugin-n';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules/', '__testfixtures__/', '!**/.*'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    plugins: {
+      n,
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'script',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      ...n.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'n/no-unpublished-require': 'off',
+    },
+  },
+  {
+    files: ['**/__tests__/**/*.js', 'transforms/**/test.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+  prettierConfig,
+];
