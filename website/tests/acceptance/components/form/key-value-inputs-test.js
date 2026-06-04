cf125db7fe65bdf74e4077ce2abe7aad5a26011b
Peter-Karl Jackson <130211933+pkj-web@users.noreply.github.com>
2026-06-04 16:08:14 -0500

    Add acceptance tests for key-value inputs and update changelog exclusions

---
+++ website/tests/acceptance/components/form/key-value-inputs-test.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright IBM Corp. 2021, 2025
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'website/tests/helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Acceptance | components/form/key-value-inputs', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /components/form/key-value-inputs', async function (assert) {
+    await visit('/components/form/key-value-inputs');
+
+    assert.strictEqual(currentURL(), '/components/form/key-value-inputs');
+  });
+
+  test('Components/form/key-value-inputs page passes automated a11y checks', async function (assert) {
+    await visit('/components/form/key-value-inputs');
+    await a11yAudit();
+    assert.ok(true, 'a11y automation audit passed');
+  });
+});
