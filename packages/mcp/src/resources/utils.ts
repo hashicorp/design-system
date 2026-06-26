88342edd5549e7a12a900f12cc9797e4feaebc2e
Zack Moore <zack.moore@hashicorp.com>
2026-06-26 17:42:29 -0400

    added error guard

--- packages/mcp/src/resources/utils.ts
+++ packages/mcp/src/resources/utils.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import { serializeCaughtError } from "../utils.js";
+
 import type { JsonObject } from "../types.js";
 import type {
   ReadResourceCallback,
