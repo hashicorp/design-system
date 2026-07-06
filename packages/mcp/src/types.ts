b28ee69de529a8435a4c501ed780f2527dedbf80
Zack Moore <zack.moore@hashicorp.com>
2026-07-06 10:56:40 -0400

    added some QoL improvements

---
+++ packages/mcp/src/types.ts
@@ -0,0 +1,9 @@
+export type JsonObject = { [key: string]: JsonValue };
+export type JsonArray = JsonValue[];
+export type JsonValue =
+  | string
+  | number
+  | boolean
+  | null
+  | JsonObject
+  | JsonArray;
