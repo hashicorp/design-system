abd24d303538111ba3af25e819086656156d4c13
Jory Tindall <jory.tindall@hashicorp.com>
2026-08-10 10:05:26 -0700

    lint

--- website/app/controllers/tooling.js
+++ website/app/controllers/tooling.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import Controller from '@ember/controller'
+import Controller from '@ember/controller';
 
 export default class ToolingController extends Controller {
   get cards() {
