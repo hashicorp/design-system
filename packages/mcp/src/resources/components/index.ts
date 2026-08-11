203548add0359871f30abd8fab20a58ddbf47de3
Zack Moore <zack.moore@hashicorp.com>
2026-08-11 16:14:38 -0400

    added constants and resources index for components

---
+++ packages/mcp/src/resources/components/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright IBM Corp. 2021, 2026
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import type { McpResource } from "../types.js";
+
+const COMPONENTS_RESOURCES: McpResource[] = [];
+
+export default COMPONENTS_RESOURCES;
