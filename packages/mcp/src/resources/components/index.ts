703689dd54e545b8898f06a84a1298ccd5ed8738
Zack Moore <zack.moore@hashicorp.com>
2026-08-11 16:24:50 -0400

    surfacing resource methods

--- packages/mcp/src/resources/components/index.ts
+++ packages/mcp/src/resources/components/index.ts
@@ -3,8 +3,14 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import getComponentByNameResource from "./get-component-by-name.js";
+import getComponentsResource from "./get-components.js";
+
 import type { McpResource } from "../types.js";
 
-const COMPONENTS_RESOURCES: McpResource[] = [];
+const COMPONENTS_RESOURCES: McpResource[] = [
+  getComponentsResource,
+  getComponentByNameResource,
+];
 
 export default COMPONENTS_RESOURCES;
