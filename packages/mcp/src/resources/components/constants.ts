56e78f9b2af49feb49ffb4ba7190de7be06dfd51
Zack Moore <zack.moore@hashicorp.com>
2026-08-14 11:15:32 -0400

    MCP - Components resources (#4062)

---
+++ packages/mcp/src/resources/components/constants.ts
@@ -0,0 +1,7 @@
+/**
+ * Copyright IBM Corp. 2021, 2026
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+export const COMPONENTS_URI = "hds://components";
+export const COMPONENT_BY_NAME_URI_TEMPLATE = `${COMPONENTS_URI}/{componentName}`;
