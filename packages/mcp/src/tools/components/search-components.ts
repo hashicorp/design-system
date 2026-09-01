23d43fa839eef9a290ea9308de79b6d93b74dc07
Zack Moore <zack.moore@hashicorp.com>
2026-09-01 19:22:34 -0400

    added icons search tool

--- packages/mcp/src/tools/components/search-components.ts
+++ packages/mcp/src/tools/components/search-components.ts
@@ -15,10 +15,9 @@ import {
   DEFAULT_SEARCH_LIMIT,
   MAX_QUERY_LENGTH,
   MAX_SEARCH_LIMIT,
-  SEARCH_COMPONENTS_TOOL_NAME,
-} from "./constants.js";
+} from "../shared/constants.js";
+import { SEARCH_COMPONENTS_TOOL_NAME } from "./constants.js";
 import { SEARCH_DOCS_TOOL_NAME } from "../docs/constants.js";
-import { catalogSourceOutputSchema } from "./utils.js";
 import { getOrLoadComponentStore } from "../../stores/components/index.js";
 
 import type { ToolRegistration } from "../define-tool.js";
