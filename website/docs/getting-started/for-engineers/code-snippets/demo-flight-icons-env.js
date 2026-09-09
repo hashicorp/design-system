9fb479b7e2a4dc1acbd0f86fa7a6d12f89a9d177
Cristiano Rastelli <public@didoo.net>
2026-09-09 16:18:55 +0100

    updated “Getting started/For engineers” page

--- website/docs/getting-started/for-engineers/code-snippets/demo-flight-icons-env.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-module.exports = function(environment) {
-  const ENV = {
-    // your other config
-    // ...
-    flightIconsSpriteLazyEmbed: true
-  };
-}
